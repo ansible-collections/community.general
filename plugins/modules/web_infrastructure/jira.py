ef1178687c71684f8f7b2d440293e84ca102a923
Per Abildgaard Toft <pto@netic.dk>
2020-03-20 09:20:14 +0100

    Added jira Operations in update function

--- plugins/modules/web_infrastructure/jira.py
+++ plugins/modules/web_infrastructure/jira.py
@@ -33,7 +33,7 @@ options:
   operation:
     required: true
     aliases: [ command ]
-    choices: [ create, comment, edit, fetch, transition , link, search ]
+    choices: [ create, comment, edit, update, fetch, transition , link, search ]
     description:
       - The operation to perform.
 
@@ -205,6 +205,22 @@ EXAMPLES = """
           - autocreated
           - ansible
 
+# Updating a field using operations: add, set & remove
+- name: Change the value of a Select dropdown
+  jira:
+    uri: '{{ server }}'
+    username: '{{ user }}'
+    password: '{{ pass }}'
+    issue: '{{ issue.meta.key }}'
+    operation: update    
+  args:
+    fields:
+      customfield_12931: [ {'set': {'value': 'Virtual'}} ]
+      customfield_13820: [ {'set': {'value':'Manually'}} ]
+  register: cmdb_issue
+  delegate_to: localhost
+
+
 # Retrieve metadata for an issue and use it to create an account
 - name: Get an issue
   jira:
@@ -360,6 +376,15 @@ def edit(restbase, user, passwd, params):
 
     return ret
 
+def update(restbase, user, passwd, params):
+    data = {
+        "update": params['fields']
+    }
+    url = restbase + '/issue/' + params['issue']
+
+    ret = put(url, user, passwd, params['timeout'], data)
+
+    return ret
 
 def fetch(restbase, user, passwd, params):
     url = restbase + '/issue/' + params['issue']
@@ -420,6 +445,7 @@ def link(restbase, user, passwd, params):
 OP_REQUIRED = dict(create=['project', 'issuetype', 'summary'],
                    comment=['issue', 'comment'],
                    edit=[],
+                   update=[],
                    fetch=['issue'],
                    transition=['status'],
                    link=['linktype', 'inwardissue', 'outwardissue'],
@@ -432,7 +458,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             uri=dict(required=True),
-            operation=dict(choices=['create', 'comment', 'edit', 'fetch', 'transition', 'link', 'search'],
+            operation=dict(choices=['create', 'comment', 'edit', 'update', 'fetch', 'transition', 'link', 'search'],
                            aliases=['command'], required=True),
             username=dict(required=True),
             password=dict(required=True, no_log=True),
