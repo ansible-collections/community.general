7d82c3aae2369aa47d5e58d4211f0b1c2de021cc
Felix Fontein <felix@fontein.de>
2020-03-24 22:11:04 +0100

    Fix netapp.ontap module docs fragment name.

--- plugins/modules/storage/netapp/na_cdot_volume.py
+++ plugins/modules/storage/netapp/na_cdot_volume.py
@@ -18,7 +18,7 @@ module: na_cdot_volume
 
 short_description: Manage NetApp cDOT volumes
 extends_documentation_fragment:
-- netapp.ontap.netapp.ontap
+- netapp.ontap.netapp.na_ontap
 
 author: Sumit Kumar (@timuster) <sumit4@netapp.com>
 
