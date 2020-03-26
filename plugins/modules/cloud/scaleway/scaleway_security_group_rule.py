2e3b612d94052fae9f2bd3450f70f14a09ecf8cd
Felix Fontein <felix@fontein.de>
2020-03-26 21:44:26 +0100

    Add copy of ipaddress.py from ansible.netcommon (#49)
    * Add copy of ipaddress.py from ansible.netcommon, use that one in non-network modules.
    
    * Copy required functions from ansible.netcommon. Simpler than using compat.ipaddress to do this.

--- plugins/modules/cloud/scaleway/scaleway_security_group_rule.py
+++ plugins/modules/cloud/scaleway/scaleway_security_group_rule.py
@@ -125,7 +125,7 @@ data:
 '''
 
 from ansible_collections.community.general.plugins.module_utils.scaleway import SCALEWAY_LOCATION, scaleway_argument_spec, Scaleway, payload_from_object
-from ansible_collections.ansible.netcommon.plugins.module_utils.compat.ipaddress import ip_network
+from ansible_collections.community.general.plugins.module_utils.compat.ipaddress import ip_network
 from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import AnsibleModule
 
