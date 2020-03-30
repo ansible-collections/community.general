0970bea3930a273807dcdcf2e19d2361e746c2e3
Felix Fontein <felix@fontein.de>
2020-03-30 14:53:47 +0200

    Localize final fragment.

--- plugins/doc_fragments/netapp.py
+++ plugins/doc_fragments/netapp.py
@@ -112,3 +112,66 @@ notes:
   - Embedded Web Services is currently available on the E2800, E5700, EF570, and newer hardware models.
   - M(netapp_e_storage_system) may be utilized for configuring the systems managed by a WSP instance.
 '''
+
+    # Documentation fragment for ONTAP (na_ontap)
+    NA_ONTAP = r'''
+options:
+  hostname:
+      description:
+      - The hostname or IP address of the ONTAP instance.
+      type: str
+      required: true
+  username:
+      description:
+      - This can be a Cluster-scoped or SVM-scoped account, depending on whether a Cluster-level or SVM-level API is required.
+        For more information, please read the documentation U(https://mysupport.netapp.com/NOW/download/software/nmsdk/9.4/).
+      type: str
+      required: true
+      aliases: [ user ]
+  password:
+      description:
+      - Password for the specified user.
+      type: str
+      required: true
+      aliases: [ pass ]
+  https:
+      description:
+      - Enable and disable https
+      type: bool
+      default: no
+  validate_certs:
+      description:
+      - If set to C(no), the SSL certificates will not be validated.
+      - This should only set to C(False) used on personally controlled sites using self-signed certificates.
+      type: bool
+      default: yes
+  http_port:
+      description:
+      - Override the default port (80 or 443) with this port
+      type: int
+  ontapi:
+      description:
+      - The ontap api version to use
+      type: int
+  use_rest:
+      description:
+      - REST API if supported by the target system for all the resources and attributes the module requires. Otherwise will revert to ZAPI.
+      - Always -- will always use the REST API
+      - Never -- will always use the ZAPI
+      - Auto -- will try to use the REST Api
+      default: Auto
+      choices: ['Never', 'Always', 'Auto']
+      type: str
+
+
+requirements:
+  - A physical or virtual clustered Data ONTAP system. The modules support Data ONTAP 9.1 and onward
+  - Ansible 2.6
+  - Python2 netapp-lib (2017.10.30) or later. Install using 'pip install netapp-lib'
+  - Python3 netapp-lib (2018.11.13) or later. Install using 'pip install netapp-lib'
+  - To enable http on the cluster you must run the following commands 'set -privilege advanced;' 'system services web modify -http-enabled true;'
+
+notes:
+  - The modules prefixed with na\\_ontap are built to support the ONTAP storage platform.
+
+'''
--- plugins/modules/storage/netapp/na_ontap_gather_facts.py
+++ plugins/modules/storage/netapp/na_ontap_gather_facts.py
@@ -19,7 +19,7 @@ deprecated:
   alternative: Use M(na_ontap_info) instead.
 author: Piotr Olczak (@dprts) <polczak@redhat.com>
 extends_documentation_fragment:
-- netapp.ontap.netapp.na_ontap
+- community.general.netapp.na_ontap
 
 short_description: NetApp information gatherer
 description:
