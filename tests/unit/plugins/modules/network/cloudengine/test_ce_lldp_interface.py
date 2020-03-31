c4432e3150a4cc2e52f3fd3c79c36ec0bab7a9fe
Felix Fontein <felix@fontein.de>
2020-03-31 10:18:43 +0200

    Fix typos.

--- tests/unit/plugins/modules/network/cloudengine/test_ce_lldp_interface.py
+++ tests/unit/plugins/modules/network/cloudengine/test_ce_lldp_interface.py
@@ -23,7 +23,7 @@ __metaclass__ = type
 from ansible_collections.community.general.tests.unit.compat.mock import patch
 from ansible_collections.community.general.plugins.modules.network.cloudengine import ce_lldp_interface
 from ansible_collections.community.general.tests.unit.plugins.modules.network.cloudengine.ce_module import TestCloudEngineModule, load_fixture
-from ansible_collections.community.general.tests.unit.plugins.modulwes.utils import set_module_args
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import set_module_args
 
 
 class TestCloudEngineLacpModule(TestCloudEngineModule):
--- tests/unit/plugins/modules/network/netvisor/test_pn_ipv6security_raguard.py
+++ tests/unit/plugins/modules/network/netvisor/test_pn_ipv6security_raguard.py
@@ -6,7 +6,7 @@ __metaclass__ = type
 
 from ansible_collections.community.general.tests.unit.compat.mock import patch
 from ansible_collections.community.general.plugins.modules.network.netvisor import pn_ipv6security_raguard
-from ansible_collections.community.general.tests.unit.plugins.modulwes.utils import set_module_args
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import set_module_args
 from .nvos_module import TestNvosModule
 
 
