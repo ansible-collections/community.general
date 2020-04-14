4807d80855ad0dc2201be777ab11d636f5ca9bdc
Andrew Klychkov <aaklychkov@mail.ru>
2020-04-14 11:15:53 +0300

    postgresql_membership: remove unused import of exec_sql function (#178)
    * postgresql_membership: remove unused import of exec_sql function
    
    * add changelog fragment

--- plugins/modules/database/postgresql/postgresql_membership.py
+++ plugins/modules/database/postgresql/postgresql_membership.py
@@ -143,7 +143,6 @@ except ImportError:
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.community.general.plugins.module_utils.postgres import (
     connect_to_db,
-    exec_sql,
     get_conn_params,
     PgMembership,
     postgres_common_argument_spec,
