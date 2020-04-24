a0d5bbee08e67af1475f1be1d52d127fa2101c36
Veltarn <dante161@gmail.com>
2020-04-24 16:48:33 +0200

    Doc description string fix, the tests thought it was an AnsibleMapping object

--- plugins/modules/packaging/language/pear.py
+++ plugins/modules/packaging/language/pear.py
@@ -40,7 +40,7 @@ options:
             - Path to the pear executable
     prompts:
         description:
-            - List of regex strings which can be used to detect prompts during pear package installation: Optionnal string to answer the expected regex
+            - List of regex strings which can be used to detect prompts during pear package installation with an ptionnal string to answer the expected regex
         version_added: "2.10"
 '''
 
