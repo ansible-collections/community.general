072a0270fa8665849ec88f41f1931d5543fe0889
Veltarn <dante161@gmail.com>
2020-04-24 16:59:04 +0200

    Added elements parameter to argument_spec definition.

--- plugins/modules/packaging/language/pear.py
+++ plugins/modules/packaging/language/pear.py
@@ -260,7 +260,7 @@ def main():
             name=dict(aliases=['pkg'], required=True),
             state=dict(default='present', choices=['present', 'installed', "latest", 'absent', 'removed']),
             executable=dict(default=None, required=False, type='path'),
-            prompts=dict(default=None, required=False, type='list')),
+            prompts=dict(default=None, required=False, type='list', elements='raw')),
         supports_check_mode=True)
 
     p = module.params
