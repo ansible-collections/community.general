643a92eefd3c385c5c4b75f56d7d233f4197f097
Veltarn <dante161@gmail.com>
2020-04-28 09:39:41 +0200

    Update plugins/modules/packaging/language/pear.py
    Co-Authored-By: Felix Fontein <felix@fontein.de>

--- plugins/modules/packaging/language/pear.py
+++ plugins/modules/packaging/language/pear.py
@@ -73,7 +73,7 @@ EXAMPLES = r'''
 
 - name: Install multiple pear/pecl packages at once with prompts.
   # Prompts will be processed on the same order as the packages order
-# If there is more prompts than packages, packages without prompts will be installed without any prompt expected.
+  # If there is more prompts than packages, packages without prompts will be installed without any prompt expected.
   # If there is more packages than prompts, additionnal prompts will be ignored
   pear:
     name: pecl/gnupg, pecl/apcu
