0869aa3f0cd88759784f92f95681a28106143ecd
imjoseangel <josea.munoz@gmail.com>
2020-05-04 12:07:07 +0200

    Improving lookup mergelist with key as parameter and tests
    Fixes tests with complete uri

--- plugins/lookup/mergelist.py
+++ plugins/lookup/mergelist.py
@@ -7,13 +7,16 @@ __metaclass__ = type
 DOCUMENTATION = '''
     lookup: mergelist
     author: Jose Angel Munoz <josea.munoz@gmail.com>
-    short_description: merges lists from a given array key
+    short_description: Merges lists of dictionaries by a given key
     description:
-      - This lookup returns the contents of two or more merged lists using a common key.
+      - This lookup returns the contents of two or more merged lists of dictionaries using a common key.
     options:
       _terms:
-        description: list(s) to merge
+        description: List(s) to merge.
         required: True
+      key:
+        description: Common key used to merge.
+        required: False
     notes:
       - if key is not defined, "name" will be the default
       - use query if you want to return a list when a single value is given.
@@ -40,12 +43,45 @@ EXAMPLES = """
   - name: Merges list1 and list2 with 'id' as key
     debug:
       msg: "{{ query('community.general.mergelist', list1, list2, key='id') }}"
+    vars:
+      list1:
+        - id: myname01
+          param01: myparam01
+        - id: myname02
+          param01: myparam02
+      list2:
+        - id: myname01
+          param01: myparam03
+        - id: myname02
+          param02: myparam04
+        - id: myname03
+          param03: myparam03
+
+  - name: Merges list1, list2 and list3 with 'id' as key
+    debug:
+      msg: "{{ query('community.general.mergelist', list1, list2, list3, key='id') }}"
+    vars:
+      list1:
+        - id: myname01
+          param01: myparam01
+        - id: myname02
+          param01: myparam02
+      list2:
+        - id: myname01
+          param01: myparam03
+        - id: myname02
+          param02: myparam04
+        - id: myname03
+          param03: myparam03
+      list3:
+        - id: myname01
+          param03: myparam05
 """
 
 RETURN = """
   _list:
     description:
-      - medged list with key and value
+      - Medged list of dictionaries.
 """
 
 from collections import defaultdict
@@ -54,22 +90,17 @@ from ansible.errors import AnsibleError
 
 
 class LookupModule(LookupBase):
-    def run(self, terms, variables=None, **kwargs):
+    def run(self, terms, variables=None, key='name', **kwargs):
 
         array = defaultdict(dict)
-        arraykey = "name"
 
-        for items in terms:
-
-            if isinstance(items, str):
-                arraykey = items
-
-            elif isinstance(items, list):
-                for item in items:
-                    try:
-                        array[item[arraykey]].update(item)
-                    except KeyError as exception:
-                        raise AnsibleError("Key not found: %s" % exception)
-            ret = list(array.values())
-
-        return ret
+        for term in terms:
+            for item in term:
+                try:
+                    array[item[key]].update(item)
+                except KeyError as keyexception:
+                    raise AnsibleError("Key not found: %s" % keyexception)
+                except TypeError as errorexception:
+                    raise AnsibleError("Incorrect formatting: %s" %
+                                       errorexception)
+        return list(array.values())
