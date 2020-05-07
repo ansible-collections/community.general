25255184df86f9035b2ce4dd02895d3ec5b3f5cb
Abhijeet Kasurde <akasurde@redhat.com>
2020-05-08 00:01:40 +0530

    Updated listen_ports_facts documentation (#301)
    Updated listen_ports_facts module documentation to reflect
    - Linux only support
    - Required netstat command
    
    Fixes: ansible/ansible#68077
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/system/listen_ports_facts.py
+++ plugins/modules/system/listen_ports_facts.py
@@ -14,14 +14,13 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 module: listen_ports_facts
-
 author:
     - Nathan Davison (@ndavison)
-
-
 description:
-    - Gather facts on processes listening on TCP and UDP ports.
-
+    - Gather facts on processes listening on TCP and UDP ports using netstat command.
+    - This module currently supports Linux only.
+requirements:
+  - netstat
 short_description: Gather facts on processes listening on TCP and UDP ports.
 '''
 
