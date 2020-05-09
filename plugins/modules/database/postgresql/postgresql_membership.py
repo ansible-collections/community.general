42892a5230eeba2d5fd54f397b46a1e010ec47c3
Andrew Klychkov <aaklychkov@mail.ru>
2020-05-09 09:25:12 +0300

    postgresql modules: improve trust_input parameter's documentation, improve CI tests (#309)
    * postgresql modules: improve trust_input parameter's documentation, improve CI tests
    
    * fix CI

--- plugins/modules/database/postgresql/postgresql_membership.py
+++ plugins/modules/database/postgresql/postgresql_membership.py
@@ -74,7 +74,9 @@ options:
     type: str
   trust_input:
     description:
-    - If C(no), check whether values of some parameters are potentially dangerous.
+    - If C(no), check whether values of parameters I(groups),
+      I(target_roles), I(session_role) are potentially dangerous.
+    - It makes sense to use C(yes) only when SQL injections via the parameters are possible.
     type: bool
     default: yes
 seealso:
