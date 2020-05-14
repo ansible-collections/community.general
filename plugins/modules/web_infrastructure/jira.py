c149a9aaf10939eee808ec7935d2a2ba2173dad1
Andrew Klychkov <aaklychkov@mail.ru>
2020-05-14 11:44:24 +0300

    Remove ANSIBLE_METADATA

--- plugins/modules/web_infrastructure/jira.py
+++ plugins/modules/web_infrastructure/jira.py
@@ -12,11 +12,6 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-
-
 DOCUMENTATION = """
 module: jira
 short_description: create and modify issues in a JIRA instance
