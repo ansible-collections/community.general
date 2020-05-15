44643b62c91e6f26376315fdd9e19db0e118f3b7
Ashish Varshney <er.ashish.av@gmail.com>
2020-05-15 16:56:21 +0530

    new feature logger_runtime

--- plugins/callback/logger_runtime.py
+++ plugins/callback/logger_runtime.py
@@ -2,6 +2,13 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = '''
+    callback: logger_runtime
+    type: notification
+    short_description: write execution time in log file
+    description:
+      - This callback will write execution time of playbook execution time and individual task time in log file
+    requirements:
+      - whitelisting in configuration
     options:
       output_limit:
         description: Number of tasks to display in the summary
@@ -40,103 +47,90 @@ def timestamp(self):
     '''
     Evaluate execution time for individual task
     '''
-    if self.current is not None:
-        self.stats[self.current]['time'] = int((time.time()*1000) - self.stats[self.current]['time'])
+    if self.current is not None:
+        self.stats[self.current]['time'] = int((time.time()*1000) - self.stats[self.current]['time'])
 
 def tasktime():
     '''
     Evaluate execution time for playbook
     '''
-    global tn
-    time_total_elapsed = (time.time()*1000) - t0
-    tn = time.time()*1000
-    return int(time_total_elapsed)
+    global tn
+    time_total_elapsed = (time.time()*1000) - t0
+    tn = time.time()*1000
+    return int(time_total_elapsed)
 
 
 class CallbackModule(CallbackBase):
-    """
-    This callback module provides per-task timing, ongoing playbook elapsed time
-    and ordered list of top 20 longest running tasks at end.
-    """
-    CALLBACK_VERSION = 2.0
-    CALLBACK_TYPE = 'aggregate'
-    CALLBACK_NAME = 'logger_runtime'
-    CALLBACK_NEEDS_WHITELIST = True
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'aggregate'
+    CALLBACK_NAME = 'logger_runtime'
+    CALLBACK_NEEDS_WHITELIST = True
+    
+    def __init__(self):
+        self.stats = collections.OrderedDict()
+        self.current = None
+        self.sort_order = None
+        self.task_output_limit = None
+        super(CallbackModule, self).__init__()
+    
+    def set_options(self, task_keys=None, var_options=None, direct=None):
+        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+        self.sort_order = self.get_option('sort_order')
+        if self.sort_order is not None:
+            if self.sort_order == 'ascending':
+                self.sort_order = False
+            elif self.sort_order == 'descending':
+                self.sort_order = True
+            elif self.sort_order == 'none':
+                self.sort_order = None
+        self.task_output_limit = self.get_option('output_limit')
+        if self.task_output_limit is not None:
+            if self.task_output_limit == 'all':
+                self.task_output_limit = None
+            else:
+                self.task_output_limit = int(self.task_output_limit)
 
-    def __init__(self):
-        self.stats = collections.OrderedDict()
-        self.current = None
+    def _record_task(self, task):
+        '''
+        Logs the start of each task
+        '''
+        timestamp(self)
+        
+        # Record the start time of the current task
+        self.current = task._uuid
+        self.stats[self.current] = {'time': (time.time()*1000), 'name': task.get_name()}
+        if self._display.verbosity >= 2:
+            self.stats[self.current]['path'] = task.get_path()
 
-        self.sort_order = None
-        self.task_output_limit = None
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        self._record_task(task)
 
-        super(CallbackModule, self).__init__()
+    def v2_playbook_on_handler_task_start(self, task):
+        self._record_task(task)
 
-    def set_options(self, task_keys=None, var_options=None, direct=None):
+    def playbook_on_setup(self):
+        log = "playbook_on_setup_execution_time " + str(tasktime())
+        logging.info(log)
 
-        super(CallbackModule, self).set_options(task_keys=task_keys, var_options=var_options, direct=direct)
+    def playbook_on_stats(self, stats):
+        log = "playbook_execution_time " + str(tasktime())
+        logging.info(log)
+        timestamp(self)
+        self.current = None
+        results = self.stats.items()
 
-        self.sort_order = self.get_option('sort_order')
-        if self.sort_order is not None:
-            if self.sort_order == 'ascending':
-                self.sort_order = False
-            elif self.sort_order == 'descending':
-                self.sort_order = True
-            elif self.sort_order == 'none':
-                self.sort_order = None
+        # Sort the tasks by the specified sort
+        if self.sort_order is not None:
+            results = sorted(
+                    self.stats.items(),
+                    key=lambda x: x[1]['time'],
+                    reverse=self.sort_order,
+                    )
 
-        self.task_output_limit = self.get_option('output_limit')
-        if self.task_output_limit is not None:
-            if self.task_output_limit == 'all':
-                self.task_output_limit = None
-            else:
-                self.task_output_limit = int(self.task_output_limit)
+        # Display the number of tasks specified or the default of 20
+        results = results[:self.task_output_limit]
 
-    def _record_task(self, task):
-        """
-        Logs the start of each task
-        """
-
-        timestamp(self)
-
-        # Record the start time of the current task
-        self.current = task._uuid
-        self.stats[self.current] = {'time': (time.time()*1000), 'name': task.get_name()}
-        if self._display.verbosity >= 2:
-            self.stats[self.current]['path'] = task.get_path()
-
-    def v2_playbook_on_task_start(self, task, is_conditional):
-        self._record_task(task)
-
-    def v2_playbook_on_handler_task_start(self, task):
-        self._record_task(task)
-
-    def playbook_on_setup(self):
-        log = "playbook_on_setup_execution_time " + str(tasktime())
-        logging.info(log)
-
-    def playbook_on_stats(self, stats):
-        log = "playbook_execution_time " + str(tasktime())
-        logging.info(log)
-
-
-        timestamp(self)
-        self.current = None
-
-        results = self.stats.items()
-        # Sort the tasks by the specified sort
-        if self.sort_order is not None:
-            results = sorted(
-                self.stats.items(),
-                key=lambda x: x[1]['time'],
-                reverse=self.sort_order,
-            )
-
-        # Display the number of tasks specified or the default of 20
-        results = results[:self.task_output_limit]
-
-        # Print the timings
-        for uuid, result in results:
-            log = str(result['name']) + " " + str(result['time'])
-            logging.info(log)
-
+        # Print the timings
+        for uuid, result in results:
+            log = str(result['name']) + " " + str(result['time'])
+            logging.info(log)
