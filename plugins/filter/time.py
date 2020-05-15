d99080a566a34064fdd0d27001cde001aab6faa6
Rene Moser <mail@renemoser.net>
2020-05-15 20:23:11 +0200

    refactor and extend

--- plugins/filter/time.py
+++ plugins/filter/time.py
@@ -5,62 +5,92 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import re
 from ansible.errors import AnsibleFilterError
 
 
 UNIT_FACTORS = {
-    'second': {
-        's': 1,
-        'm': 60,
-        'h': 60 * 60,
-        'd': 24 * 60 * 60,
-    },
-    'minute': {
-        's': 1 / 60,
-        'm': 1,
-        'h': 60,
-        'd': 24 * 60,
-    },
-    'hour': {
-        's': 1 / 60 / 60,
-        'm': 1 / 60,
-        'h': 1,
-        'd': 24,
-    }
+    'ms': [],
+    's': [1000],
+    'm': [1000, 60],
+    'h': [1000, 60, 60],
+    'd': [1000, 60, 60, 24],
+    'w': [1000, 60, 60, 24, 7],
+    'y': [1000, 60, 60, 24, 365],
 }
 
 
-def to_time_unit(human_time, unit='second'):
+def multiply(factors):
+    result = 1
+    for factor in factors:
+         result = result * factor
+    return result
+
+
+def divide(divisors):
+    result = 1
+    for divisor in divisors:
+         result = result / divisor
+    return result
+
+
+def to_time_unit(human_time, unit='ms'):
     ''' Return a time unit from a human readable string '''
     if unit not in UNIT_FACTORS:
         raise AnsibleFilterError("to_time_unit() can not convert to the following unit: %s, "
                                  "available units: %s" % (unit, ', '.join(UNIT_FACTORS.keys())))
     result = 0
-    try:
-        for time in human_time.split():
-            for short_unit, factor in UNIT_FACTORS[unit].items():
-                if time.endswith(short_unit):
-                    time_int = int(time.replace(short_unit, ""))
-                    result += time_int * factor
-        return result
-    except Exception:
-        raise AnsibleFilterError(
-            "to_time_unit() can't interpret following string: %s" % human_time)
+    for h_time_string in human_time.split():
+        res = re.match(r'(-?\d+)(\w+)', h_time_string)
+        if not res:
+            raise AnsibleFilterError(
+                "to_time_unit() can not interpret following string: %s" % human_time)
+
+        h_time_int = int(res.group(1))
+        h_time_unit = res.group(2)
+
+        if h_time_unit not in UNIT_FACTORS:
+            raise AnsibleFilterError(
+                "to_time_unit() can not interpret following string: %s" % human_time)
+
+        time_in_milliseconds = h_time_int * multiply(UNIT_FACTORS[h_time_unit])
+        result += time_in_milliseconds * divide(UNIT_FACTORS[unit])
+    return round(result, 12)
+
+
+def to_milliseconds(human_time):
+    ''' Return milli seconds from a human readable string '''
+    return to_time_unit(human_time, 'ms')
 
 
 def to_seconds(human_time):
     ''' Return seconds from a human readable string '''
-    return to_time_unit(human_time, 'second')
+    return to_time_unit(human_time, 's')
 
 
 def to_minutes(human_time):
     ''' Return minutes from a human readable string '''
-    return to_time_unit(human_time, 'minute')
+    return to_time_unit(human_time, 'm')
 
 
 def to_hours(human_time):
     ''' Return hours from a human readable string '''
-    return to_time_unit(human_time, 'hour')
+    return to_time_unit(human_time, 'h')
+
+
+def to_days(human_time):
+    ''' Return days from a human readable string '''
+    return to_time_unit(human_time, 'd')
+
+
+def to_weeks(human_time):
+    ''' Return weeks from a human readable string '''
+    return to_time_unit(human_time, 'w')
+
+
+def to_years(human_time):
+    ''' Return years from a human readable string '''
+    return to_time_unit(human_time, 'y')
 
 
 class FilterModule(object):
@@ -69,9 +99,13 @@ class FilterModule(object):
     def filters(self):
         filters = {
             'to_time_unit': to_time_unit,
+            'to_milliseconds': to_milliseconds,
             'to_seconds': to_seconds,
             'to_minutes': to_minutes,
             'to_hours': to_hours,
+            'to_days': to_days,
+            'to_weeks': to_weeks,
+            'to_years': to_years,
         }
 
         return filters
