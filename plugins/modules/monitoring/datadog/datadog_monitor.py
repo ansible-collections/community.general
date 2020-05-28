eec09e9e23c61b6e600bcda8bb536e8b1bb3dda8
Charles C <nafredy@gmail.com>
2020-05-28 15:04:19 -0700

    datadog_monitor: kwargs for _update_monitor used message vs notification_message (#389)
    * kwargs for _update_monitor wasn't corrected to new notification_message param
    
    * forgot to bump a changelog fragment
    
    * Update changelogs/fragments/389-datadog_monitor-corrects-deprecated-message-param.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/monitoring/datadog/datadog_monitor.py
+++ plugins/modules/monitoring/datadog/datadog_monitor.py
@@ -301,7 +301,7 @@ def _update_monitor(module, monitor, options):
     try:
         kwargs = dict(id=monitor['id'], query=module.params['query'],
                       name=_fix_template_vars(module.params['name']),
-                      message=_fix_template_vars(module.params['message']),
+                      message=_fix_template_vars(module.params['notification_message']),
                       escalation_message=_fix_template_vars(module.params['escalation_message']),
                       options=options)
         if module.params['tags'] is not None:
