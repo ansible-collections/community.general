605f17656e31907ed94fb0f7b83d425d72e35390
Steven Michaels <steven.michaels@gmail.com>
2020-06-07 12:08:19 -0500

    Add vpc_uuid option to droplet creation

--- plugins/modules/cloud/digital_ocean/digital_ocean_droplet.py
+++ plugins/modules/cloud/digital_ocean/digital_ocean_droplet.py
@@ -54,6 +54,11 @@ options:
      - add an additional, private network interface to droplet for inter-droplet communication.
     default: False
     type: bool
+  vpc_uuid:
+    description:
+     - A string specifying the UUID of the VPC to which the Droplet will be assigned. If excluded, Droplet will be
+       assigned to the account's default VPC for the region.
+    required: False
   user_data:
     description:
       - opaque blob of data which is made available to the droplet
@@ -116,10 +121,8 @@ EXAMPLES = '''
     wait_timeout: 500
     ssh_keys: [ .... ]
   register: my_droplet
-
 - debug:
     msg: "ID is {{ my_droplet.data.droplet.id }}, IP is {{ my_droplet.data.ip_address }}"
-
 - name: Ensure a droplet is present
   digital_ocean_droplet:
     state: present
@@ -130,7 +133,6 @@ EXAMPLES = '''
     region: sfo1
     image: ubuntu-16-04-x64
     wait_timeout: 500
-
 - name: Ensure a droplet is present with SSH keys installed
   digital_ocean_droplet:
     state: present
@@ -317,6 +319,7 @@ def main():
             region=dict(aliases=['region_id']),
             ssh_keys=dict(type='list'),
             private_networking=dict(type='bool', default=False),
+            vpc_uuid=dict(type='str'),
             backups=dict(type='bool', default=False),
             monitoring=dict(type='bool', default=False),
             id=dict(aliases=['droplet_id'], type='int'),
