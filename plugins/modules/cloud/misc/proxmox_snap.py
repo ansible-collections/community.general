12133c03159197290f2f4e97ee71901c1020bf2e
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2020-06-19 12:01:20 +0200

    This should fix the doc errors

--- plugins/modules/cloud/misc/proxmox_snap.py
+++ plugins/modules/cloud/misc/proxmox_snap.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: proxmox_snap
 short_description: snapshot management of instances in Proxmox VE cluster
