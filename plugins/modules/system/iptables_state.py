3548cba99d866ebfbf8ab3e55df6a62fa2a90ec7
quidame <quidame@poivron.org>
2020-06-27 15:47:13 +0200

    bump version_added to 1.0.0

--- plugins/modules/system/iptables_state.py
+++ plugins/modules/system/iptables_state.py
@@ -12,7 +12,7 @@ DOCUMENTATION = r'''
 ---
 module: iptables_state
 short_description: Save iptables state into a file or restore it from a file
-version_added: '0.2.0'
+version_added: '1.0.0'
 author: quidame (@quidame)
 description:
   - C(iptables) is used to set up, maintain, and inspect the tables of IP
