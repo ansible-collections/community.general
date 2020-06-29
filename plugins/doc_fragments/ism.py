868141046c89d173ea995d86d7fa5fb16a987980
ISIB-Group <lifeng@inspur.com>
2020-06-29 11:00:54 +0800

    add doc_fragments ism

---
+++ plugins/doc_fragments/ism.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+class ModuleDocFragment(object):
+
+    # Standard files documentation fragment
+    DOCUMENTATION = r'''
+options:
+  provider:
+    description:
+      - A dict object containing connection details.
+    suboptions:
+      host:
+        description:
+          - Specifies the DNS host name or address for connecting to the remote
+            device over the specified transport.  The value of host is used as
+            the destination address for the transport.
+        type: str
+        required: true
+      username:
+        description:
+          - Configures the username to use to authenticate the connection to
+            the remote device. If the value is not specified in the task, the value of environment
+            variable C(ANSIBLE_NET_USERNAME) will be used instead.
+        type: str
+      password:
+        description:
+          - Specifies the password to use to authenticate the connection to
+            the remote device. If the value is not specified in the task, the
+            value of environment variable C(ANSIBLE_NET_PASSWORD) will be used instead.
+        type: str
+'''
