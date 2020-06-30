a62c920ced3777856189a06dbd7a9212c07758dd
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2020-06-30 23:27:19 +0200

    Copied and modified unit test from terraform as a starting point

---
+++ tests/unit/plugins/modules/cloud/misc/test_proxmox_snap.py
@@ -0,0 +1,20 @@
+# Copyright: (c) 2019, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import json
+
+import pytest
+
+from ansible_collections.community.general.plugins.modules.cloud.misc import proxmox_snap
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import set_module_args
+
+
+def test_proxmox_snap_without_argument(capfd):
+    set_module_args({})
+    with pytest.raises(SystemExit) as results:
+        proxmox_snap.main()
+
+    out, err = capfd.readouterr()
+    assert not err
+    assert json.loads(out)['failed']
+    assert 'project_path' in json.loads(out)['msg']
