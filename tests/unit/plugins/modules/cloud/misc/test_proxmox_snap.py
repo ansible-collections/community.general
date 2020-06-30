b80e2ac964b9de5128a819209991934dc2708188
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2020-06-30 23:37:02 +0200

    Fixed sanity tests

--- tests/unit/plugins/modules/cloud/misc/test_proxmox_snap.py
+++ tests/unit/plugins/modules/cloud/misc/test_proxmox_snap.py
@@ -1,10 +1,10 @@
 # Copyright: (c) 2019, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
 
 import json
-
 import pytest
-
 from ansible_collections.community.general.plugins.modules.cloud.misc import proxmox_snap
 from ansible_collections.community.general.tests.unit.plugins.modules.utils import set_module_args
 
