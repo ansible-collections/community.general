80651cc79c72021615ab10cd7e3a07ed8e2e8eef
Adam C. Migus <adam@migus.org>
2020-07-13 01:45:07 -0400

    Add Thycotic DevOps Secrets Vault lookup plugin (#90)
    * Add the Thycotic DevOps Secrets Vault lookup plugin.
    
    * Update plugins/lookup/dsv.py
    
    Co-Authored-By: Felix Fontein <felix@fontein.de>
    
    * Update plugins/lookup/dsv.py
    
    Co-Authored-By: Felix Fontein <felix@fontein.de>
    
    * Update plugins/lookup/dsv.py
    
    Co-Authored-By: Felix Fontein <felix@fontein.de>
    
    * Fix import error check per code review.
    
    * Apply suggestions from code review
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Add a unittest for plugins/lookup/dsv.py
    
    * Add copyrights.
    
    * Apply suggestions from code review
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Fixed formatting bug in test_dsv.py
    
    * Apply suggestions from code review
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Apply suggestions from code review
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

---
+++ tests/unit/plugins/lookup/test_dsv.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# (c) 2020, Adam Migus <adam@migus.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# Make coding more python3-ish
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible_collections.community.general.tests.unit.compat.unittest import TestCase
+from ansible_collections.community.general.tests.unit.compat.mock import (
+    patch,
+    MagicMock,
+)
+from ansible_collections.community.general.plugins.lookup import dsv
+from ansible.plugins.loader import lookup_loader
+
+
+class MockSecretsVault(MagicMock):
+    RESPONSE = '{"foo": "bar"}'
+
+    def get_secret_json(self, path):
+        return self.RESPONSE
+
+
+class TestLookupModule(TestCase):
+    def setUp(self):
+        dsv.sdk_is_missing = False
+        self.lookup = lookup_loader.get("community.general.dsv")
+
+    @patch(
+        "ansible_collections.community.general.plugins.lookup.dsv.LookupModule.Client",
+        MockSecretsVault(),
+    )
+    def test_get_secret_json(self):
+        self.assertListEqual(
+            [MockSecretsVault.RESPONSE],
+            self.lookup.run(
+                ["/dummy"],
+                [],
+                **{"tenant": "dummy", "client_id": "dummy", "client_secret": "dummy", }
+            ),
+        )
