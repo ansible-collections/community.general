7fe18e966a2c406ef8e160388ff1a9e1cb7fdf16
ernst-s <17832702+ernst-s@users.noreply.github.com>
2020-07-21 09:10:26 +0200

    Handle 'No more variables left in this MIB View ' in snmp_facts (#613)
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/modules/net_tools/snmp_facts.py
+++ plugins/modules/net_tools/snmp_facts.py
@@ -173,6 +173,7 @@ from collections import defaultdict
 PYSNMP_IMP_ERR = None
 try:
     from pysnmp.entity.rfc3413.oneliner import cmdgen
+    from pysnmp.proto.rfc1905 import EndOfMibView
     has_pysnmp = True
 except Exception:
     PYSNMP_IMP_ERR = traceback.format_exc()
@@ -393,6 +394,8 @@ def main():
 
     for varBinds in varTable:
         for oid, val in varBinds:
+            if isinstance(val, EndOfMibView):
+                continue
             current_oid = oid.prettyPrint()
             current_val = val.prettyPrint()
             if v.ifIndex in current_oid:
