402bb01501c14b9aeac8cf367e038ac22453d149
Abhijeet Kasurde <akasurde@redhat.com>
2020-08-04 18:30:41 +0530

    filetree: Update example and documentation for plugin (#728)
    Fixes: #727
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>
    (cherry picked from commit ce48751033fecfc942837b6c4a6f430f037ee8e5)

--- plugins/lookup/dig.py
+++ plugins/lookup/dig.py
@@ -77,7 +77,7 @@ EXAMPLES = """
 RETURN = """
   _list:
     description:
-      - list of composed strings or dictonaries with key and value
+      - list of composed strings or dictionaries with key and value
         If a dictionary, fields shows the keys returned depending on query type
     contains:
        ALL:
