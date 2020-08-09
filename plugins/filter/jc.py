73ef815645b43bdd707569ce5b59998842998691
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-08-09 15:12:12 -0700

    add documentation and optional quiet and raw arguments

--- plugins/filter/jc.py
+++ plugins/filter/jc.py
@@ -30,19 +30,59 @@ except ImportError:
     HAS_LIB = False
 
 
-def jc(data, parser):
-    '''Convert returned command output to JSON using the JC library'''
+def jc(data, parser, quiet=True, raw=False):
+    """Convert returned command output to JSON using the JC library
+
+    Arguments:
+
+        parser      required    (string) the correct parser for the input data (e.g. 'ifconfig')
+                                see https://github.com/kellyjonbrazil/jc#parsers for latest list of parsers.
+        quiet       optional    (bool) True to suppress warning messages (default is True)
+        raw         optional    (bool) True to return pre-processed JSON (default is False)
+
+    Returns:
+
+        dictionary or list of dictionaries
+
+    Example: 
+
+        - name: run date command
+          hosts: ubuntu
+          tasks:
+          - shell: date
+            register: result
+          - set_fact:
+              myvar: "{{ result.stdout | community.general.jc('date') }}"
+          - debug:
+              msg: "{{ myvar }}"
+
+        produces:
+
+        ok: [192.168.1.239] => {
+            "msg": {
+                "day": 9,
+                "hour": 22,
+                "minute": 6,
+                "month": "Aug",
+                "month_num": 8,
+                "second": 22,
+                "timezone": "UTC",
+                "weekday": "Sun",
+                "weekday_num": 1,
+                "year": 2020
+            }
+        }
+    """
 
     if not HAS_LIB:
-        raise AnsibleError('You need to install "jc" prior to running '
-                           'jc filter')
+        raise AnsibleError('You need to install "jc" prior to running jc filter')
 
     try:
         jc_parser = importlib.import_module('jc.parsers.' + parser)
-        return jc_parser.parse(data, quiet=True)
+        return jc_parser.parse(data, quiet=quiet, raw=raw)
 
     except Exception as e:
-        raise AnsibleFilterError('Error in jc filter plugin: %s' % e)
+        raise AnsibleFilterError('Error in jc filter plugin:  %s' % e)
 
 
 class FilterModule(object):
