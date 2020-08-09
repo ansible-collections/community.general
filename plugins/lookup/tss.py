981e4e75ed6a8e278b88c0a68cd2425335bf2b97
Felix Fontein <felix@fontein.de>
2020-08-09 21:50:08 +0200

    Fix author entry for dsv and tss lookup plugin. (#753)

--- plugins/lookup/tss.py
+++ plugins/lookup/tss.py
@@ -7,7 +7,7 @@ __metaclass__ = type
 
 DOCUMENTATION = r"""
 lookup: tss
-author: Adam Migus (adam@migus.org)
+author: Adam Migus (@amigus) <adam@migus.org>
 short_description: Get secrets from Thycotic Secret Server
 version_added: 1.0.0
 description:
