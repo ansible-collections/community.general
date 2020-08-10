5e2fabd65ff1d862077c0daaf69fcf1a2eed6917
Kelly Brazil <kellyjonbrazil@gmail.com>
2020-08-10 07:33:55 -0700

    use pip instead of pip3

--- tests/integration/targets/filter_jc/runme.sh
+++ tests/integration/targets/filter_jc/runme.sh
@@ -7,6 +7,6 @@ source virtualenv.sh
 # Requirements have to be installed prior to running ansible-playbook
 # because plugins and requirements are loaded before the task runs
 
-pip3 install jc
+pip install jc
 
 ANSIBLE_ROLES_PATH=../ ansible-playbook runme.yml "$@"
