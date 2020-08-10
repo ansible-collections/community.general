10c963feada70d4f4944cbc542ce45833052264e
Amin Vakil <info@aminvakil.com>
2020-08-10 18:56:04 +0430

    Add copyright

--- plugins/action/system/shutdown.py
+++ plugins/action/system/shutdown.py
@@ -1,3 +1,4 @@
+# Copyright: (c) 2020, Amin Vakil <info@aminvakil.com>
 # Copyright: (c) 2016-2018, Matt Davis <mdavis@ansible.com>
 # Copyright: (c) 2018, Sam Doran <sdoran@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
