03f58894ff6d7a4432a06eaa4052dc882d33c414
Felix Fontein <felix@fontein.de>
2020-08-11 17:13:07 +0200

    Fix doc typo

--- plugins/modules/cloud/scaleway/scaleway_database_backup.py
+++ plugins/modules/cloud/scaleway/scaleway_database_backup.py
@@ -81,7 +81,7 @@ options:
     description:
         - Wait for the instance to reach its desired state before returning.
     type: bool
-    default: 'no'
+    default: false
 
   wait_timeout:
     description:
