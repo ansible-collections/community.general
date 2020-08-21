799cbe57997edbf8049c020530091592129e9eb6
Milan Ilic <35260522+ilicmilan@users.noreply.github.com>
2020-08-21 12:57:26 +0200

    postgresql_privs: Fix bug with grant_option (#796)

--- plugins/modules/database/postgresql/postgresql_privs.py
+++ plugins/modules/database/postgresql/postgresql_privs.py
@@ -864,12 +864,14 @@ class QueryBuilder(object):
                 self.query[-1] += ' WITH ADMIN OPTION;'
             else:
                 self.query[-1] += ' WITH GRANT OPTION;'
-        else:
+        elif self._grant_option is False:
             self.query[-1] += ';'
             if self._obj_type == 'group':
                 self.query.append('REVOKE ADMIN OPTION FOR {0} FROM {1};'.format(self._set_what, self._for_whom))
             elif not self._obj_type == 'default_privs':
                 self.query.append('REVOKE GRANT OPTION FOR {0} FROM {1};'.format(self._set_what, self._for_whom))
+        else:
+            self.query[-1] += ';'
 
     def add_default_priv(self):
         for obj in self._objs:
