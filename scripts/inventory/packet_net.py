9a0e910d670d220f680cd5eae2173dba0cf260c6
elreydetoda <unc741@gmail.com>
2020-09-16 01:40:19 -0400

    adding fix for packet_net.py

--- scripts/inventory/packet_net.py
+++ scripts/inventory/packet_net.py
@@ -346,9 +346,9 @@ class PacketInventory(object):
 
         # Inventory: Group by OS
         if self.group_by_operating_system:
-            self.push(self.inventory, device.operating_system['slug'], dest)
+            self.push(self.inventory, device.operating_system.__dict__['slug'], dest)
             if self.nested_groups:
-                self.push_group(self.inventory, 'operating_systems', device.operating_system['slug'])
+                self.push_group(self.inventory, 'operating_systems', device.operating_system.__dict__['slug'])
 
         # Inventory: Group by plan type
         if self.group_by_plan_type:
@@ -395,7 +395,7 @@ class PacketInventory(object):
             elif key == 'packet_facility':
                 device_vars[key] = value['code']
             elif key == 'packet_operating_system':
-                device_vars[key] = value['slug']
+                device_vars[key] = value.__dict__['slug']
             elif key == 'packet_plan':
                 device_vars[key] = value['slug']
             elif key == 'packet_tags':
