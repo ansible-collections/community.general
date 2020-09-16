18c5cdfa66fb5f05f4889048d2f536747854c161
scodeman <30977678+scodeman@users.noreply.github.com>
2020-09-16 08:08:20 +0200

    Update plugins/modules/source_control/gitlab/gitlab_group_variable.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/source_control/gitlab/gitlab_group_variable.py
+++ plugins/modules/source_control/gitlab/gitlab_group_variable.py
@@ -60,7 +60,7 @@ options:
         See GitLab documentation on acceptable values for a masked variable (U(https://docs.gitlab.com/ce/ci/variables/#masked-variables)).
     default: {}
     type: dict
-  note:
+  notes:
   - Supports I(check_mode).
 '''
 
