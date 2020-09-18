de4e6ba9eb65017244ea54cbef20497936b32e99
Tristan Le Guern <tristan.leguern-presta@deveryware.com>
2020-09-18 09:49:01 +0200

    Update plugins/modules/cloud/misc/proxmox_kvm.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/cloud/misc/proxmox_kvm.py
+++ plugins/modules/cloud/misc/proxmox_kvm.py
@@ -1153,7 +1153,7 @@ def main():
 
             proxmox_node = proxmox.nodes(vm[0]['node'])
             if getattr(proxmox_node, VZ_TYPE)(vmid).status.current.get()['status'] == 'running':
-              module.exit_json(changed=False, msg="VM %s is running. Stop it before deletion or use force=yes." % vmid)
+                module.exit_json(changed=False, msg="VM %s is running. Stop it before deletion." % vmid)
             taskid = getattr(proxmox_node, VZ_TYPE).delete(vmid)
             if not wait_for_task(module, proxmox, vm[0]['node'], taskid):
                 module.fail_json(msg='Reached timeout while waiting for removing VM. Last line in task before timeout: %s' %
