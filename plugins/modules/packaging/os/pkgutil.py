9bbaf557431ed8bfe7aa4dec0e50a9e084bd35b6
Peter Oliver <git@mavit.org.uk>
2020-09-28 11:37:27 +0100

    Fully qualify svr4pkg module name
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/packaging/os/pkgutil.py
+++ plugins/modules/packaging/os/pkgutil.py
@@ -17,7 +17,7 @@ module: pkgutil
 short_description: OpenCSW package management on Solaris
 description:
 - This module installs, updates and removes packages from the OpenCSW project for Solaris.
-- Unlike the M(svr4pkg) module, it will resolve and download dependencies.
+- Unlike the M(community.general.svr4pkg) module, it will resolve and download dependencies.
 - See U(https://www.opencsw.org/) for more information about the project.
 author:
 - Alexander Winkler (@dermute)
