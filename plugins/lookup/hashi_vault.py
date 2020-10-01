237672c9a043889eff26959cbdc910796aca9cfc
markafarrell <mark.andrew.farrell@gmail.com>
2020-10-02 08:57:10 +1000

    change precedence for validate_certs

--- plugins/lookup/hashi_vault.py
+++ plugins/lookup/hashi_vault.py
@@ -119,8 +119,8 @@ DOCUMENTATION = """
       description:
         - Controls verification and validation of SSL certificates, mostly you only want to turn off with self signed ones.
         - Will be populated with the inverse of VAULT_SKIP_VERIFY if it is set (added in community.general 1.3.0).
+        - Will default to True if neither validate_certs or VAULT_SKIP_VERIFY are set.
       type: boolean
-      default: True
     namespace:
       description: Namespace where secrets reside. Requires HVAC 0.7.0+ and Vault 0.11+.
       env:
@@ -490,19 +490,26 @@ class LookupModule(LookupBase):
         '''' return a bool or cacert '''
         ca_cert = self.get_option('ca_cert')
 
-        vault_skip_verify = os.environ.get('VAULT_SKIP_VERIFY')
+        validate_certs = self.get_option('validate_certs')
 
-        if vault_skip_verify is not None:
-            try:
-                # Check that we have a boolean value
-                vault_skip_verify = boolean(vault_skip_verify)
-                # Use the inverse of VAULT_SKIP_VERIFY
-                validate_certs = not vault_skip_verify
-            except TypeError:
-                # Not a boolean value fallback to validate_certs option
-                validate_certs = self.get_option('validate_certs')
-        else:
-            validate_certs = self.get_option('validate_certs')
+        if validate_certs is not None:
+            # Validate certs option was not explicitly set
+
+            # Check if VAULT_SKIP_VERIFY is set
+            vault_skip_verify = os.environ.get('VAULT_SKIP_VERIFY')
+
+            if vault_skip_verify is not None:
+                # VAULT_SKIP_VERIFY is set
+                try:
+                    # Check that we have a boolean value
+                    vault_skip_verify = boolean(vault_skip_verify)
+                    # Use the inverse of VAULT_SKIP_VERIFY
+                    validate_certs = not vault_skip_verify
+                except TypeError:
+                    # Not a boolean value fallback to default value (True)
+                    validate_certs = True
+            else:
+                validate_certs = True
 
         if not (validate_certs and ca_cert):
             self.set_option('ca_cert', validate_certs)
