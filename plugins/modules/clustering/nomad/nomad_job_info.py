0f2731a0473debf0f2a288dcb91e558a30cc4ae6
chris93111 <christopheferreira@ymail.com>
2020-10-18 18:17:16 +0200

    lint
    lint

--- plugins/modules/clustering/nomad/nomad_job_info.py
+++ plugins/modules/clustering/nomad/nomad_job_info.py
@@ -50,215 +50,215 @@ EXAMPLES = '''
 
 RETURN = '''
 result:
-    description: List with dictionary contains jobs info 
+    description: List with dictionary contains jobs info
     returned: success
-    type: complex
+    type: list
     sample: [
         {
-            "Affinities": null, 
-            "AllAtOnce": false, 
-            "Constraints": null, 
-            "ConsulToken": "", 
-            "CreateIndex": 13, 
+            "Affinities": null,
+            "AllAtOnce": false,
+            "Constraints": null,
+            "ConsulToken": "",
+            "CreateIndex": 13,
             "Datacenters": [
                 "dc1"
-            ], 
-            "Dispatched": false, 
-            "ID": "example", 
-            "JobModifyIndex": 13, 
-            "Meta": null, 
-            "ModifyIndex": 13, 
-            "Multiregion": null, 
-            "Name": "example", 
-            "Namespace": "default", 
-            "NomadTokenID": "", 
-            "ParameterizedJob": null, 
-            "ParentID": "", 
-            "Payload": null, 
-            "Periodic": null, 
-            "Priority": 50, 
-            "Region": "global", 
-            "Spreads": null, 
-            "Stable": false, 
-            "Status": "pending", 
-            "StatusDescription": "", 
-            "Stop": false, 
-            "SubmitTime": 1602244370615307000, 
+            ],
+            "Dispatched": false,
+            "ID": "example",
+            "JobModifyIndex": 13,
+            "Meta": null,
+            "ModifyIndex": 13,
+            "Multiregion": null,
+            "Name": "example",
+            "Namespace": "default",
+            "NomadTokenID": "",
+            "ParameterizedJob": null,
+            "ParentID": "",
+            "Payload": null,
+            "Periodic": null,
+            "Priority": 50,
+            "Region": "global",
+            "Spreads": null,
+            "Stable": false,
+            "Status": "pending",
+            "StatusDescription": "",
+            "Stop": false,
+            "SubmitTime": 1602244370615307000,
             "TaskGroups": [
                 {
-                    "Affinities": null, 
-                    "Constraints": null, 
-                    "Count": 1, 
+                    "Affinities": null,
+                    "Constraints": null,
+                    "Count": 1,
                     "EphemeralDisk": {
-                        "Migrate": false, 
-                        "SizeMB": 300, 
+                        "Migrate": false,
+                        "SizeMB": 300,
                         "Sticky": false
-                    }, 
-                    "Meta": null, 
+                    },
+                    "Meta": null,
                     "Migrate": {
-                        "HealthCheck": "checks", 
-                        "HealthyDeadline": 300000000000, 
-                        "MaxParallel": 1, 
+                        "HealthCheck": "checks",
+                        "HealthyDeadline": 300000000000,
+                        "MaxParallel": 1,
                         "MinHealthyTime": 10000000000
-                    }, 
-                    "Name": "cache", 
-                    "Networks": null, 
+                    },
+                    "Name": "cache",
+                    "Networks": null,
                     "ReschedulePolicy": {
-                        "Attempts": 0, 
-                        "Delay": 30000000000, 
-                        "DelayFunction": "exponential", 
-                        "Interval": 0, 
-                        "MaxDelay": 3600000000000, 
+                        "Attempts": 0,
+                        "Delay": 30000000000,
+                        "DelayFunction": "exponential",
+                        "Interval": 0,
+                        "MaxDelay": 3600000000000,
                         "Unlimited": true
-                    }, 
+                    },
                     "RestartPolicy": {
-                        "Attempts": 3, 
-                        "Delay": 15000000000, 
-                        "Interval": 1800000000000, 
+                        "Attempts": 3,
+                        "Delay": 15000000000,
+                        "Interval": 1800000000000,
                         "Mode": "fail"
-                    }, 
-                    "Scaling": null, 
-                    "Services": null, 
-                    "ShutdownDelay": null, 
-                    "Spreads": null, 
-                    "StopAfterClientDisconnect": null, 
+                    },
+                    "Scaling": null,
+                    "Services": null,
+                    "ShutdownDelay": null,
+                    "Spreads": null,
+                    "StopAfterClientDisconnect": null,
                     "Tasks": [
                         {
-                            "Affinities": null, 
-                            "Artifacts": null, 
-                            "CSIPluginConfig": null, 
+                            "Affinities": null,
+                            "Artifacts": null,
+                            "CSIPluginConfig": null,
                             "Config": {
-                                "image": "redis:3.2", 
+                                "image": "redis:3.2",
                                 "port_map": [
                                     {
                                         "db": 6379.0
                                     }
                                 ]
-                            }, 
-                            "Constraints": null, 
-                            "DispatchPayload": null, 
-                            "Driver": "docker", 
-                            "Env": null, 
-                            "KillSignal": "", 
-                            "KillTimeout": 5000000000, 
-                            "Kind": "", 
-                            "Leader": false, 
-                            "Lifecycle": null, 
+                            },
+                            "Constraints": null,
+                            "DispatchPayload": null,
+                            "Driver": "docker",
+                            "Env": null,
+                            "KillSignal": "",
+                            "KillTimeout": 5000000000,
+                            "Kind": "",
+                            "Leader": false,
+                            "Lifecycle": null,
                             "LogConfig": {
-                                "MaxFileSizeMB": 10, 
+                                "MaxFileSizeMB": 10,
                                 "MaxFiles": 10
-                            }, 
-                            "Meta": null, 
-                            "Name": "redis", 
+                            },
+                            "Meta": null,
+                            "Name": "redis",
                             "Resources": {
-                                "CPU": 500, 
-                                "Devices": null, 
-                                "DiskMB": 0, 
-                                "IOPS": 0, 
-                                "MemoryMB": 256, 
+                                "CPU": 500,
+                                "Devices": null,
+                                "DiskMB": 0,
+                                "IOPS": 0,
+                                "MemoryMB": 256,
                                 "Networks": [
                                     {
-                                        "CIDR": "", 
-                                        "DNS": null, 
-                                        "Device": "", 
+                                        "CIDR": "",
+                                        "DNS": null,
+                                        "Device": "",
                                         "DynamicPorts": [
                                             {
-                                                "HostNetwork": "default", 
-                                                "Label": "db", 
-                                                "To": 0, 
+                                                "HostNetwork": "default",
+                                                "Label": "db",
+                                                "To": 0,
                                                 "Value": 0
                                             }
-                                        ], 
-                                        "IP": "", 
-                                        "MBits": 10, 
-                                        "Mode": "", 
+                                        ],
+                                        "IP": "",
+                                        "MBits": 10,
+                                        "Mode": "",
                                         "ReservedPorts": null
                                     }
                                 ]
-                            }, 
+                            },
                             "RestartPolicy": {
-                                "Attempts": 3, 
-                                "Delay": 15000000000, 
-                                "Interval": 1800000000000, 
+                                "Attempts": 3,
+                                "Delay": 15000000000,
+                                "Interval": 1800000000000,
                                 "Mode": "fail"
-                            }, 
+                            },
                             "Services": [
                                 {
-                                    "AddressMode": "auto", 
-                                    "CanaryMeta": null, 
-                                    "CanaryTags": null, 
+                                    "AddressMode": "auto",
+                                    "CanaryMeta": null,
+                                    "CanaryTags": null,
                                     "Checks": [
                                         {
-                                            "AddressMode": "", 
-                                            "Args": null, 
-                                            "CheckRestart": null, 
-                                            "Command": "", 
-                                            "Expose": false, 
-                                            "FailuresBeforeCritical": 0, 
-                                            "GRPCService": "", 
-                                            "GRPCUseTLS": false, 
-                                            "Header": null, 
-                                            "InitialStatus": "", 
-                                            "Interval": 10000000000, 
-                                            "Method": "", 
-                                            "Name": "alive", 
-                                            "Path": "", 
-                                            "PortLabel": "", 
-                                            "Protocol": "", 
-                                            "SuccessBeforePassing": 0, 
-                                            "TLSSkipVerify": false, 
-                                            "TaskName": "", 
-                                            "Timeout": 2000000000, 
+                                            "AddressMode": "",
+                                            "Args": null,
+                                            "CheckRestart": null,
+                                            "Command": "",
+                                            "Expose": false,
+                                            "FailuresBeforeCritical": 0,
+                                            "GRPCService": "",
+                                            "GRPCUseTLS": false,
+                                            "Header": null,
+                                            "InitialStatus": "",
+                                            "Interval": 10000000000,
+                                            "Method": "",
+                                            "Name": "alive",
+                                            "Path": "",
+                                            "PortLabel": "",
+                                            "Protocol": "",
+                                            "SuccessBeforePassing": 0,
+                                            "TLSSkipVerify": false,
+                                            "TaskName": "",
+                                            "Timeout": 2000000000,
                                             "Type": "tcp"
                                         }
-                                    ], 
-                                    "Connect": null, 
-                                    "EnableTagOverride": false, 
-                                    "Meta": null, 
-                                    "Name": "redis-cache", 
-                                    "PortLabel": "db", 
+                                    ],
+                                    "Connect": null,
+                                    "EnableTagOverride": false,
+                                    "Meta": null,
+                                    "Name": "redis-cache",
+                                    "PortLabel": "db",
                                     "Tags": [
-                                        "global", 
+                                        "global",
                                         "cache"
-                                    ], 
+                                    ],
                                     "TaskName": ""
                                 }
-                            ], 
-                            "ShutdownDelay": 0, 
-                            "Templates": null, 
-                            "User": "", 
-                            "Vault": null, 
+                            ],
+                            "ShutdownDelay": 0,
+                            "Templates": null,
+                            "User": "",
+                            "Vault": null,
                             "VolumeMounts": null
                         }
-                    ], 
+                    ],
                     "Update": {
-                        "AutoPromote": false, 
-                        "AutoRevert": false, 
-                        "Canary": 0, 
-                        "HealthCheck": "checks", 
-                        "HealthyDeadline": 180000000000, 
-                        "MaxParallel": 1, 
-                        "MinHealthyTime": 10000000000, 
-                        "ProgressDeadline": 600000000000, 
+                        "AutoPromote": false,
+                        "AutoRevert": false,
+                        "Canary": 0,
+                        "HealthCheck": "checks",
+                        "HealthyDeadline": 180000000000,
+                        "MaxParallel": 1,
+                        "MinHealthyTime": 10000000000,
+                        "ProgressDeadline": 600000000000,
                         "Stagger": 30000000000
-                    }, 
+                    },
                     "Volumes": null
                 }
-            ], 
-            "Type": "service", 
+            ],
+            "Type": "service",
             "Update": {
-                "AutoPromote": false, 
-                "AutoRevert": false, 
-                "Canary": 0, 
-                "HealthCheck": "", 
-                "HealthyDeadline": 0, 
-                "MaxParallel": 1, 
-                "MinHealthyTime": 0, 
-                "ProgressDeadline": 0, 
+                "AutoPromote": false,
+                "AutoRevert": false,
+                "Canary": 0,
+                "HealthCheck": "",
+                "HealthyDeadline": 0,
+                "MaxParallel": 1,
+                "MinHealthyTime": 0,
+                "ProgressDeadline": 0,
                 "Stagger": 30000000000
-            }, 
-            "VaultNamespace": "", 
-            "VaultToken": "", 
+            },
+            "VaultNamespace": "",
+            "VaultToken": "",
             "Version": 0
         }
     ]
@@ -290,7 +290,8 @@ def run():
             client_cert=dict(type='path', default=None),
             client_key=dict(type='path', default=None),
             namespace=dict(type='str', default=None),
-            name=dict(type='str', default=None)
+            name=dict(type='str', default=None),
+            token=dict(type='str', default=None, no_log=True)
         ),
         supports_check_mode=True
     )
@@ -315,11 +316,11 @@ def run():
     try:
         job_list = nomad_client.jobs.get_jobs()
         for job in job_list:
-            nomad_jobs.append(nomad_client.job.get_job(job.get('ID')) )
+            nomad_jobs.append(nomad_client.job.get_job(job.get('ID')))
             result = nomad_jobs
     except Exception as e:
         module.fail_json(msg=to_native(e))
-    
+
     if module.params.get('name'):
         filter = list()
         try:
@@ -328,7 +329,7 @@ def run():
                     filter.append(job)
                     result = filter
             if not filter:
-                module.fail_json(msg="Couldn't find Job with id " + str(module.params.get('name')) )
+                module.fail_json(msg="Couldn't find Job with id " + str(module.params.get('name')))
         except Exception as e:
             module.fail_json(msg=to_native(e))
 
