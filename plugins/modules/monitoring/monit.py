23143e62b362aa37c9280d142767263e6a538e75
Simon Kelly <skelly@dimagi.com>
2020-10-20 12:18:04 +0200

    don't check rc for validate

--- plugins/modules/monitoring/monit.py
+++ plugins/modules/monitoring/monit.py
@@ -144,8 +144,9 @@ class Monit(object):
         :@param validate: Force monit to re-check the status of the process
         """
         monit_command = "validate" if validate else "status"
+        check_rc = False if validate else True  # 'validate' always has rc = 1
         command = ' '.join([self.monit_bin_path, monit_command, self.command_args, self.process_name])
-        rc, out, err = self.module.run_command(command, check_rc=True)
+        rc, out, err = self.module.run_command(command, check_rc=check_rc)
         return self._parse_status(out, err)
 
     def _parse_status(self, output, err):
