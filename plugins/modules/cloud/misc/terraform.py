f990ba856cd637c6cf8fa7bd29db8241d8e2f5d1
Damien Guihal <dguihal@talend.com>
2020-10-21 10:41:09 +0200

    feat: switch from an init_extra_args global option to an init_reconfigure specific option

--- plugins/modules/cloud/misc/terraform.py
+++ plugins/modules/cloud/misc/terraform.py
@@ -93,12 +93,12 @@ options:
     type: list
     elements: path
     version_added: '0.2.0'
-  init_extra_args:
+  init_reconfigure:
     description:
-      - Allow extra options to be given to terraform init command.
-    required: false
-    type: list
-    version_added: '1.2.0'
+      - Forces backend reconfiguration during init.
+    default: false
+    type: bool
+    version_added: '1.3.0'
 notes:
    - To just run a `terraform plan`, use check mode.
 requirements: [ "terraform" ]
@@ -195,7 +195,7 @@ def _state_args(state_file):
     return []
 
 
-def init_plugins(bin_path, project_path, backend_config, backend_config_files, init_extra_args):
+def init_plugins(bin_path, project_path, backend_config, backend_config_files, init_reconfigure):
     command = [bin_path, 'init', '-input=false']
     if backend_config:
         for key, val in backend_config.items():
@@ -206,8 +206,8 @@ def init_plugins(bin_path, project_path, backend_config, backend_config_files, i
     if backend_config_files:
         for f in backend_config_files:
             command.extend(['-backend-config', f])
-    if init_extra_args:
-        command.extend(init_extra_args)
+    if init_reconfigure:
+        command.extend('-reconfigure')
     rc, out, err = module.run_command(command, cwd=project_path)
     if rc != 0:
         module.fail_json(msg="Failed to initialize Terraform modules:\r\n{0}".format(err))
@@ -295,7 +295,7 @@ def main():
             force_init=dict(type='bool', default=False),
             backend_config=dict(type='dict', default=None),
             backend_config_files=dict(type='list', elements='path', default=None),
-            init_extra_args=dict(required=False, type='list', default=[]),
+            init_reconfigure=dict(required=False, type='bool', default=False),
         ),
         required_if=[('state', 'planned', ['plan_file'])],
         supports_check_mode=True,
@@ -313,7 +313,7 @@ def main():
     force_init = module.params.get('force_init')
     backend_config = module.params.get('backend_config')
     backend_config_files = module.params.get('backend_config_files')
-    init_extra_args = module.params.get('init_extra_args')
+    init_reconfigure = module.params.get('init_reconfigure')
 
     if bin_path is not None:
         command = [bin_path]
@@ -321,7 +321,7 @@ def main():
         command = [module.get_bin_path('terraform', required=True)]
 
     if force_init:
-        init_plugins(command[0], project_path, backend_config, backend_config_files, init_extra_args)
+        init_plugins(command[0], project_path, backend_config, backend_config_files, init_reconfigure)
 
     workspace_ctx = get_workspace_context(command[0], project_path)
     if workspace_ctx["current"] != workspace:
