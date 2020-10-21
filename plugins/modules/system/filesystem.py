a6bc625d2a66395888a658bc699db77b0cafa995
quidame <quidame@poivron.org>
2020-10-21 21:27:17 +0200

    use dd as a fallback

--- plugins/modules/system/filesystem.py
+++ plugins/modules/system/filesystem.py
@@ -160,10 +160,19 @@ class Filesystem(object):
         self.module.run_command(cmd, check_rc=True)
 
     def wipefs(self, dev):
-        wipefs = self.module.get_bin_path('wipefs', required=True)
-        cmd = list([wipefs, "--all", dev])
         if self.module.check_mode:
-            cmd.insert(1, "--no-act")
+            return
+
+        try:
+            # wipefs comes with util-linux package
+            wipefs = self.module.get_bin_path('wipefs', required=True)
+            cmd = [wipefs, "--all", dev]
+        except:
+            # No such 'wipefs' tool on FreeBSD (and more), so we'll blindly
+            # blank the first 128kB, since xfs signature is at offset 0 and
+            # reiserfs & btrfs signatures are near after the first 64kB.
+            dd = self.module.get_bin_path('dd', required=True)
+            cmd = [dd, "if=/dev/zero", "of=%s" % dev, "bs=512", "count=256"]
         self.module.run_command(cmd, check_rc=True)
 
     def grow_cmd(self, dev):
