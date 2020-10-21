63fa9133d7858efd34302c982f523664c1c22142
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-10-21 08:36:27 +0000

    Update plugins/modules/cloud/misc/terraform.py
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/modules/cloud/misc/terraform.py
+++ plugins/modules/cloud/misc/terraform.py
@@ -348,8 +348,8 @@ def main():
         plan_file, needs_application, out, err, command = build_plan(command, project_path, variables_args, state_file,
                                                                      module.params.get('targets'), state, plan_file)
         if '- destroy' in out:
-            module.fail_json(msg="Aborting command because it would destroy some resources. Consider switching the 'check_destroy' to false to "
-                             "suppress this error")
+            module.fail_json(msg="Aborting command because it would destroy some resources. "
+                                                "Consider switching the 'check_destroy' to false to suppress this error")
 
     preflight_validation(command[0], project_path, variables_args)
 
