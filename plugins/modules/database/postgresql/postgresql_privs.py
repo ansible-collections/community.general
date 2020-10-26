3d22e3f563ad72bf1a0adad8e3763da72d68beb4
Peter Zahemszky <29452238+pzahemszky@users.noreply.github.com>
2020-10-26 06:41:44 +0000

    postgresql modules: Fix documentation of trust_input parameter (#1170)
    * Fix yes/no typos
    
    * Further doc improvements for consistency

--- plugins/modules/database/postgresql/postgresql_privs.py
+++ plugins/modules/database/postgresql/postgresql_privs.py
@@ -159,7 +159,7 @@ options:
     description:
     - If C(no), check whether values of parameters I(roles), I(target_roles), I(session_role),
       I(schema) are potentially dangerous.
-    - It makes sense to use C(yes) only when SQL injections via the parameters are possible.
+    - It makes sense to use C(no) only when SQL injections via the parameters are possible.
     type: bool
     default: yes
     version_added: '0.2.0'
--- plugins/modules/database/postgresql/postgresql_schema.py
+++ plugins/modules/database/postgresql/postgresql_schema.py
@@ -68,7 +68,7 @@ options:
   trust_input:
     description:
     - If C(no), check whether values of parameters I(schema), I(owner), I(session_role) are potentially dangerous.
-    - It makes sense to use C(yes) only when SQL injections via the parameters are possible.
+    - It makes sense to use C(no) only when SQL injections via the parameters are possible.
     type: bool
     default: yes
     version_added: '0.2.0'
