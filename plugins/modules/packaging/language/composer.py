cc4c18a530f74f6c5714cde5225ebc370dfc4242
Jeff Geerling <geerlingguy@mac.com>
2020-10-26 13:31:17 -0500

    Issue #1179: Fix v2 composer require idempotence (#1180)
    * Issue #1179: Add new statement to composer require changed check for v2.
    
    * Issue #1179: Add changelog fragment.

--- plugins/modules/packaging/language/composer.py
+++ plugins/modules/packaging/language/composer.py
@@ -145,7 +145,11 @@ def parse_out(string):
 
 
 def has_changed(string):
-    return "Nothing to install or update" not in string
+    for no_change in ["Nothing to install or update", "Nothing to install, update or remove"]:
+        if no_change in string:
+            return False
+
+    return True
 
 
 def get_available_options(module, command='install'):
