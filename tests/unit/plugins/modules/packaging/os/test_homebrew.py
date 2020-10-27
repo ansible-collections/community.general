3a805aca9719de3c5d5e29ff1846af00b273774c
MichaelWasher <mwasher@redhat.com>
2020-10-28 10:46:49 +1300

    Resolve homebrew and homebrew_cask package name validation issues (#1038)
    Add basic regression tests
    Add changelog
    Rename _create_regex_group to better suit function
    Fix '-' use in Homebrew validation

---
+++ tests/unit/plugins/modules/packaging/os/test_homebrew.py
@@ -0,0 +1,22 @@
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible_collections.community.general.plugins.modules.packaging.os.homebrew import Homebrew
+
+
+class TestHomebrewModule(unittest.TestCase):
+
+    def setUp(self):
+        self.brew_app_names = [
+            'git-ssh',
+            'awscli@1',
+            'bash'
+        ]
+
+    def test_valid_package_names(self):
+        for name in self.brew_app_names:
+            self.assertTrue(Homebrew.valid_package(name))
