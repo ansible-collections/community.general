d110650c8fafc7de598e217d103242c5f9d883a2
bratw0rst <c.chmiel@speakup.nl>
2020-11-07 21:14:37 +0100

    remove default from the documentation string

--- plugins/lookup/passwordstore.py
+++ plugins/lookup/passwordstore.py
@@ -33,8 +33,9 @@ DOCUMENTATION = '''
         type: bool
         default: 'no'
       umask:
-        description: Sets the umask for the created .gpg files. The first octed must be greater than 3 (user readable).
-        default: '077'
+        description: 
+          - Sets the umask for the created .gpg files. The first octed must be greater than 3 (user readable). Note 
+            pass' default value is '077'.
         env:
           - name: PASSWORD_STORE_UMASK
         version_added: 1.3.0
