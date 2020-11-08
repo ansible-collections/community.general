181e0448e9fa981e0fbe3ff2e8bbe6ea02cb0821
Vinicius Zavam <egypcio@googlemail.com>
2020-11-08 10:17:38 +0000

    [pkgng] remove unnecessary backslashes for install_packages() call
    Signed-off-by: Vinicius Zavam <egypcio@googlemail.com>

--- plugins/modules/packaging/os/pkgng.py
+++ plugins/modules/packaging/os/pkgng.py
@@ -451,8 +451,8 @@ def main():
     # Operate on named packages
     named_packages = [pkg for pkg in pkgs if pkg != '*']
     if p["state"] in ("present", "latest") and named_packages:
-        _changed, _msg, _out, _err = install_packages(module, pkgng_path, named_packages, \
-                                                      p["cached"], p["pkgsite"], dir_arg, \
+        _changed, _msg, _out, _err = install_packages(module, pkgng_path, named_packages,
+                                                      p["cached"], p["pkgsite"], dir_arg,
                                                       p["state"], p["ignore_osver"])
         stdout += _out
         stderr += _err
