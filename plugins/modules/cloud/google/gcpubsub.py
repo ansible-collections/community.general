0fca7802cab5a3fdbc68233a587110de57e6f97e
Alexei Znamensky <russoz@gmail.com>
2020-11-11 01:08:55 +1300

    reformatted long lines

--- plugins/modules/cloud/google/gcpubsub.py
+++ plugins/modules/cloud/google/gcpubsub.py
@@ -36,18 +36,22 @@ options:
          See subfields name, push_endpoint and ack_deadline for more information.
     suboptions:
       name:
-        description: Subfield of subscription. Required if subscription is specified. See examples.
+        description:
+          - Subfield of subscription. Required if subscription is specified. See examples.
       ack_deadline:
-        description: Subfield of subscription. Not required. Default deadline for subscriptions to ACK the message before it is resent. See examples.
+        description:
+          - Subfield of subscription. Not required. Default deadline for subscriptions to ACK the message before it is resent. See examples.
       pull:
         description:
-            - Subfield of subscription. Not required. If specified, messages will be retrieved from topic via the provided subscription name.
-              max_messages (int; default None; max number of messages to pull), message_ack (bool; default False; acknowledge the message) and return_immediately
-              (bool; default True, don't wait for messages to appear). If the messages are acknowledged, changed is set to True, otherwise, changed is False.
+          - Subfield of subscription. Not required. If specified, messages will be retrieved from topic via the
+            provided subscription name. max_messages (int; default None; max number of messages to pull),
+            message_ack (bool; default False; acknowledge the message) and return_immediately
+            (bool; default True, don't wait for messages to appear). If the messages are acknowledged,
+            changed is set to True, otherwise, changed is False.
       push_endpoint:
         description:
-            - Subfield of subscription.  Not required.  If specified, message will be sent to an endpoint.
-              See U(https://cloud.google.com/pubsub/docs/advanced#push_endpoints) for more information.
+          - Subfield of subscription.  Not required.  If specified, message will be sent to an endpoint.
+            See U(https://cloud.google.com/pubsub/docs/advanced#push_endpoints) for more information.
   publish:
     type: list
     description:
