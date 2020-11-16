8361658f664dac08b520a71b7fafbe52c8edc601
Dan Jackson <dan@djackson.org>
2020-11-15 19:58:27 -0800

    Basic test case of query_port for present and active

---
+++ tests/unit/plugins/modules/packaging/os/test_macports.py
@@ -0,0 +1,37 @@
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.module_utils import basic
+from ansible_collections.community.general.plugins.modules.packaging.os import macports
+
+import pytest
+
+TESTED_MODULE = macports.__name__
+
+QUERY_PORT_TEST_CASES = [
+	pytest.param('', False, False, id='Not installed'),
+	pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28', True, False, id='Installed but not active'),
+	pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)', True, True, id='Installed and active'),
+	pytest.param('''
+  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
+  git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
+''', True, False, id='2 versions installed, neither active'),
+	pytest.param('''
+  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)
+  git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
+''', True, True, id='2 versions installed, one active'),
+]
+
+@pytest.mark.parameterize("run_cmd_return_val, present_expected, active_expected", QUERY_PORT_TEST_CASES)
+def test_macports_query_port(mocker, run_cmd_return_val, present_expected, active_expected):
+	module = mocker.patch.object(
+		basic.AnsibleModule,
+		'run_command',
+		return_value=(0, run_cmd_return_val, '')
+	)
+
+	assert query_port(module, 'port', 'git', state="present") == present_expected
+	assert query_port(module, 'port', 'git', state="active") == active_expected
+
