e5e3ce49d674ad6c66cc2a3506ae7da18fa17953
Dan Jackson <dan@djackson.org>
2020-11-15 23:29:29 -0800

    Different mock module creation, changed test cases indentation/spacing
    - picked the wrong mock code to cargo-cult. Thanks to felixfontein for this suggestion
    - 4 space indentation on continuation line. I thought I had that originally, but it looks like my editor sabotaged me with mixed tabs/spaces
    - Remove leading newline on multi-line test cases. I don't think it would make a difference, but I'd read up on how the python syntax works and want to more accurately represent macports output.
    
    fingers crossed this addresses the known build errors

--- tests/unit/plugins/modules/packaging/os/test_macports.py
+++ tests/unit/plugins/modules/packaging/os/test_macports.py
@@ -11,15 +11,13 @@ import pytest
 TESTED_MODULE = macports.__name__
 
 QUERY_PORT_TEST_CASES = [
-  pytest.param('', False, False, id='Not installed'),
-  pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28', True, False, id='Installed but not active'),
-  pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)', True, True, id='Installed and active'),
-  pytest.param('''
-  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
+    pytest.param('', False, False, id='Not installed'),
+    pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28', True, False, id='Installed but not active'),
+    pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)', True, True, id='Installed and active'),
+    pytest.param('''  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
   git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
-    ''', True, False, id='2 versions installed, neither active'),
-  pytest.param('''
-  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)
+''', True, False, id='2 versions installed, neither active'),
+    pytest.param('''  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)
   git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
 ''', True, True, id='2 versions installed, one active'),
 ]
@@ -27,11 +25,10 @@ QUERY_PORT_TEST_CASES = [
 
 @pytest.mark.parametrize("run_cmd_return_val, present_expected, active_expected", QUERY_PORT_TEST_CASES)
 def test_macports_query_port(mocker, run_cmd_return_val, present_expected, active_expected):
-    module = mocker.patch.object(
-        basic.AnsibleModule,
-        'run_command',
-        return_value=(0, run_cmd_return_val, '')
-    )
+    module = mocker.Mock()
+    run_command = mocker.Mock()
+    run_command.return_value=(0, run_cmd_return_val, '')
+    module.run_command = run_command
 
     assert macports.query_port(module, 'port', 'git', state="present") == present_expected
     assert macports.query_port(module, 'port', 'git', state="active") == active_expected
