72201f8584539edca651639dc516eb59bc44fe8a
Dan Jackson <dan@djackson.org>
2020-11-15 20:35:12 -0800

    Attempt to fix lint errors in test

--- tests/unit/plugins/modules/packaging/os/test_macports.py
+++ tests/unit/plugins/modules/packaging/os/test_macports.py
@@ -11,27 +11,27 @@ import pytest
 TESTED_MODULE = macports.__name__
 
 QUERY_PORT_TEST_CASES = [
-	pytest.param('', False, False, id='Not installed'),
-	pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28', True, False, id='Installed but not active'),
-	pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)', True, True, id='Installed and active'),
-	pytest.param('''
+  pytest.param('', False, False, id='Not installed'),
+  pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28', True, False, id='Installed but not active'),
+  pytest.param('  git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)', True, True, id='Installed and active'),
+  pytest.param('''
   git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
   git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
-''', True, False, id='2 versions installed, neither active'),
-	pytest.param('''
+    ''', True, False, id='2 versions installed, neither active'),
+  pytest.param('''
   git @2.29.2_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28 (active)
   git @2.28.1_0+credential_osxkeychain+diff_highlight+doc+pcre+perl5_28
 ''', True, True, id='2 versions installed, one active'),
 ]
 
-@pytest.mark.parameterize("run_cmd_return_val, present_expected, active_expected", QUERY_PORT_TEST_CASES)
+
+@pytest.mark.parametrize("run_cmd_return_val, present_expected, active_expected", QUERY_PORT_TEST_CASES)
 def test_macports_query_port(mocker, run_cmd_return_val, present_expected, active_expected):
-	module = mocker.patch.object(
-		basic.AnsibleModule,
-		'run_command',
-		return_value=(0, run_cmd_return_val, '')
-	)
+    module = mocker.patch.object(
+        basic.AnsibleModule,
+        'run_command',
+        return_value=(0, run_cmd_return_val, '')
+    )
 
-	assert query_port(module, 'port', 'git', state="present") == present_expected
-	assert query_port(module, 'port', 'git', state="active") == active_expected
-
+    assert macports.query_port(module, 'port', 'git', state="present") == present_expected
+    assert macports.query_port(module, 'port', 'git', state="active") == active_expected
