0805c2241b5df23bbc2104edb3d81b01ae4d4ea1
Vladimir Botka <vbotka@gmail.com>
2020-12-03 20:28:50 +0100

    Added filter plugin lists_mergeby. Fix #249 #604
    * Added tests of the attributes types
    * Presence of index in the lists is not tested
    * Empty list will be returned if index is not present in the lists
    * Fixed filters dictionary

--- plugins/filter/list.py
+++ plugins/filter/list.py
@@ -11,14 +11,30 @@ from operator import itemgetter
 
 
 def lists_mergeby(l1, l2, index):
-    '''Merge lists by attribute index. Example:
-    - debug: msg="{{ l1|community.general.lists_mergeby(l2, 'index')|list }}"
-    '''
+    ''' merge lists by attribute index. Example:
+        - debug: msg="{{ l1|community.general.lists_mergeby(l2, 'index')|list }}" '''
+
+    if not isinstance(l1, list):
+        raise AnsibleFilterError('First argument for lists_mergeby must be list. %s is %s' %
+                                 (l1, type(l1)))
+
+    if not isinstance(l2, list):
+        raise AnsibleFilterError('Second argument for lists_mergeby must be list. %s is %s' %
+                                 (l2, type(l2)))
+
+    if not isinstance(index, string_types):
+        raise AnsibleFilterError('Third argument for lists_mergeby must be string. %s is %s' %
+                                 (index, type(index)))
+
     d = defaultdict(dict)
     for l in (l1, l2):
         for elem in l:
-            d[elem[index]].update(elem)
-    return sorted(d.values(), key=itemgetter(index))
+            if index in elem.keys():
+                d[elem[index]].update(elem)
+    if d.values():
+        return sorted(d.values(), key=itemgetter(index))
+    else:
+        return d.values()
 
 
 class FilterModule(object):
@@ -26,5 +42,5 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'lists_mergeby': lists_mergeby
+            'lists_mergeby': lists_mergeby,
         }
