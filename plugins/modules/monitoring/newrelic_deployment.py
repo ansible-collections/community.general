4481e3d04ae302bb616ea38d8b53dd310806d790
Davinder Pal <dpsangwal@gmail.com>
2020-12-17 00:54:38 +0200

    * updated code to use v2 api

--- plugins/modules/monitoring/newrelic_deployment.py
+++ plugins/modules/monitoring/newrelic_deployment.py
@@ -1,94 +1,81 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright 2013 Matt Coddington <coddington@gmail.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright 2020 Davinder Pal <dpsangwal@gmail.com>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-
 DOCUMENTATION = '''
 ---
 module: newrelic_deployment
-author: "Matt Coddington (@mcodd)"
-short_description: Notify newrelic about app deployments
+version_added: "1.2.0"
+author:
+    - "Davinder Pal (@116davinder)"
+    - "Matt Coddington (@mcodd)"
+short_description: Notify newrelic about app deployments using newrelic v2 api
 description:
-   - Notify newrelic about app deployments (see https://docs.newrelic.com/docs/apm/new-relic-apm/maintenance/deployment-notifications#api)
+   - Notify newrelic about app deployments
+        (https://docs.newrelic.com/docs/apm/new-relic-apm/maintenance/record-deployments)
 options:
   token:
-    type: str
     description:
       - API token, to place in the x-api-key header.
     required: true
+    type: str
   app_name:
-    type: str
     description:
-      - (one of app_name or application_id are required) The value of app_name in the newrelic.yml file used by the application
+      - (one of app_name or application_id is required)
+        The value of app_name in the newrelic.yml file used by the application
     required: false
+    type: str
   application_id:
-    type: str
     description:
-      - (one of app_name or application_id are required) The application id, found in the URL when viewing the application in RPM
+      - (one of app_name or application_id is required)
+        (see https://rpm.newrelic.com/api/explore/applications/list)
     required: false
-  changelog:
     type: str
+  changelog:
     description:
       - A list of changes for this deployment
     required: false
-  description:
     type: str
+  description:
     description:
       - Text annotation for the deployment - notes for you
     required: false
-  revision:
     type: str
+  revision:
     description:
       - A revision number (e.g., git commit SHA)
-    required: false
-  user:
+    required: true
     type: str
+  user:
     description:
       - The name of the user/process that triggered this deployment
     required: false
-  appname:
     type: str
-    description:
-      - Name of the application
-    required: false
-  environment:
-    type: str
-    description:
-      - The environment for this deployment
-    required: false
-  validate_certs:
-    description:
-      - If C(no), SSL certificates will not be validated. This should only be used
-        on personally controlled sites using self-signed certificates.
-    required: false
-    default: 'yes'
-    type: bool
-
-requirements: []
 '''
 
 EXAMPLES = '''
 - name:  Notify newrelic about an app deployment
   community.general.newrelic_deployment:
-    token: AAAAAA
-    app_name: myapp
-    user: ansible deployment
-    revision: '1.0'
+    token: XXXXXXXXX
+    app_name: ansible_app
+    user: ansible_deployment_user
+    revision: '1.X'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import fetch_url
-from ansible.module_utils.six.moves.urllib.parse import urlencode
+import json
+
 
 # ===========================================
 # Module execution.
 #
-
 
 def main():
 
@@ -99,47 +86,62 @@ def main():
             application_id=dict(required=False),
             changelog=dict(required=False),
             description=dict(required=False),
-            revision=dict(required=False),
+            revision=dict(required=True),
             user=dict(required=False),
-            appname=dict(required=False),
-            environment=dict(required=False),
-            validate_certs=dict(default=True, type='bool'),
         ),
-        required_one_of=[['app_name', 'application_id']],
-        supports_check_mode=True
+        required_one_of=[['app_name', 'application_id']]
     )
 
-    # build list of params
-    params = {}
-    if module.params["app_name"] and module.params["application_id"]:
-        module.fail_json(msg="only one of 'app_name' or 'application_id' can be set")
+    if module.params['app_name'] and module.params['application_id']:
+        module.fail_json(msg="both app_name' and 'application_id'\
+        are defined")
 
-    if module.params["app_name"]:
-        params["app_name"] = module.params["app_name"]
-    elif module.params["application_id"]:
-        params["application_id"] = module.params["application_id"]
+    if module.params['app_name']:
+        data = 'filter[name]=' + str(module.params['app_name'])
+        newrelic_api = 'https://api.newrelic.com/v2/applications.json'
+        headers = {'x-api-key': module.params['token'],
+                   'Content-Type': 'application/x-www-form-urlencoded'}
+        (resp, info) = fetch_url(module,
+                                 newrelic_api,
+                                 headers=headers,
+                                 data=data,
+                                 method='GET')
+        if info['status'] != 200:
+            module.fail_json(msg="unable to get application list from\
+            newrelic: %s" % info['msg'])
+        else:
+            body = json.loads(resp.read())
+        if body is None:
+            module.fail_json(msg='No Data for applications')
+        else:
+            app_id = body['applications'][0]['id']
+            if app_id is None:
+                module.fail_json(msg="App not found in\
+                NewRelic Registerd Applications List")
     else:
-        module.fail_json(msg="you must set one of 'app_name' or 'application_id'")
-
-    for item in ["changelog", "description", "revision", "user", "appname", "environment"]:
-        if module.params[item]:
-            params[item] = module.params[item]
-
-    # If we're in check mode, just exit pretending like we succeeded
-    if module.check_mode:
-        module.exit_json(changed=True)
+        app_id = module.params['application_id']
 
     # Send the data to NewRelic
-    url = "https://rpm.newrelic.com/deployments.xml"
-    data = urlencode(params)
-    headers = {
-        'x-api-key': module.params["token"],
+    url = 'https://api.newrelic.com/v2/applications/' + str(app_id) \
+        + '/deployments.json'
+    data = {
+        'deployment': {
+            'revision': str(module.params['revision']),
+            'changelog': str(module.params['changelog']),
+            'description': str(module.params['description']),
+            'user': str(module.params['user']), }
     }
-    response, info = fetch_url(module, url, data=data, headers=headers)
-    if info['status'] in (200, 201):
+
+    headers = {'x-api-key': module.params['token'],
+               'Content-Type': 'application/json'}
+    (response, info) = fetch_url(module, url,
+                                 data=module.jsonify(data),
+                                 headers=headers, method='POST')
+    if info['status'] == 201:
         module.exit_json(changed=True)
     else:
-        module.fail_json(msg="unable to update newrelic: %s" % info['msg'])
+        module.fail_json(msg='unable to update newrelic: %s'
+                         % info['msg'])
 
 
 if __name__ == '__main__':
