0cadc058b318594ed3494a963150c66e0784c42a
Jianhao Tan <jianhaot.89@gmail.com>
2020-12-18 10:56:34 +0800

    added unit test for version comparison

--- tests/unit/plugins/modules/packaging/os/test_homebrew_cask.py
+++ tests/unit/plugins/modules/packaging/os/test_homebrew_cask.py
@@ -19,3 +19,23 @@ class TestHomebrewCaskModule(unittest.TestCase):
     def test_valid_cask_names(self):
         for name in self.brew_cask_names:
             self.assertTrue(HomebrewCask.valid_cask(name))
+
+    def test_version_comparison(self):
+        mock_brew_version_1 = '2.4.0'
+        mock_brew_version_2 = '2.8.0'
+
+        comparison_result_1 = HomebrewCask._compare_version(mock_brew_version_1, mock_brew_version_2)
+
+        mock_brew_version_3 = '2.8.0'
+        mock_brew_version_4 = '2.4.0'
+
+        comparison_result_2 = HomebrewCask._compare_version(mock_brew_version_3, mock_brew_version_4)
+
+        mock_brew_version_5 = '2.4.0'
+        mock_brew_version_6 = '2.4.0'
+
+        comparison_result_3 = HomebrewCask._compare_version(mock_brew_version_5, mock_brew_version_6)
+
+        self.assertEqual(comparison_result_1, -1)
+        self.assertEqual(comparison_result_1, 1)
+        self.assertEqual(comparison_result_1, 0)
