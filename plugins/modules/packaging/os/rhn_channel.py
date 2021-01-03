b5c4284de8d465f5b09b6d9cee42fdafe733cd07
Johannes Petz <PetzJohannes@users.noreply.github.com>
2021-01-03 11:15:51 +0100

    Legacy Python certificate validation fixed (#470)
    * Legacy Python certificate validation fixed
    
    * added changelog fragment
    
    * removed blank line for sanity checks
    
    * Update changelogs/fragments/470-spacewalk-legacy-python-certificate-validation.yaml
    
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>
    
    * Update changelogs/fragments/470-spacewalk-legacy-python-certificate-validation.yaml
    
    Co-authored-by: jpe <petz.johannes@afb.de>
    Co-authored-by: Felix Fontein <felix@fontein.de>
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/modules/packaging/os/rhn_channel.py
+++ plugins/modules/packaging/os/rhn_channel.py
@@ -135,7 +135,7 @@ def main():
         try:  # Python 2.7.9 and newer
             ssl_context = ssl.create_unverified_context()
         except AttributeError:  # Legacy Python that doesn't verify HTTPS certificates by default
-            ssl._create_default_context = ssl._create_unverified_context
+            ssl_context = ssl._create_unverified_context()
         else:  # Python 2.7.8 and older
             ssl._create_default_https_context = ssl._create_unverified_https_context
 
