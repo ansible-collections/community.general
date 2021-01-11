d272a99a77e8147f7749dfa3996967e2f789345f
Abhijeet Kasurde <akasurde@redhat.com>
2021-01-11 16:39:54 +0530

    Update plugins/modules/net_tools/cloudflare_dns.py
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- plugins/modules/net_tools/cloudflare_dns.py
+++ plugins/modules/net_tools/cloudflare_dns.py
@@ -22,7 +22,7 @@ options:
     description:
     - API token.
     - Required for api token authentication.
-    - "You can obtain your API token from the bottom of the Cloudflare 'My Account' page, found here: U(https://dash.cloudflare.com/)"
+    - "You can obtain your API token from the bottom of the Cloudflare 'My Account' page, found here: U(https://dash.cloudflare.com/)."
     - Can be specified in C(CLOUDFLARE_TOKEN) environment variable since community.general 2.0.0.
     type: str
     required: false
