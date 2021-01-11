1ce72801cba11f86f4e143b2bbcbb24f663d6cad
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-01-12 10:14:46 +1300

    Tidy up validate-modules ignores for modules: net_tools/nios (#1598)
    * Added ``normalize_ib_spec()``
    
    * Added suboptions
    
    - ``http_pool_connections``
    - ``http_pool_maxsize``
    - ``silent_ssl_warnings``
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_a_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_aaaa_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_cname_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_dns_view.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_fixed_address.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_host_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_member.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_mx_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_naptr_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_network.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_network_view.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_ptr_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_srv_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_txt_record.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_zone.py
    
    * fixed validation-modules for plugins/modules/net_tools/nios/nios_nsgroup.py
    
    * Added function to normalize the ``ib_spec`` for ansible usage.
    
    * Tidy up validate-modules ignores for net_tools/nios modules
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/net_tools/nios/nios_nsgroup.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * fixed missing defaults, per PR tests
    
    * added changelog fragment
    
    * Update changelogs/fragments/nios-fix-ib_spec.yaml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/net_tools/nios/nios_mx_record.py
+++ plugins/modules/net_tools/nios/nios_mx_record.py
@@ -25,6 +25,7 @@ options:
       - Specifies the fully qualified hostname to add or remove from
         the system
     required: true
+    type: str
   view:
     description:
       - Sets the DNS view to associate this a record with.  The DNS
@@ -32,27 +33,33 @@ options:
     default: default
     aliases:
       - dns_view
+    type: str
   mail_exchanger:
     description:
       - Configures the mail exchanger FQDN for this MX record.
     aliases:
       - mx
+    type: str
   preference:
     description:
       - Configures the preference (0-65535) for this MX record.
+    type: int
   ttl:
     description:
       - Configures the TTL to be associated with this host record
+    type: int
   extattrs:
     description:
       - Allows for the configuration of Extensible Attributes on the
         instance of the object.  This argument accepts a set of key / value
         pairs for configuration.
+    type: dict
   comment:
     description:
       - Configures a text string comment to be associated with the instance
         of this object.  The provided text string will be configured on the
         object instance.
+    type: str
   state:
     description:
       - Configures the intended state of the instance of the object on
@@ -63,6 +70,7 @@ options:
     choices:
       - present
       - absent
+    type: str
 '''
 
 EXAMPLES = '''
@@ -107,9 +115,9 @@ EXAMPLES = '''
 RETURN = ''' # '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.six import iteritems
 from ansible_collections.community.general.plugins.module_utils.net_tools.nios.api import WapiModule
 from ansible_collections.community.general.plugins.module_utils.net_tools.nios.api import NIOS_MX_RECORD
+from ansible_collections.community.general.plugins.module_utils.net_tools.nios.api import normalize_ib_spec
 
 
 def main():
@@ -134,7 +142,7 @@ def main():
         state=dict(default='present', choices=['present', 'absent'])
     )
 
-    argument_spec.update(ib_spec)
+    argument_spec.update(normalize_ib_spec(ib_spec))
     argument_spec.update(WapiModule.provider_spec)
 
     module = AnsibleModule(argument_spec=argument_spec,
