42d2ed4ffa01835ea8156dc9a5b0856c51230526
LOMS <lomserman@gmail.com>
2021-01-18 18:06:45 +0300

    Apply suggestions from code review
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/notification/telegram.py
+++ plugins/modules/notification/telegram.py
@@ -19,12 +19,11 @@ short_description: module for sending notifications via telegram
 
 description:
     - Send notifications via telegram bot, to a verified group or user.
-    - Also, you may try to use any other telegram bot api method, if you specify "api_method" argument.
-      This isn't tested though.
+    - Also, the user may try to use any other telegram bot API method, if you specify I(api_method) argument.
 notes:
     - You will require a telegram account and create telegram bot to use this module.
-    - Deprecated arguments left for backward compatibility, when used - will replace corresponding
-      variables in api_args
+    - The options I(msg), I(msg_format) and I(chat_id) have been deprecated and will be removed in community.general
+      4.0.0. Use the corresponding variables in I(api_args) instead. See the examples for how that works.
 options:
   token:
     type: str
@@ -33,12 +32,17 @@ options:
     required: true
   api_method:
     type: str
-    description: BOt API method. For reference, see https://core.telegram.org/bots/api
+    description:
+      - Bot API method.
+      - For reference, see U(https://core.telegram.org/bots/api).
     default: SendMessage
+    version_added: 2.0.0
   api_args:
     type: dict
     description:
-      - Any parameters for method. I.e., for SendMessage, see https://core.telegram.org/bots/api#sendmessage
+      - Any parameters for the method.
+      - For reference to default method, C(SendMessage), see U(https://core.telegram.org/bots/api#sendmessage).
+    version_added: 2.0.0
   msg:
     type: str
     description:
@@ -46,15 +50,15 @@ options:
   msg_format:
     type: str
     description:
-      - (Deprecated) Message format. Formatting options `markdown`, `MarkdownV2` and `html` described in
+      - (Deprecated) Message format. Formatting options C(markdown), C(MarkdownV2), and C(html) described in
         Telegram API docs (https://core.telegram.org/bots/api#formatting-options).
-        If option `plain` set, message will not be formatted.
+        If option C(plain) set, message will not be formatted.
     default: plain
     choices: [ "plain", "markdown", "MarkdownV2", "html" ]
   chat_id:
     type: str
     description:
-      - (Deprecated) Telegram group or user chat_id
+      - (Deprecated) Telegram group or user chat_id.
 
 '''
 
@@ -80,7 +84,7 @@ EXAMPLES = """
       disable_notification: True
       message_id: '{{ saved_msg_id }}'
 
-- name: Send a message to chat in playbook (old style)
+- name: Send a message to chat in playbook (deprecated old style)
   community.general.telegram:
     token: '9999999:XXXXXXXXXXXXXXXXXXXXXXX'
     chat_id: 000000
@@ -115,10 +119,11 @@ def main():
             token=dict(type='str', required=True, no_log=True),
             api_args=dict(type='dict'),
             api_method=dict(type="str", default="SendMessage"),
-            # backwards compatibility
-            chat_id=dict(type='str', no_log=True),
-            msg=dict(type='str'),
-            msg_format=dict(type='str', choices=['plain', 'markdown', 'html', 'MarkdownV2'], default='plain'),
+            chat_id=dict(type='str', no_log=True, removed_in_version='4.0.0',
+                         removed_from_collection='community.general'),
+            msg=dict(type='str', removed_in_version='4.0.0', removed_from_collection='community.general'),
+            msg_format=dict(type='str', choices=['plain', 'markdown', 'html', 'MarkdownV2'], default='plain',
+                            removed_in_version='4.0.0', removed_from_collection='community.general'),
         ),
         supports_check_mode=True
     )
