c12388459e94a5d7cfb9f553c9165a38e42354cf
morco <thegreatwiper@web.de>
2021-02-09 10:29:13 +0100

    Various fixes for updating existing gitlab users (#1724)
    * fixes various issues related to updating an ...
    
    ... existing gitlab user, in detail:
    
      - fixes updating admin status not working
      - fixes user passwords not updated
      - fixes confirmation skipping param ignored for user updates
      - added tests for code changes
    
    * fixing sanity issues
    
    * fixing sanity issues 02
    
    * fixing sanity issues 03
    
    * fixing sanity issues 04
    
    * fixing unit test failures
    
    * fixing unit test failures 02
    
    * add changelog fragment
    
    * fixing unit test failures 03
    
    * forgot to add changelog fragment
    
    * fix changelog sanity issues
    
    * fix changelog sanity issues 02
    
    * incorporate review suggestions
    
    Co-authored-by: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>

--- tests/unit/plugins/modules/source_control/gitlab/test_gitlab_user.py
+++ tests/unit/plugins/modules/source_control/gitlab/test_gitlab_user.py
@@ -88,15 +88,32 @@ class TestGitlabUser(GitlabModuleTestCase):
     @with_httmock(resp_get_user)
     def test_update_user(self):
         user = self.gitlab_instance.users.get(1)
-        changed, newUser = self.moduleUtil.updateUser(user, {'name': "Jack Smith", "is_admin": "true"})
+
+        changed, newUser = self.moduleUtil.updateUser(
+            user,
+            {'name': {'value': "Jack Smith"}, "is_admin": {'value': "true", 'setter': 'admin'}}, {}
+        )
 
         self.assertEqual(changed, True)
         self.assertEqual(newUser.name, "Jack Smith")
-        self.assertEqual(newUser.is_admin, "true")
+        self.assertEqual(newUser.admin, "true")
 
-        changed, newUser = self.moduleUtil.updateUser(user, {'name': "Jack Smith"})
+        changed, newUser = self.moduleUtil.updateUser(user, {'name': {'value': "Jack Smith"}}, {})
 
         self.assertEqual(changed, False)
+
+        changed, newUser = self.moduleUtil.updateUser(
+            user,
+            {}, {
+                'skip_reconfirmation': {'value': True},
+                'password': {'value': 'super_secret-super_secret'},
+            }
+        )
+
+        # note: uncheckable parameters dont set changed state
+        self.assertEqual(changed, False)
+        self.assertEqual(newUser.skip_reconfirmation, True)
+        self.assertEqual(newUser.password, 'super_secret-super_secret')
 
     @with_httmock(resp_find_user)
     @with_httmock(resp_delete_user)
