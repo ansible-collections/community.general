e41ec7c4308e3799b805383b5e2805ee05a1581a
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-02-12 18:47:23 +1300

    Removed parameter-list-no-elements validation errors from redfish modules (#1761)
    * Removed parameter-list-no-elements validation errors from redfish modules
    
    * added changelog fragment per PR
    
    * Update changelogs/fragments/1761-redfish-tidy-up-validation.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/remote_management/redfish/idrac_redfish_config.py
+++ plugins/modules/remote_management/redfish/idrac_redfish_config.py
@@ -29,6 +29,7 @@ options:
         I(SetSystemAttributes) are mutually exclusive commands when C(category)
         is I(Manager)
     type: list
+    elements: str
   baseuri:
     required: true
     description:
@@ -245,7 +246,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             category=dict(required=True),
-            command=dict(required=True, type='list'),
+            command=dict(required=True, type='list', elements='str'),
             baseuri=dict(required=True),
             username=dict(required=True),
             password=dict(required=True, no_log=True),
