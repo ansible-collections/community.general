6081298abe0ff27d41046ab65b7d6115ade17aae
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-02-12 18:47:23 +1300

    Removed parameter-list-no-elements validation errors from redfish modules (#1761)
    * Removed parameter-list-no-elements validation errors from redfish modules
    
    * added changelog fragment per PR
    
    * Update changelogs/fragments/1761-redfish-tidy-up-validation.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/remote_management/redfish/idrac_redfish_command.py
+++ plugins/modules/remote_management/redfish/idrac_redfish_command.py
@@ -26,6 +26,7 @@ options:
     description:
       - List of commands to execute on OOB controller
     type: list
+    elements: str
   baseuri:
     required: true
     description:
@@ -134,7 +135,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             category=dict(required=True),
-            command=dict(required=True, type='list'),
+            command=dict(required=True, type='list', elements='str'),
             baseuri=dict(required=True),
             username=dict(required=True),
             password=dict(required=True, no_log=True),
--- plugins/modules/remote_management/redfish/redfish_command.py
+++ plugins/modules/remote_management/redfish/redfish_command.py
@@ -28,6 +28,7 @@ options:
     description:
       - List of commands to execute on OOB controller
     type: list
+    elements: str
   baseuri:
     required: true
     description:
@@ -550,7 +551,7 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             category=dict(required=True),
-            command=dict(required=True, type='list'),
+            command=dict(required=True, type='list', elements='str'),
             baseuri=dict(required=True),
             username=dict(required=True),
             password=dict(required=True, no_log=True),
