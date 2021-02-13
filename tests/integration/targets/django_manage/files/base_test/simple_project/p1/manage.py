16c6ea372b3052af26e4d7c36da884cace409e32
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-02-14 00:09:54 +1300

    Adjusted django_manage integration test files to reduce ignore lines in sanity tests (#1805)

--- tests/integration/targets/django_manage/files/base_test/simple_project/p1/manage.py
+++ tests/integration/targets/django_manage/files/base_test/simple_project/p1/manage.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
