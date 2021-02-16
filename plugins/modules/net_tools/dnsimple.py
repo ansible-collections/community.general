f33323ca8930ee566cb29de4f8218b71e0a1417a
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-02-16 19:11:37 +1300

    Tidy up validate-modules:parameter-list-no-elements (batch 1) (#1795)
    * fixed validation-modules for plugins/modules/storage/netapp/na_ontap_gather_facts.py
    
    * fixed validation-modules for plugins/modules/source_control/gitlab/gitlab_runner.py
    
    * fixed validation-modules for plugins/modules/packaging/os/redhat_subscription.py
    
    * fixed validation-modules for plugins/modules/notification/twilio.py
    
    * fixed validation-modules for plugins/modules/notification/slack.py
    
    * fixed validation-modules for plugins/modules/notification/sendgrid.py
    
    * fixed validation-modules for plugins/modules/notification/rocketchat.py
    
    * fixed validation-modules for plugins/modules/notification/office_365_connector_card.py
    
    * fixed validation-modules for plugins/modules/notification/nexmo.py
    
    * fixed validation-modules for plugins/modules/notification/mail.py
    
    * fixed validation-modules for plugins/modules/net_tools/omapi_host.py
    
    * fixed validation-modules for plugins/modules/net_tools/nsupdate.py
    
    * fixed validation-modules for plugins/modules/net_tools/dnsimple.py
    
    * fixed validation-modules for plugins/modules/monitoring/pagerduty.py
    
    * fixed validation-modules for plugins/modules/monitoring/librato_annotation.py
    
    * fixed validation-modules for plugins/modules/identity/onepassword_info.py
    
    * fixed validation-modules for plugins/modules/identity/keycloak/keycloak_client.py
    
    * fixed validation-modules for plugins/modules/files/xml.py
    
    * fixed validation-modules for plugins/modules/cloud/softlayer/sl_vm.py
    
    * fixed validation-modules for plugins/modules/cloud/smartos/vmadm.py
    
    * fixed validation-modules for plugins/modules/cloud/pubnub/pubnub_blocks.py
    
    * fixed validation-modules for plugins/modules/cloud/packet/packet_device.py
    
    * fixed validation-modules for plugins/modules/cloud/lxd/lxd_container.py
    
    * fixed validation-modules for plugins/module_utils/oracle/oci_utils.py
    
    * fixed validation-modules for plugins/doc_fragments/oracle_creatable_resource.py
    
    * Tidy up validate-modules:parameter-list-no-elements for some modules
    
    * fixed validation-modules for plugins/modules/monitoring/statusio_maintenance.py
    
    * Fixed pending issues from CI validation
    
    * Fixed xml module elements for add_children & set_children
    
    * added changelog fragment
    
    * typo
    
    * fix wording in changelog frag

--- plugins/modules/net_tools/dnsimple.py
+++ plugins/modules/net_tools/dnsimple.py
@@ -40,6 +40,7 @@ options:
     description:
       - List of records to ensure they either exist or do not exist.
     type: list
+    elements: str
   type:
     description:
       - The type of DNS record to create.
@@ -167,7 +168,7 @@ def main():
             account_api_token=dict(type='str', no_log=True),
             domain=dict(type='str'),
             record=dict(type='str'),
-            record_ids=dict(type='list'),
+            record_ids=dict(type='list', elements='str'),
             type=dict(type='str', choices=['A', 'ALIAS', 'CNAME', 'MX', 'SPF', 'URL', 'TXT', 'NS', 'SRV', 'NAPTR', 'PTR', 'AAAA', 'SSHFP', 'HINFO',
                                            'POOL']),
             ttl=dict(type='int', default=3600),
