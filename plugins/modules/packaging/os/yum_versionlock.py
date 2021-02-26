49c8309931db2d702192e9e0be03b8f44a10e64f
Amin Vakil <info@aminvakil.com>
2021-02-26 23:45:13 +0330

    Revert "Fix module" and apply felixfontein suggestion
    This reverts commit 5936da31987219a0cfc9a7b1bdaee0c093d2ccf8.

--- plugins/modules/packaging/os/yum_versionlock.py
+++ plugins/modules/packaging/os/yum_versionlock.py
@@ -93,11 +93,9 @@ class YumVersionLock:
             self.module.fail_json(msg="Error: Please install rpm package yum-plugin-versionlock : " + to_native(err) + to_native(out))
         self.module.fail_json(msg="Error: " + to_native(err) + to_native(out))
 
-    def ensure_state(self, packages_list, command):
+    def ensure_state(self, package, command):
         """ Ensure package state """
-        packages = " ".join(packages_list)
-        cmd = "%s -q versionlock %s %s" % (self.yum_bin, command, packages)
-        rc, out, err = self.module.run_command(cmd)
+        rc, out, err = self.module.run_command([self.yum_bin, "-q", "versionlock", command] + package)
         if rc == 0:
             return True
         self.module.fail_json(msg="Error: " + to_native(err) + to_native(out))
