47fe79ff38131f1388342509c90f3429846e73fe
Eric L <ericzolf@users.noreply.github.com>
2021-02-27 17:42:45 +0100

    Add version_sort filter to properly sort list of versions (#1916)
    * Add version_sort filter to properly sort list of versions
    
    * Fix all comments from Felix
    
    - add changelog fragment
    - fix test by removing runme.sh/yml and renaming to filter_version_sort
    - use fully qualified name of filter in test case
    
    * Remove wrong plugin.test changelog fragment
    
    Ups...
    
    * Properly name the file version_sort.py
    
    * Update changelogs/fragments/1916-add-version-sort-filter.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Eric L <ewl+git@lavar.de>
    Co-authored-by: Felix Fontein <felix@fontein.de>

---
+++ plugins/filter/version_sort.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2021 Eric Lavarde <elavarde@redhat.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from distutils.version import LooseVersion
+
+
+def version_sort(value, reverse=False):
+    '''Sort a list according to loose versions so that e.g. 2.9 is smaller than 2.10'''
+    return sorted(value, key=LooseVersion, reverse=reverse)
+
+
+class FilterModule(object):
+    ''' Version sort filter '''
+
+    def filters(self):
+        return {
+            'version_sort': version_sort
+        }
