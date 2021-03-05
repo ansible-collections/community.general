2e1f582d41024a6e74d512c13a090cafe9dcbb92
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-03-05 20:17:36 +1300

    Deprecation of parameters triggering the parameter-invalid sanity-check (#1927)
    * fixed validation-modules for plugins/modules/packaging/language/composer.py
    
    * fixed validation-modules for plugins/modules/packaging/os/apt_rpm.py
    
    * fixed validation-modules for plugins/modules/packaging/os/homebrew.py
    
    * fixed validation-modules for plugins/modules/packaging/os/homebrew_cask.py
    
    * fixed validation-modules for plugins/modules/packaging/os/opkg.py
    
    * fixed validation-modules for plugins/modules/packaging/os/pacman.py
    
    * fixed validation-modules for plugins/modules/packaging/os/slackpkg.py
    
    * fixed validation-modules for plugins/modules/packaging/os/urpmi.py
    
    * fixed validation-modules for plugins/modules/packaging/os/xbps.py
    
    * fixed validation-modules for plugins/modules/source_control/github/github_deploy_key.py
    
    * fixed validation-modules for plugins/modules/system/puppet.py
    
    * added changelog fragment
    
    * adjustments from PR + fixes in changelog frag
    
    * fixed deprecation of param "show_diff" in module "puppet"
    
    * Update changelogs/fragments/1927-removed-parameter-invalid.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/system/puppet.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * removed unnecessary ignore lines, adjustment in changelog frag
    
    * no need to explicitly call deprecate() when param marked for removal
    
    * Update changelogs/fragments/1927-removed-parameter-invalid.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Adjustments in changelog fragment, per PR
    
    * bumping deprecation to 7.0.0
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit 7425e9840dcb1c39b2c2cd90eb023403402154ff)

--- plugins/modules/packaging/os/slackpkg.py
+++ plugins/modules/packaging/os/slackpkg.py
@@ -41,6 +41,7 @@ options:
     update_cache:
         description:
             - update the package database first
+            - Alias C(update-cache) has been deprecated and will be removed in community.general 5.0.0.
         required: false
         default: false
         type: bool
@@ -177,8 +178,9 @@ def main():
         argument_spec=dict(
             state=dict(default="present", choices=['installed', 'removed', 'absent', 'present', 'latest']),
             name=dict(aliases=["pkg"], required=True, type='list', elements='str'),
-            update_cache=dict(default=False, aliases=["update-cache"],
-                              type='bool'),
+            update_cache=dict(
+                default=False, aliases=["update-cache"], type='bool',
+                deprecated_aliases=[dict(name='update-cache', version='5.0.0', collection_name='community.general')]),
         ),
         supports_check_mode=True)
 
