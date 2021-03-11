47aeb4b39a46ea947b3ed0daa80f5d429632e8db
Ajpantuso <ajpantuso@gmail.com>
2021-03-11 16:08:11 -0500

    Allow tags strings containing commas in proxmox inventory plug-in (#1949)
    * Included explicit parsing for proxmox guest tags and updated corresponding unit test with tags key
    
    * Including changelog fragment for PR 1949
    
    * Removed ellipsis from test
    
    Proxmox only permits periods when surrounded by alphanumeric characters
    
    * Corrected punctuation for changelog entry
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Allowing tags string to contain commas
    
    * Incorporated new parsed tags fact with bugfix
    
    * Correcting whitespace issues
    
    * Update changelogs/fragments/1949-proxmox-inventory-tags.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/inventory/proxmox.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update changelogs/fragments/1949-proxmox-inventory-tags.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit d0bb74a03b79b968a2ba6c3867e3f071dcbfbcc4)

--- plugins/inventory/proxmox.py
+++ plugins/inventory/proxmox.py
@@ -203,6 +203,10 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
         vmtype_key = self.to_safe('%s%s' % (self.get_option('facts_prefix'), vmtype_key.lower()))
         self.inventory.set_variable(name, vmtype_key, vmtype)
 
+        plaintext_configs = [
+            'tags',
+        ]
+
         for config in ret:
             key = config
             key = self.to_safe('%s%s' % (self.get_option('facts_prefix'), key.lower()))
@@ -211,6 +215,12 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
                 # fixup disk images as they have no key
                 if config == 'rootfs' or config.startswith(('virtio', 'sata', 'ide', 'scsi')):
                     value = ('disk_image=' + value)
+
+                # Additional field containing parsed tags as list
+                if config == 'tags':
+                    parsed_key = self.to_safe('%s%s' % (key, "_parsed"))
+                    parsed_value = [tag.strip() for tag in value.split(",")]
+                    self.inventory.set_variable(name, parsed_key, parsed_value)
 
                 if not (isinstance(value, int) or ',' not in value):
                     # split off strings with commas to a dict
