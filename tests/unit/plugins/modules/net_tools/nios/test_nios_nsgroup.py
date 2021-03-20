c5fb7439004ce1c3d91ff890b4c510d1784e40a9
Felix Fontein <felix@fontein.de>
2021-03-20 13:45:42 +0100

    Fix nios modules to work with ansible-core 2.11 (#2057)
    * Fix nios modules to work with ansible-core 2.11.
    
    * Adjust tests.

--- tests/unit/plugins/modules/net_tools/nios/test_nios_nsgroup.py
+++ tests/unit/plugins/modules/net_tools/nios/test_nios_nsgroup.py
@@ -42,9 +42,13 @@ class TestNiosNSGroupModule(TestNiosModule):
 
         self.load_config = self.mock_wapi_run.start()
 
+        self.mock_check_type_dict = patch('ansible_collections.community.general.plugins.module_utils.net_tools.nios.api.check_type_dict')
+        self.mock_check_type_dict_obj = self.mock_check_type_dict.start()
+
     def tearDown(self):
         super(TestNiosNSGroupModule, self).tearDown()
         self.mock_wapi.stop()
+        self.mock_check_type_dict.stop()
 
     def _get_wapi(self, test_object):
         wapi = api.WapiModule(self.module)
@@ -73,7 +77,7 @@ class TestNiosNSGroupModule(TestNiosModule):
         res = wapi.run('testobject', test_spec)
 
         self.assertTrue(res['changed'])
-        wapi.create_object.assert_called_once_with('testobject', {'name': self.module._check_type_dict().__getitem__()})
+        wapi.create_object.assert_called_once_with('testobject', {'name': self.mock_check_type_dict_obj().__getitem__()})
 
     def test_nios_nsgroup_remove(self):
         self.module.params = {'provider': None, 'state': 'absent', 'name': 'my-simple-group',
