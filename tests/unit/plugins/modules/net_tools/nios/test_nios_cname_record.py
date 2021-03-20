24f8be834af2c7f49a326821c22d58c30c8fa0d5
Felix Fontein <felix@fontein.de>
2021-03-20 13:45:42 +0100

    Fix nios modules to work with ansible-core 2.11 (#2057)
    * Fix nios modules to work with ansible-core 2.11.
    
    * Adjust tests.

--- tests/unit/plugins/modules/net_tools/nios/test_nios_cname_record.py
+++ tests/unit/plugins/modules/net_tools/nios/test_nios_cname_record.py
@@ -40,11 +40,14 @@ class TestNiosCNameRecordModule(TestNiosModule):
         self.mock_wapi_run = patch('ansible_collections.community.general.plugins.modules.net_tools.nios.nios_cname_record.WapiModule.run')
         self.mock_wapi_run.start()
         self.load_config = self.mock_wapi_run.start()
+        self.mock_check_type_dict = patch('ansible_collections.community.general.plugins.module_utils.net_tools.nios.api.check_type_dict')
+        self.mock_check_type_dict_obj = self.mock_check_type_dict.start()
 
     def tearDown(self):
         super(TestNiosCNameRecordModule, self).tearDown()
         self.mock_wapi.stop()
         self.mock_wapi_run.stop()
+        self.mock_check_type_dict.stop()
 
     def _get_wapi(self, test_object):
         wapi = api.WapiModule(self.module)
@@ -76,7 +79,7 @@ class TestNiosCNameRecordModule(TestNiosModule):
         res = wapi.run('testobject', test_spec)
 
         self.assertTrue(res['changed'])
-        wapi.create_object.assert_called_once_with('testobject', {'name': self.module._check_type_dict().__getitem__(),
+        wapi.create_object.assert_called_once_with('testobject', {'name': self.mock_check_type_dict_obj().__getitem__(),
                                                                   'canonical': 'realhost.ansible.com'})
 
     def test_nios_a_record_update_comment(self):
