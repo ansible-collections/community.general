5def135212d198df2098c3368e510e66604573ac
zhcli <cyrus1006@gmail.com>
2021-03-27 13:23:39 +1100

    updated unittest

--- tests/unit/plugins/callback/test_loganalytics.py
+++ tests/unit/plugins/callback/test_loganalytics.py
@@ -60,5 +60,5 @@ class TestAzureLogAnalytics(unittest.TestCase):
         args, kwargs = open_url_mock.call_args
         headers = kwargs['headers']
 
-        self.assertEqual(headers['Authorization'], 'SharedKey 01234567-0123-0123-0123-01234567890a:5wukca1qmOI4SjpE+2AIWi2C1dNm/hLJ+gnPXrkFLkE=')
+        self.assertRegexpMatches(headers['Authorization'], r'^SharedKey 01234567-0123-0123-0123-01234567890a:.*=$')
         self.assertEqual(headers['Log-Type'], 'ansible_playbook')
