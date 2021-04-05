8b38139b7f90de3454e344c2df0c1cc8b6f1d7c4
Felix Fontein <felix@fontein.de>
2021-04-05 22:03:27 +0200

    Remove list_to_dict filter.

--- plugins/filter/dict.py
+++ plugins/filter/dict.py
@@ -7,16 +7,6 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-def list_to_dict(values, keys):
-    '''Convert a list of values and a list of keys to a dictionary by combining these lists.
-
-    Example: ``[1, 2, 3] | community.general.list_to_dict(['k1', 'k2'])`` results in ``{'k1': 1, 'k2': 2}``.
-
-    This is equivalent to ``[1, 2, 3] | zip(['k1', 'k2']) | community.general.dict``.
-    '''
-    return dict(zip(keys, values))
-
-
 def dict_filter(sequence):
     '''Convert a list of tuples to a dictionary.
 
@@ -30,6 +20,5 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'list_to_dict': list_to_dict,
             'dict': dict_filter,
         }
