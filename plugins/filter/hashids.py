6f217c5dce06597585494ca6e341b379b8bce1b5
Andrew Pantuso <ajpantuso@gmail.com>
2021-04-14 09:29:58 -0400

    Correcting copyright

--- plugins/filter/hashids.py
+++ plugins/filter/hashids.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2021, Andrew Pantuso (@ajpantuso) <ajpantuso@gmail.com>
-# Copyright: (c) 2018, Dag Wieers (@dagwieers) <dag@wieers.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
