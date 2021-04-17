a2d57b1e97acf92e9004f5585277051515358d8d
Ajpantuso <ajpantuso@gmail.com>
2021-04-17 14:00:03 -0400

    New filter plugins: hashids_encode and hashids_decode (#2244)
    * New filters hashids_encode and hashids_decode
    
    * Adding changelog
    
    * Correcting whitespace issue in vars file
    
    * Attempt to fix integration test failures
    
    * Correcting copyright
    
    * Addressing initial review comments
    
    * Updating decoded sequence return from tuple to list
    
    * Correcting capitilization and spelling

---
+++ tests/integration/targets/filter_hashids/runme.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eux
+
+export ANSIBLE_TEST_PREFER_VENV=1  # see https://github.com/ansible/ansible/pull/73000#issuecomment-757012395; can be removed once Ansible 2.9 and ansible-base 2.10 support has been dropped
+source virtualenv.sh
+
+# Requirements have to be installed prior to running ansible-playbook
+# because plugins and requirements are loaded before the task runs
+
+pip install hashids
+
+ANSIBLE_ROLES_PATH=../ ansible-playbook runme.yml "$@"
