83b81b8ebf0283e1955218d0b02c3cef5f0ba6e9
kris2kris <36845394+kris2kris@users.noreply.github.com>
2021-04-18 13:57:23 +0200

    Update copyright
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/identity/keycloak/keycloak_realm.py
+++ plugins/modules/identity/keycloak/keycloak_realm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2017, Eike Frost <ei@kefro.st>
 # Copyright (c) 2021, Christophe Gilles <christophe.gilles54@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
