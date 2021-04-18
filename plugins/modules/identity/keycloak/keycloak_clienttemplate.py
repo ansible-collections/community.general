82c86ae4c085bc3bea71342744e2845d855d5b31
Gaetan2907 <48204380+Gaetan2907@users.noreply.github.com>
2021-04-18 15:37:08 +0200

    Update plugins/modules/identity/keycloak/keycloak_clienttemplate.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/identity/keycloak/keycloak_clienttemplate.py
+++ plugins/modules/identity/keycloak/keycloak_clienttemplate.py
@@ -181,8 +181,8 @@ EXAMPLES = '''
     name: this_is_a_test
 
 - name: Create or update Keycloak client template (minimal), authentification with token
-  local_action:
-    module: keycloak_clienttemplate
+  delegate_to: localhost
+  community.general.keycloak_clienttemplate:
     auth_client_id: admin-cli
     auth_keycloak_url: https://auth.example.com/auth
     auth_realm: master
