f0de080b42336b5e273b888d3bc423c471a688ce
Andrew Pantuso <ajpantuso@gmail.com>
2021-04-19 06:41:10 -0400

    Adding pattern and ignore_case options

--- plugins/modules/system/pids.py
+++ plugins/modules/system/pids.py
@@ -2,6 +2,7 @@
 # Copyright: (c) 2019, Saranya Sridharan
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
+
 __metaclass__ = type
 
 DOCUMENTATION = '''
@@ -14,12 +15,20 @@ requirements:
   - psutil(python module)
 options:
   name:
-    description: the name of the process you want to get PID for.
-    required: true
+    description: the name of the process(es) you want to get PID(s) for.
     type: str
+  pattern:
+    description:
+    - the pattern to match the process(es) you want to get PID(s) for.
+    - if C(.*) is provided as the value for C(pattern), then PIDs will be returned for all processes.
+    type: str
+  ignore_case:
+    description: ignore case in pattern if using the C(pattern) option.
+    type: bool
+    default: false
 '''
 
-EXAMPLES = '''
+EXAMPLES = r'''
 # Pass the process name
 - name: Getting process IDs of the process
   community.general.pids:
@@ -29,6 +38,11 @@ EXAMPLES = '''
 - name: Printing the process IDs obtained
   ansible.builtin.debug:
     msg: "PIDS of python:{{pids_of_python.pids|join(',')}}"
+
+- name: Getting process IDs of processes matching pattern
+  community.general.pids:
+    pattern: myapp.py
+  register: myapp_pids
 '''
 
 RETURN = '''
@@ -39,9 +53,14 @@ pids:
   sample: [100,200]
 '''
 
-from ansible.module_utils.basic import AnsibleModule
+import re
+from os.path import basename
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+
 try:
     import psutil
+
     HAS_PSUTIL = True
 except ImportError:
     HAS_PSUTIL = False
@@ -66,17 +85,52 @@ def get_pid(name):
     return pids
 
 
+def get_matching_pids(pattern, ignore_case):
+    flags = 0
+    if ignore_case:
+        flags |= re.I
+
+    if pattern == '.*':
+        return psutil.pids()
+
+    regex = re.compile(re.escape(pattern), flags)
+
+    # See https://psutil.readthedocs.io/en/latest/#find-process-by-name for more information
+    return [p.pid for p in psutil.process_iter(["name", "exe", "cmdline"])
+            if regex.search(p.info["name"])
+            or (p.info["exe"] and regex.search(basename(p.info["exe"])))
+            or (p.info["cmdline"] and regex.search(' '.join(p.cmdline())))
+            ]
+
+
 def main():
     module = AnsibleModule(
         argument_spec=dict(
-            name=dict(required=True, type="str"),
+            name=dict(type="str"),
+            pattern=dict(type="str"),
+            ignore_case=dict(type="bool", default=False),
         ),
+        required_one_of=[
+            ('name', 'pattern')
+        ],
+        mutually_exclusive=[
+            ('name', 'pattern')
+        ],
         supports_check_mode=True,
     )
+
     if not HAS_PSUTIL:
-        module.fail_json(msg="Missing required 'psutil' python module. Try installing it with: pip install psutil")
+        module.fail_json(msg=missing_required_lib('psutil'))
+
     name = module.params["name"]
-    response = dict(pids=get_pid(name))
+    pattern = module.params["pattern"]
+    ignore_case = module.params["ignore_case"]
+
+    if name:
+        response = dict(pids=get_pid(name))
+    else:
+        response = dict(pids=get_matching_pids(pattern, ignore_case))
+
     module.exit_json(**response)
 
 
