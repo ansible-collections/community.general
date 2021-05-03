83bb7b12efae9faf5a4ae139cb63167c1bb2719e
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-05-03 23:27:16 +1200

    linode_v4 - fixed error message (#2430)
    * fixed error message
    
    * added changelog fragment

--- plugins/modules/cloud/linode/linode_v4.py
+++ plugins/modules/cloud/linode/linode_v4.py
@@ -208,9 +208,8 @@ def create_linode(module, client, **kwargs):
         else:
             return response._raw_json
     except TypeError:
-        module.fail_json(msg='Unable to parse Linode instance creation'
-                             ' response. Please raise a bug against this'
-                             ' module on https://github.com/ansible/ansible/issues'
+        module.fail_json(msg='Unable to parse Linode instance creation response. Please raise a bug against this'
+                             ' module on https://github.com/ansible-collections/community.general/issues'
                          )
 
 
