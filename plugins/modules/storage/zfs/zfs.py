c717714f6d73afda36bc06bd601be0cc46e16d88
Sam Lunt <samuel.j.lunt@gmail.com>
2021-05-05 08:02:45 -0500

    Avoid incorrectly marking zfs tasks as changed
    The zfs module will incorrectly mark certain tasks as having been
    changed. For example, if a dataset has a quota of "1G" and the user
    changes it to "1024M", the actual quota vale has not changed, but since
    the module is doing a simple string comparison between "1G" and "1024M",
    it marks the step as "changed".
    
    Instead of trying to handle all the corner cases of zfs (another example
    is when the zpool "altroot" property has been set), this change simply
    compares the output of "zfs-get" from before and after "zfs-set" is
    called

--- plugins/modules/storage/zfs/zfs.py
+++ plugins/modules/storage/zfs/zfs.py
@@ -184,9 +184,7 @@ class Zfs(object):
             return
         cmd = [self.zfs_cmd, 'set', prop + '=' + str(value), self.name]
         (rc, out, err) = self.module.run_command(cmd)
-        if rc == 0:
-            self.changed = True
-        else:
+        if rc != 0:
             self.module.fail_json(msg=err)
 
     def set_properties_if_changed(self):
@@ -194,15 +192,25 @@ class Zfs(object):
         for prop, value in self.properties.items():
             if current_properties.get(prop, None) != value:
                 self.set_property(prop, value)
+        if self.module.check_mode:
+            return
+        updated_properties = self.get_current_properties()
+        for prop in self.properties:
+            value = updated_properties.get(prop, None)
+            if value is None:
+                self.module.fail_json(msg="zfsprop was not present after being successfully set: " + prop)
+            if current_properties.get(prop, None) != value:
+                self.changed = True
 
     def get_current_properties(self):
-        cmd = [self.zfs_cmd, 'get', '-H']
+        cmd = [self.zfs_cmd, 'get', '-H', '-p', '-o', "property,value,source"]
         if self.enhanced_sharing:
             cmd += ['-e']
         cmd += ['all', self.name]
         rc, out, err = self.module.run_command(" ".join(cmd))
         properties = dict()
-        for prop, value, source in [l.split('\t')[1:4] for l in out.splitlines()]:
+        for line in out.splitlines():
+            prop, value, source = line.split('\t')
             # include source '-' so that creation-only properties are not removed
             # to avoids errors when the dataset already exists and the property is not changed
             # this scenario is most likely when the same playbook is run more than once
