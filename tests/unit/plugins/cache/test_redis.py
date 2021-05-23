c3ba7f8d8c1e573b2878e22b1470b4f92124f102
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-05-24 09:40:57 +1200

    Update test_redis.py

--- tests/unit/plugins/cache/test_redis.py
+++ tests/unit/plugins/cache/test_redis.py
@@ -31,7 +31,7 @@ from ansible.release import __version__ as ansible_version
 
 def test_redis_cachemodule():
     # The _uri option is required for the redis plugin
-    connection = 'ni 127.0.0.1:6379:1'
+    connection = '127.0.0.1:6379:1'
     if ansible_version.startswith('2.9.'):
         C.CACHE_PLUGIN_CONNECTION = connection
     assert isinstance(cache_loader.get('community.general.redis', **{'_uri': connection}), RedisCache)
