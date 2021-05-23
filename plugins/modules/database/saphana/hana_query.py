1df5f4aa5209d99db52068a7ead9962bfdec7bde
Rainer Leber <rainer.leber@sva.de>
2021-05-23 18:55:35 +0200

    change hana_query add test
    Update plugins/modules/database/saphana/hana_query.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Update plugins/modules/database/saphana/hana_query.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/database/saphana/hana_query.py
+++ plugins/modules/database/saphana/hana_query.py
@@ -118,8 +118,8 @@ def main():
             user=dict(type='str', required=False, default="SYSTEM"),
             password=dict(type='str', required=True, no_log=True),
             database=dict(type='str', required=False),
-            query=dict(type='list', required=False),
-            filepath=dict(type='list', required=False),
+            query=dict(type='list', elements='str', required=False),
+            filepath=dict(type='list', elements='path', required=False),
             autocommit=dict(type='bool', required=False, default=True)
         )
     )
@@ -139,15 +139,14 @@ def main():
     filepath = params['filepath']
     query = params['query']
 
-    bin_path = f"/usr/sap/{sid}/HDB{instance}/exe/hdbsql"
+    bin_path = "/usr/sap/{sid}/HDB{instance}/exe/hdbsql".format(sid=sid, instance=instance)
 
     present = filepath is not None or query is not None
 
     try:
         command = [module.get_bin_path(bin_path, required=True)]
     except Exception:
-        'hdbsql not found {0}. \r\nSTDOUT: {1}\r\n\r\nSTDERR: {2}'.format(
-            rc, out, err)
+        module.fail_json(msg='hdbsql not found at "{0}". Please check SID and instance number.'.format(bin_path))
 
     if present:
         if encrypted is True:
@@ -168,24 +167,20 @@ def main():
                 # iterates through files and append the output to var out.
                 query_command = command + [p]
                 (rc, out_raw, err) = module.run_command(query_command)
-
                 out = out + csv_to_json(out_raw)
         if query is not None:
             for q in query:
-                command.extend([q])
                 # makes a command like hdbsql -i 01 -u SYSTEM -p secret123# "select user_name from users",
                 # iterates through multiple commands and append the output to var out.
-                (rc, out_raw, err) = module.run_command(command)
-                del command[-1]
-
+                query_command = command + [q]
+                (rc, out_raw, err) = module.run_command(query_command)
                 out = out + csv_to_json(out_raw)
         changed = True
     else:
         changed = False
         out = "nothing to do, no command provided"
 
-    module.exit_json(changed=changed, message=rc, stdout=out,
-                     stderr=err, command=' '.join(command))
+    module.exit_json(changed=changed, message=rc, stdout=out, stderr=err)
 
 
 if __name__ == '__main__':
---
+++ tests/unit/plugins/modules/database/saphana/test_hana_query.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Rainer Leber (@rainerleber) <rainerleber@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible_collections.community.general.tests.unit.compat.mock import patch
+from ansible_collections.community.general.plugins.modules.database.saphana import hana_query
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, set_module_args
+from ansible.module_utils import basic
+from ansible.module_utils.common.text.converters import to_bytes
+import json
+
+def set_module_args(args):
+    """prepare arguments so that they will be picked up during module creation"""
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+class AnsibleExitJson(Exception):
+    """Exception class to be raised by module.exit_json and caught by the test case"""
+    pass
+
+
+class AnsibleFailJson(Exception):
+    """Exception class to be raised by module.fail_json and caught by the test case"""
+    pass
+
+
+def exit_json(*args, **kwargs):
+    """function to patch over exit_json; package return data into an exception"""
+    if 'changed' not in kwargs:
+        kwargs['changed'] = False
+    raise AnsibleExitJson(kwargs)
+
+
+def fail_json(*args, **kwargs):
+    """function to patch over fail_json; package return data into an exception"""
+    kwargs['failed'] = True
+    raise AnsibleFailJson(kwargs)
+
+
+def get_bin_path(self, arg, required=False):
+    """Mock AnsibleModule.get_bin_path"""
+    if arg.endswith('hdbsql'):
+        return '/usr/sap/HDB/HDB01/exe/hdbsql'
+    else:
+        if required:
+            fail_json(msg='%r not found !' % arg)
+
+
+class Testhana_query(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_module_helper = patch.multiple(basic.AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json,
+                                                 get_bin_path=get_bin_path)
+        self.mock_module_helper.start()
+        self.addCleanup(self.mock_module_helper.stop)
+
+    def test_module_fail_when_required_args_missing(self):
+        with self.assertRaises(AnsibleFailJson):
+            set_module_args({})
+            self.module.main()
+
+
+    def test_ensure_command_called(self):
+        set_module_args({
+            'sid': "HDB",
+            'instance': "01",
+            'encrypted': False,
+            'host': "localhost",
+            'user': "SYSTEM",
+            'password': "1234Qwer",
+            'database': "HDB",
+            'query': "SELECT * FROM users:"
+        })
+
+        with patch.object(basic.AnsibleModule, 'run_command') as mock_run_command:
+            stdout = 'configuration updated'
+            stderr = ''
+            rc = 0
+            mock_run_command.return_value = rc, stdout, stderr  # successful execution
+
+            with self.assertRaises(AnsibleExitJson) as result:
+                hana_query.main()
+            self.assertFalse(result.exception.args[0]['changed']) # ensure result is changed
+
+        mock_run_command.assert_called_once_with('/usr/sap/HDB/HDB01/exe/hdbsql')
