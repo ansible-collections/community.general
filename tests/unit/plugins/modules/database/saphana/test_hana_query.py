52dff9597fcd05326a591ec6c64dd95a706a766d
Rainer Leber <rainer.leber@sva.de>
2021-05-24 09:33:20 +0200

    change test

--- tests/unit/plugins/modules/database/saphana/test_hana_query.py
+++ tests/unit/plugins/modules/database/saphana/test_hana_query.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 from ansible_collections.community.general.plugins.modules import hana_query
 from ansible_collections.community.general.tests.unit.plugins.modules.utils import (
     AnsibleExitJson,
@@ -43,53 +44,23 @@ class Testhana_query(ModuleTestCase):
 =======
 from ansible_collections.community.general.tests.unit.compat import unittest
 from ansible_collections.community.general.tests.unit.compat.mock import patch
+=======
+>>>>>>> 61aafaec... change test
 from ansible_collections.community.general.plugins.modules import hana_query
-from ansible_collections.community.general.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, set_module_args, json
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args
+from ansible_collections.community.general.tests.unit.compat.mock import patch
 from ansible.module_utils import basic
-from ansible.module_utils.common.text.converters import to_bytes
 
-
-def set_module_args(args):
-    """prepare arguments so that they will be picked up during module creation"""
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
-
-
-class AnsibleExitJson(Exception):
-    """Exception class to be raised by module.exit_json and caught by the test case"""
-    pass
-
-
-class AnsibleFailJson(Exception):
-    """Exception class to be raised by module.fail_json and caught by the test case"""
-    pass
-
-
-def exit_json(*args, **kwargs):
-    """function to patch over exit_json; package return data into an exception"""
-    if 'changed' not in kwargs:
-        kwargs['changed'] = False
-    raise AnsibleExitJson(kwargs)
-
-
-def fail_json(*args, **kwargs):
-    """function to patch over fail_json; package return data into an exception"""
-    kwargs['failed'] = True
-    raise AnsibleFailJson(kwargs)
-
-
-def get_bin_path(self, arg, required=False):
+def get_bin_path(*args, **kwargs):
     """Function to return path of hdbsql"""
-    if arg.endswith('hdbsql'):
-        return '/usr/sap/HDB/HDB01/exe/hdbsql'
-    else:
-        if required:
-            fail_json(msg='%r not found !' % arg)
+    return "/usr/sap/HDB/HDB01/exe/hdbsql"
 
 
-class Testhana_query(unittest.TestCase):
+class Testhana_query(ModuleTestCase):
+    """Main class for testing hana_query module."""
 
     def setUp(self):
+<<<<<<< HEAD
         self.mock_module_helper = patch.multiple(basic.AnsibleModule,
                                                  exit_json=exit_json,
                                                  fail_json=fail_json,
@@ -99,10 +70,26 @@ class Testhana_query(unittest.TestCase):
 
     def test_module_fail_when_required_args_missing(self):
 >>>>>>> 8358dba9... change hana_query add test
+=======
+        """Setup."""
+        super(Testhana_query, self).setUp()
+        self.module = hana_query
+        self.mock_get_bin_path = patch.object(basic.AnsibleModule, 'get_bin_path', get_bin_path)
+        self.mock_get_bin_path.start()
+        self.addCleanup(self.mock_get_bin_path.stop)  # ensure that the patching is 'undone'
+
+    def tearDown(self):
+        """Teardown."""
+        super(Testhana_query, self).tearDown()
+
+    def test_without_required_parameters(self):
+        """Failure must occurs when all parameters are missing."""
+>>>>>>> 61aafaec... change test
         with self.assertRaises(AnsibleFailJson):
             set_module_args({})
             self.module.main()
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     def test_hana_query(self):
@@ -113,6 +100,10 @@ class Testhana_query(unittest.TestCase):
 >>>>>>> 2d09b71d... change test
     def test_ensure_command_called(self):
 >>>>>>> 8358dba9... change hana_query add test
+=======
+    def test_hana_query(self):
+        """Check that result is changed."""
+>>>>>>> 61aafaec... change test
         set_module_args({
             'sid': "HDB",
             'instance': "01",
@@ -133,19 +124,18 @@ class Testhana_query(unittest.TestCase):
 =======
             'query': "SELECT * FROM users:"
         })
-
-        with patch.object(basic.AnsibleModule, 'run_command') as mock_run_command:
-            stdout = 'configuration updated'
-            stderr = ''
-            rc = 0
-            mock_run_command.return_value = rc, stdout, stderr  # successful execution
-
+        with patch.object(basic.AnsibleModule, 'run_command') as run_command:
+            run_command.return_value = 0, '', ''  # successful execution, no output
             with self.assertRaises(AnsibleExitJson) as result:
                 hana_query.main()
-            self.assertFalse(result.exception.args[0]['changed'])  # ensure result is changed
+                self.assertTrue(result.exception.args[0]['changed'])
 
+<<<<<<< HEAD
         mock_run_command.assert_called_once_with('/usr/sap/HDB/HDB01/exe/hdbsql')
 <<<<<<< HEAD
 >>>>>>> 8358dba9... change hana_query add test
 =======
 >>>>>>> 2d09b71d... change test
+=======
+        self.assertEqual(run_command.call_count, 2)
+>>>>>>> 61aafaec... change test
