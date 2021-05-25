d228f98f25a2ddc4471e4646e5e60b3269d58ba2
Rainer Leber <rainer.leber@sva.de>
2021-05-25 15:37:46 +0200

    change future

--- plugins/modules/files/sapcar_extract.py
+++ plugins/modules/files/sapcar_extract.py
@@ -2,7 +2,7 @@
 
 # Copyright: (c) 2021, Rainer Leber <rainerleber@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import (absolute_import, division, print_function)
+from __future__ import (absolute_import, division, print_function, with_statement)
 __metaclass__ = type
 
 DOCUMENTATION = '''
