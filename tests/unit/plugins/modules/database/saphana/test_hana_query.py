318d5ab5d61abe99669dca1e0f3aaaacadb11439
Rainer Leber <rainer.leber@sva.de>
2021-05-25 07:53:08 +0200

    test

--- tests/unit/plugins/modules/database/saphana/test_hana_query.py
+++ tests/unit/plugins/modules/database/saphana/test_hana_query.py
@@ -104,7 +104,7 @@ class Testhana_query(ModuleTestCase):
             with self.assertRaises(AnsibleFailJson) as result:
                 set_module_args({})
                 self.module.main()
-            self.assertEqual(hana_sql.call_count, 1)
+            self.assertEqual(hana_sql.call_count, 0)
             self.assertEqual(hana_sql.call_args, ({'sid': 'HDB', 'instance': '01', 'password': '1234Qwer'},))
             self.assertEqual(result.exception.args[0]['query_result']['username'], 'testuser')
 
@@ -118,7 +118,7 @@ class Testhana_query(ModuleTestCase):
                     'password': "1234Qwer",
                 })
                 self.module.main()
-            self.assertEqual(hana_sql.call_count, 1)
+            self.assertEqual(hana_sql.call_count, 0)
             self.assertEqual(hana_sql.call_args, ({'sid': 'HDB', 'instance': '01', 'password': '1234Qwer'},))
             self.assertEqual(result.exception.args[0]['query_result']['username'], 'testuser')
 >>>>>>> b43a653e... new test
