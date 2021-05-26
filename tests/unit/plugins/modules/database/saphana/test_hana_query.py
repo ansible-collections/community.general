f7dbdae8ab3d7be6325ecf62afe5502878b618db
rainerleber <39616583+rainerleber@users.noreply.github.com>
2021-05-26 07:16:59 +0200

    Update tests/unit/plugins/modules/database/saphana/test_hana_query.py
    Co-authored-by: Andrew Klychkov <aaklychkov@mail.ru>

--- tests/unit/plugins/modules/database/saphana/test_hana_query.py
+++ tests/unit/plugins/modules/database/saphana/test_hana_query.py
@@ -7,7 +7,12 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible_collections.community.general.plugins.modules import hana_query
-from ansible_collections.community.general.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson, ModuleTestCase, set_module_args
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import (
+    AnsibleExitJson,
+    AnsibleFailJson,
+    ModuleTestCase,
+    set_module_args,
+)
 from ansible_collections.community.general.tests.unit.compat.mock import patch
 from ansible.module_utils import basic
 
