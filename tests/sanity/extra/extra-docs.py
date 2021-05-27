25f7168bcc82c1f238d6833a36adaf4d34d27b00
Felix Fontein <felix@fontein.de>
2021-05-27 22:59:42 +0200

    Add extra docs tests (#2663)
    * Add extra docs tests.
    
    * Linting.
    
    * Fix copy'n'paste error.

---
+++ tests/sanity/extra/extra-docs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""Check extra collection docs with antsibull-lint."""
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import sys
+import subprocess
+
+
+def main():
+    """Main entry point."""
+    if not os.path.isdir(os.path.join('docs', 'docsite')):
+        return
+    p = subprocess.run(['antsibull-lint', 'collection-docs', '.'], check=False)
+    if p.returncode not in (0, 3):
+        print('{0}:0:0: unexpected return code {1}'.format(sys.argv[0], p.returncode))
+
+
+if __name__ == '__main__':
+    main()
