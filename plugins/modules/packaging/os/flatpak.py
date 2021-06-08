54163023d1a0eb1855fee9c3f2f3f72c9d3587c3
Felix Fontein <felix@fontein.de>
2021-06-08 12:21:19 +0200

    Fix bug I introduced.

--- plugins/modules/packaging/os/flatpak.py
+++ plugins/modules/packaging/os/flatpak.py
@@ -190,13 +190,17 @@ def install_flat(module, binary, remote, names, method, no_dependencies):
 def uninstall_flat(module, binary, names, method):
     """Remove existing flatpaks."""
     global result
+    installed_flat_names = [
+        _match_installed_flat_name(module, binary, name, method)
+        for name in names
+    ]
     command = [binary, "uninstall"]
     flatpak_version = _flatpak_version(module, binary)
     if StrictVersion(flatpak_version) < StrictVersion('1.1.3'):
         command += ["-y"]
     else:
         command += ["--noninteractive"]
-    command += ["--{0}".format(method)] + names
+    command += ["--{0}".format(method)] + installed_flat_names
     _flatpak_command(module, module.check_mode, command)
     result['changed'] = True
 
