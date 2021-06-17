2f040c847f997674278bbe61a0a4d218b6078da7
TizeN85 <tenou.sylvain@outlook.com>
2021-06-17 19:08:42 +0200

    fix sudorule_add_allow_command_group (#2821)
    * fix sudorule_add_allow_command_group
    
    fix sudorule_add_allow_command_group is not working on freeIPA 4.8.7 at least, sudorule_add_allow_command should be used instead with item sudocmdgroup
    
    * Added changelog fragment

--- plugins/modules/identity/ipa/ipa_sudorule.py
+++ plugins/modules/identity/ipa/ipa_sudorule.py
@@ -237,7 +237,7 @@ class SudoRuleIPAClient(IPAClient):
         return self._post_json(method='sudorule_add_allow_command', name=name, item={'sudocmd': item})
 
     def sudorule_add_allow_command_group(self, name, item):
-        return self._post_json(method='sudorule_add_allow_command_group', name=name, item={'sudocmdgroup': item})
+        return self._post_json(method='sudorule_add_allow_command', name=name, item={'sudocmdgroup': item})
 
     def sudorule_remove_allow_command(self, name, item):
         return self._post_json(method='sudorule_remove_allow_command', name=name, item=item)
