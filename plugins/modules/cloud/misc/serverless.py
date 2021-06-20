58362726b6e1561c0d0d7df90b3a65da1c040379
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-06-20 23:07:45 +1200

    serverless - deprecating unused param (#2845)
    * deprecating unused param
    
    * added changelog fragment
    
    * deprecate param in arg_spec
    
    * Update plugins/modules/cloud/misc/serverless.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/cloud/misc/serverless.py
+++ plugins/modules/cloud/misc/serverless.py
@@ -38,6 +38,7 @@ options:
     description:
       - A list of specific functions to deploy.
       - If this is not provided, all functions in the service will be deployed.
+      - Deprecated parameter, it will be removed in community.general 5.0.0.
     type: list
     elements: str
     default: []
@@ -78,13 +79,6 @@ EXAMPLES = r'''
   community.general.serverless:
     service_path: '{{ project_dir }}'
     state: present
-
-- name: Deploy specific functions
-  community.general.serverless:
-    service_path: '{{ project_dir }}'
-    functions:
-      - my_func_one
-      - my_func_two
 
 - name: Deploy a project, then pull its resource list back into Ansible
   community.general.serverless:
@@ -165,7 +159,8 @@ def main():
         argument_spec=dict(
             service_path=dict(type='path', required=True),
             state=dict(type='str', default='present', choices=['absent', 'present']),
-            functions=dict(type='list', elements='str'),
+            functions=dict(type='list', elements='str',
+                           removed_in_version="5.0.0", removed_from_collection="community.general"),
             region=dict(type='str', default=''),
             stage=dict(type='str', default=''),
             deploy=dict(type='bool', default=True),
