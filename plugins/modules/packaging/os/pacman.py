14c9429fade91d0e57e7aa725da3b5e475a3defb
Amin Vakil <info@aminvakil.com>
2021-07-07 00:36:36 +0430

    pacman: fix changed status when ignorepkg has been defined (#2936)
    * pacman: fix returned code when ignorepkg has been defined
    
    * add changelog
    
    * make ignored check preciser

--- plugins/modules/packaging/os/pacman.py
+++ plugins/modules/packaging/os/pacman.py
@@ -254,16 +254,23 @@ def upgrade(module, pacman_path):
         # e.g., "ansible 2.7.1-1 -> 2.7.2-1"
         regex = re.compile(r'([\w+\-.@]+) (\S+-\S+) -> (\S+-\S+)')
         for p in data:
-            m = regex.search(p)
-            packages.append(m.group(1))
-            if module._diff:
-                diff['before'] += "%s-%s\n" % (m.group(1), m.group(2))
-                diff['after'] += "%s-%s\n" % (m.group(1), m.group(3))
+            if '[ignored]' not in p:
+                m = regex.search(p)
+                packages.append(m.group(1))
+                if module._diff:
+                    diff['before'] += "%s-%s\n" % (m.group(1), m.group(2))
+                    diff['after'] += "%s-%s\n" % (m.group(1), m.group(3))
         if module.check_mode:
-            module.exit_json(changed=True, msg="%s package(s) would be upgraded" % (len(data)), packages=packages, diff=diff)
+            if packages:
+                module.exit_json(changed=True, msg="%s package(s) would be upgraded" % (len(data)), packages=packages, diff=diff)
+            else:
+                module.exit_json(changed=False, msg='Nothing to upgrade', packages=packages)
         rc, stdout, stderr = module.run_command(cmdupgrade, check_rc=False)
         if rc == 0:
-            module.exit_json(changed=True, msg='System upgraded', packages=packages, diff=diff)
+            if packages:
+                module.exit_json(changed=True, msg='System upgraded', packages=packages, diff=diff)
+            else:
+                module.exit_json(changed=False, msg='Nothing to upgrade', packages=packages)
         else:
             module.fail_json(msg="Could not upgrade")
     else:
