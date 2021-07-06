b99c80116711fd5d6282518268122c363a7961b3
Reto Kupferschmid <kupferschmid@puzzle.ch>
2021-07-06 16:33:15 +0200

    update dnsimple module

--- plugins/modules/net_tools/dnsimple.py
+++ plugins/modules/net_tools/dnsimple.py
@@ -73,7 +73,7 @@ options:
     type: 'bool'
     default: no
 requirements:
-  - "dnsimple >= 1.0.0"
+  - "dnsimple >= 2.0.0"
 author: "Alex Coomans (@drcapulet)"
 '''
 
@@ -150,9 +150,10 @@ from distutils.version import LooseVersion
 
 DNSIMPLE_IMP_ERR = None
 try:
-    from dnsimple import DNSimple
-    from dnsimple.dnsimple import __version__ as dnsimple_version
-    from dnsimple.dnsimple import DNSimpleException
+    from dnsimple import Client, DNSimpleException
+    from dnsimple.service import Accounts
+    from dnsimple.version import version as dnsimple_version
+    from dnsimple.struct.zone_record import ZoneRecordUpdateInput, ZoneRecordInput
     HAS_DNSIMPLE = True
 except ImportError:
     DNSIMPLE_IMP_ERR = traceback.format_exc()
@@ -186,9 +187,9 @@ def main():
     if not HAS_DNSIMPLE:
         module.fail_json(msg=missing_required_lib('dnsimple'), exception=DNSIMPLE_IMP_ERR)
 
-    if LooseVersion(dnsimple_version) < LooseVersion('1.0.0'):
-        module.fail_json(msg="Current version of dnsimple Python module [%s] uses 'v1' API which is deprecated."
-                             " Please upgrade to version 1.0.0 and above to use dnsimple 'v2' API." % dnsimple_version)
+    if LooseVersion(dnsimple_version) < LooseVersion('2.0.0'):
+        module.fail_json(msg="community.general.dnsimple requires dnsimple Python module >= 2.0.0 (installed version: [%s])."
+                             " Please upgrade dnsimple to version 2.0.0 or above." % dnsimple_version)
 
     account_email = module.params.get('account_email')
     account_api_token = module.params.get('account_api_token')
@@ -202,49 +203,81 @@ def main():
     state = module.params.get('state')
     is_solo = module.params.get('solo')
 
-    if account_email and account_api_token:
-        client = DNSimple(email=account_email, api_token=account_api_token)
-    elif os.environ.get('DNSIMPLE_EMAIL') and os.environ.get('DNSIMPLE_API_TOKEN'):
-        client = DNSimple(email=os.environ.get('DNSIMPLE_EMAIL'), api_token=os.environ.get('DNSIMPLE_API_TOKEN'))
-    else:
-        client = DNSimple()
+    try:
+        if account_email and account_api_token:
+            client = Client(email=account_email, access_token=account_api_token)
+        elif os.environ.get('DNSIMPLE_EMAIL') and os.environ.get('DNSIMPLE_API_TOKEN'):
+            client = Client(email=os.environ.get('DNSIMPLE_EMAIL'), access_token=os.environ.get('DNSIMPLE_API_TOKEN'))
+        else:
+            client = Client()
+
+        account = client.identity.whoami().data.account
+
+        # user supplied a user token instead of account api token
+        if not account:
+          accounts = Accounts(client).list_accounts().data
+          if (len(accounts) != 1):
+              module.fail_json(msg="Provided dnsimple token is a user token with multiple accounts. Use an account token or a user token with access to a single account. See https://support.dnsimple.com/articles/api-access-token/")
+          else:
+            account = accounts[0]
+
+    except DNSimpleException as e:
+        module.fail_json(msg="Unable to contact DNSimple: %s" % e.message)
+
 
     try:
         # Let's figure out what operation we want to do
 
         # No domain, return a list
         if not domain:
-            domains = client.domains()
-            module.exit_json(changed=False, result=[d['domain'] for d in domains])
+            domain_list = []
+            domains_pagination = client.domains.list_domains(account.id).pagination
+            for page in range(1, domains_pagination.total_pages + 1):
+                page_data = client.domains.list_domains(account.id, page=page).data
+                domain_list.extend(page_data)
+            module.exit_json(changed=False, result=[d.__dict__ for d in domain_list])
 
         # Domain & No record
+        
         if domain and record is None and not record_ids:
-            domains = [d['domain'] for d in client.domains()]
-            if domain.isdigit():
-                dr = next((d for d in domains if d['id'] == int(domain)), None)
-            else:
-                dr = next((d for d in domains if d['name'] == domain), None)
+            try:
+                if domain.isdigit():
+                    dr = client.domains.get_domain(account.id, int(domain)).data
+                else:
+                    dr = client.domains.get_domain(account.id, str(domain)).data
+            # domain does not exist
+            except DNSimpleException:
+                dr = None
             if state == 'present':
                 if dr:
-                    module.exit_json(changed=False, result=dr)
+                    module.exit_json(changed=False, result=dr.__dict__)
                 else:
                     if module.check_mode:
                         module.exit_json(changed=True)
                     else:
-                        module.exit_json(changed=True, result=client.add_domain(domain)['domain'])
-
+                        response = client.domains.create_domain(account.id, domain)
+                        module.exit_json(changed=True, result=response.data.__dict__)
+                        
             # state is absent
             else:
                 if dr:
                     if not module.check_mode:
-                        client.delete(domain)
+                        client.domains.delete_domain(account.id, domain)
                     module.exit_json(changed=True)
                 else:
                     module.exit_json(changed=False)
 
         # need the not none check since record could be an empty string
         if domain and record is not None:
-            records = [r['record'] for r in client.records(str(domain), params={'name': record})]
+            dr = client.domains.get_domain(account.id, domain).data
+
+            records_list = []
+            records_pagination = client.zones.list_records(account.id, dr.name, filter={'name': record}).pagination
+            for page in range(1, records_pagination.total_pages + 1):
+                page_data = client.zones.list_records(account.id, dr.name, filter={'name': record}, page=page).data
+                records_list.extend(page_data)
+
+            records = [r for r in records_list]
 
             if not record_type:
                 module.fail_json(msg="Missing the record type")
@@ -252,62 +285,54 @@ def main():
             if not value:
                 module.fail_json(msg="Missing the record value")
 
-            rr = next((r for r in records if r['name'] == record and r['type'] == record_type and r['content'] == value), None)
+            rr = next((r for r in records if r.name == record and r.type == record_type and r.content == value), None)
 
             if state == 'present':
                 changed = False
                 if is_solo:
                     # delete any records that have the same name and record type
-                    same_type = [r['id'] for r in records if r['name'] == record and r['type'] == record_type]
+                    same_type = [r.id for r in records if r.name == record and r.type == record_type]
                     if rr:
-                        same_type = [rid for rid in same_type if rid != rr['id']]
+                        same_type = [rid for rid in same_type if rid != rr.id]
                     if same_type:
                         if not module.check_mode:
                             for rid in same_type:
-                                client.delete_record(str(domain), rid)
+                                client.zones.delete_record(account.id, dr.name, rid)
                         changed = True
+                
                 if rr:
                     # check if we need to update
-                    if rr['ttl'] != ttl or rr['priority'] != priority:
-                        data = {}
-                        if ttl:
-                            data['ttl'] = ttl
-                        if priority:
-                            data['priority'] = priority
+                    
+                    if rr.ttl != ttl or rr.priority != priority:                        
+                        zr = ZoneRecordUpdateInput(ttl=ttl, priority=priority)
                         if module.check_mode:
                             module.exit_json(changed=True)
                         else:
-                            module.exit_json(changed=True, result=client.update_record(str(domain), str(rr['id']), data)['record'])
+                            response = client.zones.update_record(account.id, str(dr.name), str(rr.id), zr)
+                            module.exit_json(changed=True, result=response.data.__dict__)
                     else:
-                        module.exit_json(changed=changed, result=rr)
+                        module.exit_json(changed=changed, result=rr.__dict__)
                 else:
                     # create it
-                    data = {
-                        'name': record,
-                        'type': record_type,
-                        'content': value,
-                    }
-                    if ttl:
-                        data['ttl'] = ttl
-                    if priority:
-                        data['priority'] = priority
+                    zr = ZoneRecordInput(name=record, type=record_type, content=value, ttl=ttl, priority=priority)
                     if module.check_mode:
                         module.exit_json(changed=True)
                     else:
-                        module.exit_json(changed=True, result=client.add_record(str(domain), data)['record'])
-
+                        response = client.zones.create_record(account.id, str(dr.name), zr)
+                        module.exit_json(changed=True, result=response.data.__dict__)
             # state is absent
             else:
                 if rr:
                     if not module.check_mode:
-                        client.delete_record(str(domain), rr['id'])
+                        client.zones.delete_record(account.id, str(dr.name), rr.id)
                     module.exit_json(changed=True)
                 else:
                     module.exit_json(changed=False)
 
         # Make sure these record_ids either all exist or none
         if domain and record_ids:
-            current_records = [str(r['record']['id']) for r in client.records(str(domain))]
+            dr = client.domains.get_domain(account.id, domain).data
+            current_records = [str(r.id) for r in client.zones.list_records(account.id, dr.name).data]
             wanted_records = [str(r) for r in record_ids]
             if state == 'present':
                 difference = list(set(wanted_records) - set(current_records))
@@ -322,7 +347,7 @@ def main():
                 if difference:
                     if not module.check_mode:
                         for rid in difference:
-                            client.delete_record(str(domain), rid)
+                            client.zones.delete_record(account.id, str(dr.name), rid)
                     module.exit_json(changed=True)
                 else:
                     module.exit_json(changed=False)
