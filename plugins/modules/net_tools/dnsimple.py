d2f98b1c47a6375135c96751b0b74fed28e3eb02
Reto Kupferschmid <kupferschmid@puzzle.ch>
2021-07-07 08:17:28 +0200

    dnsimple: fixes for python-dnsimple >= 2.0.0

--- plugins/modules/net_tools/dnsimple.py
+++ plugins/modules/net_tools/dnsimple.py
@@ -14,8 +14,6 @@ module: dnsimple
 short_description: Interface with dnsimple.com (a DNS hosting service)
 description:
    - "Manages domains and records via the DNSimple API, see the docs: U(http://developer.dnsimple.com/)."
-notes:
-  - DNSimple API v1 is deprecated. Please install dnsimple-python>=1.0.0 which uses v2 API.
 options:
   account_email:
     description:
@@ -202,6 +200,7 @@ def main():
     priority = module.params.get('priority')
     state = module.params.get('state')
     is_solo = module.params.get('solo')
+    pagination_per_page = 30
 
     try:
         if account_email and account_api_token:
@@ -210,20 +209,18 @@ def main():
             client = Client(email=os.environ.get('DNSIMPLE_EMAIL'), access_token=os.environ.get('DNSIMPLE_API_TOKEN'))
         else:
             client = Client()
-
         account = client.identity.whoami().data.account
 
         # user supplied a user token instead of account api token
         if not account:
-          accounts = Accounts(client).list_accounts().data
-          if (len(accounts) != 1):
-              module.fail_json(msg="Provided dnsimple token is a user token with multiple accounts. Use an account token or a user token with access to a single account. See https://support.dnsimple.com/articles/api-access-token/")
-          else:
-            account = accounts[0]
+            accounts = Accounts(client).list_accounts().data
+            if len(accounts) != 1:
+                module.fail_json(msg="Provided dnsimple token is a user token with multiple accounts. Use an account token or a user token with access to a single account. See https://support.dnsimple.com/articles/api-access-token/")
+            else:
+                account = accounts[0]
 
     except DNSimpleException as e:
         module.fail_json(msg="Unable to contact DNSimple: %s" % e.message)
-
 
     try:
         # Let's figure out what operation we want to do
@@ -231,14 +228,13 @@ def main():
         # No domain, return a list
         if not domain:
             domain_list = []
-            domains_pagination = client.domains.list_domains(account.id).pagination
+            domains_pagination = client.domains.list_domains(account.id, per_page=pagination_per_page).pagination
             for page in range(1, domains_pagination.total_pages + 1):
-                page_data = client.domains.list_domains(account.id, page=page).data
+                page_data = client.domains.list_domains(account.id, page=page, per_page=pagination_per_page).data
                 domain_list.extend(page_data)
             module.exit_json(changed=False, result=[d.__dict__ for d in domain_list])
 
         # Domain & No record
-        
         if domain and record is None and not record_ids:
             try:
                 if domain.isdigit():
@@ -257,7 +253,6 @@ def main():
                     else:
                         response = client.domains.create_domain(account.id, domain)
                         module.exit_json(changed=True, result=response.data.__dict__)
-                        
             # state is absent
             else:
                 if dr:
@@ -272,12 +267,10 @@ def main():
             dr = client.domains.get_domain(account.id, domain).data
 
             records_list = []
-            records_pagination = client.zones.list_records(account.id, dr.name, filter={'name': record}).pagination
+            records_pagination = client.zones.list_records(account.id, dr.name, filter={'name': record}, per_page=pagination_per_page).pagination
             for page in range(1, records_pagination.total_pages + 1):
-                page_data = client.zones.list_records(account.id, dr.name, filter={'name': record}, page=page).data
+                page_data = client.zones.list_records(account.id, dr.name, filter={'name': record}, page=page, per_page=pagination_per_page).data
                 records_list.extend(page_data)
-
-            records = [r for r in records_list]
 
             if not record_type:
                 module.fail_json(msg="Missing the record type")
@@ -285,13 +278,13 @@ def main():
             if not value:
                 module.fail_json(msg="Missing the record value")
 
-            rr = next((r for r in records if r.name == record and r.type == record_type and r.content == value), None)
+            rr = next((r for r in records_list if r.name == record and r.type == record_type and r.content == value), None)
 
             if state == 'present':
                 changed = False
                 if is_solo:
                     # delete any records that have the same name and record type
-                    same_type = [r.id for r in records if r.name == record and r.type == record_type]
+                    same_type = [r.id for r in records_list if r.name == record and r.type == record_type]
                     if rr:
                         same_type = [rid for rid in same_type if rid != rr.id]
                     if same_type:
@@ -299,11 +292,9 @@ def main():
                             for rid in same_type:
                                 client.zones.delete_record(account.id, dr.name, rid)
                         changed = True
-                
                 if rr:
                     # check if we need to update
-                    
-                    if rr.ttl != ttl or rr.priority != priority:                        
+                    if rr.ttl != ttl or rr.priority != priority:
                         zr = ZoneRecordUpdateInput(ttl=ttl, priority=priority)
                         if module.check_mode:
                             module.exit_json(changed=True)
@@ -332,7 +323,12 @@ def main():
         # Make sure these record_ids either all exist or none
         if domain and record_ids:
             dr = client.domains.get_domain(account.id, domain).data
-            current_records = [str(r.id) for r in client.zones.list_records(account.id, dr.name).data]
+            current_records = []
+            current_records_pagination = client.zones.list_records(account.id, dr.name, per_page=pagination_per_page).pagination
+            for page in range(1, current_records_pagination.total_pages + 1):
+                page_data = client.zones.list_records(account.id, dr.name, page=page, per_page=pagination_per_page).data
+                records = [str(r.id) for r in page_data]
+                current_records.extend(records)
             wanted_records = [str(r) for r in record_ids]
             if state == 'present':
                 difference = list(set(wanted_records) - set(current_records))
