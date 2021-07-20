6640ceac1a57ff78f0e53a5d03e818fe0e1444b1
Alexei Znamensky <russoz@gmail.com>
2021-07-20 21:12:24 +1200

    updated c.g version

--- plugins/modules/packaging/language/ansible_galaxy_install.py
+++ plugins/modules/packaging/language/ansible_galaxy_install.py
@@ -12,7 +12,7 @@ module: ansible_galaxy_install
 author:
     - "Alexei Znamensky (@russoz)"
 short_description: Install Ansible roles or collections using ansible-galaxy
-version_added: 3.4.0
+version_added: 3.5.0
 description:
   - This module allows the installation of Ansible collections or roles using C(ansible-galaxy).
 options:
