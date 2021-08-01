047b7ada3ca0e2a0ab5b6e83de22174839c1741e
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-08-01 22:36:53 +1200

    uf8 marker batch2 (#3128)
    * added utf-8 markers to all .py files in plugins/filter
    
    * added utf-8 markers to all .py files in plugins/inventory
    
    * added utf-8 markers to all .py files in plugins/lookup

--- plugins/lookup/hiera.py
+++ plugins/lookup/hiera.py
@@ -1,3 +1,4 @@
+# coding: utf-8 -*-
 # (c) 2017, Juan Manuel Parrilla <jparrill@redhat.com>
 # (c) 2012-17 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
