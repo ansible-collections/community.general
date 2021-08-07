bc10863eb50c9f2f1855f505d57b2f195501e942
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-08-08 01:02:21 +1200

    fixed the utf-8 marker (#3162)
    (cherry picked from commit da11a98cb734e99cc57f4ae6ec09d9199875c39b)

--- plugins/callback/selective.py
+++ plugins/callback/selective.py
@@ -1,4 +1,4 @@
-# coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # (c) Fastly, inc 2016
 # (c) 2017 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
