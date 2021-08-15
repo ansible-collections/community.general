893e28e3d9bd57dc2b2b0d56f5d16018599725e1
Cliff Hults <cliff.hults@gmail.com>
2021-08-15 15:33:38 -0400

    Added tests for icinga2 inventory

--- tests/unit/plugins/inventory/test_icinga2.py
+++ tests/unit/plugins/inventory/test_icinga2.py
@@ -83,9 +83,9 @@ def test_populate(inventory, mocker):
     # check if host in the home_servers group
     assert 'home_servers' in inventory.inventory.groups
     group1_data = inventory.inventory.groups['home_servers']
-    print(group1_info.hosts)
+    print(group1_data.hosts)
     group2_data = inventory.inventory.groups['servers_hp']
-    print(group2_info.hosts)
+    print(group2_data.hosts)
     assert "test-host1.home.local" in group1_data.hosts
     assert "test-host1.home.local" not in group2_data.hosts
 
