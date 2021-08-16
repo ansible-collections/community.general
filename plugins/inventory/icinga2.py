2e56bf322e984b58a6ae5f9b4e35fdb1595176b6
Cliff Hults <cliff.hults@gmail.com>
2021-08-16 12:43:13 -0400

    Missed copywriting

--- plugins/inventory/icinga2.py
+++ plugins/inventory/icinga2.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2021, Cliff Hults <cliff.hlts@gmail.com>
 # Copyright (c) 2021 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
