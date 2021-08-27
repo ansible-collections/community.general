538289a6a8cf5d9797f18f0ef47621c35dac20ba
froebela <32922546+froebela@users.noreply.github.com>
2021-08-27 16:38:28 +0200

    added an extra dict in the diff data structure to hold the zfs properties

--- plugins/modules/storage/zfs/zfs.py
+++ plugins/modules/storage/zfs/zfs.py
@@ -194,14 +194,14 @@ class Zfs(object):
             self.module.fail_json(msg=err)
 
     def set_properties_if_changed(self):
-        diff = {'before': {}, 'after': {}}
+        diff = {'before': {'extra_zfs_properties': {}}, 'after': {'extra_zfs_properties': {}}}
         current_properties = self.get_current_properties()
         for prop, value in self.properties.items():
             current_value = current_properties.get(prop, None)
             if current_value != value:
                 self.set_property(prop, value)
-                diff['before'][prop] = current_value
-                diff['after'][prop] = value
+                diff['before']['extra_zfs_properties'][prop] = current_value
+                diff['after']['extra_zfs_properties'][prop] = value
         if self.module.check_mode:
             return diff
         updated_properties = self.get_current_properties()
@@ -211,8 +211,8 @@ class Zfs(object):
                 self.module.fail_json(msg="zfsprop was not present after being successfully set: %s" % prop)
             if current_properties.get(prop, None) != value:
                 self.changed = True
-            if prop in diff['after']:
-                diff['after'][prop] = value
+            if prop in diff['after']['extra_zfs_properties']:
+                diff['after']['extra_zfs_properties'][prop] = value
         return diff
 
     def get_current_properties(self):
