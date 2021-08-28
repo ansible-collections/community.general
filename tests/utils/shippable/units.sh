fe38f52ae0c0bff18f8c114c8c98064931bbbea1
Felix Fontein <felix@fontein.de>
2021-08-29 00:10:14 +0200

    Make sure we don't install a too new python-gitlab for Ansible 2.10.

--- tests/utils/shippable/units.sh
+++ tests/utils/shippable/units.sh
@@ -22,6 +22,10 @@ esac
 
 ansible-test env --timeout "${timeout}" --color -v
 
+if [ "$2" == "2.10" ]; then
+    echo "python-gitlab < 2.10.1 ; python_version >= '3.6'" >> tests/unit/requirements.txt
+fi
+
 # shellcheck disable=SC2086
 ansible-test units --color -v --docker default --python "${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
     "${options[@]:+${options[@]}}" \
