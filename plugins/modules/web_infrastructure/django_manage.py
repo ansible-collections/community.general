005519d32345e24285d06bef717bed0b8629d921
Alexei Znamensky <russoz@gmail.com>
2021-09-05 22:37:36 +1200

    check for None

--- plugins/modules/web_infrastructure/django_manage.py
+++ plugins/modules/web_infrastructure/django_manage.py
@@ -305,10 +305,11 @@ def main():
 
     # these params always get tacked on the end of the command
     for param in end_of_command_params:
-        if param in ('fixtures', 'apps'):
-            run_cmd_args.extend(module.params[param].split())
-        else:
-            run_cmd_args.append(module.params[param])
+        if module.params[param]:
+            if param in ('fixtures', 'apps'):
+                run_cmd_args.extend(module.params[param].split())
+            else:
+                run_cmd_args.append(module.params[param])
 
     rc, out, err = module.run_command(run_cmd_args, cwd=project_path)
     if rc != 0:
