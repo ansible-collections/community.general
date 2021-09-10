58d018ebbd60e2cebcd8050cd61b0a80a6e4b1a1
mkschuel <77283980+mkschuel@users.noreply.github.com>
2021-09-10 23:38:26 +0200

    Adds few more gitlab group options (#3248)
    * Adds few more gitlab group options
    
    * Update plugins/modules/source_control/gitlab/gitlab_group.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Removes default for new string options
    
    * Removes default from argument_spec
    
    * Adds changelog fragment
    
    * Update plugins/modules/source_control/gitlab/gitlab_group.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/source_control/gitlab/gitlab_group.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/modules/source_control/gitlab/gitlab_group.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Maik Schueller <maik.schueller@sap.com>
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- tests/unit/plugins/modules/source_control/gitlab/gitlab.py
+++ tests/unit/plugins/modules/source_control/gitlab/gitlab.py
@@ -194,6 +194,7 @@ def resp_get_group(url, request):
                '"lfs_enabled": true, "avatar_url": "http://localhost:3000/uploads/group/avatar/1/foo.jpg",'
                '"web_url": "http://localhost:3000/groups/foo-bar", "request_access_enabled": false,'
                '"full_name": "Foobar Group", "full_path": "foo-bar",'
+               '"project_creation_level": "maintainer", "subgroup_creation_level": "maintainer",'
                '"file_template_project_id": 1, "parent_id": null, "projects": [{"id": 1,"description": null, "default_branch": "master",'
                '"ssh_url_to_repo": "git@example.com:diaspora/diaspora-client.git",'
                '"http_url_to_repo": "http://example.com/diaspora/diaspora-client.git",'
@@ -225,7 +226,8 @@ def resp_create_group(url, request):
                '"lfs_enabled": true, "avatar_url": "http://localhost:3000/uploads/group/avatar/1/foo.jpg",'
                '"web_url": "http://localhost:3000/groups/foo-bar", "request_access_enabled": false,'
                '"full_name": "Foobar Group", "full_path": "foo-bar",'
-               '"file_template_project_id": 1, "parent_id": null}')
+               '"file_template_project_id": 1, "parent_id": null,'
+               '"project_creation_level": "developer", "subgroup_creation_level": "maintainer"}')
     content = content.encode("utf-8")
     return response(200, content, headers, None, 5, request)
 
@@ -238,7 +240,8 @@ def resp_create_subgroup(url, request):
                '"lfs_enabled": true, "avatar_url": "http://localhost:3000/uploads/group/avatar/2/bar.jpg",'
                '"web_url": "http://localhost:3000/groups/foo-bar/bar-foo", "request_access_enabled": false,'
                '"full_name": "BarFoo Group", "full_path": "foo-bar/bar-foo",'
-               '"file_template_project_id": 1, "parent_id": 1}')
+               '"file_template_project_id": 1, "parent_id": 1,'
+               '"project_creation_level": "noone"}')
     content = content.encode("utf-8")
     return response(200, content, headers, None, 5, request)
 
