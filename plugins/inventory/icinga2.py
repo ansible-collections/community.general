660d1e625d433ca953679a90ca9b350e74a043d0
Felix Fontein <felix@fontein.de>
2021-09-18 15:00:04 +0200

    Update plugins/inventory/icinga2.py

--- plugins/inventory/icinga2.py
+++ plugins/inventory/icinga2.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 
 DOCUMENTATION = '''
     name: icinga2
-    short_description: Icinga2 inventory source.
+    short_description: Icinga2 inventory source
     version_added: 3.7.0
     author:
         - Cliff Hults (@BongoEADGC6) <cliff.hults@gmail.com>
