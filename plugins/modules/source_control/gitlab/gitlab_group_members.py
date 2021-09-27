d59e7fcf1173f96f5bf6e12559192ff41c2f95bb
Frederic Werner <frederic.werner@cancom.de>
2021-09-27 14:25:24 +0200

    fix continuation line under-indented

--- plugins/modules/source_control/gitlab/gitlab_group_members.py
+++ plugins/modules/source_control/gitlab/gitlab_group_members.py
@@ -186,8 +186,9 @@ class GitLabGroup(object):
                 return group.id
         for group in groups:
             if group.path == gitlab_group or group.name == gitlab_group:
-                self._module.deprecate(msg="Setting 'gitlab_group' to 'name' or 'path' is deprecated. Use 'full_path' instead.",
-                version="6.0.0", collection_name="community.general")
+                self._module.deprecate(
+                    msg="Setting 'gitlab_group' to 'name' or 'path' is deprecated. Use 'full_path' instead.",
+                    version="6.0.0", collection_name="community.general")
                 return group.id
 
     # get all members in a group
