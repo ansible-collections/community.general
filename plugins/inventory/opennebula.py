593343bbc850175bbf4c345570df544ba6d4187b
Kristian Feldsam <feldsam@gmail.com>
2021-09-27 23:14:33 +0200

    Added group_by_labels, refactored so can be unit tested
    Signed-off-by: Kristian Feldsam <feldsam@gmail.com>

--- plugins/inventory/opennebula.py
+++ plugins/inventory/opennebula.py
@@ -45,7 +45,7 @@ DOCUMENTATION = r'''
             type: string
         api_password:
             description:
-              - Password of the user to login into OpenNebula RPC server.
+              - Password or a token of the user to login into OpenNebula RPC server.
               - If not set, the value of the C(ONE_PASSWORD) environment variable is used.
             env:
               - name: ONE_PASSWORD
@@ -71,6 +71,10 @@ DOCUMENTATION = r'''
         filter_by_label:
             description: Only return servers filtered by this label.
             type: string
+        group_by_labels:
+            description: Create host groups by vm labels
+            type: bool
+            default: True
 '''
 
 EXAMPLES = r'''
@@ -98,81 +102,6 @@ from collections import namedtuple
 import os
 
 
-def _get_connection_info(url, username, password, authfile):
-    if not username and not password:
-        if authfile is None:
-            authfile = os.path.join(os.environ.get("HOME"), ".one", "one_auth")
-        try:
-            with open(authfile, "r") as fp:
-                authstring = fp.read().rstrip()
-            username, password = authstring.split(":")
-        except (OSError, IOError):
-            raise AnsibleError("Could not find or read ONE_AUTH file at '{e}'".format(e=authfile))
-        except Exception:
-            raise AnsibleError("Error occurs when reading ONE_AUTH file at '{e}'".format(e=authfile))
-
-    auth_params = namedtuple('auth', ('url', 'username', 'password'))
-
-    return auth_params(url=url, username=username, password=password)
-
-
-def _get_vm_ipv4(vm):
-    nic = vm.TEMPLATE.get('NIC')
-
-    if isinstance(nic, dict):
-        nic = [nic]
-
-    for net in nic:
-        return net['IP']
-
-    return False
-
-
-def _get_vm_ipv6(vm):
-    nic = vm.TEMPLATE.get('NIC')
-
-    if isinstance(nic, dict):
-        nic = [nic]
-
-    for net in nic:
-        if net.get('IP6_GLOBAL'):
-            return net['IP6_GLOBAL']
-
-    return False
-
-
-def _retrieve_servers(one_client, label_filter=None):
-    # get hosts (VMs)
-    try:
-        vm_pool = one_client.vmpool.infoextended(-2, -1, -1, 3)
-    except Exception as e:
-        raise AnsibleError("Something happened during XML-RPC call: {e}".format(e=to_native(e)))
-
-    result = []
-
-    # iterate over hosts
-    for vm in vm_pool.VM:
-        server = vm.USER_TEMPLATE
-
-        labels = []
-        if vm.USER_TEMPLATE.get('LABELS'):
-            labels = vm.USER_TEMPLATE.get('LABELS').split(',')
-
-        # filter by label
-        if label_filter is not None:
-            if label_filter not in labels:
-                continue
-
-        server['name'] = vm.NAME
-        server['LABELS'] = labels
-        server['v4_first_ip'] = _get_vm_ipv4(vm)
-        server['v6_first_ip'] = _get_vm_ipv6(vm)
-
-        result.append(server)
-
-    return result
-
-
 class InventoryModule(BaseInventoryPlugin, Constructable):
     NAME = 'community.general.opennebula'
 
@@ -183,34 +112,115 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
                 valid = True
         return valid
 
-    def parse(self, inventory, loader, path, cache=True):
-        super(InventoryModule, self).parse(inventory, loader, path)
-        self._read_config_data(path=path)
+    def _get_connection_info(self):
+        url = self.get_option('api_url')
+        username = self.get_option('api_username')
+        password = self.get_option('api_password')
+        authfile = self.get_option('api_authfile')
 
-        if not HAS_PYONE:
-            raise AnsibleError('OpenNebula Inventory plugin requires pyone to work!')
+        if not username and not password:
+            if authfile is None:
+                authfile = os.path.join(os.environ.get("HOME"), ".one", "one_auth")
+            try:
+                with open(authfile, "r") as fp:
+                    authstring = fp.read().rstrip()
+                username, password = authstring.split(":")
+            except (OSError, IOError):
+                raise AnsibleError("Could not find or read ONE_AUTH file at '{e}'".format(e=authfile))
+            except Exception:
+                raise AnsibleError("Error occurs when reading ONE_AUTH file at '{e}'".format(e=authfile))
 
-        auth = _get_connection_info(self.get_option('api_url'),
-                                    self.get_option('api_username'),
-                                    self.get_option('api_password'),
-                                    self.get_option('api_authfile')
-                                    )
+        auth_params = namedtuple('auth', ('url', 'username', 'password'))
+
+        return auth_params(url=url, username=username, password=password)
+
+    def _get_vm_ipv4(self, vm):
+        nic = vm.TEMPLATE.get('NIC')
+
+        if isinstance(nic, dict):
+            nic = [nic]
+
+        for net in nic:
+            return net['IP']
+
+        return False
+
+    def _get_vm_ipv6(self, vm):
+        nic = vm.TEMPLATE.get('NIC')
+
+        if isinstance(nic, dict):
+            nic = [nic]
+
+        for net in nic:
+            if net.get('IP6_GLOBAL'):
+                return net['IP6_GLOBAL']
+
+        return False
+
+    def _get_vm_pool(self):
+        auth = self._get_connection_info()
 
         if not (auth.username and auth.password):
             raise AnsibleError('API Credentials missing. Check OpenNebula inventory file.')
         else:
             one_client = pyone.OneServer(auth.url, session=auth.username + ':' + auth.password)
 
+        # get hosts (VMs)
+        try:
+            vm_pool = one_client.vmpool.infoextended(-2, -1, -1, 3)
+        except Exception as e:
+            raise AnsibleError("Something happened during XML-RPC call: {e}".format(e=to_native(e)))
+
+        return vm_pool
+
+    def _retrieve_servers(self, label_filter=None):
+        vm_pool = self._get_vm_pool()
+
+        result = []
+
+        # iterate over hosts
+        for vm in vm_pool.VM:
+            server = vm.USER_TEMPLATE
+
+            labels = []
+            if vm.USER_TEMPLATE.get('LABELS'):
+                labels = [s for s in vm.USER_TEMPLATE.get('LABELS') if s == ',' or s == '-' or s.isalnum() or s.isspace()]
+                labels = ''.join(labels)
+                labels = labels.replace(' ', '_')
+                labels = labels.replace('-', '_')
+                labels = labels.split(',')
+
+
+            # filter by label
+            if label_filter is not None:
+                if label_filter not in labels:
+                    continue
+
+            server['name'] = vm.NAME
+            server['LABELS'] = labels
+            server['v4_first_ip'] = self._get_vm_ipv4(vm)
+            server['v6_first_ip'] = self._get_vm_ipv6(vm)
+
+            result.append(server)
+
+        return result
+
+    def _populate(self):
         hostname_preference = self.get_option('hostname')
+        group_by_labels = self.get_option('group_by_labels')
 
         # Add a top group 'one'
-        self.inventory.add_group(group='one')
+        self.inventory.add_group(group='all')
 
-        # Filter by label is supported by the api with a query
         filter_by_label = self.get_option('filter_by_label')
-        for server in _retrieve_servers(one_client, filter_by_label):
+        for server in self._retrieve_servers(filter_by_label):
+            # check for labels
+            if group_by_labels and server['LABELS']:
+                for label in server['LABELS']:
+                    self.inventory.add_group(group=label)
+                    self.inventory.add_host(host=server['name'], group=label)
 
-            self.inventory.add_host(host=server['name'], group='one')
+            self.inventory.add_host(host=server['name'], group='all')
 
             for attribute, value in server.items():
                 self.inventory.set_variable(server['name'], attribute, value)
@@ -220,3 +230,12 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
             if server.get('SSH_PORT'):
                 self.inventory.set_variable(server['name'], 'ansible_port', server['SSH_PORT'])
+
+    def parse(self, inventory, loader, path, cache=True):
+        if not HAS_PYONE:
+            raise AnsibleError('OpenNebula Inventory plugin requires pyone to work!')
+
+        super(InventoryModule, self).parse(inventory, loader, path)
+        self._read_config_data(path=path)
+
+        self._populate()
