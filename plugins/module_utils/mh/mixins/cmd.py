9f6c61d410e4703c04282e19c5342ac31544183f
Felix Fontein <felix@fontein.de>
2021-09-28 20:56:07 +0200

    Remove deprecated options, aliases and defaults.

--- plugins/module_utils/mh/mixins/cmd.py
+++ plugins/module_utils/mh/mixins/cmd.py
@@ -141,11 +141,7 @@ class CmdMixin(object):
                     fmt = find_format(param)
                     value = extra_params[param]
                 else:
-                    self.module.deprecate("Cannot determine value for parameter: {0}. "
-                                          "From version 4.0.0 onwards this will generate an exception".format(param),
-                                          version="4.0.0", collection_name="community.general")
-                    continue
-
+                    raise self.ModuleHelperException('Cannot determine value for parameter: {0}'.format(param))
             else:
                 raise self.ModuleHelperException("run_command parameter must be either a str or a dict: {0}".format(param))
             cmd_args = add_arg_formatted_param(cmd_args, fmt, value)
