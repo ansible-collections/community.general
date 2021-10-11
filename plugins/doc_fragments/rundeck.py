d433cd90498ab687b03b5e63f16bcd4547740033
Phillipe Smith <phsmithcc@gmail.com>
2021-10-11 01:55:47 -0300

    Add new modules rundeck_job_run and rundeck_job_executions_info (#3521)
    * Add new module rundeck_job_run
    
    * Add new module rundeck_job_executions_info
    
    * Removed supports_check_mode
    
    * Fix supports_check_mode
    
    * Fix version_added
    
    * Fixes for PR#3521
    
    * Fix default value for loglevel in the doc
    
    * Fix job_status_check loop
    
    * Add proposed changes in PR#3521
    
    * Add proposed changes in PR#3521
    
    * Change executions_info output to executions
    
    * Add rundeck integration tests
    
    * Fix rundeck integration test
    
    * Add more tests to rundeck integration tests
    
    * Update job_options doc
    
    * Add more tests to rundeck integration tests
    
    * Add more examples to rundeck_job_run doc
    
    * Add proposed fixes for PR#3521
    
    * Add proposed fixes for PR#3521
    
    * Fix job_options
    
    * Add proposed changes for PR#3521

---
+++ plugins/doc_fragments/rundeck.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Phillipe Smith <phsmithcc@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+
+    # Standard files documentation fragment
+    DOCUMENTATION = r'''
+options:
+  url:
+    type: str
+    description:
+      - Rundeck instance URL.
+    required: true
+  api_version:
+    type: int
+    description:
+      - Rundeck API version to be used.
+      - API version must be at least 14.
+    default: 39
+  api_token:
+    type: str
+    description:
+      - Rundeck User API Token.
+    required: true
+'''
