ce26d3310da4feb37455189e8b5fa62927ab662a
Matyáš Kroupa <kroupa.matyas@gmail.com>
2021-10-14 22:45:12 +0200

    nmcli: Fix ipv6.dns not being recongnized as list
    There was a missing comma on the previous line.

--- plugins/modules/net_tools/nmcli.py
+++ plugins/modules/net_tools/nmcli.py
@@ -1472,7 +1472,7 @@ class Nmcli(object):
                          'ipv4.dns-search',
                          'ipv4.routes',
                          'ipv4.routing-rules',
-                         'ipv4.route-metric'
+                         'ipv4.route-metric',
                          'ipv6.dns',
                          'ipv6.dns-search',
                          '802-11-wireless-security.group',
