1dd679f7797547b0b581d4af723fc1e279f00a2d
Vitaly Khabarov <vitkhab@users.noreply.github.com>
2021-10-18 14:53:14 +0300

    Misc doc issues (#3572)
    * Add names to tasks in oneview module examples
    
    * Fix task name in github_webhook module example
    
    * Fix trailing whitespace
    
    * Add changelog fragment
    
    * Remove changelog fragment
    
    (cherry picked from commit 3731064368895e54ad2b47c478bf6227868b3d63)

--- plugins/modules/remote_management/oneview/oneview_fc_network_info.py
+++ plugins/modules/remote_management/oneview/oneview_fc_network_info.py
@@ -39,7 +39,8 @@ EXAMPLES = '''
   delegate_to: localhost
   register: result
 
-- ansible.builtin.debug:
+- name: Print fetched information about Fibre Channel Networks
+  ansible.builtin.debug:
     msg: "{{ result.fc_networks }}"
 
 - name: Gather paginated, filtered and sorted information about Fibre Channel Networks
@@ -52,7 +53,9 @@ EXAMPLES = '''
       filter: 'fabricType=FabricAttach'
   delegate_to: localhost
   register: result
-- ansible.builtin.debug:
+
+- name: Print fetched information about paginated, filtered and sorted list of Fibre Channel Networks
+  ansible.builtin.debug:
     msg: "{{ result.fc_networks }}"
 
 - name: Gather information about a Fibre Channel Network by name
@@ -62,7 +65,8 @@ EXAMPLES = '''
   delegate_to: localhost
   register: result
 
-- ansible.builtin.debug:
+- name: Print fetched information about Fibre Channel Network found by name
+  ansible.builtin.debug:
     msg: "{{ result.fc_networks }}"
 '''
 
