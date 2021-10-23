3723d0716fcc76b44a4247a75ac41d0db6d646a7
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-10-23 20:10:53 +0200

    Update plugins/modules/web_infrastructure/supervisorctl.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/web_infrastructure/supervisorctl.py
+++ plugins/modules/web_infrastructure/supervisorctl.py
@@ -21,7 +21,7 @@ options:
       - The name of the supervisord program or group to manage.
       - The name will be taken as group name when it ends with a colon I(:)
       - Group support is only available in Ansible version 1.6 or later.
-      - If "all", all programs and program groups will be managed.
+      - If I(name=all), all programs and program groups will be managed.
     required: true
   config:
     type: path
