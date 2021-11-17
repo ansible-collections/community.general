c9a10a385eccd86a273ef030b099b8534f4c935d
Groshev Alexandr <haddystuff@gmail.com>
2021-11-17 20:36:53 +0300

    Add several tests and change documentation

--- plugins/modules/net_tools/nmcli.py
+++ plugins/modules/net_tools/nmcli.py
@@ -69,7 +69,7 @@ options:
         type: str
     ip4:
         description:
-            - The IPv4 address to this interface.
+            - List of IPv4 addresses to this interface.
             - Use the format C(192.0.2.24/24).
             - If defined and I(method4) is not specified, automatically set C(ipv4.method) to C(manual).
         type: list
@@ -823,7 +823,9 @@ EXAMPLES = r'''
 # nmcli_ethernet:
 #   - conn_name: em1
 #     ifname: em1
-#     ip4: '{{ tenant_ip }}'
+#     ip4:
+#       - '{{ tenant_ip }}'
+#       - '{{ second_tenant_ip }}'
 #     gw4: '{{ tenant_gw }}'
 #   - conn_name: em2
 #     ifname: em2
@@ -845,6 +847,7 @@ EXAMPLES = r'''
 # storage_ip: "192.0.2.91/23"
 # external_ip: "198.51.100.23/21"
 # tenant_ip: "203.0.113.77/23"
+# second_tenant_ip: "204.0.113.77/23"
 # ```
 
 
@@ -996,6 +999,16 @@ EXAMPLES = r'''
       conn_name: my-eth1
       mtu: 9000
       type: ethernet
+      state: present
+
+  - name: Add second ip4 address
+    community.general.nmcli:
+      conn_name: my-eth1
+      ifname: eth1
+      type: ethernet
+      ip4:
+        - 192.0.2.100/24
+        - 192.0.3.100/24
       state: present
 
   - name: Add VxLan
