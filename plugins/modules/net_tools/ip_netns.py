3d0cc75af5d18129adf1acc085100451b47c8fad
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2021-12-01 19:10:46 +1300

    ip_netns - invoke run_command passing list (#3822)
    * ip_netns - invoke run_command passing list
    
    * added changelog fragment

--- plugins/modules/net_tools/ip_netns.py
+++ plugins/modules/net_tools/ip_netns.py
@@ -76,7 +76,7 @@ class Namespace(object):
 
     def exists(self):
         '''Check if the namespace already exists'''
-        rc, out, err = self.module.run_command('ip netns list')
+        rc, out, err = self.module.run_command(['ip', 'netns', 'list'])
         if rc != 0:
             self.module.fail_json(msg=to_text(err))
         return self.name in out
