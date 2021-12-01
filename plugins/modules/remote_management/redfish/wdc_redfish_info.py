a258cff50cf6ad06a8bc02054396525699c66174
Mike Moerk <michael.moerk@wdc.com>
2021-12-01 16:00:26 -0700

    WDC Redfish Info / Command modules for Western Digital Ultrastar Data102 storage enclosures. Initial commands include: * FWActivate * UpdateAndActivate * SimpleUpdateStatus

---
+++ plugins/modules/remote_management/redfish/wdc_redfish_info.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022 Western Digital Corporation
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = '''
+---
+module: wdc_redfish_info
+short_description: Manages WDC UltraStar Data102 Out-Of-Band controllers using Redfish APIs
+description:
+  - Builds Redfish URIs locally and sends them to remote OOB controllers to
+    get information back.
+options:
+  category:
+    required: true
+    description:
+      - Category to execute on OOB controller
+    type: str
+  command:
+    required: true
+    description:
+      - List of commands to execute on OOB controller
+    type: list
+    elements: str
+  baseuri:
+    description:
+      - Base URI of OOB controller.  Must include this or ioms
+    type: str
+  ioms:
+    description:
+      - List of IOM FQDNs for the enclosure.  Must include this or baseuri
+    type: list
+    elements: str
+  username:
+    description:
+      - User for authentication with OOB controller
+    type: str
+  password:
+    description:
+      - Password for authentication with OOB controller
+    type: str
+  auth_token:
+    description:
+      - Security token for authentication with OOB controller
+    type: str
+  timeout:
+    description:
+      - Timeout in seconds for URL requests to OOB controller
+    default: 10
+    type: int
+
+requirements:
+  - dnspython (2.1.0 for Python 3, 1.16.0 for Python 2)
+
+notes:
+  - In the inventory, you can specify baseuri or ioms.
+  - ioms is a list of FQDNs for the enclosure's IOMs.
+  - See sample below.  This can be in your hosts file such as /etc/ansible/hosts.
+  - my_enclosure ioms='["oobm-11-22-33-44-55-66.wdc.com", "oobm-22-33-44-55-66-77.wdc.com"]'
+  - my_enclosure2 baseuri="oobm-00-11-22-33-44-55.wdc.com"
+
+author: Mike Moerk (@mikemoerk)
+'''
+
+EXAMPLES = '''
+- name: Get Simple Update Status
+  community.general.wdc_redfish_info:
+    category: Update
+    command: SimpleUpdateStatus
+    ioms: "{{ ioms }}"
+    username: "{{ username }}"
+    password: "{{ password }}"
+'''
+
+RETURN = '''
+Description:
+    description: Firmware update status description.
+    returned: always
+    type: str
+    sample:
+      - Ready for FW update
+      - FW update in progress
+      - FW update completed. Waiting for activation.
+ErrorCode:
+    description: Numeric error code for firmware update status.  Non-zero indicates an error condition.
+    returned: always
+    type: int
+    sample:
+      - 0
+EstimatedRemainingMinutes:
+    description: Estimated number of minutes remaining in firmware update operation.
+    returned: always
+    type: int
+    sample:
+      - 0
+      - 20
+StatusCode:
+    description: Firmware update status code.
+    returned: always
+    type: int
+    sample:
+      - 0 (Ready for FW update)
+      - 1 (FW update in progress)
+      - 2 (FW update completed.  Waiting for activation.)
+      - 3 (FW update failed.)
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.common.text.converters import to_native
+from ansible_collections.community.general.plugins.module_utils.wdc_redfish_utils import WdcRedfishUtils
+
+try:
+    from dns import resolver
+    DNS_AVAILABLE = True
+except ImportError:
+    DNS_AVAILABLE = False
+
+CATEGORY_COMMANDS_ALL = {
+    "Update": ["SimpleUpdateStatus"]
+}
+
+
+def dns_available():
+    return DNS_AVAILABLE
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            category=dict(required=True),
+            command=dict(required=True, type='list', elements='str'),
+            ioms=dict(type='list', elements='str'),
+            baseuri=dict(),
+            username=dict(),
+            password=dict(no_log=True),
+            auth_token=dict(no_log=True),
+            timeout=dict(type='int', default=10)
+        ),
+        required_together=[
+            ('username', 'password'),
+        ],
+        required_one_of=[
+            ('username', 'auth_token'),
+            ('baseuri', 'ioms')
+        ],
+        mutually_exclusive=[
+            ('username', 'auth_token'),
+        ],
+        supports_check_mode=True
+    )
+
+    if not dns_available():
+        module.fail_json(msg="The dnspython library is not installed")
+
+    category = module.params['category']
+    command_list = module.params['command']
+
+    # admin credentials used for authentication
+    creds = {'user': module.params['username'],
+             'pswd': module.params['password'],
+             'token': module.params['auth_token']}
+
+    # timeout
+    timeout = module.params['timeout']
+
+    # Build root URI(s)
+    if module.params.get("baseuri") is not None:
+        root_uris = ["https://" + module.params['baseuri']]
+    else:
+        root_uris = [
+            "https://" + iom for iom in module.params['ioms']
+        ]
+    rf_utils = WdcRedfishUtils(creds, root_uris, timeout, module,
+                               resource_id=None,
+                               data_modification=False
+                               )
+
+    # Check that Category is valid
+    if category not in CATEGORY_COMMANDS_ALL:
+        module.fail_json(msg=to_native("Invalid Category '%s'. Valid Categories = %s" % (category, list(CATEGORY_COMMANDS_ALL.keys()))))
+
+    # Check that all commands are valid
+    for cmd in command_list:
+        # Fail if even one command given is invalid
+        if cmd not in CATEGORY_COMMANDS_ALL[category]:
+            module.fail_json(msg=to_native("Invalid Command '%s'. Valid Commands = %s" % (cmd, CATEGORY_COMMANDS_ALL[category])))
+
+    # Organize by Categories / Commands
+
+    if category == "Update":
+        # execute only if we find UpdateService resources
+        resource = rf_utils._find_updateservice_resource()
+        if resource['ret'] is False:
+            module.fail_json(msg=resource['msg'])
+        for command in command_list:
+            if command == "SimpleUpdateStatus":
+                result = rf_utils.get_simple_update_status()
+                if result['ret'] is False:
+                    module.fail_json(msg=to_native(result['msg']))
+                if result['ret'] is True:
+                    del result['ret']
+                    module.exit_json(changed=False, redfish_facts=result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/plugins/modules/remote_management/wdc/test_wdc_redfish_info.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible_collections.community.general.tests.unit.compat.mock import patch
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible.module_utils import basic
+import ansible_collections.community.general.plugins.modules.remote_management.redfish.wdc_redfish_info as module
+from ansible_collections.community.general.plugins.module_utils.wdc_redfish_utils import DnsCacheBypass
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import AnsibleExitJson, AnsibleFailJson
+from ansible_collections.community.general.tests.unit.plugins.modules.utils import set_module_args, exit_json, fail_json
+
+
+def get_bin_path(self, arg, required=False):
+    """Mock AnsibleModule.get_bin_path"""
+    return arg
+
+
+class TestWdcRedfishInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_module_helper = patch.multiple(basic.AnsibleModule,
+                                                 exit_json=exit_json,
+                                                 fail_json=fail_json,
+                                                 get_bin_path=get_bin_path)
+        self.mock_module_helper.start()
+        self.addCleanup(self.mock_module_helper.stop)
+
+    def test_module_fail_when_required_args_missing(self):
+        with self.assertRaises(AnsibleFailJson):
+            set_module_args({})
+            module.main()
+
+    def test_module_fail_when_unknown_category(self):
+        with self.assertRaises(AnsibleFailJson):
+            set_module_args({
+                'category': 'unknown',
+                'command': 'SimpleUpdateStatus',
+                'username': 'USERID',
+                'password': 'PASSW0RD=21',
+                'ioms': [],
+            })
+            module.main()
+
+    def test_module_fail_when_unknown_command(self):
+        with self.assertRaises(AnsibleFailJson):
+            set_module_args({
+                'category': 'Update',
+                'command': 'unknown',
+                'username': 'USERID',
+                'password': 'PASSW0RD=21',
+                'ioms': [],
+            })
+            module.main()
+
+    def test_module_simple_update_status_pass(self):
+        set_module_args({
+            'category': 'Update',
+            'command': 'SimpleUpdateStatus',
+            'username': 'USERID',
+            'password': 'PASSW0RD=21',
+            'ioms': ["example1.example.com"],
+        })
+
+        def mock_simple_update_status(*args, **kwargs):
+            return {
+                "ret": True,
+                "data": {
+                    "Description": "Ready for FW update",
+                    "ErrorCode": 0,
+                    "EstimatedRemainingMinutes": 0,
+                    "StatusCode": 0
+                }
+            }
+
+        def mocked_string_response(*args, **kwargs):
+            return "mockedUrl"
+
+        def empty_return(*args, **kwargs):
+            return {"ret": True}
+
+        with patch.object(DnsCacheBypass, '_resolve') as mock__resolve:
+            mock__resolve.return_value = ("127.0.0.1",)
+            with patch.multiple(module.WdcRedfishUtils,
+                                _simple_update_status_uri=mocked_string_response,
+                                _find_updateservice_resource=empty_return,
+                                _find_updateservice_additional_uris=empty_return,
+                                get_request=mock_simple_update_status):
+                with patch.object(module, 'dns_available') as mock_dns_available:
+                    mock_dns_available.return_value = True
+                    with self.assertRaises(AnsibleExitJson) as exit_json:
+                        module.main()
+                        self.assertEqual(mock_simple_update_status,
+                                         exit_json["redfish_facts"]["entries"])
+
+    def test_module_simple_update_status_updateservice_resource_not_found(self):
+        set_module_args({
+            'category': 'Update',
+            'command': 'SimpleUpdateStatus',
+            'username': 'USERID',
+            'password': 'PASSW0RD=21',
+            'ioms': ["example1.example.com"],
+        })
+        with patch.object(DnsCacheBypass, '_resolve') as mock__resolve:
+            mock__resolve.return_value = ("127.0.0.1",)
+            with patch.object(module.WdcRedfishUtils, 'get_request') as mock_get_request:
+                mock_get_request.return_value = {
+                    "ret": True,
+                    "data": {}  # Missing UpdateService property
+                }
+                with patch.object(module, 'dns_available') as mock_dns_available:
+                    mock_dns_available.return_value = True
+                    with self.assertRaises(AnsibleFailJson) as exit_json:
+                        module.main()
+                        self.assertEqual("UpdateService resource not found",
+                                         exit_json["msg"])
+
+    def test_module_simple_update_status_service_does_not_support_simple_update(self):
+        set_module_args({
+            'category': 'Update',
+            'command': 'SimpleUpdateStatus',
+            'username': 'USERID',
+            'password': 'PASSW0RD=21',
+            'ioms': ["example1.example.com"],
+        })
+
+        def mock_get_request_function(uri):
+            mock_url_string = "mockURL"
+            if mock_url_string in uri:
+                return {
+                    "ret": True,
+                    "data": {
+                        "Actions": {  # No #UpdateService.SimpleUpdate
+                        }
+                    }
+                }
+            else:
+                return {
+                    "ret": True,
+                    "data": mock_url_string
+                }
+
+        with patch.object(DnsCacheBypass, '_resolve') as mock__resolve:
+            mock__resolve.return_value = ("127.0.0.1",)
+            with patch.object(module.WdcRedfishUtils, 'get_request') as mock_get_request:
+                mock_get_request.side_effect = mock_get_request_function
+                with patch.object(module, 'dns_available') as mock_dns_available:
+                    mock_dns_available.return_value = True
+                    with self.assertRaises(AnsibleFailJson) as exit_json:
+                        module.main()
+                        self.assertEqual("UpdateService resource not found",
+                                         exit_json["msg"])
+
+    def test_module_simple_update_status_service_does_not_support_fw_activate(self):
+        set_module_args({
+            'category': 'Update',
+            'command': 'SimpleUpdateStatus',
+            'username': 'USERID',
+            'password': 'PASSW0RD=21',
+            'ioms': ["example1.example.com"],
+        })
+
+        def mock_get_request_function(uri):
+            mock_url_string = "mockURL"
+            if mock_url_string in uri:
+                return {
+                    "ret": True,
+                    "data": {
+                        "Actions": {
+                            "#UpdateService.SimpleUpdate": "mocked value",
+                            "Oem": {
+                                "WDC": {}  # No #UpdateService.FWActivate
+                            }
+                        }
+                    }
+                }
+            else:
+                return {
+                    "ret": True,
+                    "data": mock_url_string
+                }
+
+        with patch.object(DnsCacheBypass, '_resolve') as mock__resolve:
+            mock__resolve.return_value = ("127.0.0.1",)
+            with patch.object(module.WdcRedfishUtils, 'get_request') as mock_get_request:
+                mock_get_request.side_effect = mock_get_request_function
+                with patch.object(module, 'dns_available') as mock_dns_available:
+                    mock_dns_available.return_value = True
+                    with self.assertRaises(AnsibleFailJson) as exit_json:
+                        module.main()
+                        self.assertEqual("Service does not support FWActivate",
+                                         exit_json["msg"])
