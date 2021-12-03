cd84ee88a39dce72e8bcb19ec59d22de083a37da
Alexei Znamensky <russoz@gmail.com>
2021-12-03 22:39:57 +1300

    MH StateMixin - improvements
    - state_param can be any variable in self.vars (includes module params)
    - removed extraneous code
    - added tests for state_param being an arbitrary var

--- plugins/module_utils/mh/mixins/state.py
+++ plugins/module_utils/mh/mixins/state.py
@@ -11,29 +11,21 @@ class StateMixin(object):
     state_param = 'state'
     default_state = None
 
-    def _state(self):
-        state = self.module.params.get(self.state_param)
+    @property
+    def _state_value(self):
+        state = self.vars.get(self.state_param)
         return self.default_state if state is None else state
 
     def _method(self, state):
         return "{0}_{1}".format(self.state_param, state)
 
     def __run__(self):
-        state = self._state()
-        self.vars.state = state
-
-        # resolve aliases
-        if state not in self.module.params:
-            aliased = [name for name, param in self.module.argument_spec.items() if state in param.get('aliases', [])]
-            if aliased:
-                state = aliased[0]
-                self.vars.effective_state = state
-
-        method = self._method(state)
-        if not hasattr(self, method):
-            return self.__state_fallback__()
-        func = getattr(self, method)
+        method = self._method(self._state_value)
+        try:
+            func = getattr(self, method)
+        except AttributeError:
+            func = self.__state_fallback__
         return func()
 
     def __state_fallback__(self):
-        raise ValueError("Cannot find method: {0}".format(self._method(self._state())))
+        raise ValueError("Cannot find method: {0}".format(self._method(self._state_value)))
