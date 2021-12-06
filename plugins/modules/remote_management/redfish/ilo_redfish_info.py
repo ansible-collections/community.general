9b4a4ea773f99d703b2463c472525d57fb063322
Bhavya <44067558+Bhavya06@users.noreply.github.com>
2021-12-06 13:39:57 +0530

    Update plugins/modules/remote_management/redfish/ilo_redfish_info.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/remote_management/redfish/ilo_redfish_info.py
+++ plugins/modules/remote_management/redfish/ilo_redfish_info.py
@@ -9,6 +9,7 @@ DOCUMENTATION = '''
 ---
 module: ilo_redfish_info
 short_description: Gathers server information through iLO using Redfish APIs
+version_added: 4.2.0
 description:
   - Builds Redfish URIs locally and sends them to iLO to
     get information back.
