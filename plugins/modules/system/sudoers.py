3cffdfc1459f2206d9ee4cb4131c3656a13f4734
Jon Ellis <ellis.jp@gmail.com>
2021-12-14 22:28:34 +0000

    Update handle

--- plugins/modules/system/sudoers.py
+++ plugins/modules/system/sudoers.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-# Copyright: (c) 2019, Jon Ellis (@JonEllis0) <ellis.jp@gmail.com>
+# Copyright: (c) 2019, Jon Ellis (@JonEllis) <ellis.jp@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
