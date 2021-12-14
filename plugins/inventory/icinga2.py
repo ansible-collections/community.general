165a2c1d408af6939f421c708a0dead329100694
Cliff Hults <cliff.hults@gmail.com>
2021-12-14 17:14:54 -0500

    Error handling

--- plugins/inventory/icinga2.py
+++ plugins/inventory/icinga2.py
@@ -3,7 +3,6 @@
 # Copyright (c) 2021 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
-from json.decoder import JSONDecodeError
 
 __metaclass__ = type
 
