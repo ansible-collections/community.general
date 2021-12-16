1553b4522bdd5c2136cb0d03f98fc2fa559a8590
Jean Raby <jean@raby.sh>
2021-12-16 12:57:40 -0500

    Update copyright

--- plugins/modules/packaging/os/pacman.py
+++ plugins/modules/packaging/os/pacman.py
@@ -4,6 +4,7 @@
 # Copyright: (c) 2012, Afterburn <https://github.com/afterburn>
 # Copyright: (c) 2013, Aaron Bull Schaefer <aaron@elasticdog.com>
 # Copyright: (c) 2015, Indrajit Raychaudhuri <irc+code@indrajit.com>
+# Copyright: (c) 2021, Jean Raby <jean@raby.sh>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
