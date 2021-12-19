01f201d7d990dc41ab6feda5414a1faf98a072d5
Alexei Znamensky <russoz@gmail.com>
2021-12-19 18:10:03 +1300

    further clarified code

--- plugins/module_utils/mh/mixins/state.py
+++ plugins/module_utils/mh/mixins/state.py
@@ -8,16 +8,29 @@ __metaclass__ = type
 
 
 class StateMixin(object):
-    state_param = 'state'
+    state_param = None
     default_state = None
+    default_state_param = 'state'
+
+    @property
+    def _state_param_name(self):
+        param_name = self.state_param or self.default_state_param
+        if param_name in self.module.params:
+            return param_name
+
+        aliased_args = [arg for arg, spec in self.module.argument_spec.items() if 'aliases' in spec]
+        if param_name not in aliased_args:
+            return param_name
+
+        return [param for param in self.module.params if param in aliased_args[param_name]['aliases']][0]
 
     @property
     def _state_value(self):
-        state = self.vars.get(self.state_param)
+        state = self.vars.get(self._state_param_name)
         return self.default_state if state is None else state
 
     def _method(self, state):
-        return "{0}_{1}".format(self.state_param, state)
+        return "{0}_{1}".format(self.state_param or self.default_state_param, state)
 
     def __run__(self):
         method = self._method(self._state_value)
@@ -26,6 +39,8 @@ class StateMixin(object):
         except AttributeError:
             func = self.__state_fallback__
         return func()
+        if self.state_param is None:
+            return self.default_state_param
 
     def __state_fallback__(self):
         raise ValueError("Cannot find method: {0}".format(self._method(self._state_value)))
