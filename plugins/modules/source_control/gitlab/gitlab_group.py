52ad0a5fbb973a64c9d14ce59e2e8ee03483951f
Josh <josham@users.noreply.github.com>
2021-12-20 15:59:12 -0500

    Add additional auth support to Gitlab (#705) (#3918)
    * Add additional auth support to Gitlab (#705)
    
    - removed unused imports from module_utils.gitlab
    - fix bug in gitlab_project to check if avatar_path is provided
    
    * add doc_fragment and argument_spec for gitlab auth
    
    * doc fixes and remove avatar_path bug fix
    
    * small doc changes, pass validate_certs to requests call
    
    * update changelog

--- plugins/modules/source_control/gitlab/gitlab_group.py
+++ plugins/modules/source_control/gitlab/gitlab_group.py
@@ -22,13 +22,10 @@ requirements:
   - python >= 2.7
   - python-gitlab python module
 extends_documentation_fragment:
-- community.general.auth_basic
+  - community.general.auth_basic
+  - community.general.gitlab
 
 options:
-  api_token:
-    description:
-      - GitLab token for logging in.
-    type: str
   name:
     description:
       - Name of the group you want to create.
@@ -175,7 +172,7 @@ from ansible.module_utils.api import basic_auth_argument_spec
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.common.text.converters import to_native
 
-from ansible_collections.community.general.plugins.module_utils.gitlab import find_group, gitlab_authentication
+from ansible_collections.community.general.plugins.module_utils.gitlab import auth_argument_spec, find_group, gitlab_authentication
 
 
 class GitLabGroup(object):
@@ -309,8 +306,8 @@ class GitLabGroup(object):
 
 def main():
     argument_spec = basic_auth_argument_spec()
+    argument_spec.update(auth_argument_spec())
     argument_spec.update(dict(
-        api_token=dict(type='str', no_log=True),
         name=dict(type='str', required=True),
         path=dict(type='str'),
         description=dict(type='str'),
@@ -328,13 +325,16 @@ def main():
         argument_spec=argument_spec,
         mutually_exclusive=[
             ['api_username', 'api_token'],
-            ['api_password', 'api_token'],
+            ['api_username', 'api_oauth_token'],
+            ['api_username', 'api_job_token'],
+            ['api_token', 'api_oauth_token'],
+            ['api_token', 'api_job_token'],
         ],
         required_together=[
             ['api_username', 'api_password'],
         ],
         required_one_of=[
-            ['api_username', 'api_token']
+            ['api_username', 'api_token', 'api_oauth_token', 'api_job_token']
         ],
         supports_check_mode=True,
     )
