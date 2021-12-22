c21052ca0897604a0fb91e6bb099d76be5313f50
Martijn <martijn@mrtijn.nl>
2021-12-22 22:31:27 +0100

    Adjust module options dependencies

--- plugins/modules/cloud/misc/proxmox.py
+++ plugins/modules/cloud/misc/proxmox.py
@@ -556,9 +556,14 @@ def main():
             proxmox_default_behavior=dict(type='str', default='no_defaults', choices=['compatibility', 'no_defaults']),
             clone=dict(type='int'),
         ),
-        required_if=[('state', 'present', ['node', 'hostname', 'ostemplate'])],
+        required_if=[
+          ('state', 'present', ['node', 'hostname']),
+          ('state', 'present', ('clone', 'ostemplate'), True), # Require one of clone and ostemplate. Together with mutually_exclusive this ensures that we
+                                                               # either clone a container or create a new one from a template file.
+        ],
         required_together=[('api_token_id', 'api_token_secret')],
         required_one_of=[('api_password', 'api_token_id')],
+        mutually_exclusive=[('clone', 'ostemplate')], # Creating a new container is done either by cloning an existing one, or based on a template.
     )
 
     if not HAS_PROXMOXER:
