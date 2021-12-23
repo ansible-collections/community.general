8de1d5051e0532a0dfed94b60ce03c2027ef65b7
Felix Fontein <felix@fontein.de>
2021-12-23 09:35:10 +0100

    Fix casing, now that the object is no longer called Version.

--- plugins/module_utils/version.py
+++ plugins/module_utils/version.py
@@ -3,7 +3,7 @@
 # Copyright: (c) 2021, Felix Fontein <felix@fontein.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-"""Provide Version object to compare version numbers."""
+"""Provide version object to compare version numbers."""
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
