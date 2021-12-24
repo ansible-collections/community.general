5e27eb6046c3666a91d35191eabd6093aeec8c24
Felix Fontein <felix@fontein.de>
2021-12-24 18:34:48 +0100

    Prepare for distutils.version being removed in Python 3.12 (#3936)
    * Prepare for distutils.version being removed in Python 2.12.
    
    * Fix copy'n'paste error.
    
    * Re-add Loose prefix.
    
    * Fix Python version typos.
    
    * Improve formulation.
    
    * Move message into own line.
    
    * Fix casing, now that the object is no longer called Version.
    
    (cherry picked from commit a2f72be6c8d36c0b836755093e2837518b2e5869)

--- plugins/modules/cloud/centurylink/clc_modify_server.py
+++ plugins/modules/cloud/centurylink/clc_modify_server.py
@@ -311,7 +311,8 @@ __version__ = '${version}'
 import json
 import os
 import traceback
-from distutils.version import LooseVersion
+
+from ansible_collections.community.general.plugins.module_utils.version import LooseVersion
 
 REQUESTS_IMP_ERR = None
 try:
@@ -355,8 +356,7 @@ class ClcModifyServer:
             self.module.fail_json(msg=missing_required_lib('clc-sdk'), exception=CLC_IMP_ERR)
         if not REQUESTS_FOUND:
             self.module.fail_json(msg=missing_required_lib('requests'), exception=REQUESTS_IMP_ERR)
-        if requests.__version__ and LooseVersion(
-                requests.__version__) < LooseVersion('2.5.0'):
+        if requests.__version__ and LooseVersion(requests.__version__) < LooseVersion('2.5.0'):
             self.module.fail_json(
                 msg='requests library  version should be >= 2.5.0')
 
