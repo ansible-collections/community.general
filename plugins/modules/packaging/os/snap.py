4a9a33d93086191430c34fd47acf52a835151cf3
Marcus Rickert <marcus.rickert@web.de>
2021-12-25 14:39:00 +0100

    Add functionality proposed in https://github.com/ansible-collections/community.general/issues/666

--- plugins/modules/packaging/os/snap.py
+++ plugins/modules/packaging/os/snap.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2021, Alexei Znamensky (russoz) <russoz@gmail.com>
+# Copyright: (c) 2021, Marcus Rickert <marcus.rickert@web.de>
 # Copyright: (c) 2018, Stanislas Lange (angristan) <angristan@pm.me>
 # Copyright: (c) 2018, Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
 
@@ -46,6 +47,14 @@ options:
         type: str
         required: false
         default: stable
+    options:
+        description:
+            - Set options with pattern key=value or snap:key=value. If a snap name is given, the option will be applied
+              to that snap only. If the snap name is omitted, the options will be applied to all snaps. Options will 
+              only be applied to active snaps.
+        required: false
+        type: list
+        elements: str
 
 author:
     - Victor Carceler (@vcarceler) <vcarceler@iespuigcastellar.xeill.net>
@@ -62,6 +71,26 @@ EXAMPLES = '''
     name:
       - foo
       - bar
+      
+# Install "foo" snap with options par1=A and par2=B 
+- name: Install "foo" with options
+  community.general.snap:
+    name:
+      - foo
+    options:
+      - par1=A
+      - par2=B
+
+# Install "foo" and "bar" snaps with common option com=A and specific options fooPar=X and barPar=Y  
+- name: Install "foo" and "bar" with options
+  community.general.snap:
+    name:
+      - foo
+      - bar
+    options:
+      - com=A
+      - foo:fooPar=X
+      - bar:barPar=Y
 
 # Remove "foo" snap
 - name: Remove foo
@@ -103,9 +132,14 @@ snaps_removed:
     description: The list of actually removed snaps
     type: list
     returned: When any snaps have been removed
+options_changed:
+    description: The list of options set/changed in format "snap:key=value"
+    type: list
+    returned: When any options have been changed/set
 '''
 
 import re
+import json
 
 from ansible.module_utils.common.text.converters import to_native
 
@@ -121,6 +155,8 @@ __state_map = dict(
     disabled='disable',
     info='info',  # not public
     list='list',  # not public
+    set='set', # not public
+    get='get', # not public
 )
 
 
@@ -130,6 +166,7 @@ def _state_map(value):
 
 class Snap(CmdStateModuleHelper):
     __disable_re = re.compile(r'(?:\S+\s+){5}(?P<notes>\S+)')
+    __set_param_re = re.compile(r'(?P<snap_prefix>\S+:)?(?P<key>\S+)=(?P<value>\S+)')
     module = dict(
         argument_spec={
             'name': dict(type='list', elements='str', required=True),
@@ -137,6 +174,7 @@ class Snap(CmdStateModuleHelper):
                           choices=['absent', 'present', 'enabled', 'disabled']),
             'classic': dict(type='bool', default=False),
             'channel': dict(type='str', default='stable'),
+            'options': dict(type='list', elements='str'),
         },
         supports_check_mode=True,
     )
@@ -146,6 +184,8 @@ class Snap(CmdStateModuleHelper):
         state=dict(fmt=_state_map),
         classic=dict(fmt="--classic", style=ArgFormat.BOOLEAN),
         channel=dict(fmt=lambda v: [] if v == 'stable' else ['--channel', '{0}'.format(v)]),
+        options=dict(fmt=lambda v: [p for p in v]),
+        json_format=dict(fmt="-d", style=ArgFormat.BOOLEAN),
     )
     check_rc = False
 
@@ -171,6 +211,45 @@ class Snap(CmdStateModuleHelper):
             '\n'.join(results[3]),
         ]
 
+    def convert_json_subtree_to_map(self, json_subtree, prefix=None):
+        option_map = {}
+
+        for key, value in json_subtree.items():
+            full_key = key if prefix is None else prefix + "." + key
+
+            if isinstance(value, str) or isinstance(value, int) or isinstance(value, float) or isinstance(value, bool):
+                option_map[full_key] = str(value)
+
+            else:
+                option_map.update(self.convert_json_subtree_to_map(json_subtree=value, prefix=full_key))
+
+        return option_map
+
+    def convert_json_to_map(self, json_string):
+        json_object = json.loads(json_string)
+        return self.convert_json_subtree_to_map(json_object)
+
+    def retrieve_option_map(self, snap_name):
+        params = [{'state': 'get'}, {'name': snap_name}, {'json_format': True}]
+        rc, out, err = self.run_command(params=params)
+
+        if rc != 0:
+            return {}
+
+        result = out.splitlines()
+
+        if "has no configuration" in result[0]:
+            return {}
+
+        try:
+            option_map = self.convert_json_to_map(out)
+
+        except Exception as e:
+            raise ModuleHelperException(
+                msg="Parsing option map returned by 'snap get {0}' triggers exception '{1}', output:\n'{2}'".format(snap_name, str(e), out))
+
+        return option_map
+
     def is_snap_installed(self, snap_name):
         return 0 == self.run_command(params=[{'state': 'list'}, {'name': snap_name}])[0]
 
@@ -186,36 +265,91 @@ class Snap(CmdStateModuleHelper):
         return "disabled" not in notes.split(',')
 
     def state_present(self):
+
         self.vars.meta('classic').set(output=True)
         self.vars.meta('channel').set(output=True)
         actionable_snaps = [s for s in self.vars.name if not self.is_snap_installed(s)]
-        if not actionable_snaps:
-            return
-        self.changed = True
-        self.vars.snaps_installed = actionable_snaps
-        if self.module.check_mode:
-            return
-        params = ['state', 'classic', 'channel']  # get base cmd parts
-        has_one_pkg_params = bool(self.vars.classic) or self.vars.channel != 'stable'
-        has_multiple_snaps = len(actionable_snaps) > 1
-        if has_one_pkg_params and has_multiple_snaps:
-            commands = [params + [{'actionable_snaps': [s]}] for s in actionable_snaps]
-        else:
-            commands = [params + [{'actionable_snaps': actionable_snaps}]]
-        self.vars.cmd, rc, out, err = self._run_multiple_commands(commands)
-        if rc == 0:
-            return
 
-        classic_snap_pattern = re.compile(r'^error: This revision of snap "(?P<package_name>\w+)"'
-                                          r' was published using classic confinement')
-        match = classic_snap_pattern.match(err)
-        if match:
-            err_pkg = match.group('package_name')
-            msg = "Couldn't install {name} because it requires classic confinement".format(name=err_pkg)
-        else:
-            msg = "Ooops! Snap installation failed while executing '{cmd}', please examine logs and " \
-                  "error output for more details.".format(cmd=self.vars.cmd)
-        raise ModuleHelperException(msg=msg)
+        if actionable_snaps:
+            self.changed = True
+            self.vars.snaps_installed = actionable_snaps
+
+            if self.module.check_mode:
+                return
+
+            params = ['state', 'classic', 'channel']  # get base cmd parts
+            has_one_pkg_params = bool(self.vars.classic) or self.vars.channel != 'stable'
+            has_multiple_snaps = len(actionable_snaps) > 1
+            if has_one_pkg_params and has_multiple_snaps:
+                commands = [params + [{'actionable_snaps': [s]}] for s in actionable_snaps]
+            else:
+                commands = [params + [{'actionable_snaps': actionable_snaps}]]
+            self.vars.cmd, rc, out, err = self._run_multiple_commands(commands)
+
+            if rc != 0:
+                classic_snap_pattern = re.compile(r'^error: This revision of snap "(?P<package_name>\w+)"'
+                                                  r' was published using classic confinement')
+                match = classic_snap_pattern.match(err)
+                if match:
+                    err_pkg = match.group('package_name')
+                    msg = "Couldn't install {name} because it requires classic confinement".format(name=err_pkg)
+                else:
+                    msg = "Ooops! Snap installation failed while executing '{cmd}', please examine logs and " \
+                          "error output for more details.".format(cmd=self.vars.cmd)
+                raise ModuleHelperException(msg=msg)
+
+        self.set_options()
+
+    def set_options(self):
+        actionable_snaps = [s for s in self.vars.name if self.is_snap_installed(s)]
+        overall_options_changed = []
+
+        for snap_name in actionable_snaps:
+            option_map = self.retrieve_option_map(snap_name=snap_name)
+
+            options_changed = []
+
+            for option_string in self.vars.options:
+                match = self.__set_param_re.match(option_string)
+
+                if not match:
+                    msg = "Cannot parse set option '{option_string}'".format(option_string=option_string)
+                    raise ModuleHelperException(msg)
+
+                snap_prefix = match.group("snap_prefix")
+                selected_snap_name = snap_prefix[:-1] if snap_prefix else None
+
+                if selected_snap_name is not None and selected_snap_name not in self.vars.name:
+                    msg = "Snap option '{option_string}' refers to snap which is not in the list of snap names".format(option_string=option_string)
+                    raise ModuleHelperException(msg)
+
+                if selected_snap_name is None or (snap_name is not None and snap_name == selected_snap_name):
+                    key = match.group("key")
+                    value = match.group("value")
+
+                    if key not in option_map or key in option_map and option_map[key] != value:
+                        option_without_prefix = key + "=" + value
+                        option_with_prefix = option_string if selected_snap_name is not None else snap_name + ":" + option_string
+                        options_changed.append(option_without_prefix)
+                        overall_options_changed.append(option_with_prefix)
+
+            if len(options_changed) > 0:
+                self.changed = True
+                params = [{'state': 'set'}, {'name': snap_name}, {'options': options_changed}]
+
+                rc, out, err = self.run_command(params=params)
+
+                if rc != 0:
+                    if 'has no "configure" hook' in err:
+                        msg = "Snap '{snap}' does not have any configurable options".format(snap=snap_name)
+                        raise ModuleHelperException(msg)
+
+                    msg = "Cannot set options '{options}' for snap '{snap}': error={error}".format(
+                        options=" ".join(options_changed), snap=snap_name, error=err)
+                    raise ModuleHelperException(msg)
+
+            if len(overall_options_changed) > 0:
+                self.vars.options_changed = overall_options_changed
 
     def _generic_state_action(self, actionable_func, actionable_var, params=None):
         actionable_snaps = [s for s in self.vars.name if actionable_func(s)]
