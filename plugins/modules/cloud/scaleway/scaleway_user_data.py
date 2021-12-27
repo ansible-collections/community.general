6bf9167d37ceb53178da46d08bbd69b05faa47b1
pmangin <96626847+pmangin@users.noreply.github.com>
2021-12-27 19:40:55 +0100

    fix  scaleway_user_data (#3940)
    * fix  scaleway_user_data 
    
    scaleway_user_data put cloud-init valuer with 2 unexpected " (begin and end of value)
    
    If Content-Type is not change , it's jsonify ( file module_utils/scaleway.py ligne 131 )
    
    fix the probleme  when "Content-Type" is used instead of "Content-type"
    
    * Create 3940_fix_contenttype_scaleway_user_data.yml
    
    * Update changelogs/fragments/3940_fix_contenttype_scaleway_user_data.yml
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/cloud/scaleway/scaleway_user_data.py
+++ plugins/modules/cloud/scaleway/scaleway_user_data.py
@@ -75,7 +75,7 @@ def patch_user_data(compute_api, server_id, key, value):
     compute_api.module.debug("Starting patching user_data attributes")
 
     path = "servers/%s/user_data/%s" % (server_id, key)
-    response = compute_api.patch(path=path, data=value, headers={"Content-type": "text/plain"})
+    response = compute_api.patch(path=path, data=value, headers={"Content-Type": "text/plain"})
     if not response.ok:
         msg = 'Error during user_data patching: %s %s' % (response.status_code, response.body)
         compute_api.module.fail_json(msg=msg)
