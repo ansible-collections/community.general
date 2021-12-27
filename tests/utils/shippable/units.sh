85d82fbb282a23b51c7c3f598b78c31c5ab768d8
Felix Fontein <felix@fontein.de>
2021-12-27 22:48:21 +0100

    [stable-1] Restrict redis to < 4.1.0 for ansible-base 2.10 (#3960)
    * Restrict redis to < 4.1.0 for ansible-base 2.10. (#3955)
    
    (cherry picked from commit 3f2364574d3c3980c1215da6c105f6ddaa7b5e0c)
    
    * Add missing piece.

--- tests/utils/shippable/units.sh
+++ tests/utils/shippable/units.sh
@@ -22,6 +22,10 @@ esac
 
 ansible-test env --timeout "${timeout}" --color -v
 
+if [ "$2" == "2.10" ]; then
+    sed -i -E 's/^redis($| .*)/redis < 4.1.0/g' tests/unit/requirements.txt
+fi
+
 # shellcheck disable=SC2086
 ansible-test units --color -v --docker default --python "${version}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} \
     "${options[@]:+${options[@]}}" \
