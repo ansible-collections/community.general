ansible-collections/community.general

commit 29298da31516ade7b92b9ec942c1a49ad1d4b302

Update plugins/modules/system/puppet.py
Co-authored-by: Felix Fontein <felix@fontein.de>

plugins/modules/system/puppet.py
@@ -179,7 +179,9 @@ def main():
             manifest=dict(type='str'),
             noop=dict(type='bool'),
             logdest=dict(type='str', default='stdout', choices=['all', 'stdout', 'syslog']),
-            show_diff=dict(type='bool', default=False, aliases=['show-diff']),
+            show_diff=dict(
+                type='bool', default=False, aliases=['show-diff'],
+                deprecated_aliases=[dict(name='show-diff', version='7.0.0', collection_name='community.general')])),
             facts=dict(type='dict'),
             facter_basename=dict(type='str', default='ansible'),
             environment=dict(type='str'),

commit b0f749d984c353364d3e2363bd47beed6992ebfc

Fixing syntax error introduced in 29298da3

plugins/modules/system/puppet.py
@@ -182,7 +182,7 @@ def main():
             logdest=dict(type='str', default='stdout', choices=['all', 'stdout', 'syslog']),
             show_diff=dict(
                 type='bool', default=False, aliases=['show-diff'],
-                deprecated_aliases=[dict(name='show-diff', version='7.0.0', collection_name='community.general')])),
+                deprecated_aliases=[dict(name='show-diff', version='7.0.0', collection_name='community.general')]),
             facts=dict(type='dict'),
             facter_basename=dict(type='str', default='ansible'),
             environment=dict(type='str'),