12d4cf1116fd02079b173431a1005c56cf523e18
Felix Fontein <felix@fontein.de>
2022-01-05 21:19:49 +0100

    Use vendored copy of distutils.version. (#3984)

--- plugins/module_utils/version.py
+++ plugins/module_utils/version.py
@@ -8,13 +8,10 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from ansible.module_utils.six import raise_from
 
-try:
-    from ansible.module_utils.compat.version import LooseVersion
-except ImportError:
-    try:
-        from distutils.version import LooseVersion
-    except ImportError as exc:
-        msg = 'To use this plugin or module with ansible-core < 2.11, you need to use Python < 3.12 with distutils.version present'
-        raise_from(ImportError(msg), exc)
+# Once we drop support for Ansible 2.9 and ansible-base 2.10, we can
+# remove the _version.py file, and replace the following import by
+#
+#     from ansible.module_utils.compat.version import LooseVersion
+
+from ._version import LooseVersion
