7f5ce40543a1082f798436202ec8a5667f98d7e0
Felix Fontein <felix@fontein.de>
2022-01-08 12:12:19 +0100

    Fix 2.9 unit tests (#4002)
    * Fix 2.9 unit tests.
    
    * Another try.

--- tests/utils/shippable/units.sh
+++ tests/utils/shippable/units.sh
@@ -22,6 +22,11 @@ esac
 
 ansible-test env --timeout "${timeout}" --color -v
 
+if [ "$2" == "2.9" ]; then
+    # 1.5.0+ will not install for Python 3.6+ in the 2.9 setting (due to `enum` being installed)
+    echo "pynacl >= 1.4.0, < 1.5.0; python_version >= '3.6'" >> tests/unit/requirements.txt
+fi
+
 if [ "$2" == "2.10" ]; then
     sed -i -E 's/^redis($| .*)/redis < 4.1.0/g' tests/unit/requirements.txt
     sed -i -E 's/^python-gitlab($| .*)/python-gitlab < 2.10.1 ; python_version >= '\'3.6\''/g' tests/unit/requirements.txt
