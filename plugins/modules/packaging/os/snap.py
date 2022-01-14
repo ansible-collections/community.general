5a3eb9a874d621675d68f825181d1378bfe4eb35
Marcus Rickert <marcus.rickert@web.de>
2022-01-15 00:14:45 +0100

    Re-arrange code to reduce indentation level (suggested by reviewer, continued) Raise exception if option map returned by `snap set` contains list container (suggested by reviewer) Handle Python2 type `long` correctly (suggested by reviewer)

--- plugins/modules/packaging/os/snap.py
+++ plugins/modules/packaging/os/snap.py
@@ -141,6 +141,7 @@ options_changed:
 
 import re
 import json
+import numbers
 
 from ansible.module_utils.common.text.converters import to_native
 
@@ -215,10 +216,14 @@ class Snap(CmdStateModuleHelper):
     def convert_json_subtree_to_map(self, json_subtree, prefix=None):
         option_map = {}
 
+        if not isinstance(json_subtree, dict):
+            raise ModuleHelperException("Non-dict non-leaf element encountered while parsing option map. "
+                                        "The output format of 'snap set' may have changed. Aborting!")
+
         for key, value in json_subtree.items():
             full_key = key if prefix is None else prefix + "." + key
 
-            if isinstance(value, str) or isinstance(value, int) or isinstance(value, float) or isinstance(value, bool):
+            if isinstance(value, (str, float, bool, numbers.Integral)):
                 option_map[full_key] = str(value)
 
             else:
@@ -282,17 +287,19 @@ class Snap(CmdStateModuleHelper):
             commands = [params + [{'actionable_snaps': actionable_snaps}]]
         self.vars.cmd, rc, out, err = self._run_multiple_commands(commands)
 
-        if rc != 0:
-            classic_snap_pattern = re.compile(r'^error: This revision of snap "(?P<package_name>\w+)"'
-                                              r' was published using classic confinement')
-            match = classic_snap_pattern.match(err)
-            if match:
-                err_pkg = match.group('package_name')
-                msg = "Couldn't install {name} because it requires classic confinement".format(name=err_pkg)
-            else:
-                msg = "Ooops! Snap installation failed while executing '{cmd}', please examine logs and " \
-                      "error output for more details.".format(cmd=self.vars.cmd)
-            raise ModuleHelperException(msg=msg)
+        if rc == 0:
+            return
+
+        classic_snap_pattern = re.compile(r'^error: This revision of snap "(?P<package_name>\w+)"'
+                                          r' was published using classic confinement')
+        match = classic_snap_pattern.match(err)
+        if match:
+            err_pkg = match.group('package_name')
+            msg = "Couldn't install {name} because it requires classic confinement".format(name=err_pkg)
+        else:
+            msg = "Ooops! Snap installation failed while executing '{cmd}', please examine logs and " \
+                  "error output for more details.".format(cmd=self.vars.cmd)
+        raise ModuleHelperException(msg=msg)
 
     def state_present(self):
 
