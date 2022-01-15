b9db83449cec5efb3c7c76f2766eb9b571a57899
pastral <52627592+pastral@users.noreply.github.com>
2022-01-15 13:15:09 +0100

    fix  line too long

--- plugins/module_utils/scaleway.py
+++ plugins/module_utils/scaleway.py
@@ -173,11 +173,31 @@ SCALEWAY_LOCATION = {
     'par2': {'name': 'Paris 2', 'country': 'FR', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/fr-par-2', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/fr-par-2'},
     'EMEA-FR-PAR2': {'name': 'Paris 2', 'country': 'FR', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/fr-par-2', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/fr-par-2'},
 
-    'ams1': {'name': 'Amsterdam 1', 'country': 'NL', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/nl-ams-1', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/nl-ams-1'},
-    'EMEA-NL-EVS': {'name': 'Amsterdam 1', 'country': 'NL', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/nl-ams-1', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/nl-ams-1'},
-
-    'waw1': {'name': 'Warsaw 1', 'country': 'PL', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/pl-waw-1', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/pl-waw-1'},
-    'EMEA-PL-WAW1': {'name': 'Warsaw 1', 'country': 'PL', "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/pl-waw-1', "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/pl-waw-1'},
+    'ams1': {
+        'name': 'Amsterdam 1',
+        'country': 'NL',
+        "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/nl-ams-1',
+        "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/nl-ams-1
+        },
+    'EMEA-NL-EVS': {
+        'name': 'Amsterdam 1',
+        'country': 'NL',
+        "api_endpoint":
+        'https://api.scaleway.com/instance/v1/zones/nl-ams-1',
+        "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/nl-ams-1'
+        },
+    'waw1': {
+        'name': 'Warsaw 1',
+        'country': 'PL',
+        "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/pl-waw-1',
+        "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/pl-waw-1'
+        },
+    'EMEA-PL-WAW1': {
+        'name': 'Warsaw 1',
+        'country': 'PL',
+        "api_endpoint": 'https://api.scaleway.com/instance/v1/zones/pl-waw-1',
+        "api_endpoint_vpc": 'https://api.scaleway.com/vpc/v1/zones/pl-waw-1'
+        },
 }
 
 SCALEWAY_ENDPOINT = "https://api.scaleway.com"
