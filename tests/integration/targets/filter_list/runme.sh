e2f31f4dd9bfab94be233e3c414a7e5e1f12c040
Vladimir Botka <vbotka@gmail.com>
2022-01-19 13:08:48 +0100

    Update filter lists_mergeby #4057
    * Added options 'recursive' and 'list_merge'. The functionality of the
      added options is the same as in the filter 'combine'.
    * Allow the user to do [list1, list2, ...]|lists_mergeby('index')
    * Use the function merge_hash from ansible.utils.vars

---
+++ tests/integration/targets/filter_list/runme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+export ANSIBLE_TEST_PREFER_VENV=1  # see https://github.com/ansible/ansible/pull/73000#issuecomment-757012395; can be removed once Ansible 2.9 and ansible-base 2.10 support has been dropped
+# source virtualenv.sh
+
+ANSIBLE_ROLES_PATH=../ ansible-playbook runme.yml "$@"
