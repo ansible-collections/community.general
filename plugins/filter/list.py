3bc8feb62a0f9e4794fd25250733042d207672da
Vladimir Botka <vbotka@gmail.com>
2022-01-20 06:45:27 +0100

    Add merge_hash_wrapper to test Ansible version
    * Enable Ansible 2.9 and lower versions with default options of
      lists_mergeby only.
    * Non-default options of lists_mergeby trigger error in 2.9 and lower
      versions.
    * Update messages and tests.

--- plugins/filter/list.py
+++ plugins/filter/list.py
@@ -9,30 +9,54 @@ from ansible.errors import AnsibleFilterError
 from ansible.module_utils.six import string_types
 from ansible.module_utils.common._collections_compat import Mapping, Sequence
 from ansible.utils.vars import merge_hash
+from ansible.release import __version__ as ansible_version
+
 from collections import defaultdict
 from operator import itemgetter
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
 
 
-def list_mergeby(x, y, index, recursive=True, list_merge='replace'):
+def merge_hash_wrapper(x, y, recursive=False, list_merge='replace'):
+    ''' Wrapper of the function merge_hash from ansible.utils.vars. Only 2 paramaters are allowed
+        for Ansible 2.9 and lower.'''
+
+    vspec = SpecifierSet(">=2.10")
+
+    if Version(ansible_version) not in vspec:
+        if list_merge != 'replace' or recursive:
+            msg = ("Non default options of list_merge(default=replace) or recursive(default=False) "
+                   "are not allowed in Ansible version 2.9 or lower. Ansible version is %s, "
+                   "recursive=%s, and list_merge=%s.")
+            raise AnsibleFilterError(msg % (ansible_version, recursive, list_merge))
+        else:
+            return merge_hash(x, y)
+    else:
+        return merge_hash(x, y, recursive, list_merge)
+
+
+def list_mergeby(x, y, index, recursive=False, list_merge='replace'):
     ''' Merge 2 lists by attribute 'index'. The function merge_hash from ansible.utils.vars is used.
-        This function is used by the function lists_mergeby. '''
+        This function is used by the function lists_mergeby.
+    '''
 
     d = defaultdict(dict)
     for l in (x, y):
         for elem in l:
             if not isinstance(elem, Mapping):
-                raise AnsibleFilterError('Elements of list arguments for lists_mergeby must be dictionaries. Found {0!r}.'.format(elem))
+                msg = "Elements of list arguments for lists_mergeby must be dictionaries. %s is %s"
+                raise AnsibleFilterError(msg % (elem, type(elem)))
             if index in elem.keys():
-                d[elem[index]].update(merge_hash(d[elem[index]], elem, recursive, list_merge))
+                d[elem[index]].update(merge_hash_wrapper(d[elem[index]], elem, recursive, list_merge))
     return sorted(d.values(), key=itemgetter(index))
 
 
 def lists_mergeby(*terms, **kwargs):
-    '''Merge 2 or more lists by attribute 'index'. Optional parameters 'recursive' and 'list_merge'
-       control the merging of the lists in values. The function merge_hash from ansible.utils.vars
-       is used. To learn details on how to use the parameters 'recursive' and 'list_merge' see Ansible
-       User's Guide chapter "Using filters to manipulate data" section "Combining
-       hashes/dictionaries".
+    ''' Merge 2 or more lists by attribute 'index'. Optional parameters 'recursive' and 'list_merge'
+        control the merging of the lists in values. The function merge_hash from ansible.utils.vars
+        is used. To learn details on how to use the parameters 'recursive' and 'list_merge' see
+        Ansible User's Guide chapter "Using filters to manipulate data" section "Combining
+        hashes/dictionaries".
 
         Example:
         - debug:
@@ -47,7 +71,7 @@ def lists_mergeby(*terms, **kwargs):
     recursive = kwargs.pop('recursive', False)
     list_merge = kwargs.pop('list_merge', 'replace')
     if kwargs:
-        raise AnsibleFilterError("'recursive' and 'list_merge' are the only valid keyword argument.")
+        raise AnsibleFilterError("'recursive' and 'list_merge' are the only valid keyword arguments.")
     if len(terms) < 2:
         raise AnsibleFilterError("At least one list and index are needed.")
 
@@ -55,7 +79,9 @@ def lists_mergeby(*terms, **kwargs):
     flat_list = []
     for sublist in terms[:-1]:
         if not isinstance(sublist, Sequence):
-            raise AnsibleFilterError('All arguments before the argument index for community.general.lists_mergeby must be lists.')
+            msg = ("All arguments before the argument index for community.general.lists_mergeby "
+                   "must be lists.")
+            raise AnsibleFilterError(msg)
         if len(sublist) > 0:
             if all(isinstance(l, Sequence) for l in sublist):
                 for item in sublist:
@@ -73,7 +99,9 @@ def lists_mergeby(*terms, **kwargs):
     index = terms[-1]
 
     if not isinstance(index, string_types):
-        raise AnsibleFilterError('First argument after the lists for community.general.lists_mergeby must be string. %s is %s' % (index, type(index)))
+        msg = ("First argument after the lists for community.general.lists_mergeby must be string. "
+               "%s is %s")
+        raise AnsibleFilterError(msg % (index, type(index)))
 
     high_to_low_prio_list_iterator = reversed(lists)
     result = next(high_to_low_prio_list_iterator)
