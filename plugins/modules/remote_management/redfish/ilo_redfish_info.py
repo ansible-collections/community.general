1ede7942e610be3a2de90b2a7a4d1f5a4291dbe2
Felix Fontein <felix@fontein.de>
2022-02-18 23:14:51 +0100

    Fix some more instances of ansible.module_utils._text. (#4232)
    (cherry picked from commit a262a30122635413fea7aca828f358c22717b133)

--- plugins/modules/remote_management/redfish/ilo_redfish_info.py
+++ plugins/modules/remote_management/redfish/ilo_redfish_info.py
@@ -105,7 +105,7 @@ CATEGORY_COMMANDS_DEFAULT = {
 }
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_native
+from ansible.module_utils.common.text.converters import to_native
 from ansible_collections.community.general.plugins.module_utils.ilo_redfish_utils import iLORedfishUtils
 
 
