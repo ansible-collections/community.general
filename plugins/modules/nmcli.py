6d3ad8a62a0c460200fbb44a7324da24dd067a8c
Matteo Caruso <m.caruso425@gmail.com>
2022-03-03 00:45:04 +0100

    Added version added
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/nmcli.py
+++ plugins/modules/nmcli.py
@@ -196,6 +196,7 @@ options:
             - A list of DNS options.
         elements: str
         type: list
+        version_added: 4.6.0
     dns4_ignore_auto:
         description:
             - Ignore automatically configured IPv4 name servers.
