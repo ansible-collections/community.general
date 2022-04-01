26c0e1e09920ddf70c8fdb10886e5a5976abcc6b
Alexei Znamensky <russoz@gmail.com>
2022-04-02 11:51:41 +1300

    added license to sleeper.c file

--- tests/integration/targets/pids/files/sleeper.c
+++ tests/integration/targets/pids/files/sleeper.c
@@ -1,3 +1,8 @@
+/*
+ * (c) 2022, Alexei Znamensky <russoz@gmail.com>
+ * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 
