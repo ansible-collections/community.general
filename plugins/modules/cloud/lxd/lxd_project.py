c62e51973650ac00e986f2d8405386b9049ba1d7
we10710aa <xrayjemmy@gmail.com>
2022-04-22 00:40:53 +0800

    use os.path.expanduser

--- plugins/modules/cloud/lxd/lxd_project.py
+++ plugins/modules/cloud/lxd/lxd_project.py
@@ -210,12 +210,10 @@ class LXDProjectManagement(object):
 
         self.key_file = self.module.params.get('client_key')
         if self.key_file is None:
-            self.key_file = '{0}/.config/lxc/client.key'.format(
-                os.environ['HOME'])
+            self.key_file = os.path.expanduser('~/.config/lxc/client.key')
         self.cert_file = self.module.params.get('client_cert')
         if self.cert_file is None:
-            self.cert_file = '{0}/.config/lxc/client.crt'.format(
-                os.environ['HOME'])
+            self.cert_file = os.path.expanduser('~/.config/lxc/client.crt')
         self.debug = self.module._verbosity >= 4
 
         try:
