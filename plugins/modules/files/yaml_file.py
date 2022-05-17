42ba700576baf912ea843ccd0b530ac2050451e3
Indelog <maxime@indelog.fr>
2022-05-17 19:27:30 +0200

    Add module to add, remove or update values in a yaml file.

---
+++ plugins/modules/files/yaml_file.py
@@ -0,0 +1,544 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, DEMAREST Maxime <maxime@indelog.fr>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import yaml
+import os
+
+from ansible_collections.community.general.plugins.module_utils.common.validation import check_type_dict_or_list
+from ansible_collections.community.general.plugins.module_utils.mh.module_helper_dest_file import (
+    DestFileModuleHelper,
+    ModuleHelperException,
+)
+from ansible_collections.community.general.plugins.module_utils.data_merge_utils import (
+    DataMergeUtils,
+    MergeType,
+    ListDiffType,
+)
+
+
+DOCUMENTATION = r'''
+---
+module: yaml_file
+short_description: Managage (add, remove, update) elements in a yaml file.
+description:
+  - This module ensures that a set of values is present or absent in a yaml
+    file.
+  - The check can be done in all childrens of an object.
+  - It also makes sure a value is present or absent in a list or in a
+    specific index of the list.
+  - In background, this module use the python yaml lib
+    U(https://pyyaml.org/wiki/PyYAMLDocumentation).
+notes:
+  - Take note of the following warning before using this module, B(if you have
+    some comments in your yaml file, this module will not preserve them !) So
+    don't use it if it's important for you to keep these comments.
+  - This module supports C(--check) and C(--diff) flags.
+options:
+  path:
+    description:
+      - Absolute path of the yaml file.
+    type: path
+    required: true
+    alias: [ dest ]
+  state:
+    description:
+      - If set to C(present), that ensure the values are present in the yaml
+        file.
+      - If set to C(absent), that ensure the values are absent in the yaml
+        file.
+      - If set to C(identic), that ensure the values in the yaml are identic to
+        those provided in arguments.
+    type: str
+    choices: [ present, absent, identic ]
+    default: present
+  list_diff_type:
+    description:
+      - This describes how the list are compared.
+      - If set to C(value), it checks if a value is present in the list
+        elements.  With this, you can't check a list element in another list
+        element.  Only the first level of a list can be checked.
+      - If set to C(index), the value in a list are compared by their index.
+        With this, you can check an element in another list, but you make sure
+        that the expected value is at this position in the list.
+    type: str
+    choices: [ value, index ]
+    default: value
+  value:
+    description:
+      - The values that you want to compare with the one present in the yaml
+        file.
+    type: dict
+    required: true
+  create:
+    description:
+      - If set to C(no), the module will fail if the yaml file not exists.
+        Else, the yaml file will be created.
+    type: bool
+    default: false
+  diff_on_value:
+    description:
+      - This describes how check the if it has a difference between the datas
+        present in the yaml file and the expected values. This will also
+        impact the output in diff mode (in a case you get indents or lines
+        breaks in the diff will in the other you only gets values).
+      - If set to C(true), it checks the difference by only comparing the
+        values of elements in the yaml file and in the expected datas. In this
+        case, change indentation or sort keys will not be considered as a change
+        on the file will the values are the sames (just like any other
+        difference in yaml file unrelated to values of elements like line breaks
+        or comments).
+      - If set to C(false), it checks the difference by comparing the lines
+        output with the actual lines in the yaml file. In this case, all the
+        change on the indentation or on the sort keys will be considered as a
+        change on the file even if it has no difference on the values (if a
+        comment, or an empty line break is present in the file, a change will
+        be detected).
+    type: bool
+    default: true
+  backup:
+    description:
+      - Create a backup file including the timestamp information so you can get
+        the original file back if you somehow clobbered it incorrectly.
+    type: bool
+    default: no
+  indent:
+    description:
+      - Set the indentation for elements in the yaml file.
+    type: int
+    default: 4
+  sort_keys:
+    description:
+      - If set to C(true), the values in the will sorted by their keys.
+    type: bool
+    default: false
+  explicit_start:
+    description:
+      - If C(true) then add C(---) at the document begin.
+    type: bool
+    default: true
+  explicit_end:
+    description:
+      - If C(true) then add C(...) at the document begin.
+    type: bool
+    default: false
+  default_flow_style:
+    description:
+      - If C(true) then the yaml will be in flow inline form.
+      - If C(false) then the yaml will be in indented block form.
+      - This option is mutually exclusive with C(canonical).
+    type: bool
+    default: false
+  canonical:
+    description:
+      - If C(true) then the yaml will be in canonical form.
+      - This option is mutually exclusive with C(default_flow_style).
+    type: bool
+    default: false
+  width:
+    description:
+      - Set the maximum line width.
+      - C(0) mean there is not max line width.
+    type: int
+    default: 0
+requirements:
+  - python3-yaml
+author:
+    - DEMAREST Maxime (Indelog) <maxime@indelog.fr>
+'''
+
+EXAMPLES = r'''
+# We consider the following content in /my/dest/file.yaml on the remote host :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+# The values are kept in all samples.
+
+- name: "Ensure values are present in the yaml."
+  namespace.collection.module
+    community.general.yaml_file:
+      path: "/my/dest/file.yaml"
+      value:
+        B: 3
+        C:
+         F: 8
+      state: "present"
+# {'B': 3} and {'C':{'F': 8}} are not present in the original values, they will
+# be added.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+---
+# A: 1
+# B: 3
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+#   F: 8
+
+- name: "Ensure values are absent in the yaml file files."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      B: 3
+      C:
+       F: 8
+    state: "absent"
+# {'B': 3} and {'C':{'F': 8}} are not present in the original values, so there
+# is no change.
+# There is no change.
+# Get the following content in /my/dest/file.yaml :
+---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+- name: "Ensure values are absent in the yaml file files."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      B: 2
+      C:
+       D: 3
+    state: "absent"
+# {'B': 2} and {'C':{'D': 3}} are present in the original values, so it will
+# be removed.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# C:
+#   E:
+#   - 4
+#   - 5
+#   - 6
+- name: "Ensure values are identic in the yaml file files."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      B: 4
+      C:
+       D: 6
+      Z: 8
+    state: "identic"
+# The values in the file will be set to identical that expected values.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# B: 4
+# C:
+#   D: 6
+# Z: 8
+
+- name: "Ensure values are present in a list."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      C:
+       E:
+       - 5
+    state: "present"
+# In the original values, 5 is already present in C.D.
+# There is no change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+
+- name: "Ensure values are present in a list."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      C:
+       E:
+        - 7
+    state: "present"
+# In the original values, 7 is not present in C.D, it will be added.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+#   - 7
+
+- name: "Ensure values are absent in a list."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    value:
+      C:
+       E:
+        - 5
+    state: "absent"
+# In the orginal values, 5 is present in C.D, it will be removed.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 6
+
+- name: "Ensure values are present depending their index in a list."
+  community.general.yaml_file:
+    path: "/my/dest/file.yaml"
+    list_diff_type: index
+    value:
+      C:
+       E:
+        -
+        - 7
+    state: "present"
+# In the original value, the second index for C.E list is not 7, values will
+# be updated.
+# Note, if you do not want to change the value in on some check you can set it
+# to none like the first index of C.E in the previous sample.
+# There is no change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 7
+#   - 6
+
+- name: "Ensure values are absent depending their index in a list."
+  namespace.collection.module
+    community.general.yaml_file:
+      path: "/my/dest/file.yaml"
+      list_diff_type: index
+      value:
+        C:
+         E:
+          -
+          - 7
+          - 6
+      state: "absent"
+# In the original datas, the second value of C.E list is not 7 so it will be
+# keept, but the third value is 6, so it will be removed.
+# Note, if you do not want to change the value in on some check you can set it
+# to none like the first index of C.E in the previous sample.
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+
+- name: "Ensure values are present in the yaml diff, check only on values."
+  namespace.collection.module
+    community.general.yaml_file:
+      path: "/my/dest/file.yaml"
+      indent: 4
+      value:
+        A: 1
+      state: "present"
+# We will change the indent but when the change are only detected on the values
+# the yaml file will not be update ("A": 1 is already in the originals values).
+# There is no change.
+# Get the following content in /my/dest/file.json :
+# ---
+# A: 1
+# B: 2
+# C:
+#   D: 3
+#   E:
+#   - 4
+#   - 5
+#   - 6
+
+- name: "Ensure values are present in the yaml diff, also check file format."
+  namespace.collection.module
+    community.general.yaml_file:
+      path: "/my/dest/file.yaml"
+      indent: 2
+      diff_on_value: false
+      value:
+        A: 1
+      state: "present"
+# There is not diff in the values but the file format is changed (intent 4
+to 2).
+# There is a change.
+# Get the following content in /my/dest/file.yaml :
+# ---
+# A: 1
+# B: 2
+# C:
+#     D: 3
+#     E:
+#     - 4
+#     - 5
+#     - 6
+'''
+
+RETURN = r'''
+  changed:
+    description:
+      - Indicate whether the yaml file is changed or not.
+    returned: always
+    type: bool
+  created:
+    description:
+      - Indicate if the file yaml is created or not.
+    returned: success
+    type: bool
+  diff:
+    description:
+      - Contain a dict with two elements, C(after) contain the value after
+        the change and C(before) contain values before the change.
+    returned: changed
+    type: dict
+  failed:
+    description:
+      - Indicate if the module has failed or not.
+    returned: always
+    type: bool
+  result:
+    description:
+      - Contain the values that are finally present in the yaml file.
+    returned: success
+    type: dict
+'''
+
+
+class YamlFile(DestFileModuleHelper):
+
+    module = dict(
+        argument_spec=dict(
+            path=dict(type='path', required=True, aliases=['dest']),
+            state=dict(
+                type='str',
+                default='present',
+                choices=[
+                        'present',
+                        'absent',
+                        'identic',
+                ]
+            ),
+            list_diff_type=dict(
+                type='str',
+                default='value',
+                choices=[
+                        'value',
+                        'index',
+                ],
+            ),
+            value=dict(type=check_type_dict_or_list, required=True),
+            create=dict(type='bool', defaut=False),
+            backup=dict(type='bool', default=False),
+            diff_on_value=dict(type='bool', default=True),
+            indent=dict(type='int', default=2),
+            explicit_start=dict(type='bool', default=True),
+            explicit_end=dict(type='bool', default=False),
+            sort_keys=dict(type='bool', default=False),
+            default_flow_style=dict(type='bool', default=False),
+            canonical=dict(type='bool', default=False),
+            width=dict(type='int', default=0),
+        ),
+        mutually_exclusive=[
+            ['canonical', 'default_flow_style'],
+        ],
+        supports_check_mode=True,
+    )
+
+    def __load_result_data__(self) -> dict:
+        content = []
+        try:
+            with open(self.vars.path, 'r') as file:
+                content = file.readlines()
+        except FileNotFoundError:
+            pass
+        except yaml.scanner.ScannerError as ex:
+            raise ModuleHelperException(
+                msg=f'Failed to decode yaml in {self.vars["path"]}',
+                exception=ex)
+        if ''.join(content).strip() == '':
+            content = ['{}']
+        if self.vars['diff_on_value']:
+            self.vars.set(self.var_result_data,
+                          yaml.safe_load(''.join(content)), diff=True)
+        else:
+            self.vars.set(self.var_result_data, content, diff=True)
+
+    def __run__(self) -> None:
+        merge_util = DataMergeUtils(MergeType(self.vars.state),
+                                    ListDiffType(self.vars.list_diff_type))
+        self._set_result(merge_util.get_new_merged_data(
+            self._get_current(), self.vars.value))
+
+    def _get_current(self) -> None:
+        if self.vars['diff_on_value']:
+            return self.vars[self.var_result_data]
+        else:
+            return yaml.safe_load(''.join(self.vars[self.var_result_data]))
+
+    def _set_result(self, result: dict) -> None:
+        if self.vars['diff_on_value']:
+            self.vars.set(self.var_result_data, result)
+        else:
+            self.vars.set(self.var_result_data,
+                          self._yaml_dumps(result).splitlines(keepends=True))
+
+    @DestFileModuleHelper.write_tempfile    # provide kwargs['fd']
+    def __write_temp__(self, *args, **kwargs) -> None:
+        if self.vars['diff_on_value']:
+            yaml_string = self._yaml_dumps(self.vars[self.var_result_data])
+        else:
+            yaml_string = ''.join(self.vars[self.var_result_data])
+        os.write(kwargs['fd'], bytes(yaml_string, 'utf-8'))
+
+    def _yaml_dumps(self, yaml_str: str) -> dict:
+        return yaml.safe_dump(
+            yaml_str,
+            explicit_start=self.vars['explicit_start'],
+            explicit_end=self.vars['explicit_end'],
+            sort_keys=self.vars['sort_keys'],
+            default_flow_style=self.vars['default_flow_style'],
+            canonical=self.vars['canonical'],
+            indent=self.vars['indent'],
+            width=self.vars['width'])
+
+
+def main() -> None: YamlFile().execute()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/unit/plugins/modules/files/test_yaml_file.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, DEMAREST Maxime <maxime@indelog.fr>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+import json
+import yaml
+
+from unittest.mock import mock_open
+
+from ansible_collections.community.general.plugins.modules.files\
+    .yaml_file import YamlFile
+from ansible_collections.community.general.tests.unit.plugins.module_utils\
+    .test_data_merge_utils import (
+        DICT_CURRENT,
+        DICT_MODIF,
+        DICT_EXPECTED_PRESENT,
+        DICT_EXPECTED_ABSENT)
+
+DEFAULT_MODULE_ARGS = {
+    key: val.get('default')
+    for key, val in YamlFile.module['argument_spec'].items()
+    if val.get('default') is not None}
+
+FAKE_PATH = '/path/to/fake/file.yaml'
+YAML_CURRENT = yaml.safe_dump(
+    DICT_CURRENT,
+    indent=DEFAULT_MODULE_ARGS['indent'],
+    explicit_start=DEFAULT_MODULE_ARGS['explicit_start'],
+    explicit_end=DEFAULT_MODULE_ARGS['explicit_end'],
+    sort_keys=DEFAULT_MODULE_ARGS['sort_keys'],
+    default_flow_style=DEFAULT_MODULE_ARGS['default_flow_style'],
+    canonical=DEFAULT_MODULE_ARGS['canonical'],
+    width=DEFAULT_MODULE_ARGS['width'],
+)
+
+
+TEST_CASE_EXEC = [
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT},
+        {
+            'id': 'test_dest_not_changed',
+            'expected_value': DICT_CURRENT,
+            'read_data': YAML_CURRENT,
+            'expected_changed': False,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_MODIF, 'state': 'present'},
+        {
+            'id': 'test_changed_present',
+            'expected_value': DICT_EXPECTED_PRESENT,
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_MODIF, 'state': 'absent'},
+        {
+            'id': 'test_changed_absent',
+            'expected_value': DICT_EXPECTED_ABSENT,
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_MODIF, 'state': 'identic'},
+        {
+            'id': 'test_changed_identic',
+            'expected_value': DICT_MODIF,
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT,
+            'indent': 8, 'diff_on_value': False},
+        {
+            'id': 'test_reformat_on_indent',
+            'expected_value': yaml.safe_dump(
+                DICT_CURRENT,
+                indent=8,
+                explicit_start=DEFAULT_MODULE_ARGS['explicit_start'],
+                explicit_end=DEFAULT_MODULE_ARGS['explicit_end'],
+                sort_keys=DEFAULT_MODULE_ARGS['sort_keys'],
+                default_flow_style=DEFAULT_MODULE_ARGS['default_flow_style'],
+                canonical=DEFAULT_MODULE_ARGS['canonical'],
+                width=DEFAULT_MODULE_ARGS['width'],
+            ).splitlines(keepends=True),
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT,
+            'sort_keys': True, 'diff_on_value': False},
+        {
+            'id': 'test_reformat_on_sort_keys',
+            'expected_value': yaml.safe_dump(
+                DICT_CURRENT,
+                indent=DEFAULT_MODULE_ARGS['sort_keys'],
+                explicit_start=DEFAULT_MODULE_ARGS['explicit_start'],
+                explicit_end=DEFAULT_MODULE_ARGS['explicit_end'],
+                sort_keys=True,
+                default_flow_style=DEFAULT_MODULE_ARGS['default_flow_style'],
+                canonical=DEFAULT_MODULE_ARGS['canonical'],
+                width=DEFAULT_MODULE_ARGS['width'],
+            ).splitlines(keepends=True),
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT,
+            'explicit_end': True, 'diff_on_value': False},
+        {
+            'id': 'test_reformat_on_explicit_end',
+            'expected_value': yaml.safe_dump(
+                DICT_CURRENT,
+                indent=DEFAULT_MODULE_ARGS['sort_keys'],
+                explicit_start=DEFAULT_MODULE_ARGS['explicit_start'],
+                explicit_end=True,
+                sort_keys=DEFAULT_MODULE_ARGS['sort_keys'],
+                default_flow_style=DEFAULT_MODULE_ARGS['default_flow_style'],
+                canonical=DEFAULT_MODULE_ARGS['canonical'],
+                width=DEFAULT_MODULE_ARGS['width'],
+            ).splitlines(keepends=True),
+            'read_data': YAML_CURRENT,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT, 'diff_on_value': True},
+        {
+            'id': 'empty_file_diff_on_value',
+            'expected_value': DICT_CURRENT,
+            'read_data': None,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT, 'diff_on_value': False},
+        {
+            'id': 'empty_file_no_diff_on_value',
+            'expected_value': YAML_CURRENT.splitlines(keepends=True),
+            'read_data': None,
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT, 'diff_on_value': True},
+        {
+            'id': 'blank_file_diff_on_value',
+            'expected_value': DICT_CURRENT,
+            'read_data': '\n\t\n   ',
+            'expected_changed': True,
+        },
+    ],
+    [
+        {'path': FAKE_PATH, 'value': DICT_CURRENT, 'diff_on_value': False},
+        {
+            'id': 'blank_file_no_diff_on_value',
+            'expected_value': YAML_CURRENT.splitlines(keepends=True),
+            'read_data': '\n\t\n   ',
+            'expected_changed': True,
+        },
+    ],
+]
+TEST_CASES_EXEC_IDS = [item[1]['id'] for item in TEST_CASE_EXEC]
+
+
+@pytest.mark.parametrize('patch_ansible_module, testcase',
+                         TEST_CASE_EXEC,
+                         ids=TEST_CASES_EXEC_IDS,
+                         indirect=['patch_ansible_module'])
+@pytest.mark.usefixtures('patch_ansible_module')
+def test_execute(testcase, capfd, mocker):
+    mocker.patch('ansible_collections.community.general.plugins.module_utils' +
+                 '.mh.module_helper_dest_file.dest_file_sanity_check',
+                 return_value=False)
+    mocker.patch('builtins.open', mock_open(read_data=testcase['read_data']))
+    mocker.patch('os.write')
+    mocker.patch('os.close')
+    mocker.patch('tempfile.mkstemp', return_value=(1234, '/fake/temp/file'))
+    mocker.patch.multiple(YamlFile,
+                          __make_backup__=mocker.DEFAULT,
+                          __move_temp__=mocker.DEFAULT)
+    with pytest.raises(SystemExit):
+        YamlFile().execute()
+    result = json.loads(capfd.readouterr().out)
+    assert(result['failed'] is False)
+    assert(result['changed'] == testcase['expected_changed'])
+    assert(result['result'] == testcase['expected_value'])
