b2ba2cfedfdeee5e7810ffcb14a7b09c3c28a1e5
Indelog <maxime@indelog.fr>
2022-05-17 15:34:44 +0200

    Add module helper for module with destination file
    Add a new `DestFileModuleHelper` class which extend `ModuleHelper` and
    that which simplify the creation or maintenance of modules that
    expect to modify a destination file on the remote host by wrapping some
    redundant tasks.

---
+++ plugins/module_utils/mh/module_helper_dest_file.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# (c) 2022, DEMAREST Maxime <maxime@indelog.fr>
+# Copyright: (c) 2020, Ansible Project
+# Simplified BSD License (see licenses/simplified_bsd.txt or
+# https://opensource.org/licenses/BSD-2-Clause)
+"""
+Toolbox that help to do somme basics on a destination file for module that need
+it.
+"""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+import os
+import tempfile
+
+from abc import abstractmethod
+from functools import wraps
+from typing import Callable, Union
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.community.general.plugins.module_utils.mh.module_helper import ModuleHelper
+from ansible_collections.community.general.plugins.module_utils.mh.exceptions import ModuleHelperException
+from ansible_collections.community.general.plugins.module_utils.mh.deco import(
+    cause_changes,
+    check_mode_skip)
+
+
+class DestNotExists(ModuleHelperException):
+    pass
+
+
+class ParentNotWriteable(ModuleHelperException):
+    pass
+
+
+class DestNotReadable(ModuleHelperException):
+    pass
+
+
+class DestNotRegularFile(ModuleHelperException):
+    pass
+
+
+class CantCreateBackup(ModuleHelperException):
+    pass
+
+
+def check_if_dest_exists(dest: str) -> bool:
+    try:
+        os.stat(dest)
+    except FileNotFoundError:
+        return False
+    except PermissionError:
+        pass
+    return True
+
+
+def check_if_parent_is_writable(dest: str) -> bool:
+    if os.access(os.path.dirname(dest), os.W_OK):
+        return True
+    return False
+
+
+def check_if_dest_is_readable(dest: str) -> bool:
+    if os.access(dest, os.R_OK):
+        return True
+    return False
+
+
+def check_if_dest_is_regular_file(dest: str) -> bool:
+    if os.path.isfile(dest):
+        return True
+    return False
+
+
+def dest_file_sanity_check(dest: str,
+                           create: bool = False,
+                           backup: bool = False) -> bool:
+    """
+    Ensure that the destination exist and is readable or if not exists and
+    'create' is True, ensure that the parent of the destination is writable. If
+    'backup' is True, also ensure that the backup file can be created.
+
+    Return `True` if the file will be created else `False`.
+    """
+    exists = check_if_dest_exists(dest)
+    writable_parent = check_if_parent_is_writable(dest)
+    if not create and not exists:
+        raise DestNotExists(msg=f"{dest} does not not exist !")
+    elif create and not exists and not writable_parent:
+        raise ParentNotWriteable(msg=f"Can't create {dest}, " +
+                                 "parent directory is not writable !")
+    elif exists and not check_if_dest_is_readable(dest):
+        raise DestNotReadable(msg=f"Destination {dest} is not readable !")
+    elif exists and not check_if_dest_is_regular_file(dest):
+        raise DestNotRegularFile(
+            msg=f"Destination {dest} is not a regular file !")
+    elif exists and backup and not writable_parent:
+        raise CantCreateBackup(msg=f"Can't create backup file for {dest} " +
+                               "because parrent dir is not writable !")
+    return (not exists and create)
+
+
+class DestFileModuleHelper(ModuleHelper):
+
+    def __init__(self,
+                 module: Union[dict, AnsibleModule, None] = None,
+                 var_dest_file: str = 'path',
+                 var_result_data: str = 'result') -> None:
+        self._tmpfile = None
+        self._created = False
+        self.var_dest_file = var_dest_file
+        self.var_result_data = var_result_data
+        super().__init__(module)
+
+    def __init_module__(self) -> None:
+        self._created = dest_file_sanity_check(
+            self.vars[self.var_dest_file],
+            self.vars['create'],
+            self.vars['backup'])
+        self.__load_result_data__()
+
+    @abstractmethod
+    def __load_result_data__(self):
+        """
+        Implement it in the module class to describe how read data in the
+        destination file and how save the result in `self.vars`.
+        """
+        pass
+
+    def __quit_module__(self) -> None:
+        if self.has_changed():
+            self.__update_dest_file__()
+        self.vars.set('created', self._created)
+
+    @cause_changes(on_success=True)
+    @check_mode_skip
+    def __update_dest_file__(self) -> None:
+        self.__write_temp__()
+        self.__make_backup__()
+        self.__move_temp__()
+
+    @abstractmethod
+    def __write_temp__(self, *args, **kwargs) -> None:
+        """
+        Implement it in the module to describe how the destination must be
+        written in the tempfile.
+
+        This function must set `self.__tmpfile` with the created tempfile.
+
+        If cat you use the wrapper `DestFileModuleHelper.write_tempfile`
+        decorator you car automaticall create the temp file(with the file
+        descriptor set in `kwargs['fd']) and set self.__tempfile.
+
+        Example:
+            class MyModule(DestFileModuleHelper)
+                ...
+
+                @DestFileModuleHelper.write_tempfile decorator
+                def __write_temp(self, *args, **kwargs):
+                    temp_file_descriptor = kwargs['fd']
+                    ...
+        """
+        pass
+
+    def __make_backup__(self) -> None:
+        if self.vars.get('backup') and not self._created:
+            self.vars.set(
+                'backup_file',
+                self.module.backup_local(self.vars.get(self.var_dest_file)))
+
+    def __move_temp__(self) -> None:
+        try:
+            self.module.atomic_move(self._tmpfile, self.vars['path'])
+        except Exception as ex:
+            if self.vars.get('backup_file'):
+                self.module.add_cleanup_file(self.vars.backup_file)
+            raise ex
+
+    @staticmethod
+    def write_tempfile(func: Callable) -> Callable:
+        """
+        Decorator to create tempfile to write destination content and set
+        `self.__tempfile`.
+
+        Return Callable with file descriptor in `kwargs['fd']`.
+        """
+        @wraps(func)
+        def wrapped(self, *args, **kwargs):
+            kwargs['fd'], self._tmpfile = tempfile.mkstemp(
+                dir=self.module.tmpdir)
+            self.module.add_cleanup_file(self._tmpfile)
+            res = func(self, *args, **kwargs)
+            os.close(kwargs['fd'])
+            return res
+        return wrapped
---
+++ tests/unit/plugins/module_utils/test_dest_file_module_helper.py
@@ -0,0 +1,433 @@
+# -*- coding: utf-8 -*-
+# (c) 2022, DEMAREST Maxime <maxime@indelog.fr>
+# Copyright: (c) 2020, Ansible Project
+# Simplified BSD License (see licenses/simplified_bsd.txt or
+# https://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+import os
+import json
+
+from ansible_collections.community.general.tests.unit.plugins.modules\
+    .conftest import patch_ansible_module
+from ansible_collections.community.general.plugins.module_utils.mh\
+    .exceptions import ModuleHelperException
+
+from ansible_collections.community.general.plugins.module_utils.mh.\
+    module_helper_dest_file import (
+        dest_file_sanity_check,
+        check_if_dest_exists,
+        check_if_parent_is_writable,
+        check_if_dest_is_readable,
+        check_if_dest_is_regular_file,
+        DestNotExists,
+        DestNotReadable,
+        DestNotRegularFile,
+        ParentNotWriteable,
+        CantCreateBackup,
+        DestFileModuleHelper)
+
+
+MODULE_PATH = 'ansible_collections.community.general.plugins.module_utils.' +\
+              'mh.module_helper_dest_file.{}'
+
+FAKE_DEST = '/this/is/a/fake/dest'
+FAKE_TEMP_FILE = '/fake/temp/file'
+FAKE_DEST_BACKUP = FAKE_DEST + '.bkp'
+FAKE_DATA_CURRENT = 'fake data'
+FAKE_DATA_CHANGED = 'fake data changed'
+
+
+FILE_SANITY_CHECK_TEST_CASE = {
+    'dest_not_exists_no_create': {
+        'create': False,
+        'backup': False,
+        'exists': False,
+        'writable_parent': False,
+        'readable': False,
+        'regular_file': False,
+        'expect_raise': DestNotExists,
+    },
+    'dest_not_exists_create_parent_not_writable': {
+        'create': True,
+        'backup': False,
+        'exists': False,
+        'writable_parent': False,
+        'readable': False,
+        'regular_file': False,
+        'expect_raise': ParentNotWriteable,
+    },
+    'dest_not_exists_create_parent_writable': {
+        'create': True,
+        'backup': False,
+        'exists': False,
+        'writable_parent': True,
+        'readable': False,
+        'regular_file': False,
+        'expect_raise': None,
+    },
+    'dest_exists_not_readable': {
+        'create': False,
+        'backup': False,
+        'exists': True,
+        'writable_parent': False,
+        'readable': False,
+        'regular_file': False,
+        'expect_raise': DestNotReadable,
+    },
+    'dest_exists_readable_not_regular_file': {
+        'create': False,
+        'backup': False,
+        'exists': True,
+        'writable_parent': False,
+        'readable': True,
+        'regular_file': False,
+        'expect_raise': DestNotRegularFile,
+    },
+    'dest_not_exists_create_backup': {
+        'create': True,
+        'backup': True,
+        'exists': False,
+        'writable_parent': True,
+        'readable': False,
+        'regular_file': False,
+        'expect_raise': None,
+    },
+    'dest_exists_backup_parent_not_writable': {
+        'create': False,
+        'backup': True,
+        'exists': True,
+        'writable_parent': False,
+        'readable': True,
+        'regular_file': True,
+        'expect_raise': CantCreateBackup,
+    },
+    'dest_exists_backup_parent_writable': {
+        'create': False,
+        'backup': True,
+        'exists': True,
+        'writable_parent': True,
+        'readable': True,
+        'regular_file': True,
+        'expect_raise': None,
+    },
+}
+
+
+class TestDestFileSanityCheck():
+
+    @ pytest.mark.parametrize(
+        'exists,os_stat_side_effect', (
+            (False, FileNotFoundError),
+            (True, PermissionError),
+            (True, None)),
+        ids=['dest_not_exists',
+             'dest_not_readable',
+             'dest_exists_and_readable'])
+    def test_check_if_dest_exist(self,
+                                 exists, os_stat_side_effect,
+                                 mocker):
+        mocker.patch('os.stat', return_value=exists,
+                     side_effect=os_stat_side_effect)
+        assert(check_if_dest_exists(FAKE_DEST) == exists)
+        os.stat.assert_called_once_with(FAKE_DEST)
+
+    @ pytest.mark.parametrize('expected', ((True), (False)))
+    def test_if_parent_not_writeable(self, expected, mocker):
+        mocker.patch('os.access', return_value=expected)
+        assert(check_if_parent_is_writable(FAKE_DEST) == expected)
+        os.access.assert_called_once_with(os.path.dirname(FAKE_DEST), os.W_OK)
+
+    @ pytest.mark.parametrize('expected', ((True), (False)))
+    def test_check_if_dest_is_readable(self, expected, mocker):
+        mocker.patch('os.access', return_value=expected)
+        assert(check_if_dest_is_readable(FAKE_DEST) == expected)
+        os.access.assert_called_once_with(FAKE_DEST, os.R_OK)
+
+    @ pytest.mark.parametrize('expected', ((True), (False)))
+    def test_check_if_dest_is_regular_file(self, expected, mocker):
+        mocker.patch('os.path.isfile', return_value=expected)
+        assert(check_if_dest_is_regular_file(FAKE_DEST) == expected)
+        os.path.isfile.assert_called_once_with(FAKE_DEST)
+
+    @ pytest.mark.parametrize(
+        'create, backup, exists, writable_parent,' +
+        'readable, regular_file, expect_raise',
+        (elem.values() for elem in FILE_SANITY_CHECK_TEST_CASE.values()),
+        ids=(FILE_SANITY_CHECK_TEST_CASE.keys()))
+    def test_check(self, create, backup, exists, writable_parent,
+                   readable, regular_file, expect_raise, mocker):
+        mocker.patch(MODULE_PATH.format('check_if_dest_exists'),
+                     return_value=exists)
+        mocker.patch(MODULE_PATH.format('check_if_parent_is_writable'),
+                     return_value=writable_parent)
+        mocker.patch(MODULE_PATH.format('check_if_dest_is_readable'),
+                     return_value=readable)
+        mocker.patch(MODULE_PATH.format('check_if_dest_is_regular_file'),
+                     return_value=regular_file)
+        if expect_raise is None:
+            file_will_be_created = dest_file_sanity_check(
+                FAKE_DEST, create, backup)
+            assert((create and not exists) == file_will_be_created)
+        else:
+            with pytest.raises(expect_raise):
+                dest_file_sanity_check(FAKE_DEST, create, backup)
+
+
+TEST_CASE_DEST_FILE_MODULE = [
+    [
+        {
+            'path': FAKE_DEST,
+            'create': True,
+            'backup': False,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'dest_not_exists_created',
+            'exists': False,
+            'changed': True,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': True,
+            'backup': True,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'dest_not_exists_created_ensure_no_backup',
+            'exists': False,
+            'changed': True,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': True,
+            'value': FAKE_DATA_CURRENT,
+        },
+        {
+            'id': 'dest_exists_not_chanded',
+            'exists': True,
+            'changed': False,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': False,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'dest_exists_changed_no_backup',
+            'exists': True,
+            'changed': True,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': True,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'dest_exists_changed_with_backup',
+            'exists': True,
+            'changed': True,
+            'check_mode': False,
+            'backup_file': FAKE_DEST_BACKUP,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': False,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'check_mode_skip',
+            'exists': True,
+            'changed': True,
+            'check_mode': True,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': False,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'sanity_check_raise',
+            'exists': False,
+            'changed': False,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': True,
+            'atomic_move_raise': False,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': False,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'atomic_move_raise_no_backup',
+            'exists': False,
+            'changed': False,
+            'check_mode': False,
+            'backup_file': None,
+            'sanity_check_raise': False,
+            'atomic_move_raise': True,
+        },
+    ],
+    [
+        {
+            'path': FAKE_DEST,
+            'create': False,
+            'backup': True,
+            'value': FAKE_DATA_CHANGED,
+        },
+        {
+            'id': 'atomic_move_raise_with_backup',
+            'exists': True,
+            'changed': False,
+            'check_mode': False,
+            'backup_file': FAKE_DEST_BACKUP,
+            'sanity_check_raise': False,
+            'atomic_move_raise': True,
+        },
+    ],
+]
+
+
+TEST_CASE_DEST_FILE_MODULE_IDS = (item[1]['id']
+                                  for item in TEST_CASE_DEST_FILE_MODULE)
+
+
+class TestDestFileModuleHelper():
+
+    class _FakeDestFileModule(DestFileModuleHelper):
+
+        module = dict(
+            argument_spec=dict(
+                path=dict(type='path', required=True, aliases=['dest']),
+                create=dict(type='bool', default=True),
+                backup=dict(type='bool', default=True),
+                value=dict(type='str', required=True),
+            ),
+        )
+
+        @DestFileModuleHelper.write_tempfile
+        def __write_temp__(self, *args, **kwargs) -> None:
+            """impement abstract DestFileModuleHelper.__write_temp__"""
+            pass
+
+        def __load_result_data__(self):
+            """impement abstract DestFileModuleHelper.__load_result_data__"""
+            self.vars.set(self.var_result_data, FAKE_DATA_CURRENT, diff=True)
+
+        def __run__(self):
+            self.vars.set(self.var_result_data, self.vars.value)
+
+    @pytest.mark.parametrize('patch_ansible_module, test_case',
+                             TEST_CASE_DEST_FILE_MODULE,
+                             ids=TEST_CASE_DEST_FILE_MODULE_IDS,
+                             indirect=['patch_ansible_module'])
+    @pytest.mark.usefixtures('patch_ansible_module')
+    def test_fake_dest_file_module(self, test_case, capfd, mocker):
+
+        mock_tempfile_mkstemp = mocker.patch(
+            'tempfile.mkstemp', return_value=(1234, FAKE_TEMP_FILE))
+        mock_os_close = mocker.patch('os.close')
+        mock_dest_file_sanity_check = mocker.patch(
+            MODULE_PATH.format('dest_file_sanity_check'))
+        mock_atomic_move = mocker.patch(
+            'ansible_collections.community.general.plugins.module_utils' +
+            '.mh.module_helper.AnsibleModule.atomic_move')
+        mock_cleanup = mocker.patch(
+            'ansible_collections.community.general.plugins.module_utils' +
+            '.mh.module_helper.AnsibleModule.cleanup')
+        mock_backup_local = mocker.patch(
+            'ansible_collections.community.general.plugins.module_utils' +
+            '.mh.module_helper.AnsibleModule.backup_local',
+            return_value=FAKE_DEST_BACKUP)
+
+        if test_case['sanity_check_raise']:
+            mock_dest_file_sanity_check.side_effect = ModuleHelperException
+        else:
+            mock_dest_file_sanity_check.return_value = not test_case['exists']
+        if test_case['atomic_move_raise']:
+            mock_atomic_move.side_effect = OSError
+
+        module = self._FakeDestFileModule()
+        with pytest.raises(SystemExit):
+            module.module.check_mode = test_case['check_mode']
+            module.run()
+        result = json.loads(capfd.readouterr().out)
+
+        mock_dest_file_sanity_check.assert_called_once_with(
+            module.vars['path'], module.vars['create'], module.vars['backup'])
+        if test_case['sanity_check_raise'] or test_case['atomic_move_raise']:
+            assert(result['failed'])
+        if result['failed']:
+            assert(test_case['sanity_check_raise']
+                   or test_case['atomic_move_raise'])
+            if test_case['atomic_move_raise']:
+                if module.vars.get('backup_file'):
+                    mock_backup_local.assert_called_once()
+                    mock_cleanup.assert_called_with(FAKE_DEST_BACKUP)
+                else:
+                    mock_backup_local.assert_not_called()
+                    with pytest.raises(AssertionError):
+                        mock_cleanup.assert_called_with(FAKE_DEST_BACKUP)
+                mock_cleanup.assert_any_call(FAKE_TEMP_FILE)
+        else:
+            if module.has_changed() and not test_case['check_mode']:
+                mock_tempfile_mkstemp.assert_called_once()
+                mock_cleanup.assert_called_with(FAKE_TEMP_FILE)
+                mock_os_close.assert_called_once_with(1234)
+                mock_atomic_move.assert_called_once_with(FAKE_TEMP_FILE,
+                                                         module.vars['path'])
+            else:
+                mock_tempfile_mkstemp.assert_not_called()
+                mock_cleanup.assert_not_called()
+                mock_os_close.assert_not_called()
+                mock_atomic_move.assert_not_called()
+            assert(result.get('backup_file') == test_case['backup_file'])
+            assert(result['result'] == module.vars['value'])
+            assert(result['created'] == (not test_case['exists']))
+            assert(result['changed'] == test_case['changed'])
