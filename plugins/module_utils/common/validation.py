1cecffc7893eba043eac8c7989faa94b1e16da70
Indelog <maxime@indelog.fr>
2022-05-17 17:12:38 +0200

    Add validation type for dict or list.

---
+++ plugins/module_utils/common/validation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, DEMAREST Maxime <maxime@indelog.fr>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from ansible.module_utils.common.validation import (
+    check_type_dict,
+    check_type_list,
+    string_types,
+)
+
+
+def check_type_dict_or_list(value):
+    """
+    Verify that value is a dict or string that can be convertied to a dict or
+    a list.
+    """
+    try:
+        if isinstance(value, (list, dict)):
+            return value
+        elif (isinstance(value, string_types) and
+              (value.startswith("{") or '=' in value)):
+            return check_type_dict(value)
+        else:
+            return check_type_list(value)
+    except TypeError:
+        raise TypeError(
+            '%s cannot be converted to dict or a list' % type(value))
