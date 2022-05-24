f279b977ee7012a7229b19d17b2e69119799edfd
bluikko <14869000+bluikko@users.noreply.github.com>
2022-05-24 14:32:55 +0700

    idrac_redfish_command: documentation language fix
    Call it "HTTP request" and not "URL request".

--- plugins/modules/remote_management/redfish/idrac_redfish_command.py
+++ plugins/modules/remote_management/redfish/idrac_redfish_command.py
@@ -47,7 +47,7 @@ options:
     version_added: 2.3.0
   timeout:
     description:
-      - Timeout in seconds for URL requests to iDRAC.
+      - Timeout in seconds for HTTP requests to iDRAC.
     default: 10
     type: int
   resource_id:
