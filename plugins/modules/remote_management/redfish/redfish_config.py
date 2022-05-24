3270a4e64745a1da7d471221187333c286ab8c56
bluikko <14869000+bluikko@users.noreply.github.com>
2022-05-25 01:07:10 +0700

    redfish modules: documentation language and formatting (#4722)
    * redfish_command: documentation language and formatting
    
    Update the documentation block with more English language
    more consistent with other modules and finish each description
    with a full stop.
    
    * redfish_command: do not end short desc in full stop
    
    * redfish_command: remove more "the" on documentation
    
    Remove "The" from beginning of descriptions.
    
    * redfish_command: start documentation description with capital case
    
    * redfish_config: update documentation language and format
    
    Add full stops at end of descriptions, small updates to language,
    ensure descriptions start with a capital case letter.
    
    * redfish_config: documentation consistency
    
    Use "username" for description similarly to redfish_command.
    
    * redfish_info: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * idrac_redfish_command: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * idrac_redfish_command: more doc consistency fixes
    
    Call it iDRAC everywhere and not sometimes OOB controller.
    
    * idrac_redfish_command: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * idrac_redfish_info: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * ilo_redfish_config: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * ilo_redfish_info: documentation formatting and language
    
    Update documentation block for consistency with other modules
    with full stop at end of descriptions and small language updates.
    
    * idrac_redfish_info: documentation language
    
    Call it "HTTP request" and not "URL request".
    
    * idrac_redfish_command: documentation language fix
    
    Call it "HTTP request" and not "URL request".
    
    * idrac_redfish_config: documentation language fix
    
    Call it "HTTP request" and not "URL request".
    
    * redfish_command: documentation language fix
    
    Call it "HTTP request" and not "URL request".
    
    * redfish_config: documentation language fix
    
    Call it "HTTP request" and not "URL request".
    
    * redfish_info: documentation language fix
    
    Call it "HTTP request" and not "URL request".
    
    * redfish_command: documentation language fix
    
    Boot device should be two words.
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/remote_management/redfish/redfish_config.py
+++ plugins/modules/remote_management/redfish/redfish_config.py
@@ -20,48 +20,48 @@ options:
   category:
     required: true
     description:
-      - Category to execute on OOB controller
+      - Category to execute on OOB controller.
     type: str
   command:
     required: true
     description:
-      - List of commands to execute on OOB controller
+      - List of commands to execute on OOB controller.
     type: list
     elements: str
   baseuri:
     required: true
     description:
-      - Base URI of OOB controller
+      - Base URI of OOB controller.
     type: str
   username:
     description:
-      - User for authentication with OOB controller
+      - Username for authenticating to OOB controller.
     type: str
   password:
     description:
-      - Password for authentication with OOB controller
+      - Password for authenticating to OOB controller.
     type: str
   auth_token:
     description:
-      - Security token for authentication with OOB controller
+      - Security token for authenticating to OOB controller.
     type: str
     version_added: 2.3.0
   bios_attributes:
     required: false
     description:
-      - dictionary of BIOS attributes to update
+      - Dictionary of BIOS attributes to update.
     default: {}
     type: dict
     version_added: '0.2.0'
   timeout:
     description:
-      - Timeout in seconds for URL requests to OOB controller
+      - Timeout in seconds for HTTP requests to OOB controller.
     default: 10
     type: int
   boot_order:
     required: false
     description:
-      - list of BootOptionReference strings specifying the BootOrder
+      - List of BootOptionReference strings specifying the BootOrder.
     default: []
     type: list
     elements: str
@@ -69,26 +69,26 @@ options:
   network_protocols:
     required: false
     description:
-      -  setting dict of manager services to update
+      - Setting dict of manager services to update.
     type: dict
     version_added: '0.2.0'
   resource_id:
     required: false
     description:
-      - The ID of the System, Manager or Chassis to modify
+      - ID of the System, Manager or Chassis to modify.
     type: str
     version_added: '0.2.0'
   nic_addr:
     required: false
     description:
-      - EthernetInterface Address string on OOB controller
+      - EthernetInterface Address string on OOB controller.
     default: 'null'
     type: str
     version_added: '0.2.0'
   nic_config:
     required: false
     description:
-      - setting dict of EthernetInterface on OOB controller
+      - Setting dict of EthernetInterface on OOB controller.
     type: dict
     version_added: '0.2.0'
   strip_etag_quotes:
