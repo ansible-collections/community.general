50863a11d4bfa61279149d62dc916a7f7ed7487d
Alexei Znamensky <russoz@gmail.com>
2022-05-28 21:18:02 +1200

    fixed docs for gconftool2_info

--- plugins/modules/system/gconftool2_info.py
+++ plugins/modules/system/gconftool2_info.py
@@ -7,113 +7,41 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 DOCUMENTATION = '''
-module: xfconf_info
+module: gconftool2_info
 author:
     - "Alexei Znamensky (@russoz)"
-short_description: Retrieve XFCE4 configurations
-version_added: 3.5.0
+short_description: Retrieve GConf configurations
+version_added: 5.1.0
 description:
-  - This module allows retrieving Xfce 4 configurations with the help of C(xfconf-query).
+  - This module allows retrieving application preferences from the GConf database, with the help of C(gconftool-2).
 options:
-  channel:
+  key:
     description:
     - >
-      A Xfconf preference channel is a top-level tree key, inside of the
-      Xfconf repository that corresponds to the location for which all
-      application properties/keys are stored.
-    - If not provided, the module will list all available channels.
-    type: str
-  property:
-    description:
-    - >
-      A Xfce preference key is an element in the Xfconf repository
-      that corresponds to an application preference.
-    - If provided, then I(channel) is required.
-    - If not provided and a I(channel) is provided, then the module will list all available properties in that I(channel).
+      The key name for an element in the GConf database.
     type: str
 notes:
-  - See man xfconf-query(1) for more details.
+  - See man gconftool-2(1) for more details.
+seealso:
+  - name: gconf repository (archived)
+    description: Git repository for the project. It is an archived project, so the repository is read-only.
+    link: https://gitlab.gnome.org/Archive/gconf
 '''
 
 EXAMPLES = """
-- name: Get list of all available channels
-  community.general.xfconf_info: {}
-  register: result
-
-- name: Get list of all properties in a specific channel
-  community.general.xfconf_info:
-    channel: xsettings
-  register: result
-
-- name: Retrieve the DPI value
-  community.general.xfconf_info:
-    channel: xsettings
-    property: /Xft/DPI
-  register: result
-
-- name: Get workspace names (4)
-  community.general.xfconf_info:
-    channel: xfwm4
-    property: /general/workspace_names
+- name: Get value for a certain key in the database.
+  community.general.gconftool2_info:
+    key: /desktop/gnome/background/picture_filename
   register: result
 """
 
 RETURN = '''
-  channels:
-    description:
-        - List of available channels.
-        - Returned when the module receives no parameter at all.
-    returned: success
-    type: list
-    elements: str
-    sample:
-    - xfce4-desktop
-    - displays
-    - xsettings
-    - xfwm4
-  properties:
-    description:
-        - List of available properties for a specific channel.
-        - Returned by passed only the I(channel) parameter to the module.
-    returned: success
-    type: list
-    elements: str
-    sample:
-        - /Gdk/WindowScalingFactor
-        - /Gtk/ButtonImages
-        - /Gtk/CursorThemeSize
-        - /Gtk/DecorationLayout
-        - /Gtk/FontName
-        - /Gtk/MenuImages
-        - /Gtk/MonospaceFontName
-        - /Net/DoubleClickTime
-        - /Net/IconThemeName
-        - /Net/ThemeName
-        - /Xft/Antialias
-        - /Xft/Hinting
-        - /Xft/HintStyle
-        - /Xft/RGBA
-  is_array:
-    description:
-    - Flag indicating whether the property is an array or not.
-    returned: success
-    type: bool
   value:
     description:
-    - The value of the property. Empty if the property is of array type.
+    - The value of the property.
     returned: success
     type: str
     sample: Monospace 10
-  value_array:
-    description:
-    - The array value of the property. Empty if the property is not of array type.
-    returned: success
-    type: list
-    elements: str
-    sample:
-    - Main
-    - Work
-    - Tmp
 '''
 
 from ansible_collections.community.general.plugins.module_utils.module_helper import ModuleHelper
@@ -141,8 +69,7 @@ class GConftoolInfo(ModuleHelper):
 
 
 def main():
-    xfconf = GConftoolInfo()
-    xfconf.run()
+    GConftoolInfo.execute()
 
 
 if __name__ == '__main__':
