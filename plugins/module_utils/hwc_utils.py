a58191225e675f7ec174dd73b1b4813ecc184c9a
Felix Fontein <felix@fontein.de>
2022-06-02 07:30:06 +0200

    Add simplified_bsd.txt license file (#4759)
    * Add simplified_bsd.txt and adjust references.
    
    * Add changelog.
    
    (cherry picked from commit 0be68bf04bd9b8dcf99583ebf5ad6deac4736283)

--- plugins/module_utils/hwc_utils.py
+++ plugins/module_utils/hwc_utils.py
@@ -1,5 +1,5 @@
 # Copyright (c), Google Inc, 2017
-# Simplified BSD License (see licenses/simplified_bsd.txt or
+# Simplified BSD License (see simplified_bsd.txt or
 # https://opensource.org/licenses/BSD-2-Clause)
 
 from __future__ import (absolute_import, division, print_function)
