bb616eac876d5595b56cf0c4afb0e0045dbabf89
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2022-06-04 21:58:30 +1200

    Update plugins/module_utils/mh/exceptions.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/mh/exceptions.py
+++ plugins/module_utils/mh/exceptions.py
@@ -11,7 +11,7 @@ from ansible.module_utils.common.text.converters import to_native
 
 class ModuleHelperException(Exception):
     def __init__(self, msg, update_output=None, *args, **kwargs):
-        self.msg = to_native(msg)
+        self.msg = to_native(msg or "Module failed with exception: {0}".format(self))
         if update_output is None:
             update_output = {}
         self.update_output = update_output
