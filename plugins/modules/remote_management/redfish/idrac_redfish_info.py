1cca79f02da927954468cd54d3965a4efaa73096
Felix Fontein <felix@fontein.de>
2022-07-04 21:23:02 +0200

    Fix license filenames. (#4923)

--- plugins/modules/remote_management/redfish/idrac_redfish_info.py
+++ plugins/modules/remote_management/redfish/idrac_redfish_info.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2019 Dell EMC Inc.
-# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
