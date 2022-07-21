f14a83da81ae0ee7d59997100de94d873fc46e8f
Sylvia van Os <sylvia@hackerchick.me>
2022-07-21 07:19:31 +0200

    Fix path detection for gopass (#4955)
    * Fix path detection for gopass
    
    As per https://github.com/gopasspw/gopass/blob/fc8c9a228618fa4a146a87c9027fa0434b0737fa/docs/features.md#initializing-a-password-store, gopass defaults to ~/.local/share/gopass/stores/root for its password store root location.
    
    However, the user can also override this, and this will be stored in the gopass config file (https://github.com/gopasspw/gopass/blob/ed7451678c5e8138d5a8eaafa278d5065a9eb9fe/docs/config.md#configuration-options).
    
    This patch ensures that the config setting in gopass is respected, falling back to the default gopass path. pass' behaviour remains unchanged.
    
    * Formatting improvements
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Add changelog fragment
    
    * Formatting improvement
    
    Co-authored-by: Alexei Znamensky <103110+russoz@users.noreply.github.com>
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    Co-authored-by: Alexei Znamensky <103110+russoz@users.noreply.github.com>

--- plugins/lookup/passwordstore.py
+++ plugins/lookup/passwordstore.py
@@ -21,8 +21,14 @@ DOCUMENTATION = '''
         description: query key.
         required: True
       passwordstore:
-        description: location of the password store.
-        default: '~/.password-store'
+        description:
+          - Location of the password store.
+          - 'The value is decided by checking the following in order:'
+          - If set, this value is used.
+          - If C(directory) is set, that value will be used.
+          - If I(backend=pass), then C(~/.password-store) is used.
+          - If I(backend=gopass), then the C(path) field in C(~/.config/gopass/config.yml) is used,
+            falling back to C(~/.local/share/gopass/stores/root) if not defined.
       directory:
         description: The directory of the password store.
         env:
@@ -428,11 +434,22 @@ class LookupModule(LookupBase):
             raise AnsibleError("{0} is not a correct value for locktimeout".format(timeout))
         unit_to_seconds = {"s": 1, "m": 60, "h": 3600}
         self.lock_timeout = int(timeout[:-1]) * unit_to_seconds[timeout[-1]]
+
+        directory = variables.get('passwordstore', os.environ.get('PASSWORD_STORE_DIR', None))
+
+        if directory is None:
+            if self.backend == 'gopass':
+                try:
+                    with open(os.path.expanduser('~/.config/gopass/config.yml')) as f:
+                        directory = yaml.safe_load(f)['path']
+                except (FileNotFoundError, KeyError, yaml.YAMLError):
+                    directory = os.path.expanduser('~/.local/share/gopass/stores/root')
+            else:
+                directory = os.path.expanduser('~/.password-store')
+
         self.paramvals = {
             'subkey': 'password',
-            'directory': variables.get('passwordstore', os.environ.get(
-                                       'PASSWORD_STORE_DIR',
-                                       os.path.expanduser('~/.password-store'))),
+            'directory': directory,
             'create': False,
             'returnall': False,
             'overwrite': False,
