841b1365c849d7227437b653b7cf26d13cf7ff56
Julien Riou <julien@riou.xyz>
2022-08-03 17:10:01 +0200

    feat: Add crc32 filter
    Compute CRC32 checksum of a string and return its hex representation. Can be
    use to create short checksums.
    
    Signed-off-by: Julien Riou <julien@riou.xyz>

---
+++ tests/unit/plugins/filter/test_crc32.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022, Julien Riou <julien@riou.xyz>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible.errors import AnsibleError
+from ansible_collections.community.general.plugins.filter.crc32 import crc32s
+
+
+class TestFilterCrc32(unittest.TestCase):
+
+    def test_checksum(self):
+        self.assertEqual(crc32s('test'), 'd87f7e0c')
