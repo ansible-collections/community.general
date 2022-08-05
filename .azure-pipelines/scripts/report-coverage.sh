1ab2a5f1bcf32596a27901a25322880c3ea6260b
Felix Fontein <felix@fontein.de>
2022-08-05 14:03:38 +0200

    Add default license header to files which have no copyright or license header yet (#5074)
    * Add default license header to files which have no copyright or license header yet.
    
    * yml extension should have been xml...

--- .azure-pipelines/scripts/report-coverage.sh
+++ .azure-pipelines/scripts/report-coverage.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # Generate code coverage reports for uploading to Azure Pipelines and codecov.io.
 
 set -o pipefail -eu
