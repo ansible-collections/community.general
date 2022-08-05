0cafc39c65bdb95f9002aa7002748afa3c134fa0
Felix Fontein <felix@fontein.de>
2022-08-05 12:28:29 +0200

    Move licenses to LICENSES/, run add-license.py, add LICENSES/MIT.txt (#5065)
    * Move licenses to LICENSES/, run add-license.py, add LICENSES/MIT.txt.
    
    * Replace 'Copyright:' with 'Copyright'
    
    sed -i 's|Copyright:\(.*\)|Copyright\1|' $(rg -l 'Copyright:')
    
    Co-authored-by: Maxwell G <gotmax@e.email>

--- plugins/module_utils/mh/exceptions.py
+++ plugins/module_utils/mh/exceptions.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # (c) 2020, Alexei Znamensky <russoz@gmail.com>
-# Copyright: (c) 2020, Ansible Project
-# Simplified BSD License (see simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+# Copyright (c) 2020, Ansible Project
+# Simplified BSD License (see LICENSES/BSD-2-Clause.txt or https://opensource.org/licenses/BSD-2-Clause)
+# SPDX-License-Identifier: BSD-2-Clause
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
--- plugins/modules/identity/onepassword_info.py
+++ plugins/modules/identity/onepassword_info.py
@@ -4,7 +4,8 @@
 # (c) 2018, Ryan Conway (@rylon)
 # (c) 2018, Scott Buchanan <sbuchanan@ri.pn> (onepassword.py used as starting point)
 # (c) 2018, Ansible Project
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 
 from __future__ import (absolute_import, division, print_function)
