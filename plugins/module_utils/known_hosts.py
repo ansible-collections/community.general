b77b8971695f6fccad7d08c3bc95b030dd80eef9
Felix Fontein <felix@fontein.de>
2022-08-05 12:28:29 +0200

    Move licenses to LICENSES/, run add-license.py, add LICENSES/MIT.txt (#5065)
    * Move licenses to LICENSES/, run add-license.py, add LICENSES/MIT.txt.
    
    * Replace 'Copyright:' with 'Copyright'
    
    sed -i 's|Copyright:\(.*\)|Copyright\1|' $(rg -l 'Copyright:')
    
    Co-authored-by: Maxwell G <gotmax@e.email>
    (cherry picked from commit 123c7efe5e98a22e9493461347e30bf76e686268)

--- plugins/module_utils/known_hosts.py
+++ plugins/module_utils/known_hosts.py
@@ -7,7 +7,8 @@
 #
 # Copyright (c), Michael DeHaan <michael.dehaan@gmail.com>, 2012-2013
 #
-# Simplified BSD License (see simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
+# Simplified BSD License (see LICENSES/BSD-2-Clause.txt or https://opensource.org/licenses/BSD-2-Clause)
+# SPDX-License-Identifier: BSD-2-Clause
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
--- plugins/modules/packaging/os/zypper_repository.py
+++ plugins/modules/packaging/os/zypper_repository.py
@@ -4,7 +4,8 @@
 # (c) 2013, Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
 # (c) 2014, Justin Lecher <jlec@gentoo.org>
 #
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
