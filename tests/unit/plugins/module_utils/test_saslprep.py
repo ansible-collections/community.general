d6975be214b9c883d818183bb3aaf03e67e1c05c
Felix Fontein <felix@fontein.de>
2022-08-07 13:37:23 +0200

    Add more license statements (#5079)
    * Add more license statements. These were modified manually incorporating existing data.
    
    * Remove accidentally added line.

--- tests/unit/plugins/module_utils/test_saslprep.py
+++ tests/unit/plugins/module_utils/test_saslprep.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019, Andrey Tuzhilin <andrei.tuzhilin@gmail.com>
 # Copyright (c) 2020, Andrew Klychkov (@Andersson007) <aaklychkov@mail.ru>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
