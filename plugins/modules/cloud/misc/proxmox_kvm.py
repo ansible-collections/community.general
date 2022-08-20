6f4f70eca2120c21cc0b08c2d5caf9e0a516f6b0
Markus Reiter <me@reitermark.us>
2022-08-20 13:21:07 +0200

    Change Proxmox `agent` argument to string. (#5107)
    * Change Proxmox `agent` argument to string.
    
    * Add changelog entry.
    
    * Pass boolean directly to `proxmoxer`.

--- plugins/modules/cloud/misc/proxmox_kvm.py
+++ plugins/modules/cloud/misc/proxmox_kvm.py
@@ -24,7 +24,9 @@ options:
   agent:
     description:
       - Specify if the QEMU Guest Agent should be enabled/disabled.
-    type: bool
+      - Since community.general 5.5.0, this can also be a string instead of a boolean.
+        This allows to specify values such as C(enabled=1,fstrim_cloned_disks=1).
+    type: str
   args:
     description:
       - Pass arbitrary arguments to kvm.
@@ -808,6 +810,7 @@ from ansible_collections.community.general.plugins.module_utils.proxmox import (
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.common.text.converters import to_native
+from ansible.module_utils.parsing.convert_bool import boolean
 
 
 def parse_mac(netstr):
@@ -959,7 +962,14 @@ class ProxmoxKvmAnsible(ProxmoxAnsible):
                 kwargs.update(kwargs[k])
                 del kwargs[k]
 
-        # Rename numa_enabled to numa. According the API documentation
+        try:
+            # The API also allows booleans instead of e.g. `enabled=1` for backward-compatibility.
+            kwargs['agent'] = boolean(kwargs['agent'], strict=True)
+        except TypeError:
+            # Not something that Ansible would parse as a boolean.
+            pass
+
+        # Rename numa_enabled to numa, according the API documentation
         if 'numa_enabled' in kwargs:
             kwargs['numa'] = kwargs['numa_enabled']
             del kwargs['numa_enabled']
@@ -1039,7 +1049,7 @@ def main():
     module_args = proxmox_auth_argument_spec()
     kvm_args = dict(
         acpi=dict(type='bool'),
-        agent=dict(type='bool'),
+        agent=dict(type='str'),
         args=dict(type='str'),
         autostart=dict(type='bool'),
         balloon=dict(type='int'),
