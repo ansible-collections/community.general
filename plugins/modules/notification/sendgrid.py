35a283918a58f3b5bdf80bb72d39ff899a3df16c
Felix Fontein <felix@fontein.de>
2022-08-24 19:59:46 +0200

    Adjust booleans in notification modules. (#5157)

--- plugins/modules/notification/sendgrid.py
+++ plugins/modules/notification/sendgrid.py
@@ -81,7 +81,7 @@ options:
     description:
       - Whether the body is html content that should be rendered.
     type: bool
-    default: 'no'
+    default: false
   headers:
     type: dict
     description:
@@ -90,7 +90,7 @@ options:
     type: str
     description:
       - The e-mail body content.
-    required: yes
+    required: true
 author: "Matt Makai (@makaimc)"
 '''
 
