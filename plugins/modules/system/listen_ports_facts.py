198517e21ee08038dcf705d3f376a9fff2c692d8
PKehnel <ga65coy@mytum.de>
2022-09-03 12:04:42 +0200

    Bugfix - EnvironmentError with wrong indentation (#5202)
    * The EnvironmentError is now handled in the splid_pid_name function.
    
    The error also had a wrong indentation. See previous setup with correct setup: https://github.com/ansible-collections/community.general/blob/6a7811f6963ad1f5f92342b786e70a809e1c9e08/plugins/modules/system/listen_ports_facts.py
    
    * Add changelog fragment
    
    * Sanity Check failed before
    
    * Update changelogs/fragments/5202-bugfix-environmentError-wrong-indentation.yaml
    
    Co-authored-by: Paul-Kehnel <paul.kehnel@ocean.ibm.com>
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/system/listen_ports_facts.py
+++ plugins/modules/system/listen_ports_facts.py
@@ -256,8 +256,6 @@ def netStatParse(raw):
             }
             if result not in results:
                 results.append(result)
-            else:
-                raise EnvironmentError('Could not get process information for the listening ports.')
     return results
 
 
