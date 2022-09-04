f3a84a7a86be47dbe0af0a392e1c59ca3cf44196
Shaul Shnaidman <shaul@seekingalpha.com>
2022-09-04 13:55:27 +0300

    fix

--- plugins/modules/cloud/misc/proxmox.py
+++ plugins/modules/cloud/misc/proxmox.py
@@ -613,9 +613,7 @@ def main():
     if module.params['unprivileged'] is None:
         module.params['unprivileged'] = False
         module.deprecate(
-            'The default value `false` for the parameter "unprivileged" is deprecated and it will be replaced by `true`'
-                False, True
-            ),
+            'The default value `false` for the parameter "unprivileged" is deprecated and it will be replaced by `true`',
             version='7.0.0',
             collection_name='community.general'
         )
