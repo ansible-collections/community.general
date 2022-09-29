a7c6e8519d070a9901de4bcd664e619b07d9c38f
Håkon Heggernes Lerring <hakon@lerring.no>
2022-09-29 16:13:53 +0200

    Add copyright information

--- plugins/modules/clustering/consul/consul_token.py
+++ plugins/modules/clustering/consul/consul_token.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-#
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022, Håkon Lerring
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
