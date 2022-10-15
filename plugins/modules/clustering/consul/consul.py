79781a31fe62aaefa2ce823be71e6da56100d33c
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2022-10-15 21:05:51 +1300

    Update plugins/modules/clustering/consul/consul.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/clustering/consul/consul.py
+++ plugins/modules/clustering/consul/consul.py
@@ -362,7 +362,7 @@ def get_consul_api(module):
 def get_service_by_id_or_name(consul_api, service_id_or_name):
     ''' iterate the registered services and find one with the given id '''
     for dummy, service in consul_api.agent.services().items():
-        if service_id_or_name in [service['ID'], service['Service']]:
+        if service_id_or_name in (service['ID'], service['Service']):
             return ConsulService(loaded=service)
 
 
