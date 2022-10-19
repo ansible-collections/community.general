2b48cfaa6f77e1398d21aa65531c3bea0aa0061c
Martin van Es <martin@mrvanes.com>
2022-10-19 10:09:44 +0200

    Fix formatted string and ldap import

--- plugins/modules/net_tools/ldap/ldap_attrs.py
+++ plugins/modules/net_tools/ldap/ldap_attrs.py
@@ -166,11 +166,11 @@ modlist:
 '''
 
 import traceback
-import ldap
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.common.text.converters import to_native, to_bytes
-from ansible_collections.community.general.plugins.module_utils.ldap import LdapGeneric, gen_specs
+from ansible_collections.community.general.plugins.module_utils.ldap import ldap, LdapGeneric, gen_specs
+
 import re
 
 LDAP_IMP_ERR = None
@@ -264,7 +264,7 @@ class LdapAttrs(LdapGeneric):
     def _is_value_present(self, name, value):
         """ True if the target attribute has the given value. """
         try:
-            filterstr = f"({name}={value.decode()})"
+            filterstr = "(%s=%s)" % (name, value.decode())
             dns = self.connection.search_s(self.dn, ldap.SCOPE_BASE, filterstr)
             is_present = bool(len(dns) == 1)
         except (ldap.INVALID_DN_SYNTAX, ldap.NO_SUCH_OBJECT):
