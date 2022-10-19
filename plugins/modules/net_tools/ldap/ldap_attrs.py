c32c107fbe2f54ba330a311af2e1d3d06da8b089
Martin van Es <martin@mrvanes.com>
2022-10-19 10:34:55 +0200

    Remove superfluous import ldap

--- plugins/modules/net_tools/ldap/ldap_attrs.py
+++ plugins/modules/net_tools/ldap/ldap_attrs.py
@@ -169,7 +169,7 @@ import traceback
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.common.text.converters import to_native, to_bytes
-from ansible_collections.community.general.plugins.module_utils.ldap import ldap, LdapGeneric, gen_specs
+from ansible_collections.community.general.plugins.module_utils.ldap import LdapGeneric, gen_specs
 
 import re
 
