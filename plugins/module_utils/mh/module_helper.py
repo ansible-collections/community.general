a1d1e48b1aa8dfe5d70c4a705a3a9f29a8909e6e
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2022-10-24 03:34:04 +1300

    MH CmdModuleHelper: deprecation (#5370)
    * MH CmdModuleHelper: deprecation
    
    * add changelog fragment
    
    * add deprecation comments in many parts of the code

--- plugins/module_utils/mh/module_helper.py
+++ plugins/module_utils/mh/module_helper.py
@@ -83,8 +83,16 @@ class StateModuleHelper(StateMixin, ModuleHelper):
 
 
 class CmdModuleHelper(CmdMixin, ModuleHelper):
+    """
+    THIS CLASS IS BEING DEPRECATED.
+    See the deprecation notice in ``CmdMixin.__init__()``.
+    """
     pass
 
 
 class CmdStateModuleHelper(CmdMixin, StateMixin, ModuleHelper):
+    """
+    THIS CLASS IS BEING DEPRECATED.
+    See the deprecation notice in ``CmdMixin.__init__()``.
+    """
     pass
