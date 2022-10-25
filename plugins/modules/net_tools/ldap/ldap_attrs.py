c85412a34663c0f22abeeb807e6e2a749209ea0f
Martin <github@mrvanes.com>
2022-10-25 08:01:57 +0200

    ldap_attrs: search_s based _is_value_present (#5385)
    * search_s based _is_value_present
    
    * Fix formatted string and ldap import
    
    * Add changelog fragment
    
    * Remove superfluous import ldap
    
    * Improve fragment
    
    * Code format {x} prefix
    
    * Lower-case fixes
    
    * Fix suggestions to changelog
    
    * Break with the past and let bools be bools
    
    * Let ldap_attrs break on invalid DN's

--- plugins/modules/net_tools/ldap/ldap_attrs.py
+++ plugins/modules/net_tools/ldap/ldap_attrs.py
@@ -170,6 +170,7 @@ import traceback
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 from ansible.module_utils.common.text.converters import to_native, to_bytes
 from ansible_collections.community.general.plugins.module_utils.ldap import LdapGeneric, gen_specs
+
 import re
 
 LDAP_IMP_ERR = None
@@ -263,9 +264,10 @@ class LdapAttrs(LdapGeneric):
     def _is_value_present(self, name, value):
         """ True if the target attribute has the given value. """
         try:
-            is_present = bool(
-                self.connection.compare_s(self.dn, name, value))
-        except ldap.NO_SUCH_ATTRIBUTE:
+            filterstr = "(%s=%s)" % (name, value.decode())
+            dns = self.connection.search_s(self.dn, ldap.SCOPE_BASE, filterstr)
+            is_present = len(dns) == 1
+        except ldap.NO_SUCH_OBJECT:
             is_present = False
 
         return is_present
