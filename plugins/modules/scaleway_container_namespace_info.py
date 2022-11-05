59e0fd8be95aa624e1bb81065ac3b88000f8e159
Guillaume MARTINEZ <lunik@tiwabbit.fr>
2022-11-05 21:44:33 +0100

    [Scaleway] Add module to manage container namespaces (#5416)
    * [Scaleway] Add module to manage container namespaces
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    * Fix CI
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    * fix botmeta
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    * fix typo in loop var name
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    * Add missing required lib check
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    * fix integration tests assertions
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

---
+++ plugins/modules/scaleway_container_namespace_info.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Scaleway Serverless container namespace info module
+#
+# Copyright (c) 2022, Guillaume MARTINEZ <lunik@tiwabbit.fr>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = '''
+---
+module: scaleway_container_namespace_info
+short_description: Retrieve information on Scaleway Container namespace
+version_added: 6.0.0
+author: Guillaume MARTINEZ (@Lunik)
+description:
+  - This module return information about a container namespace on Scaleway account.
+extends_documentation_fragment:
+  - community.general.scaleway
+
+
+options:
+  project_id:
+    type: str
+    description:
+      - Project identifier.
+    required: true
+
+  region:
+    type: str
+    description:
+      - Scaleway region to use (for example C(fr-par)).
+    required: true
+    choices:
+      - fr-par
+      - nl-ams
+      - pl-waw
+
+  name:
+    type: str
+    description:
+      - Name of the container namespace.
+    required: true
+'''
+
+EXAMPLES = '''
+- name: Get a container namespace info
+  community.general.scaleway_container_namespace_info:
+    project_id: '{{ scw_project }}'
+    region: fr-par
+    name: my-awesome-container-namespace
+  register: container_namespace_info_task
+'''
+
+RETURN = '''
+container_namespace:
+  description: The container namespace information.
+  returned: always
+  type: dict
+  sample:
+    description: ""
+    environment_variables:
+      MY_VAR: my_value
+    error_message: null
+    id: 531a1fd7-98d2-4a74-ad77-d398324304b8
+    name: my-awesome-container-namespace
+    organization_id: e04e3bdc-015c-4514-afde-9389e9be24b0
+    project_id: d44cea58-dcb7-4c95-bff1-1105acb60a98
+    region: fr-par
+    registry_endpoint: ""
+    registry_namespace_id: ""
+    secret_environment_variables: SENSITIVE_VALUE
+    status: pending
+'''
+
+from ansible_collections.community.general.plugins.module_utils.scaleway import (
+    SCALEWAY_ENDPOINT, SCALEWAY_REGIONS, scaleway_argument_spec, Scaleway,
+    filter_sensitive_attributes
+)
+from ansible.module_utils.basic import AnsibleModule
+
+SENSITIVE_ATTRIBUTES = (
+    "secret_environment_variables",
+)
+
+
+def info_strategy(api, wished_cn):
+    cn_list = api.fetch_all_resources("namespaces")
+    cn_lookup = dict((fn["name"], fn)
+                     for fn in cn_list)
+
+    if wished_cn["name"] not in cn_lookup:
+        msg = "Error during container namespace lookup: Unable to find container namespace named '%s' in project '%s'" % (wished_cn["name"],
+                                                                                                                          wished_cn["project_id"])
+
+        api.module.fail_json(msg=msg)
+
+    target_cn = cn_lookup[wished_cn["name"]]
+
+    response = api.get(path=api.api_path + "/%s" % target_cn["id"])
+    if not response.ok:
+        msg = "Error during container namespace lookup: %s: '%s' (%s)" % (response.info['msg'],
+                                                                          response.json['message'],
+                                                                          response.json)
+        api.module.fail_json(msg=msg)
+
+    return response.json
+
+
+def core(module):
+    region = module.params["region"]
+    wished_container_namespace = {
+        "project_id": module.params["project_id"],
+        "name": module.params["name"]
+    }
+
+    api = Scaleway(module=module)
+    api.api_path = "containers/v1beta1/regions/%s/namespaces" % region
+
+    summary = info_strategy(api=api, wished_cn=wished_container_namespace)
+
+    module.exit_json(changed=False, container_namespace=filter_sensitive_attributes(summary, SENSITIVE_ATTRIBUTES))
+
+
+def main():
+    argument_spec = scaleway_argument_spec()
+    argument_spec.update(dict(
+        project_id=dict(type='str', required=True),
+        region=dict(type='str', required=True, choices=SCALEWAY_REGIONS),
+        name=dict(type='str', required=True)
+    ))
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    core(module)
+
+
+if __name__ == '__main__':
+    main()
