cc223053acd245806c54a9383cb71d4f0dddf96f
Sam Doran <sdoran@redhat.com>
2022-11-06 05:32:35 -0500

    onepassword - Support v2  (#4728)
    * Begin building out separate classes to support  different op cli versions
    
    Create separet base classes for each major version.
    Define the main interface in the base class.
    Create methods for getting the current version and instantiating the
    appropriate class based on the found version.
    
    * First pass at mostly working CLI version classes
    
    * Correct mismathched parameters
    
    * Update _run() method to allow updating enviroment
    
    This allows passing in the app secret as an env var, which is more
    secure than using a command line arg.
    
    * Continuing to improve the interface
    
    * Tear existing tests down to the studs
    
    These tests were based off of the LastPass unit tests. I’m going to
    just start from scratch given the new plugin code is vastly diffenent.
    
    * Fix sanity test
    
    * CLI config file path can be None
    
    * Improve required param checking
    
    - only report missing params
    - use proper grammer based on number of missing params
    
    * Change assert_logged_in() method return value
    
    Return a boolean value indicating whether or not account is signed in
    
    * Improve full login for v2
    
    Have to do a bit of a dance to avoid hitting the interactive prompt
    if there are no accounts configured.
    
    * Remove unused methods
    
    * Add some tests
    
    * Fix linting errors
    
    * Move fixtures to separate file
    
    * Restructure mock test data and add more tests
    
    * Add boilerplate
    
    * Add test scenario for op v2 and increase coverage
    
    * Fix up copyright statements
    
    * Test v1 and v2 in all cases
    
    * Use a more descriptive variable name
    
    * Use docstrings rather than pass in abstract class
    
    This adds coverage to abstract methods with the least amount of hackery.
    
    * Increase test coverage for CLI classes
    
    * Sort test parameters to avoid collection errors
    
    * Update version tested in docs
    
    * Revere test parameter sorting for now
    
    The parameters need to be sorted to avoid the issue in older Python
    versions in CI, but I’m having trouble working out how to do that
    currently.
    
    * Allow passing kwargs to the lookup module under test
    
    * Favor label over id for v2 when looking for values
    
    Add tests
    
    * Display a warning for section on op v2 or greater
    
    There is no “value” in section fields. If we wanted to support sections
    in v2, we would also have to allow specifying the field name in
    order to override “value”.
    
    * Move test cases to their own file
    
    Getting a bit unwieldy having it in the test file
    
    * Move output into JSON files fore easier reuse
    
    * Switch to using get_options()
    
    * Add licenses for fixture files
    
    * Use get_option() since get_options() was added in Ansible Core 2.12
    
    * Rearrange fixtures
    
    * Add changelog
    
    * Move common classes to module_utils
    
    * Move common classes back to lookup
    
    The plugin relies on AnsibleLookupError() quite a bit which is not available
    in module code.
    
    Remove use of display for errors since section isn’t actually deprecated.
    
    * Properly handle sections
    
    Still room for improvement, but this is at least a start.
    
    * Remove some comments that won’t be addressed
    
    * Make test gathering more deterministic to avoid failures
    
    * Update changelog fragment
    
    * Simple fix for making tests reliable

---
+++ tests/unit/plugins/module_utils/test_onepassword.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2022 Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+
+import pytest
+
+from ansible_collections.community.general.plugins.module_utils.onepassword import OnePasswordConfig
+
+
+@pytest.fixture
+def os_expanduser(mocker):
+    def _os_expanduser(path):
+        return path.replace("~", "/home/testuser")
+
+    mocker.patch("os.path.expanduser", side_effect=_os_expanduser)
+
+
+@pytest.fixture
+def exists(mocker):
+    def _exists(path):
+        if "op/" in path:
+            return True
+
+        return os.path.exists(path)
+
+
+def test_op_config(mocker, os_expanduser):
+    mocker.patch("os.path.exists", side_effect=[False, True])
+    op_config = OnePasswordConfig()
+
+    assert "/home/testuser/.config/op/config" == op_config.config_file_path
+
+
+def test_op_no_config(mocker, os_expanduser):
+    mocker.patch("os.path.exists", return_value=False)
+    op_config = OnePasswordConfig()
+
+    assert op_config.config_file_path is None
