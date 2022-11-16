4b74249e1f2f557193f144bcb64753ad1e53c358
David Peng <dpeng1@gmail.com>
2022-11-15 20:04:58 -0800

    Fix FUSE comment header

--- plugins/modules/greppy.py
+++ plugins/modules/greppy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2022, David Peng <dpeng1@gmail.com>
+# SPDX-FileCopyrightText: 2022, David Peng <dpeng1@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
