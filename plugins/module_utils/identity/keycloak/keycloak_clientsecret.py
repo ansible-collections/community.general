9851ea9bc30ac2dafb92974dd774eac55435813e
John Cant <a.johncant@gmail.com>
2022-12-04 11:25:14 +0000

    Keycloak_clientsecret module_utils: boilerplate

--- plugins/module_utils/identity/keycloak/keycloak_clientsecret.py
+++ plugins/module_utils/identity/keycloak/keycloak_clientsecret.py
@@ -4,6 +4,9 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
 
 from ansible.module_utils.basic import AnsibleModule
 
