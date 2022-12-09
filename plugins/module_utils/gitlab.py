2ad55f658e99c728d1253b1903c441ee52c2c3e4
Markus Bergholz <git@osuv.de>
2022-12-09 13:27:05 +0100

    return a copy

--- plugins/module_utils/gitlab.py
+++ plugins/module_utils/gitlab.py
@@ -114,7 +114,7 @@ def gitlab_authentication(module):
 
 def filter_returned_variables(gitlab_variables):
     # pop properties we don't know
-    existing_variables = [x.attributes for x in gitlab_variables]
+    existing_variables = [dict(x.attributes) for x in gitlab_variables]
     KNOWN = ['key', 'value', 'masked', 'protected', 'variable_type', 'environment_scope']
     for item in existing_variables:
         for key in list(item.keys()):
