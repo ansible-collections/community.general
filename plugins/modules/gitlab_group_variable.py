c3bc172bf6fccdca9c16155da058ce95a57e6076
Markus Bergholz <git@osuv.de>
2022-12-10 21:40:36 +0100

    respect new variable property in gitlab_group_variable and gitlab_project_variable (#5667)
    * draft
    
    * add changelog fragment
    
    * rework
    
    * rework group variables
    
    * add new line at end of file
    
    * Update plugins/module_utils/gitlab.py
    
    Co-authored-by: Nejc Habjan <hab.nejc@gmail.com>
    
    * rename
    
    * revert
    
    * return a copy
    
    * Update plugins/modules/gitlab_project_variable.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    Co-authored-by: Nejc Habjan <hab.nejc@gmail.com>
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/gitlab_group_variable.py
+++ plugins/modules/gitlab_group_variable.py
@@ -165,7 +165,7 @@ from ansible.module_utils.six import string_types
 from ansible.module_utils.six import integer_types
 
 from ansible_collections.community.general.plugins.module_utils.gitlab import (
-    auth_argument_spec, gitlab_authentication, ensure_gitlab_package
+    auth_argument_spec, gitlab_authentication, ensure_gitlab_package, filter_returned_variables
 )
 
 
@@ -296,11 +296,7 @@ def native_python_main(this_gitlab, purge, requested_variables, state, module):
     before = [x.attributes for x in gitlab_keys]
 
     gitlab_keys = this_gitlab.list_all_group_variables()
-    existing_variables = [x.attributes for x in gitlab_keys]
-
-    # preprocessing:filter out and enrich before compare
-    for item in existing_variables:
-        item.pop('group_id')
+    existing_variables = filter_returned_variables(gitlab_keys)
 
     for item in requested_variables:
         item['key'] = item.pop('name')
@@ -331,9 +327,7 @@ def native_python_main(this_gitlab, purge, requested_variables, state, module):
         if purge:
             # refetch and filter
             gitlab_keys = this_gitlab.list_all_group_variables()
-            existing_variables = [x.attributes for x in gitlab_keys]
-            for item in existing_variables:
-                item.pop('group_id')
+            existing_variables = filter_returned_variables(gitlab_keys)
 
             remove = [x for x in existing_variables if x not in requested_variables]
             for item in remove:
