97733087a90b552763de834a2b40b96d5dc5ca7b
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2022-12-22 19:59:23 +1300

    Update plugins/module_utils/ssh.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/ssh.py
+++ plugins/module_utils/ssh.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2015, Björn Andersson
+# Copyright (c) 2021, Ansible Project
+# Copyright (c) 2021, Abhijeet Kasurde <akasurde@redhat.com>
 # Copyright (c) 2022, Alexei Znamensky <russoz@gmail.com>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
