eeed98e6ea4a42e86a1c258d660c370db9854f58
Ed Schaller <schallee@darkmist.net>
2022-12-27 22:24:44 -0700

    ksusu: attemt to satisfy pep8

--- tests/unit/plugins/become/test_ksusu.py
+++ tests/unit/plugins/become/test_ksusu.py
@@ -14,23 +14,25 @@ from ansible import context
 
 from .helper import call_become_plugin
 
+
 def _test_ksu_task(mocker, parser, task, shell='/bin/bash', cmd='/bin/foo'):
     context._init_global_context(parser.parse_args([]))
 
-    # Task arguments and defaults
+    # Task arguments and defaults:
     become_user_ksu = task.get('become_user_ksu', 'root')
     become_user = task.get('become_user', 'root')
     become_exe_ksu = task.get('become_exe_ksu', 'ksu')
     become_exe_su = task.get('become_exe_su', '/bin/su')
-    become_flags_ksu = task.get('become_flags_ksu', '-Z\\s+-q' )
+    become_flags_ksu = task.get('become_flags_ksu', '-Z\\s+-q')
     become_flags = task.get('become_flags', '')
     become_pass = task.get('become_pass', None)
     prompt_l10n = task.get('prompt_l10n', [])
 
-    task['become_method'] = 'community.general.ksusu' # force this
+    task['become_method'] = 'community.general.ksusu'  # Force the become method.
 
-    # We are appending here instead of formatting so the parts are easily understood
-    regex  = become_exe_ksu
+    # We are appending here instead of formatting so the parts are easily understood.
+    regex = ''
+    regex += become_exe_ksu
     regex += '\\s+' + become_user_ksu
     regex += '\\s+' + become_flags_ksu
     regex += '\\s+-e\\s+' + become_exe_su
@@ -55,6 +57,7 @@ def test_ksusu_defaults(mocker, parser, reset_cli_args):
 
     _test_ksu_task(mocker, parser, task)
 
+
 def test_ksusu_user_ksu(mocker, parser, reset_cli_args):
 
     task = {
@@ -62,6 +65,7 @@ def test_ksusu_user_ksu(mocker, parser, reset_cli_args):
     }
 
     _test_ksu_task(mocker, parser, task)
+
 
 def test_ksusu_user(mocker, parser, reset_cli_args):
 
@@ -71,6 +75,7 @@ def test_ksusu_user(mocker, parser, reset_cli_args):
 
     _test_ksu_task(mocker, parser, task)
 
+
 def test_ksusu_exe_ksu(mocker, parser, reset_cli_args):
 
     task = {
@@ -78,6 +83,7 @@ def test_ksusu_exe_ksu(mocker, parser, reset_cli_args):
     }
 
     _test_ksu_task(mocker, parser, task)
+
 
 def test_ksusu_exe_su(mocker, parser, reset_cli_args):
 
@@ -87,6 +93,7 @@ def test_ksusu_exe_su(mocker, parser, reset_cli_args):
 
     _test_ksu_task(mocker, parser, task)
 
+
 def test_ksusu_flags_ksu(mocker, parser, reset_cli_args):
 
     task = {
@@ -94,6 +101,7 @@ def test_ksusu_flags_ksu(mocker, parser, reset_cli_args):
     }
 
     _test_ksu_task(mocker, parser, task)
+
 
 def test_ksusu_flags(mocker, parser, reset_cli_args):
 
