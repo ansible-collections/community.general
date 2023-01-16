62260cd4a5779ffdecfdb9073a4297dab072163a
Ed Schaller <schallee@darkmist.net>
2023-01-16 16:13:42 -0700

    Update plugins/become/ksusu.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/become/ksusu.py
+++ plugins/become/ksusu.py
@@ -12,7 +12,7 @@ DOCUMENTATION = """
         - This become plugins allows your remote/login user to execute commands as another user by calling C(su) through the C(ksu) utility.
     author:
         - Ed Schaller (@schallee)
-    version_added: 6.2.0
+    version_added: 6.3.0
     options:
         become_user_ksu:
             description: User you use with C(ksu) to run C(su).
