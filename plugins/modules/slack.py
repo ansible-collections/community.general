2878897878f9d8015c0cbcf35ad9d307902e51d3
Lars Krahl <57526005+mmslkr@users.noreply.github.com>
2023-02-03 11:02:14 +0100

    Replace missing default favicon with docs.ansible.com favicon (#5928)
    * replace missing default favicon with docs.ansible.com
    
    * create changelog fragment for PR 5928
    
    * move changelog fragment
    
    * fix parameter description
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * fix parameter description
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * add affected modules in changelog fragment
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    ---------
    
    Co-authored-by: Lars Krahl <lkr@mms-dresden.de>
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/slack.py
+++ plugins/modules/slack.py
@@ -81,8 +81,8 @@ options:
   icon_url:
     type: str
     description:
-      - Url for the message sender's icon (default C(https://www.ansible.com/favicon.ico))
-    default: https://www.ansible.com/favicon.ico
+      - URL for the message sender's icon (default C(https://docs.ansible.com/favicon.ico))
+    default: https://docs.ansible.com/favicon.ico
   icon_emoji:
     type: str
     description:
@@ -439,7 +439,7 @@ def main():
             channel=dict(type='str'),
             thread_id=dict(type='str'),
             username=dict(type='str', default='Ansible'),
-            icon_url=dict(type='str', default='https://www.ansible.com/favicon.ico'),
+            icon_url=dict(type='str', default='https://docs.ansible.com/favicon.ico'),
             icon_emoji=dict(type='str'),
             link_names=dict(type='int', default=1, choices=[0, 1]),
             parse=dict(type='str', choices=['none', 'full']),
