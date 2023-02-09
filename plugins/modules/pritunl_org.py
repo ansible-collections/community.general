0e9a03f302292649027e506e2d7281f193be890c
Felix Fontein <felix@fontein.de>
2023-02-09 16:49:22 +0100

    Add attributes to apache2, cobbler, dimensiondata, icinga2, lxca, pritunl, and spectrum modules.

--- plugins/modules/pritunl_org.py
+++ plugins/modules/pritunl_org.py
@@ -18,6 +18,12 @@ description:
     - A module to manage Pritunl organizations using the Pritunl API.
 extends_documentation_fragment:
     - community.general.pritunl
+    - community.general.attributes
+attributes:
+    check_mode:
+        support: none
+    diff_mode:
+        support: none
 options:
     name:
         type: str
