199dcc282c6e60b03bf218ae2da2c9bb41c9eec8
dima1206 <32818228+dima1206@users.noreply.github.com>
2023-02-15 17:41:31 +0200

    github_webhook: Don't include secret in the config if it's absent

--- plugins/modules/github_webhook.py
+++ plugins/modules/github_webhook.py
@@ -154,12 +154,17 @@ from ansible.module_utils.common.text.converters import to_native
 
 
 def _create_hook_config(module):
-    return {
+    hook_config = {
         "url": module.params["url"],
         "content_type": module.params["content_type"],
-        "secret": module.params.get("secret"),
         "insecure_ssl": "1" if module.params["insecure_ssl"] else "0"
     }
+
+    secret = module.params.get("secret")
+    if secret is not None and len(secret) > 0:
+      hook_config["secret"] = secret
+
+    return hook_config
 
 
 def create_hook(repo, module):
