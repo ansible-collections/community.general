982ef4e046e683242b05569a02c18501b4a822e7
Groshev Alexandr <haddystuff@gmail.com>
2023-02-18 20:25:13 +0100

    fix unused import

--- tests/unit/plugins/modules/test_modprobe.py
+++ tests/unit/plugins/modules/test_modprobe.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 
 import sys
 from ansible_collections.community.general.tests.unit.plugins.modules.utils import ModuleTestCase, set_module_args
-from ansible_collections.community.general.tests.unit.compat.mock import call, patch
+from ansible_collections.community.general.tests.unit.compat.mock import patch
 from ansible_collections.community.general.tests.unit.compat.mock import Mock
 from ansible_collections.community.general.tests.unit.compat.mock import mock_open
 from ansible.module_utils.basic import AnsibleModule
