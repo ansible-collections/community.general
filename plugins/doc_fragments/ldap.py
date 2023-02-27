af5104d794061b7d11b33f61292f3ba9ff581493
Reto Kupferschmid <kupferschmid@puzzle.ch>
2023-02-27 20:26:01 +0100

    add xorder_discovery parameter (#6109)
    * add xorder_discovery parameter
    
    * fix regex raw string
    
    * use dn logic from LdapGeneric
    
    * Update documentation.
    
    * Update changelog fragment.
    
    * Improve if.
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/doc_fragments/ldap.py
+++ plugins/doc_fragments/ldap.py
@@ -65,4 +65,15 @@ options:
     choices: ['external', 'gssapi']
     default: external
     version_added: "2.0.0"
+  xorder_discovery:
+    description:
+      - Set the behavior on how to process Xordered DNs.
+      - C(enable) will perform a C(ONELEVEL) search below the superior RDN to find the matching DN.
+      - C(disable) will always use the DN unmodified (as passed by the I(dn) parameter).
+      - C(auto) will only perform a search if the first RDN does not contain an index number (C({x})).
+      - Possible choices are C(enable), C(auto), C(disable).
+    type: str
+    choices: ['enable', 'auto', 'disable']
+    default: auto
+    version_added: "6.4.0"
 '''
