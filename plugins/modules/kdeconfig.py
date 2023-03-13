b5951957a882d5390ed9d2bd7498a5ae767dac03
Salvatore Mesoraca <s.mesoraca16@gmail.com>
2023-03-14 00:21:28 +0100

    Fix license issues

--- plugins/modules/kdeconfig.py
+++ plugins/modules/kdeconfig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2023, Salvatore Mesoraca <s.mesoraca16@gmail.com>
+# Copyright (c) 2023, Salvatore Mesoraca <s.mesoraca16@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import (absolute_import, division, print_function)
