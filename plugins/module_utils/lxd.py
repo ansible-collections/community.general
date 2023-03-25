ffce4eb05df9980e53f0e81422918e8788424b32
Frank Dornheim <524257+conloos@users.noreply.github.com>
2023-03-25 12:44:02 +0100

    Add better error message

--- plugins/module_utils/lxd.py
+++ plugins/module_utils/lxd.py
@@ -57,17 +57,17 @@ class LXDClient(object):
         self.debug = debug
         self.logs = []
         if url.startswith('https:'):
-            try:
+            try: 
                 with open(cert_file, 'r') as fh:
                     self.cert_file = cert_file
-            except FileNotFoundError:
-                raise LXDClientException('The certificate is not valid.')
+            except FileNotFoundError: 
+                raise LXDClientException('The certificate "{0}" does not exist.'.format(cert_file))
 
-            try:
+            try: 
                 with open(key_file, 'r') as fh:
                     self.key_file = key_file
-            except FileNotFoundError:
-                raise LXDClientException('The certificate key is not valid.')
+            except FileNotFoundError: 
+                raise LXDClientException('The certificate key "{0}" does not exist.'.format(key_file))
 
             parts = generic_urlparse(urlparse(self.url))
             ctx = ssl._create_unverified_context(purpose=ssl.Purpose.SERVER_AUTH)
