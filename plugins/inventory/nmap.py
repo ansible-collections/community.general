9b08a2c01bceba1dca9cfb4075aaa9b42e0f7790
efrikin <sadeless@gmail.com>
2023-04-17 20:11:49 +0300

    Fix https://github.com/ansible-collections/community.general/pull/6352#discussion_r1168014445

--- plugins/inventory/nmap.py
+++ plugins/inventory/nmap.py
@@ -31,8 +31,8 @@ DOCUMENTATION = '''
             description: Network IP or range of IPs to scan, you can use a simple range (10.2.2.15-25) or CIDR notation.
             required: true
             env:
-              - name: ANSIBLE_NMAP_ADDRESS
-                version_added: 6.6.0
+                - name: ANSIBLE_NMAP_ADDRESS
+                  version_added: 6.6.0
         exclude:
             description:
               - List of addresses to exclude.
@@ -40,8 +40,8 @@ DOCUMENTATION = '''
             type: list
             elements: string
             env:
-              - name: ANSIBLE_NMAP_EXCLUDE
-                version_added: 6.6.0
+                - name: ANSIBLE_NMAP_EXCLUDE
+                  version_added: 6.6.0
         port:
             description:
                 - Only scan specific port or port range (C(-p)).
