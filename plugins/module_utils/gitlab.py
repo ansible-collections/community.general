846b571dac48c6e1c43f8df0ba8868fcea2308bb
Alexei Znamensky <russoz@gmail.com>
2023-04-20 21:49:04 +1200

    gitlab modules: minor refactor

--- plugins/module_utils/gitlab.py
+++ plugins/module_utils/gitlab.py
@@ -10,6 +10,8 @@ __metaclass__ = type
 
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils.common.text.converters import to_native
+from ansible.module_utils.six import string_types
+from ansible.module_utils.six import integer_types
 
 from ansible_collections.community.general.plugins.module_utils.version import LooseVersion
 
@@ -121,3 +123,39 @@ def filter_returned_variables(gitlab_variables):
             if key not in KNOWN:
                 item.pop(key)
     return existing_variables
+
+
+def vars_to_variables(vars, module):
+    # transform old vars to new variables structure
+    variables = list()
+    for item, value in vars.items():
+        if (isinstance(value, string_types) or
+           isinstance(value, (integer_types, float))):
+            variables.append(
+                {
+                    "name": item,
+                    "value": str(value),
+                    "masked": False,
+                    "protected": False,
+                    "variable_type": "env_var",
+                }
+            )
+
+        elif isinstance(value, dict):
+            new_item = {
+                "name": item,
+                "value": value.get('value'),
+                "masked": value.get('masked'),
+                "protected": value.get('protected'),
+                "variable_type": value.get('variable_type'),
+            }
+
+            if value.get('environment_scope'):
+                new_item['environment_scope'] = value.get('environment_scope')
+
+            variables.append(new_item)
+
+        else:
+            module.fail_json(msg="value must be of type string, integer, float or dict")
+
+    return variables
