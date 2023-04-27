a1348faf2d4ebf884d89b48e3ca52942bebf9516
jantari <jantari@github.com>
2023-04-27 21:50:43 +0200

    update 'value of a secret' example after f6c4492c

--- plugins/lookup/bitwarden_secrets_manager.py
+++ plugins/lookup/bitwarden_secrets_manager.py
@@ -31,12 +31,12 @@ DOCUMENTATION = """
 """
 
 EXAMPLES = """
-- name: "Get a secret relying on the BWS_ACCESS_TOKEN environment variable for authentication"
+- name: Get a secret relying on the BWS_ACCESS_TOKEN environment variable for authentication
   ansible.builtin.debug:
     msg: >-
       {{ lookup("community.general.bitwarden_secrets_manager", "2bc23e48-4932-40de-a047-5524b7ddc972") }}
 
-- name: "Get a secret passing an explicit access token for authentication"
+- name: Get a secret passing an explicit access token for authentication
   ansible.builtin.debug:
     msg: >-
       {{
@@ -47,7 +47,7 @@ EXAMPLES = """
         )
       }}
 
-- name: "Get two different secrets each using a different access token for authentication"
+- name: Get two different secrets each using a different access token for authentication
   ansible.builtin.debug:
     msg:
       - '{{ lookup("community.general.bitwarden_secrets_manager", "2bc23e48-4932-40de-a047-5524b7ddc972", bws_access_token=token1) }}'
@@ -56,10 +56,10 @@ EXAMPLES = """
     token1: "9.4f570d14-4b54-42f5-bc07-60f4450b1db5.YmluYXJ5LXNvbWV0aGluZy0xMjMK:d2h5IGhlbGxvIHRoZXJlCg=="
     token2: "1.69b72797-6ea9-4687-a11e-848e41a30ae6.YW5zaWJsZSBpcyBncmVhdD8K:YW5zaWJsZSBpcyBncmVhdAo="
 
-- name: "Get just the value of a secret"
+- name: Get just the value of a secret
   ansible.builtin.debug:
     msg: >-
-      {{ lookup("community.general.bitwarden_secrets_manager", "2bc23e48-4932-40de-a047-5524b7ddc972")[0].value }}
+      {{ lookup("community.general.bitwarden_secrets_manager", "2bc23e48-4932-40de-a047-5524b7ddc972").value }}
 """
 
 RETURN = """
