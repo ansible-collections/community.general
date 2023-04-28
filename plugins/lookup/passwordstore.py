10757fab0c918d137b7628474623f2cfe57a2917
Felix Fontein <felix@fontein.de>
2023-04-28 12:08:45 +0200

    passwordstore plugin: vendor FileLock that was removed from ansible-core devel (#6447)
    Vendor FileLock that was removed from ansible-core devel.

--- plugins/lookup/passwordstore.py
+++ plugins/lookup/passwordstore.py
@@ -209,13 +209,14 @@ import time
 import yaml
 
 from ansible.errors import AnsibleError, AnsibleAssertionError
-from ansible.module_utils.common.file import FileLock
 from ansible.module_utils.common.text.converters import to_bytes, to_native, to_text
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.utils.display import Display
 from ansible.utils.encrypt import random_password
 from ansible.plugins.lookup import LookupBase
 from ansible import constants as C
+
+from ansible_collections.community.general.plugins.module_utils._filelock import FileLock
 
 display = Display()
 
