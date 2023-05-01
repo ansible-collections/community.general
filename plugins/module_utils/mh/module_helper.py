6c879126e97df98d49aae4dea6fd1fb9b184c77d
Alexei Znamensky <russoz@gmail.com>
2023-05-01 20:37:56 +1200

    pylint disable unused imports

--- plugins/module_utils/mh/module_helper.py
+++ plugins/module_utils/mh/module_helper.py
@@ -7,6 +7,9 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+# pylint: disable=unused-import
+
+
 from ansible.module_utils.common.dict_transformations import dict_merge
 
 # (TODO: remove AnsibleModule!) pylint: disable-next=unused-import
