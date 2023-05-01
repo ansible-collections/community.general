b4fc4aad79f145f937e36de7382b651d1b6996fa
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2023-05-01 20:31:34 +1200

    Setup docker (#6396)
    * setup-docker: install and run docker inside integration test VMs
    
    * must pip install requests to satisfy community.docker requirements
    
    * add Ubuntu configuration to setup_docker
    
    * Update tests/integration/targets/mssql_script/tasks/main.yml
    
    * Update tests/integration/targets/mssql_script/tasks/main.yml
    
    * docker_pacakges variable non-existent by default
    
    * add setup_docker to keycloak_groups
    
    * add setup_docker to keycloak_groups
    
    * removed unused file tasks/nothing.yml
    
    * add README
    
    * add copyright notice to readme file
    
    * Update tests/integration/targets/setup_docker/README.md
    
    * rolled back the boilerplate disclaimer to mssql_script tasks/main.yml

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -85,6 +85,7 @@ if [ "${script}" != "sanity" ] || [ "${test}" == "sanity/extra" ]; then
     # Nothing further should be added to this list.
     # This is to prevent modules or plugins in this collection having a runtime dependency on other collections.
     retry git clone --depth=1 --single-branch https://github.com/ansible-collections/community.internal_test_tools.git "${ANSIBLE_COLLECTIONS_PATHS}/ansible_collections/community/internal_test_tools"
+    retry git clone --depth=1 --single-branch https://github.com/ansible-collections/community.docker.git "${ANSIBLE_COLLECTIONS_PATHS}/ansible_collections/community/docker"
     # NOTE: we're installing with git to work around Galaxy being a huge PITA (https://github.com/ansible/galaxy/issues/2429)
     # retry ansible-galaxy -vvv collection install community.internal_test_tools
 fi
