3637ba5ffc931512807fe7445d43a68a37b1b471
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2023-05-03 06:48:46 +1200

    ModuleHelper: updated/fixed imports in module_utils/module_helper.py (#6464)
    * ModuleHelper: updated/fixed imports in module_utils/module_helper.py
    
    * add changelog frag
    
    * pylint disable unused imports
    
    * now in the right file

--- plugins/module_utils/module_helper.py
+++ plugins/module_utils/module_helper.py
@@ -7,14 +7,17 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+# pylint: disable=unused-import
 
-from ansible_collections.community.general.plugins.module_utils.mh.module_helper import (  # noqa: F401, pylint: disable=unused-import
+
+from ansible_collections.community.general.plugins.module_utils.mh.module_helper import (
     ModuleHelper, StateModuleHelper, CmdModuleHelper, CmdStateModuleHelper, AnsibleModule
 )
-from ansible_collections.community.general.plugins.module_utils.mh.mixins.cmd import CmdMixin, ArgFormat  # noqa: F401, pylint: disable=unused-import
-from ansible_collections.community.general.plugins.module_utils.mh.mixins.state import StateMixin  # noqa: F401, pylint: disable=unused-import
-from ansible_collections.community.general.plugins.module_utils.mh.mixins.deps import DependencyCtxMgr  # noqa: F401, pylint: disable=unused-import
-from ansible_collections.community.general.plugins.module_utils.mh.exceptions import ModuleHelperException  # noqa: F401, pylint: disable=unused-import
-# pylint: disable-next=unused-import
-from ansible_collections.community.general.plugins.module_utils.mh.deco import cause_changes, module_fails_on_exception  # noqa: F401
-from ansible_collections.community.general.plugins.module_utils.mh.mixins.vars import VarMeta, VarDict  # noqa: F401, pylint: disable=unused-import
+from ansible_collections.community.general.plugins.module_utils.mh.mixins.cmd import CmdMixin, ArgFormat  # noqa: F401
+from ansible_collections.community.general.plugins.module_utils.mh.mixins.state import StateMixin  # noqa: F401
+from ansible_collections.community.general.plugins.module_utils.mh.mixins.deps import DependencyCtxMgr, DependencyMixin  # noqa: F401
+from ansible_collections.community.general.plugins.module_utils.mh.exceptions import ModuleHelperException  # noqa: F401
+from ansible_collections.community.general.plugins.module_utils.mh.deco import (
+    cause_changes, module_fails_on_exception, check_mode_skip, check_mode_skip_returns,
+)
+from ansible_collections.community.general.plugins.module_utils.mh.mixins.vars import VarMeta, VarDict, VarsMixin  # noqa: F401
