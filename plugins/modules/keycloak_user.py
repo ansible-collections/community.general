c333def15415bfb6dc726bd9361fc7bd8ac072cd
Philippe Gauthier <philippe.gauthier@inspq.qc.ca>
2023-05-02 15:50:53 -0400

    Fix Copyright for keycloak_user module

--- plugins/modules/keycloak_user.py
+++ plugins/modules/keycloak_user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2019, INSPQ <philippe.gauthier@inspq.qc.ca>
+# Copyright (c) 2019, INSPQ (@elfelip)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
