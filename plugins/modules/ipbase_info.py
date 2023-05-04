938daf02493719e18304dac301a78e0f044bd876
Dominik Kukacka <dominik.kukacka@gmail.com>
2023-05-04 10:59:38 +0200

    Move to _info

--- plugins/modules/ipbase_info.py
+++ plugins/modules/ipbase_info.py
@@ -8,105 +8,110 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-
 DOCUMENTATION = '''
 ---
-module: ipbase_facts
+module: ipbase_info
+version_added: 7.0.0
 short_description: Retrieve IP geolocation and other facts of a host's IP address
 description:
   - "Retrieve IP geolocation and other facts of a host's IP address using the ipbase.com API"
 author: "Dominik Kukacka (@dominikkukacka)"
 extends_documentation_fragment:
   - community.general.attributes
-  - community.general.attributes.facts
-  - community.general.attributes.facts_module
+  - community.general.attributes.info_module
 options:
+  ip:
+    description:
+      - "The IP you want to get the info for."
+    required: false
+    default: "The primary outgoing IP address of the host."
+    type: str
   http_agent:
     description:
-      - Set http user agent
+      - "HTTP user agent to use."
     required: false
     default: "ansible-ipbase-module/0.0.1"
     type: str
 notes:
-  - "Check http://ipbase.com/ for more information"
+  - "Check U(https://ipbase.com/) for more information."
 '''
 
 EXAMPLES = '''
 # Retrieve IP geolocation and other facts of a host's IP address
-- name: Get IP geolocation information
-  community.general.ipbase_facts:
+- name: "Get IP geolocation information of the primary outgoing IP"
+  community.general.ipbase_info:
+
+- name: "Get IP geolocation information of a specific IP (and set a custom http user agent)"
+  community.general.ipbase_info:
+    ip: "8.8.8.8"
+    http_agent: "my-user-agent/0.0.0"
 '''
 
 RETURN = '''
-ansible_facts:
-  description: "Dictionary of ip facts for a host's IP address"
-  returned: changed
-  type: complex
-  contains:
-    ip:
-      description: "Public IP address of the host"
-      type: str
-      sample: "1.1.1.1"
-    hostname:
-      description: The hostname of the IP address
-      type: str
-      sample: "one.one.one.one"
-    continent_code:
-      description: ISO 3166-1 alpha-2 continent code
-      type: str
-      sample: "NA"
-    continent_name:
-      description: The continent name
-      type: str
-      sample: "North America"
-    country_code:
-      description: ISO 3166-1 alpha-2 country code
-      type: str
-      sample: "US"
-    country_name:
-      description: The country name
-      type: str
-      sample: "United States"
-    is_in_european_union:
-      description: Is the country in the European Union?
-      type: bool
-      sample: true
-    region_code:
-      description: ISO 3166-2 alpha-2 region code
-      type: str
-      sample: "US-CA"
-    region_name:
-      description: State or province name
-      type: str
-      sample: "California"
-    city_name:
-      description: City name
-      type: str
-      sample: "Los Angeles"
-    latitude:
-      description: Latitude of the location
-      type: float
-      sample: 34.053611755371094
-    longitude:
-      description: Longitude of the location
-      type: float
-      sample: -118.24549865722656
-    as_name:
-      description: "The autonomous system name (AS name)"
-      type: str
-      sample: "Cloudflare, Inc."
-    as_number:
-      description: "The autonomous system number (ASN)"
-      type: int
-      sample: 13335
-    zip:
-      description: Zip code
-      type: str
-      sample: "90012"
-    timezone:
-      description: "Timezone ID"
-      type: str
-      sample: "America/Los_Angeles"
+ip:
+  description: "Public IP address of the host."
+  type: str
+  sample: "1.1.1.1"
+hostname:
+  description: "The hostname of the IP address."
+  type: str
+  sample: "one.one.one.one"
+continent_code:
+  description: The ISO 3166-1 alpha-2 continent code."
+  type: str
+  sample: "NA"
+continent_name:
+  description: "The continent name."
+  type: str
+  sample: "North America"
+country_code:
+  description: "The ISO 3166-1 alpha-2 country code."
+  type: str
+  sample: "US"
+country_name:
+  description: "The country name."
+  type: str
+  sample: "United States"
+is_in_european_union:
+  description: "Is the country in the European Union?"
+  type: bool
+  sample: true
+region_code:
+  description: "The ISO 3166-2 alpha-2 region code."
+  type: str
+  sample: "US-CA"
+region_name:
+  description: "The state or province name."
+  type: str
+  sample: "California"
+city_name:
+  description: City name
+  type: str
+  sample: "Los Angeles"
+latitude:
+  description: "The latitude of the location."
+  type: float
+  sample: 34.053611755371094
+longitude:
+  description: "The longitude of the location."
+  type: float
+  sample: -118.24549865722656
+as_name:
+  description: "The autonomous system name (AS name)"
+  type: str
+  sample: "Cloudflare, Inc."
+as_number:
+  description: "The autonomous system number (ASN)"
+  type: int
+  sample: 13335
+zip:
+  description: "The zip code."
+  type: str
+  sample: "90012"
+timezone:
+  description: "The timezone ID."
+  type: str
+  sample: "America/Los_Angeles"
 '''
 from ansible.module_utils.basic import AnsibleModule
 
@@ -122,9 +127,13 @@ class IpbaseFacts(object):
     def __init__(self, module):
         self.module = module
 
-    def info(self):
+    def info(self, ip):
+        url = BASE_URL
+        if ip:
+            url = '{0}{1}'.format(BASE_URL, ip)
+
         response, info = fetch_url(
-            self.module, BASE_URL, force=True, timeout=10)
+            self.module, url, force=True, timeout=10)
 
         try:
             info['status'] == 200
@@ -164,19 +173,20 @@ class IpbaseFacts(object):
 
 
 def main():
+    module_args = dict(
+        http_agent=dict(default=USER_AGENT),
+        ip=dict(type='str', required=False, no_log=False),
+    )
+
     module = AnsibleModule(
-        argument_spec=dict(
-            http_agent=dict(default=USER_AGENT)
-        ),
+        argument_spec=module_args,
         supports_check_mode=True,
     )
 
+    ip = module.params['ip']
+
     ipbase = IpbaseFacts(module)
-    ipbase_result = dict(
-        changed=False,
-        ansible_facts=ipbase.info()
-    )
-    module.exit_json(**ipbase_result)
+    module.exit_json(**ipbase.info(ip))
 
 
 if __name__ == '__main__':
