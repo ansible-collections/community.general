1ca4b599a18fb97da268f885bab2a1a3e676b08b
Dominik Kukacka <dominik.kukacka@gmail.com>
2023-05-04 11:36:06 +0200

    Add apikey

--- plugins/modules/ipbase_info.py
+++ plugins/modules/ipbase_info.py
@@ -26,85 +26,93 @@ options:
     required: false
     default: "The primary outgoing IP address of the host."
     type: str
+  apikey:
+    description:
+      - "The api key for the request if you need more requests."
+    required: false
+    type: str
 notes:
   - "Check U(https://ipbase.com/) for more information."
 '''
 
 EXAMPLES = '''
-# Retrieve IP geolocation and other facts of a host's IP address
 - name: "Get IP geolocation information of the primary outgoing IP"
   community.general.ipbase_info:
+  register: my_ip_info
 
 - name: "Get IP geolocation information of a specific IP
   community.general.ipbase_info:
     ip: "8.8.8.8"
+  register: my_ip_info
 '''
 
 RETURN = '''
-ip:
-  description: "Public IP address of the host."
-  type: str
-  sample: "1.1.1.1"
-hostname:
-  description: "The hostname of the IP address."
-  type: str
-  sample: "one.one.one.one"
-continent_code:
-  description: The ISO 3166-1 alpha-2 continent code."
-  type: str
-  sample: "NA"
-continent_name:
-  description: "The continent name."
-  type: str
-  sample: "North America"
-country_code:
-  description: "The ISO 3166-1 alpha-2 country code."
-  type: str
-  sample: "US"
-country_name:
-  description: "The country name."
-  type: str
-  sample: "United States"
-is_in_european_union:
-  description: "Is the country in the European Union?"
-  type: bool
-  sample: true
-region_code:
-  description: "The ISO 3166-2 alpha-2 region code."
-  type: str
-  sample: "US-CA"
-region_name:
-  description: "The state or province name."
-  type: str
-  sample: "California"
-city_name:
-  description: City name
-  type: str
-  sample: "Los Angeles"
-latitude:
-  description: "The latitude of the location."
-  type: float
-  sample: 34.053611755371094
-longitude:
-  description: "The longitude of the location."
-  type: float
-  sample: -118.24549865722656
-as_name:
-  description: "The autonomous system name (AS name)"
-  type: str
-  sample: "Cloudflare, Inc."
-as_number:
-  description: "The autonomous system number (ASN)"
-  type: int
-  sample: 13335
-zip:
-  description: "The zip code."
-  type: str
-  sample: "90012"
-timezone:
-  description: "The timezone ID."
-  type: str
-  sample: "America/Los_Angeles"
+---
+data:
+  ip:
+    description: "Public IP address of the host."
+    type: str
+    sample: "1.1.1.1"
+  hostname:
+    description: "The hostname of the IP address."
+    type: str
+    sample: "one.one.one.one"
+  continent_code:
+    description: The ISO 3166-1 alpha-2 continent code."
+    type: str
+    sample: "NA"
+  continent_name:
+    description: "The continent name."
+    type: str
+    sample: "North America"
+  country_code:
+    description: "The ISO 3166-1 alpha-2 country code."
+    type: str
+    sample: "US"
+  country_name:
+    description: "The country name."
+    type: str
+    sample: "United States"
+  is_in_european_union:
+    description: "Is the country in the European Union?"
+    type: bool
+    sample: true
+  region_code:
+    description: "The ISO 3166-2 alpha-2 region code."
+    type: str
+    sample: "US-CA"
+  region_name:
+    description: "The state or province name."
+    type: str
+    sample: "California"
+  city_name:
+    description: "The city name."
+    type: str
+    sample: "Los Angeles"
+  latitude:
+    description: "The latitude of the location."
+    type: float
+    sample: 34.053611755371094
+  longitude:
+    description: "The longitude of the location."
+    type: float
+    sample: -118.24549865722656
+  as_name:
+    description: "The autonomous system name (AS name)"
+    type: str
+    sample: "Cloudflare, Inc."
+  as_number:
+    description: "The autonomous system number (ASN)"
+    type: int
+    sample: 13335
+  zip:
+    description: "The zip code."
+    type: str
+    sample: "90012"
+  timezone:
+    description: "The timezone ID."
+    type: str
+    sample: "America/Los_Angeles"
 '''
 from ansible.module_utils.basic import AnsibleModule
 
@@ -120,10 +128,13 @@ class IpbaseFacts(object):
     def __init__(self, module):
         self.module = module
 
-    def info(self, ip):
+    def info(self, ip, apikey):
         url = BASE_URL
         if ip:
             url += '&ip=' + str(ip)
+
+        if apikey:
+            url += '&apikey=' + str(apikey)
 
         response, info = fetch_url(
             self.module,
@@ -135,7 +146,7 @@ class IpbaseFacts(object):
                 'User-Agent': USER_AGENT,
             })
 
-        if(info['status'] != 200):
+        if (info['status'] != 200):
             self.module.fail_json(msg='The API request to ipbase.com returned an error status code {0}'.format(info['status']))
         else:
             try:
@@ -166,12 +177,13 @@ class IpbaseFacts(object):
                     msg='Failed to parse the ipbase.com response: '
                     '{0} {1}'.format(url, content))
             else:
-                return result
+                return dict(data=result)
 
 
 def main():
     module_args = dict(
         ip=dict(type='str', required=False, no_log=False),
+        apikey=dict(type='str', required=False, no_log=True),
     )
 
     module = AnsibleModule(
@@ -180,9 +192,10 @@ def main():
     )
 
     ip = module.params['ip']
+    apikey = module.params['apikey']
 
     ipbase = IpbaseFacts(module)
-    module.exit_json(**ipbase.info(ip))
+    module.exit_json(**ipbase.info(ip, apikey))
 
 
 if __name__ == '__main__':
