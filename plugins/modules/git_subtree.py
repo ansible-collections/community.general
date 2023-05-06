744931ec7bda9f7c36457cd4ad603ee96de8a1ae
riadhhamdi <ryadh.hamdi@gmail.com>
2023-05-06 15:08:37 -0700

    Update license in git_subtree.py

--- plugins/modules/git_subtree.py
+++ plugins/modules/git_subtree.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Written by Riadh Hamdi <rhamdi@redhat.com> <ryadh.hamdi@gmail.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Copyright Riadh Hamdi <ryadh.hamdi@gmail.com>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
