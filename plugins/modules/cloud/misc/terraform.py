406bcc6775c48ecf3ceecc3062f5b7fa800929c5
Max Juhlke <max.juhlke@gmail.com>
2023-05-08 19:46:10 +0200

    fix typo in `terraform` docs (#6503)
    fix typo
    
    The [Docs](https://docs.ansible.com/ansible/latest/collections/community/general/terraform_module.html) have a typo in the examples. `camplex_vars` should be spelled `complex_vars`.
    
    Since many people are copy-and-pasting from docs and modify them to their needs this typo is an annoyance for users.
    
    (cherry picked from commit 6558c9981f67e499410577aaa5ca5598823fbc51)

--- plugins/modules/cloud/misc/terraform.py
+++ plugins/modules/cloud/misc/terraform.py
@@ -211,7 +211,7 @@ EXAMPLES = """
   community.general.terraform:
     project_path: '{{ project_dir }}'
     state: present
-    camplex_vars: true
+    complex_vars: true
     variables:
       vm_name: "{{ inventory_hostname }}"
       vm_vcpus: 2
