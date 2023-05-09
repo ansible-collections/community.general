eecf5fb70525ad85bee7a7222c54ca9bee893db5
Jonathan Kamens <jik@kamens.us>
2023-05-09 17:10:48 -0400

    Update plugins/modules/perlmod_install_info.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/perlmod_install_info.py
+++ plugins/modules/perlmod_install_info.py
@@ -113,7 +113,7 @@ EXAMPLES = r'''
   failed_when: perlmod_info.missing is defined
 
 - name: Search for two modules, even if they're already installed
-  perlmod_install_info:
+  community.general.perlmod_install_info:
     name:
     - URI
     - WWW::Mechanize
