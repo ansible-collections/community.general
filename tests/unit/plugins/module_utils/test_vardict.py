74b9f222486efbca0b1d0ed7166a7f0a91745f2d
Alexei Znamensky <russoz@gmail.com>
2023-05-10 19:47:23 +1200

    remove unused import pytest

--- tests/unit/plugins/module_utils/test_vardict.py
+++ tests/unit/plugins/module_utils/test_vardict.py
@@ -7,7 +7,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import pytest
 
 from ansible_collections.community.general.plugins.module_utils.vardict import VarDict
 
