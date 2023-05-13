a09469f48a7a3e50359954db53f38d3578366938
Justine Jose <59870720+justinpjose@users.noreply.github.com>
2023-05-13 22:52:53 +0100

    use updated constant
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/mas.py
+++ plugins/modules/mas.py
@@ -165,7 +165,7 @@ class Mas(object):
         # Only check this once per execution
         if self._checked_signin:
             return
-        if LooseVersion(self._mac_version) > LooseVersion(WORKING_MAC_VERSION_MAS_ACCOUNT):
+        if LooseVersion(self._mac_version) >= LooseVersion(NOT_WORKING_MAC_VERSION_MAS_ACCOUNT):
             # Checking if user is signed-in is disabled due to https://github.com/mas-cli/mas/issues/417
             self.module.log('WARNING: You must be signed in via the Mac App Store GUI beforehand else error will occur')
         else:
