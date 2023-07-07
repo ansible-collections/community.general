269f5423c8ec9a7e0cb1247ef5ed7035bdde7b6d
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2023-07-08 08:24:32 +1200

    proxmox: default value when checking for dict key (#6838)
    * proxmox: default value when checking for dict key
    
    * add changelog frag

--- plugins/modules/proxmox.py
+++ plugins/modules/proxmox.py
@@ -448,7 +448,7 @@ class ProxmoxLxcAnsible(ProxmoxAnsible):
         """Check if the specified container is a template."""
         proxmox_node = self.proxmox_api.nodes(node)
         config = getattr(proxmox_node, VZ_TYPE)(vmid).config.get()
-        return config['template']
+        return config.get('template', False)
 
     def create_instance(self, vmid, node, disk, storage, cpus, memory, swap, timeout, clone, **kwargs):
 
