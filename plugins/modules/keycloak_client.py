3eff2bf888e2ea5a0399c3c4b0a0d585496a7ec3
Andre Desrosiers <andre.desrosiers@ssss.gouv.qc.ca>
2023-08-03 09:32:28 -0400

    Add documentation

--- plugins/modules/keycloak_client.py
+++ plugins/modules/keycloak_client.py
@@ -247,6 +247,7 @@ options:
     protocol:
         description:
             - Type of client.
+            - At creation only, default value will be 'openid-connect' if 'protocol' is omitted
         type: str
         choices: ['openid-connect', 'saml']
 
