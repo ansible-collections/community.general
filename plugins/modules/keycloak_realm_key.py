3dcd1a110170ed0d80b62a954e2a4d38dd71dd42
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2023-09-05 12:11:41 +0300

    keycloak_realm_key: small documentation fix

--- plugins/modules/keycloak_realm_key.py
+++ plugins/modules/keycloak_realm_key.py
@@ -129,9 +129,9 @@ notes:
     - If certificate is not explicitly provided it will be dynamically created
       by Keycloak. Therefore comparing the current state of the certificate to
       the desired state (which may be empty) is not possible.
-    - Due to the private key and certificate options the module is B(not fully
-      idempotent). You can use O(force=true) to force the module to always
-      update if you know that the private key might have changed.
+    - Due to the private key and certificate options the module is
+      B(not fully idempotent). You can use O(force=true) to force the module
+      to always update if you know that the private key might have changed.
 
 extends_documentation_fragment:
     - community.general.keycloak
