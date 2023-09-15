78a005e2f1da73e6f8da5b9e057cdd7d09090fd0
Jonathan Kamens <jik@kamens.us>
2023-09-15 19:56:02 -0400

    remove unused import

--- tests/unit/plugins/modules/test_ini_file.py
+++ tests/unit/plugins/modules/test_ini_file.py
@@ -6,8 +6,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import pytest
-
 from ansible_collections.community.general.plugins.modules import ini_file
 
 
