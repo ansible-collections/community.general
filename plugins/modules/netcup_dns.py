731d21a1c087e45ba5a7a9ce90d8d1edd29664f0
buzz-tee <11776936+buzz-tee@users.noreply.github.com>
2023-11-08 08:11:11 +0100

    Apply suggestions from code review
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/netcup_dns.py
+++ plugins/modules/netcup_dns.py
@@ -54,8 +54,8 @@ options:
     description:
       - Record type.
       - Support for V(OPENPGPKEY), V(SMIMEA) and V(SSHFP) was added in community.general 8.1.0.
-      - Record types V(OPENPGPKEY) and V(SMIMEA) require nc-dnsapi >= 0.1.5
-      - Record type V(SSHFP) requires nc-dnsapi >= 0.1.6
+      - Record types V(OPENPGPKEY) and V(SMIMEA) require nc-dnsapi >= 0.1.5.
+      - Record type V(SSHFP) requires nc-dnsapi >= 0.1.6.
     choices: ['A', 'AAAA', 'MX', 'CNAME', 'CAA', 'SRV', 'TXT', 'TLSA', 'NS', 'DS', 'OPENPGPKEY', 'SMIMEA', 'SSHFP']
     required: true
     type: str
