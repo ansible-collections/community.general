21f8e9f2b0af0ce7f71f07cf4821aec202650fd8
Jakob Lund <jakob@avforlaget.dk>
2023-11-09 13:21:27 +0100

    docstring version_added

--- plugins/modules/ini_file.py
+++ plugins/modules/ini_file.py
@@ -61,6 +61,7 @@ options:
       - Among possibly multiple sections of the same name, select the one that contains these values.
       - With O(state=present), if a suitable section is not found, a new section will be added, including the required options.
       - With O(state=absent), at most one O(section) is removed if it contains the values.
+    version_added: 8.0.1
   option:
     description:
       - If set (required for changing a O(value)), this is the name of the option.
