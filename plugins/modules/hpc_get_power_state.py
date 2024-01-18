1f260e580d6b8510bbeb86843236a8cfb0c8ff25
Srujana-2000 <85608509+Srujana-2000@users.noreply.github.com>
2024-01-18 16:46:23 +0530

    Import update hpc_get_power_state.py

--- plugins/modules/hpc_get_power_state.py
+++ plugins/modules/hpc_get_power_state.py
@@ -97,7 +97,7 @@ csv:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.system_firmware_utils import CrayRedfishUtils
+from ansible_collections.community.general.plugins.module_utils.hpc_system_firmware_utils import CrayRedfishUtils
 from ansible.module_utils.common.text.converters import to_native
 
 # More will be added as module features are expanded
