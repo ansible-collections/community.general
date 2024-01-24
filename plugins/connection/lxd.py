13e3161f2ae24f851724fa3740907b32d4c8e3c2
Simon Deziel <simon.deziel@canonical.com>
2024-01-24 12:35:17 +0000

    Refer to LXD containers/VMs as instances (#7873)
    * plugins/connection/lxd: rename container to instance
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    
    * plugins/inventory/lxd: rename container to instance
    
    It seems that a previous search and replace was done but it
    missed those `containe_name` due to missing `r` in `container`.
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>
    
    ---------
    
    Signed-off-by: Simon Deziel <simon.deziel@canonical.com>

--- plugins/connection/lxd.py
+++ plugins/connection/lxd.py
@@ -10,9 +10,9 @@ __metaclass__ = type
 DOCUMENTATION = '''
     author: Matt Clay (@mattclay) <matt@mystile.com>
     name: lxd
-    short_description: Run tasks in lxc containers via lxc CLI
+    short_description: Run tasks in LXD instances via C(lxc) CLI
     description:
-        - Run commands or put/fetch files to an existing lxc container using lxc CLI
+        - Run commands or put/fetch files to an existing instance using C(lxc) CLI.
     options:
       remote_addr:
         description:
@@ -26,7 +26,7 @@ DOCUMENTATION = '''
             - name: ansible_lxd_host
       executable:
         description:
-            - shell to use for execution inside container
+            - Shell to use for execution inside instance.
         default: /bin/sh
         vars:
             - name: ansible_executable
@@ -71,7 +71,7 @@ class Connection(ConnectionBase):
             raise AnsibleError("lxc command not found in PATH")
 
         if self._play_context.remote_user is not None and self._play_context.remote_user != 'root':
-            self._display.warning('lxd does not support remote_user, using container default: root')
+            self._display.warning('lxd does not support remote_user, using default: root')
 
     def _host(self):
         """ translate remote_addr to lxd (short) hostname """
