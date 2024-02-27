20540d9bea0546df669d3410d0d67603bdc5afc2
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-27 21:53:30 +0100

    Fix instruction fo debian 12 for htpasswd depends

--- plugins/modules/htpasswd.py
+++ plugins/modules/htpasswd.py
@@ -71,6 +71,7 @@ options:
 notes:
   - "This module depends on the C(passlib) Python library, which needs to be installed on all target systems."
   - "On Debian, Ubuntu, or Fedora: install C(python-passlib)."
+  - "On Debian 12: install C(python3-passlib)."
   - "On RHEL or CentOS: Enable EPEL, then install C(python-passlib)."
 requirements: [ passlib>=1.6 ]
 author: "Ansible Core Team"
