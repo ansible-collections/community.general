57e228147e0caef31744773d578bbaf72ce66b10
Kristof Wevers <kwevers@users.noreply.github.com>
2024-03-20 08:15:38 +0100

    Fix plugin call in example (#8114)

--- plugins/lookup/github_app_access_token.py
+++ plugins/lookup/github_app_access_token.py
@@ -49,7 +49,7 @@ EXAMPLES = '''
     dest: /srv/checkout
   vars:
     github_token: >-
-      lookup('github_app_token', key_path='/home/to_your/key',
+      lookup('community.general.github_app_access_token', key_path='/home/to_your/key',
              app_id='123456', installation_id='64209')
 '''
 
