03927e35ce164367daceb641c32e9e890412ea5e
Codey Schoettle <165081359+c-cschoettle@users.noreply.github.com>
2024-03-28 13:04:32 +0000

    Fixed versioning and documentation, added changelog fragment

--- plugins/modules/nmcli.py
+++ plugins/modules/nmcli.py
@@ -64,16 +64,16 @@ options:
             - Type V(infiniband) is added in community.general 2.0.0.
             - Type V(loopback) is added in community.general 8.1.0.
             - Type V(macvlan) is added in community.general 6.6.0.
-            - Type V(ovs-bridge) is added in community.general 8.5.0.
-            - Type V(ovs-interface) is added in community.general 8.5.0.
-            - Type V(ovs-port) is added in community.general 8.5.0.
+            - Type V(ovs-bridge) is added in community.general 8.6.0.
+            - Type V(ovs-interface) is added in community.general 8.6.0.
+            - Type V(ovs-port) is added in community.general 8.6.0.
             - Type V(wireguard) is added in community.general 4.3.0.
             - Type V(vpn) is added in community.general 5.1.0.
             - Using V(bond-slave), V(bridge-slave), or V(team-slave) implies V(ethernet) connection type with corresponding O(slave_type) option.
             - If you want to control non-ethernet connection attached to V(bond), V(bridge), or V(team) consider using O(slave_type) option.
         type: str
         choices: [ bond, bond-slave, bridge, bridge-slave, dummy, ethernet, generic, gre, infiniband, ipip, macvlan, sit, team, team-slave, vlan, vxlan,
-            wifi, gsm, wireguard, vpn, loopback ]
+            wifi, gsm, wireguard, ovs-bridge, ovs-port, ovs-interface, vpn, loopback ]
     mode:
         description:
             - This is the type of device or network connection that you wish to create for a bond or bridge.
