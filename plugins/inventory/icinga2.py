e4789e0f4a98106e542522e11c6e31e333a5e13b
Felix Fontein <felix@fontein.de>
2024-04-20 09:39:42 +0200

    inventory plugins: make wrapping variables as unsafe smarter to avoid triggering an AWX bug (#8225)
    Make wrapping variables as unsafe smarter to avoid triggering an AWX bug.
    
    (cherry picked from commit 7fd37ea247ba351b541d472cbedefc60fb98473f)

--- plugins/inventory/icinga2.py
+++ plugins/inventory/icinga2.py
@@ -96,7 +96,8 @@ from ansible.errors import AnsibleParserError
 from ansible.plugins.inventory import BaseInventoryPlugin, Constructable
 from ansible.module_utils.urls import open_url
 from ansible.module_utils.six.moves.urllib.error import HTTPError
-from ansible.utils.unsafe_proxy import wrap_var as make_unsafe
+
+from ansible_collections.community.general.plugins.plugin_utils.unsafe import make_unsafe
 
 
 class InventoryModule(BaseInventoryPlugin, Constructable):
