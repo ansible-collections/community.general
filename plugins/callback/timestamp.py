0abf6b32e747366a07c70d4f850934d29b173328
kurokobo <2920259+kurokobo@users.noreply.github.com>
2024-05-06 13:48:34 +0900

    fix: shorten long lines

--- plugins/callback/timestamp.py
+++ plugins/callback/timestamp.py
@@ -31,7 +31,9 @@ DOCUMENTATION = r"""
     format_string:
       description:
         - Format of the timestamp shown to user in 1989 C standard format.
-        - Refer to U(the Python documentation,https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes) for the available format codes.
+        - >
+          Refer to U(the Python documentation,https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes)
+          for the available format codes.
       ini:
         - section: callback_timestamp
           key: format_string
