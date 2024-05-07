20d7176f4d6f22f81643393233f7236b4bd1a0d4
zerchevack <51019718+zerchevack@users.noreply.github.com>
2024-05-07 12:07:18 +0200

    Fix sanity errors

--- plugins/modules/proxmox_backup_info.py
+++ plugins/modules/proxmox_backup_info.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2024, Leverrier Dylan leverrierd@gmail.com
+# Copyright (c) 2024, Leverrier Dylan <leverrierd@gmail.com>
 #
-# This file is part of Ansible
+# This module is part of Ansible, and is free software; you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3 of the License,
+# or (at your option) any later version.
 #
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# Ansible is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+# along with Ansible. If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
