3e67dcc69529764ac88fa35d8ab8e58018319424
Alexei Znamensky <russoz@gmail.com>
2024-05-12 13:53:00 +1200

    add stdout, stderr and cmd to django module output

--- plugins/module_utils/django.py
+++ plugins/module_utils/django.py
@@ -71,8 +71,12 @@ class DjangoBase(ModuleHelper):
                               check_rc=True)
         with runner() as ctx:
             results = ctx.run()
-            if self.verbosity >= 4:
+            self.vars.stdout = ctx.results_out
+            self.vars.stderr = ctx.results_err
+            self.vars.cmd = ctx.cmd
+            if self.verbosity >= 3:
                 self.vars.run_info = ctx.run_info
+
         return results
 
     @classmethod
