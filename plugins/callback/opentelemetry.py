0a0e132d80889a620b80f2849892cb8ff9d94240
Felix Fontein <felix@fontein.de>
2024-05-19 21:57:27 +0200

    [stabe-8] fix(opentelemetry): avoid storing inmemory if logs are disabled (#8373) (#8390)
    fix(opentelemetry): avoid storing inmemory if logs are disabled (#8373)
    
    * fix(opentelemetry): avoid storing inmemory if logs are disabled
    
    * changelog
    
    * fix syntax
    
    * refactor
    
    * chore
    
    * chore
    
    * chore
    
    * fix
    
    (cherry picked from commit d87b9fe0dce577aad2e58f56626a5df50839a884)
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- plugins/callback/opentelemetry.py
+++ plugins/callback/opentelemetry.py
@@ -498,6 +498,12 @@ class CallbackModule(CallbackBase):
         # See https://github.com/open-telemetry/opentelemetry-specification/issues/740
         self.traceparent = self.get_option('traceparent')
 
+    def dump_results(self, result):
+        """ dump the results if disable_logs is not enabled """
+        if self.disable_logs:
+            return ""
+        return self._dump_results(result._result)
+
     def v2_playbook_on_start(self, playbook):
         self.ansible_playbook = basename(playbook._file_name)
 
@@ -547,7 +553,7 @@ class CallbackModule(CallbackBase):
             self.tasks_data,
             status,
             result,
-            self._dump_results(result._result)
+            self.dump_results(result)
         )
 
     def v2_runner_on_ok(self, result):
@@ -555,7 +561,7 @@ class CallbackModule(CallbackBase):
             self.tasks_data,
             'ok',
             result,
-            self._dump_results(result._result)
+            self.dump_results(result)
         )
 
     def v2_runner_on_skipped(self, result):
@@ -563,7 +569,7 @@ class CallbackModule(CallbackBase):
             self.tasks_data,
             'skipped',
             result,
-            self._dump_results(result._result)
+            self.dump_results(result)
         )
 
     def v2_playbook_on_include(self, included_file):
