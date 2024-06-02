e477e50836440d0e277b1977963b32ade0201545
Thomas Gouverneur <thomas@espix.net>
2024-06-02 12:09:19 +0200

    Update plugins/doc_fragments/proxmox.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/doc_fragments/proxmox.py
+++ plugins/doc_fragments/proxmox.py
@@ -19,6 +19,7 @@ options:
   api_port:
     description:
       - Specify the target port of the Proxmox VE cluster.
+      - Uses the E(PROXMOX_PORT) environment variable if not specified.
     type: int
     required: false
     version_added: 9.1.0
