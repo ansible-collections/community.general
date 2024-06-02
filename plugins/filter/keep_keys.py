c97fc2d0b6ce3c0d5c0bdbb52d9c0ab1a598b3d2
Vladimir Botka <vbotka@gmail.com>
2024-06-02 17:58:28 +0200

    Add filter keep_keys. Implement feature request #8438

---
+++ plugins/filter/keep_keys.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 Vladimir Botka <vbotka@gmail.com>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = '''
+    name: keep_keys
+    short_description: Keep specific keys from dictionaries in a list
+    version_added: "9.1.0"
+    author: Vladimir Botka (@vbotka)
+    description: This filter keeps only specified keys from a provided list of dictionaries.
+    options:
+      _input:
+        description:
+          - A list of dictionaries.
+          - All keys must be strings.
+        type: list
+        elements: dictionary
+        required: true
+      target:
+        description:
+          - A list of keys or keys patterns to keep.
+          - The interpretation of O(target) depends on the option O(matching_parameter)
+          - Single item is required in O(target) list for O(matching_parameter=regex)
+          - The O(target) can be a string for O(matching_parameter=regex)
+        type: raw
+        required: true
+      matching_parameter:
+        description: Specify the matching option of target keys.
+        type: str
+        default: equal
+        choices:
+          equal: Matches keys of exactly one of the O(target) items.
+          starts_with: Matches keys that start with one of the O(target) items.
+          ends_with: Matches keys that end with one of the O(target) items.
+          regex:
+            - Matches keys that match the regular expresion provided in O(target).
+            - In this case, O(target) must be a regex string or a list with single regex string.
+'''
+
+EXAMPLES = '''
+  l:
+    - {k0_x0: A0, k1_x1: B0, k2_x2: [C0], k3_x3: foo}
+    - {k0_x0: A1, k1_x1: B1, k2_x2: [C1], k3_x3: bar}
+
+  # By default match equal keys.
+  t: [k0_x0, k1_x1]
+  r: "{{ l | keep_keys(target=t) }}"
+
+  # Match keys that starts with any of the items in the target.
+  t: [k0, k1]
+  r: "{{ l | keep_keys(target=t, matching_parameter='starts_with') }}"
+
+  # Match keys that ends with any of the items in target.
+  t: [x0, x1]
+  r: "{{ l | keep_keys(target=t, matching_parameter='ends_with') }}"
+
+  # Match keys by the regex.
+  t: ['^.*[01]_x.*$']
+  r: "{{ l | keep_keys(target=t, matching_parameter='regex') }}"
+
+  # Match keys by the regex. The regex does not have to be in list.
+  t: '^.*[01]_x.*$'
+  r: "{{ l | keep_keys(target=t, matching_parameter='regex') }}"
+
+  # The results of all examples are all the same.
+  r:
+    - {k0_x0: A0, k1_x1: B0}
+    - {k0_x0: A1, k1_x1: B1}
+'''
+
+RETURN = '''
+  _value:
+    description: The list of dictionaries with selected keys.
+    type: list
+    elements: dictionary
+'''
+
+from ansible_collections.community.general.plugins.plugin_utils.keys_filter import (
+    _keys_filter_params,
+    _keys_filter_target_str)
+
+
+def keep_keys(data, target=None, matching_parameter='equal'):
+    """keep specific keys from dictionaries in a list"""
+
+    # test parameters
+    _keys_filter_params(data, target, matching_parameter)
+    # test and transform target
+    tt = _keys_filter_target_str(target, matching_parameter)
+
+    if matching_parameter == 'equal':
+        def keep_key(key):
+            return key in tt
+    elif matching_parameter == 'starts_with':
+        def keep_key(key):
+            return key.startswith(tt)
+    elif matching_parameter == 'ends_with':
+        def keep_key(key):
+            return key.endswith(tt)
+    elif matching_parameter == 'regex':
+        def keep_key(key):
+            return tt.match(key) is not None
+
+    return [dict((k, v) for k, v in d.items() if keep_key(k)) for d in data]
+
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'keep_keys': keep_keys,
+        }
---
+++ plugins/plugin_utils/keys_filter.py
@@ -0,0 +1,105 @@
+ # Copyright (c) 2024 Vladimir Botka <vbotka@gmail.com>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import re
+
+from ansible.errors import AnsibleFilterError
+from ansible.module_utils.six import string_types
+from ansible.module_utils.common._collections_compat import Mapping, Sequence
+
+
+def _keys_filter_params(data, target, matching_parameter):
+    """test parameters:
+       * data must be a list of dictionaries. All keys must be strings.
+       * target must a non-empty sequence.
+       * matching_parameter is member of a list.
+    """
+
+    mp = matching_parameter
+    ml = ['equal', 'starts_with', 'ends_with', 'regex']
+
+    if not isinstance(data, Sequence):
+        msg = "First argument must be a list. %s is %s"
+        raise AnsibleFilterError(msg % (data, type(data)))
+
+    for elem in data:
+        if not isinstance(elem, Mapping):
+            msg = "The data items must be dictionaries. %s is %s"
+            raise AnsibleFilterError(msg % (elem, type(elem)))
+
+    for elem in data:
+        if not all(isinstance(item, string_types) for item in elem.keys()):
+            msg = "All keys must be strings. keys: %s"
+            raise AnsibleFilterError(msg % elem.keys())
+
+    if not isinstance(target, Sequence):
+        msg = ("The target must be a list. It can be a string if matching_parameter is regex."
+               "target is %s.")
+        raise AnsibleFilterError(msg % target)
+
+    if len(target) == 0:
+        msg = ("The target can't be empty.")
+        raise AnsibleFilterError(msg)
+
+    if mp not in ml:
+        msg = ("The matching_parameter must be one of %s. matching_parameter is %s")
+        raise AnsibleFilterError(msg % (ml, mp))
+
+    return
+
+
+def _keys_filter_target_str(target, matching_parameter):
+    """test:
+       * target is a list of strings, or
+       * target is a string or list with single string if matching_parameter=regex
+       convert and return:
+       * tuple of unique target items, or
+       * compiled regex if matching_parameter=regex
+    """
+
+    if type(target) is list:
+        for elem in target:
+            if not isinstance(elem, string_types):
+                msg = "The target items must be strings. %s is %s"
+                raise AnsibleFilterError(msg % (elem, type(elem)))
+
+    if matching_parameter == 'regex':
+        if isinstance(target, string_types):
+            r = target
+        else:
+            if len(target) > 1:
+                msg = ("Single item is required in the target list if matching_parameter is regex.")
+                raise AnsibleFilterError(msg)
+            else:
+                r = target[0]
+        try:
+            tt = re.compile(r)
+        except re.error:
+            msg = ("The target must be a valid regex if matching_parameter is regex."
+                   "tt is %s")
+            raise AnsibleFilterError(msg % tt)
+    else:
+        tt = tuple(target)
+
+    return tt
+
+
+def _keys_filter_target_dict(target, matching_parameter):
+    """test:
+       * target is a list of dictionaries
+       * ...
+    """
+
+    # TODO: Complete and use this in filter replace_kees
+
+    if type(target) is list:
+        for elem in target:
+            if not isinstance(elem, Mapping):
+                msg = "The target items must be dictionary. %s is %s"
+                raise AnsibleFilterError(msg % (elem, type(elem)))
+
+    return
