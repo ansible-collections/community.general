fd9ef5d079679c548ad9fab7cc8f1fa63d106414
Vladimir Botka <vbotka@gmail.com>
2024-06-02 22:52:43 +0200

    Update plugins/plugin_utils/keys_filter.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/plugin_utils/keys_filter.py
+++ plugins/plugin_utils/keys_filter.py
@@ -82,6 +82,8 @@ def _keys_filter_target_str(target, matching_parameter):
             msg = ("The target must be a valid regex if matching_parameter is regex."
                    "target is %s")
             raise AnsibleFilterError(msg % r)
+    elif isinstance(target, string_types):
+        tt = (target, )
     else:
         tt = tuple(target)
 
