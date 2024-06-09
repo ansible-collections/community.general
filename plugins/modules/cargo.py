903e024d601dfba3ffb7fc9e0c5077020d410008
Colin Nolan <cn580@alumni.york.ac.uk>
2024-06-09 17:16:50 +0000

    Updates copyright.

--- plugins/modules/cargo.py
+++ plugins/modules/cargo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Copyright (c) 2021 Radek Sprta <mail@radeksprta.eu>
+# Copyright (c) 2024 Colin Nolan <cn580@alumni.york.ac.uk>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
