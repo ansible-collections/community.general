76ea52c0cb50155d25a6c844a02450cbb33be677
Julian Euler <code@lithimlin.de>
2024-07-05 11:32:31 +0200

    proxmox: ensure values of `disk_volume` and `mount_volumes.*` dicts are strings

--- plugins/modules/proxmox.py
+++ plugins/modules/proxmox.py
@@ -763,6 +763,9 @@ class ProxmoxLxcAnsible(ProxmoxAnsible):
         if disk is not None:
             kwargs["disk_volume"] = parse_disk_string(disk)
         if "disk_volume" in kwargs:
+            if not all(isinstance(val, str) for val in kwargs["disk_volume"].values()):
+              self.module.warn("All disk_volume values must be strings. Converting non-string values to strings.")
+              kwargs["disk_volume"] = {key: str(val) for key, val in kwargs["disk_volume"].items()}
             disk_dict = build_volume(key="rootfs", **kwargs.pop("disk_volume"))
             kwargs.update(disk_dict)
         if memory is not None:
@@ -776,6 +779,9 @@ class ProxmoxLxcAnsible(ProxmoxAnsible):
         if "mount_volumes" in kwargs:
             mounts_list = kwargs.pop("mount_volumes")
             for mount_config in mounts_list:
+                if not all(isinstance(val, str) for val in mount_config.values()):
+                  self.module.warn("All mount_volumes values must be strings. Converting non-string values to strings.")
+                  mount_config = {key: str(val) for key, val in mount_config.items()}
                 key = mount_config.pop("id")
                 mount_dict = build_volume(key=key, **mount_config)
                 kwargs.update(mount_dict)
