c757e7f5f8e7452d1c770415285fc92318ddab60
Wilfried ROSET <wilfriedroset@users.noreply.github.com>
2024-08-12 07:42:05 +0200

    fix(opentelemetry): adjust default value for `store_spans_in_file` causing traces to be produced to a file named `None` (#8741)
    * fix(opentelemetry): adjust default value for `store_spans_in_file` causing traces to be produced to a file named `None`
    
    The commit 5f481939d introduced `store_spans_in_file` with the default
    value `None` as a string. This causes the value of `store_spans_in_file`
    to be a not empty string, value=None as a string and not a null value.
    The rest of the code check if the store_spans_in_file is not null which
    squeezes the rest of the code. The following commit set the default
    value as an empty string.
    
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>
    
    * fix(opentelemetry): No default value is better, reword changelog
    
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- plugins/callback/opentelemetry.py
+++ plugins/callback/opentelemetry.py
@@ -85,7 +85,6 @@ DOCUMENTATION = '''
             key: disable_attributes_in_logs
         version_added: 7.1.0
       store_spans_in_file:
-        default: None
         type: str
         description:
           -  It stores the exported spans in the given file
