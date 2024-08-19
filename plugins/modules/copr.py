a05989fed381348702b4f8caba1a4aa7b8870b97
shubhamsugara22 <shubhamsugara22@gmail.com>
2024-08-20 01:49:29 +0530

    Limit package for Copr using includepkgs

--- plugins/modules/copr.py
+++ plugins/modules/copr.py
@@ -260,7 +260,8 @@ class CoprModule(object):
         if not repo_content:
             repo_content = self._download_repo_info()
         if self.ansible_module.params.get("includepkgs"):
-            repo_content += "\nincludepkgs={}\n".format(self.ansible_module.params['includepkgs'])
+            includepkgs = self.ansible_module.params['includepkgs']
+            repo_content += f"\nincludepkgs={includepkgs}\n"
         if self._compare_repo_content(repo_filename_path, repo_content):
             return False
         if not self.check_mode:
