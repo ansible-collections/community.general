9f6962e930f9348e58542b7e8d7daefb15a9dc78
abakanovskii <abakanovskii@astralinux.ru>
2024-09-25 20:16:04 +0300

    PR Doc fixes 2

--- plugins/modules/ipa_host.py
+++ plugins/modules/ipa_host.py
@@ -81,7 +81,7 @@ options:
     type: str
   force_creation:
     description:
-    - Create host if O("state=disabled") or O("state=enabled") but not present.
+    - Create host if O(state=disabled) or O(state=enabled) but not present.
     default: false
     type: bool
   update_dns:
