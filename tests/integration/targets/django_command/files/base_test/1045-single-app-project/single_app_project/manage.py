5e11736180101855df9ec176bda8496c54bb96ff
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2024-09-30 19:45:32 +1300

    python_runner/django_command: bugfixes (#8944)
    * python_runner/django_command: bugfixes
    
    * fix indentation
    
    * join path_prefix with : when concatenating with PATH
    
    * add changelog frag

---
+++ tests/integration/targets/django_command/files/base_test/1045-single-app-project/single_app_project/manage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+# single_app_project/manage.py
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'single_app_project.core.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
