3de46821939bf767ba9fac337e2f72f1b779fe50
alexander <79072457+abakanovskii@users.noreply.github.com>
2024-10-10 23:03:30 +0300

    krb_ticket: Create module (#8953)
    * Add kutils module
    
    * PR Fixes
    
    * PR Fixes 2
    
    * PR Fixes
    
    * Fix executables
    
    * Fix comment
    
    * Fix functions
    
    * PR Fix
    
    * PR Fix 2
    
    * Fix list name
    
    * Fix list name 2
    
    * Rever check_for_none func
    
    * Rever check_for_none func 2
    
    * Update tests
    
    * Update tests 2
    
    * Fix principal
    
    * Fix cmdrunner args
    
    * Fix multiline
    
    * Fix backslash
    
    * Fix tests
    
    * Fix elif
    
    * Fix bool arg
    
    * Update doc
    
    * Fix doc
    
    * Add man reference
    
    * Fix doc YAML-quoting
    
    * PR Fixes
    
    * Fix indent
    
    * Fix version_added and name
    
    * Fix units name
    
    * Fix module name

---
+++ tests/unit/plugins/modules/test_krb_ticket.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Alexei Znamensky (russoz@gmail.com)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+from ansible_collections.community.general.plugins.modules import krb_ticket
+from .helper import Helper, RunCommandMock  # pylint: disable=unused-import
+
+
+Helper.from_module(krb_ticket, __name__)
