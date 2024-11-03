d69f329e25b60bebb4394a9ff102c27e5cd9ebde
greg <marianigregory@pm.me>
2024-11-03 20:01:19 +0100

    Update datadog downtime module with datadog API v2 (#9080)

--- plugins/modules/datadog_downtime.py
+++ plugins/modules/datadog_downtime.py
@@ -7,8 +7,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-__metaclass__ = type
-
 DOCUMENTATION = """
 ---
 module: datadog_downtime
@@ -16,12 +14,12 @@ short_description: Manages Datadog downtimes
 version_added: 2.0.0
 description:
   - Manages downtimes within Datadog.
-  - Options as described on U(https://docs.datadoghq.com/api/v1/downtimes/).
+  - Options as described on U(https://docs.datadoghq.com/api/v2/downtimes/).
 author:
   - Datadog (@Datadog)
 requirements:
   - datadog-api-client
-  - Python 3.6+
+  - Python 3.7+
 extends_documentation_fragment:
   - community.general.attributes
 attributes:
@@ -68,10 +66,17 @@ options:
         elements: str
     scope:
         description:
-          - A list of scopes to which the downtime applies.
-          - The resulting downtime applies to sources that matches ALL provided scopes.
-        type: list
-        elements: str
+          - A scope to which the downtime applies.
+          - For example, 'host:app2' or 'env:staging'
+        type: str
+    duration:
+        description:
+          - The duration of the downtime.
+        type: str
+    monitor_identifier:
+        description:
+          - The monitor identifier configuration.
+        type: dict
     monitor_id:
         description:
           - The ID of the monitor to mute. If not provided, the downtime applies to all monitors.
@@ -81,6 +86,10 @@ options:
           - A message to include with notifications for this downtime.
           - Email notifications can be sent to specific users by using the same "@username" notation as events.
         type: str
+    mute_first_recovery_notification:
+        description:
+          - If true, mutes the first recovery notification.
+        type: bool
     start:
         type: int
         description:
@@ -125,47 +134,62 @@ EXAMPLES = """
 
 RETURN = """
 # Returns the downtime JSON dictionary from the API response under the C(downtime) key.
-# See https://docs.datadoghq.com/api/v1/downtimes/#schedule-a-downtime for more details.
+# See https://docs.datadoghq.com/api/v2/downtimes/#schedule-a-downtime for more details.
 downtime:
     description: The downtime returned by the API.
     type: dict
     returned: always
-    sample: {
-        "active": true,
-        "canceled": null,
-        "creator_id": 1445416,
-        "disabled": false,
-        "downtime_type": 2,
-        "end": null,
-        "id": 1055751000,
-        "message": "Downtime for foo:bar",
-        "monitor_id": null,
-        "monitor_tags": [
-            "foo:bar"
-        ],
-        "parent_id": null,
-        "recurrence": null,
-        "scope": [
-            "test"
-        ],
-        "start": 1607015009,
-        "timezone": "UTC",
-        "updater_id": null
-    }
+    sample:
+        active: true
+        canceled: null
+        creator_id: 1445416
+        disabled: false
+        downtime_type: 2
+        end: null
+        id: 1055751000
+        message: "Downtime for foo:bar"
+        monitor_id: null
+        monitor_tags:
+            - "foo:bar"
+        parent_id: null
+        recurrence: null
+        scope:
+            - "test"
+        start: 1607015009
+        timezone: "UTC"
+        updater_id: null
+        monitor_identifier:
+            monitor_id: 12345
+        mute_first_recovery_notification: false
+        schedule:
+            start: 1607015009
+            end: null
+            timezone: "UTC"
+            recurrences:
+                - type: "rrule"
+                  rrule: "FREQ=MONTHLY;BYMONTHDAY=1"
+                  duration: "PT1H"
 """
 
+__metaclass__ = type
 import traceback
-
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+
 # Import Datadog
 
 DATADOG_IMP_ERR = None
 HAS_DATADOG = True
 try:
-    from datadog_api_client.v1 import Configuration, ApiClient, ApiException
-    from datadog_api_client.v1.api.downtimes_api import DowntimesApi
-    from datadog_api_client.v1.model.downtime import Downtime
-    from datadog_api_client.v1.model.downtime_recurrence import DowntimeRecurrence
+    from datadog_api_client.v2.api.downtimes_api import DowntimesApi
+    from datadog_api_client.api_client import ApiClient
+    from datadog_api_client.configuration import Configuration
+    from datadog_api_client.exceptions import ApiException
+    from datadog_api_client.v2.model.downtime_create_request_attributes import DowntimeCreateRequestAttributes
+    from datadog_api_client.v2.model.downtime_create_request_data import DowntimeCreateRequestData
+    from datadog_api_client.v2.model.downtime_create_request import DowntimeCreateRequest
+    from datadog_api_client.v2.model.downtime_schedule_recurrence_create_update_request import (
+        DowntimeScheduleRecurrenceCreateUpdateRequest
+    )
 except ImportError:
     DATADOG_IMP_ERR = traceback.format_exc()
     HAS_DATADOG = False
@@ -177,22 +201,26 @@ def main():
             api_key=dict(required=True, no_log=True),
             api_host=dict(required=False, default="https://api.datadoghq.com"),
             app_key=dict(required=True, no_log=True),
-            state=dict(required=False, choices=["present", "absent"], default="present"),
+            state=dict(required=False, choices=[
+                       "present", "absent"], default="present"),
             monitor_tags=dict(required=False, type="list", elements="str"),
-            scope=dict(required=False, type="list", elements="str"),
+            scope=dict(required=False, type="str"),
             monitor_id=dict(required=False, type="int"),
+            monitor_identifier=dict(required=False, type="dict"),
             downtime_message=dict(required=False, no_log=True),
+            mute_first_recovery_notification=dict(required=False, type="bool"),
             start=dict(required=False, type="int"),
             end=dict(required=False, type="int"),
             timezone=dict(required=False, type="str"),
             rrule=dict(required=False, type="str"),
+            duration=dict(required=False, type="str"),
             id=dict(required=False, type="int"),
         )
     )
 
-    # Prepare Datadog
     if not HAS_DATADOG:
-        module.fail_json(msg=missing_required_lib("datadog-api-client"), exception=DATADOG_IMP_ERR)
+        module.fail_json(msg=missing_required_lib(
+            "datadog-api-client"), exception=DATADOG_IMP_ERR)
 
     configuration = Configuration(
         host=module.params["api_host"],
@@ -207,109 +235,157 @@ def main():
         )
         api_instance = DowntimesApi(api_client)
 
-        # Validate api and app keys
         try:
             api_instance.list_downtimes(current_only=True)
         except ApiException as e:
-            module.fail_json(msg="Failed to connect Datadog server using given app_key and api_key: {0}".format(e))
+            module.fail_json(
+                msg="Failed to connect Datadog server using given app_key and api_key: {0}".format(e))
 
-        if module.params["state"] == "present":
-            schedule_downtime(module, api_client)
-        elif module.params["state"] == "absent":
-            cancel_downtime(module, api_client)
+        schedule_downtime(module, api_instance)
 
 
-def _get_downtime(module, api_client):
-    api = DowntimesApi(api_client)
-    downtime = None
-    if module.params["id"]:
-        try:
-            downtime = api.get_downtime(module.params["id"])
-        except ApiException as e:
-            module.fail_json(msg="Failed to retrieve downtime with id {0}: {1}".format(module.params["id"], e))
-    return downtime
+def _get_downtime(module, api_instance):
+    downtime_id = module.params.get("id")
+
+    if not downtime_id:
+        return None
+
+    try:
+        downtime = api_instance.get_downtime(downtime_id=str(downtime_id))
+        if downtime.data.attributes.status == 'disabled':
+            return None
+        return downtime
+    except ApiException as e:
+        if e.status == 404:
+            return None
+        else:
+            module.fail_json(
+                msg="Failed to get downtime %s: %s" % (downtime_id, e))
 
 
 def build_downtime(module):
-    downtime = Downtime()
-    if module.params["monitor_tags"]:
-        downtime.monitor_tags = module.params["monitor_tags"]
-    if module.params["scope"]:
-        downtime.scope = module.params["scope"]
-    if module.params["monitor_id"]:
-        downtime.monitor_id = module.params["monitor_id"]
-    if module.params["downtime_message"]:
-        downtime.message = module.params["downtime_message"]
-    if module.params["start"]:
-        downtime.start = module.params["start"]
-    if module.params["end"]:
-        downtime.end = module.params["end"]
-    if module.params["timezone"]:
-        downtime.timezone = module.params["timezone"]
-    if module.params["rrule"]:
-        downtime.recurrence = DowntimeRecurrence(
-            rrule=module.params["rrule"],
+    recurrence = None
+    if module.params.get("rrule"):
+        recurrence = DowntimeScheduleRecurrenceCreateUpdateRequest(
             type="rrule",
+            rrule=module.params.get("rrule"),
+            duration=module.params.get("duration")
         )
-    return downtime
+    body = DowntimeCreateRequest(
+        data=DowntimeCreateRequestData(
+            attributes=DowntimeCreateRequestAttributes(
+                message=module.params.get("downtime_message"),
+                monitor_identifier={"monitor_id": module.params.get(
+                    "monitor_id")} if module.params.get("monitor_id") else None,
+                mute_first_recovery_notification=module.params.get(
+                    "mute_first_recovery_notification"),
+                scope=module.params.get("scope"),
+                monitor_tags=module.params.get("monitor_tags"),
+                schedule={
+                    "start": module.params.get("start"),
+                    "end": module.params.get("end"),
+                    "timezone": module.params.get("timezone"),
+                    "recurrences": [recurrence] if recurrence else None
+                }
+            ),
+            type="downtime"
+        )
+    )
+    return body
 
 
-def _post_downtime(module, api_client):
-    api = DowntimesApi(api_client)
+def _post_downtime(module, api_instance):
     downtime = build_downtime(module)
     try:
-        resp = api.create_downtime(downtime)
-        module.params["id"] = resp.id
-        module.exit_json(changed=True, downtime=resp.to_dict())
+        resp = api_instance.create_downtime(body=downtime)
+        module.params["id"] = resp.data.id
+        module.exit_json(changed=True, downtime=resp.data.to_dict())
     except ApiException as e:
         module.fail_json(msg="Failed to create downtime: {0}".format(e))
 
 
-def _equal_dicts(a, b, ignore_keys):
-    ka = set(a).difference(ignore_keys)
-    kb = set(b).difference(ignore_keys)
-    return ka == kb and all(a[k] == b[k] for k in ka)
+def _equal_dicts(a, b, ignore_keys=None):
+    if ignore_keys is None:
+        ignore_keys = set()
+
+    if isinstance(a, dict) and isinstance(b, dict):
+        ka = set(a).difference(ignore_keys)
+        kb = set(b).difference(ignore_keys)
+        if ka != kb:
+            return False
+        for k in ka:
+            if not _equal_dicts(a[k], b[k], ignore_keys):
+                return False
+        return True
+
+    elif isinstance(a, list) and isinstance(b, list):
+        if len(a) != len(b):
+            return False
+        return all(_equal_dicts(x, y, ignore_keys) for x, y in zip(a, b))
+
+    else:
+        return a == b
 
 
-def _update_downtime(module, current_downtime, api_client):
-    api = DowntimesApi(api_client)
+def _update_downtime(module, current_downtime, api_instance):
     downtime = build_downtime(module)
     try:
-        if current_downtime.disabled:
-            resp = api.create_downtime(downtime)
+        are_equal = _equal_dicts(
+            downtime.data.attributes.to_dict(),
+            current_downtime.data.attributes.to_dict(),
+            {"active", "creator_id", "updater_id", "status"}
+        )
+        if are_equal:
+            module.exit_json(
+                changed=False, downtime=current_downtime.to_dict())
         else:
-            resp = api.update_downtime(module.params["id"], downtime)
-        if _equal_dicts(
-                resp.to_dict(),
-                current_downtime.to_dict(),
-                ["active", "creator_id", "updater_id"]
-        ):
-            module.exit_json(changed=False, downtime=resp.to_dict())
-        else:
+            resp = api_instance.update_downtime(
+                downtime_id=module.params["id"], body=downtime)
             module.exit_json(changed=True, downtime=resp.to_dict())
     except ApiException as e:
-        module.fail_json(msg="Failed to update downtime: {0}".format(e))
+
+        module.fail_json(msg="Failed to update downtime: %s" % (e,))
 
 
-def schedule_downtime(module, api_client):
-    downtime = _get_downtime(module, api_client)
-    if downtime is None:
-        _post_downtime(module, api_client)
+def schedule_downtime(module, api_instance):
+    downtime = _get_downtime(module, api_instance)
+
+    if module.params["state"] == "absent":
+        if downtime is None:
+            module.exit_json(
+                changed=False,
+                msg="Downtime %s does not exist" % (module.params['id'],)
+            )
+        else:
+            cancel_downtime(module, api_instance)
     else:
-        _update_downtime(module, downtime, api_client)
+        if downtime is None:
+            _post_downtime(module, api_instance)
+        else:
+            _update_downtime(module, downtime, api_instance)
 
 
-def cancel_downtime(module, api_client):
-    downtime = _get_downtime(module, api_client)
-    api = DowntimesApi(api_client)
-    if downtime is None:
-        module.exit_json(changed=False)
+def cancel_downtime(module, api_instance):
+    downtime_id = module.params["id"]
+
+    if not downtime_id:
+        module.fail_json(msg="id parameter is required for state=absent")
+
     try:
-        api.cancel_downtime(downtime["id"])
+        api_instance.cancel_downtime(downtime_id=str(downtime_id))
+        module.exit_json(
+            changed=True,
+            id=downtime_id,
+            msg="Downtime %s cancelled successfully" % (downtime_id,)
+        )
     except ApiException as e:
-        module.fail_json(msg="Failed to create downtime: {0}".format(e))
-
-    module.exit_json(changed=True)
+        if e.status == 404:
+            module.exit_json(
+                changed=False,
+                msg="Downtime %s does not exist" % (downtime_id)
+            )
+        module.fail_json(
+            msg="Failed to cancel downtime %s: %s") % (downtime_id, e)
 
 
 if __name__ == "__main__":
