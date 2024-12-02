0911abfb6d5765a351f6ddde1952a1556bafc891
jurelou <louis@jurczyk.fr>
2024-12-02 08:09:25 +0100

    Update plugins/modules/iso_extract.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/iso_extract.py
+++ plugins/modules/iso_extract.py
@@ -71,7 +71,7 @@ options:
     description:
     - Password used to decrypt files from the ISO.
     - Will only be used if 7z is used.
-    - Password is used as an argument to 7z. This is a security risk that allows password to be revealed.
+    - The password is used as a command line argument to 7z. This is a **potential security risk** that allows passwords to be revealed if someone else can list running processes on the same machine in the right moment.
     type: str
     version_added: 10.1.0
 notes:
