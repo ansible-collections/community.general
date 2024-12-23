f02631b1cfcab2a9322fd3cb203ece0a0bd46408
Alexei Znamensky <russoz@gmail.com>
2024-12-24 10:08:40 +1300

    replace tab with spaces in test yamls

--- plugins/modules/xfconf_info.py
+++ plugins/modules/xfconf_info.py
@@ -124,7 +124,7 @@ version:
   returned: success
   type: str
   sample: 4.18.1
-  vesion_added: 10.2.0
+  version_added: 10.2.0
 """
 
 from ansible_collections.community.general.plugins.module_utils.module_helper import ModuleHelper
