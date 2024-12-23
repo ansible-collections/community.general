94c73f52c94b7a27eba42637e5389694d91db59d
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2024-12-24 09:30:11 +1300

    utm*: normalize docs (#9335)
    * utm*: normalize docs
    
    * Update plugins/modules/utm_aaa_group.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit c141f8688305c78c0dc53636f6906dc879221915)

--- plugins/modules/utm_network_interface_address.py
+++ plugins/modules/utm_network_interface_address.py
@@ -8,62 +8,58 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-DOCUMENTATION = '''
----
+DOCUMENTATION = r"""
 module: utm_network_interface_address
 
 author:
-    - Juergen Wiebe (@steamx)
+  - Juergen Wiebe (@steamx)
 
 short_description: Create, update or destroy network/interface_address object
 
 description:
-    - Create, update or destroy a network/interface_address object in SOPHOS UTM.
-    - This module needs to have the REST Ability of the UTM to be activated.
-
+  - Create, update or destroy a network/interface_address object in SOPHOS UTM.
+  - This module needs to have the REST Ability of the UTM to be activated.
 attributes:
-    check_mode:
-        support: none
-    diff_mode:
-        support: none
+  check_mode:
+    support: none
+  diff_mode:
+    support: none
 
 options:
-    name:
-        type: str
-        description:
-          - The name of the object. Will be used to identify the entry
-        required: true
-    address:
-        type: str
-        description:
-          - The ip4 address of the network/interface_address object.
-        required: true
-    address6:
-        type: str
-        description:
-          - The ip6 address of the network/interface_address object.
-        required: false
-    comment:
-        type: str
-        description:
-          - An optional comment to add to the object
-        default: ''
-    resolved:
-        type: bool
-        description:
-          - Whether or not the object is resolved
-    resolved6:
-        type: bool
-        description:
-          - Whether or not the object is resolved
-
+  name:
+    type: str
+    description:
+      - The name of the object. Will be used to identify the entry.
+    required: true
+  address:
+    type: str
+    description:
+      - The ip4 address of the network/interface_address object.
+    required: true
+  address6:
+    type: str
+    description:
+      - The ip6 address of the network/interface_address object.
+    required: false
+  comment:
+    type: str
+    description:
+      - An optional comment to add to the object.
+    default: ''
+  resolved:
+    type: bool
+    description:
+      - Whether or not the object is resolved.
+  resolved6:
+    type: bool
+    description:
+      - Whether or not the object is resolved.
 extends_documentation_fragment:
-- community.general.utm
-- community.general.attributes
+  - community.general.utm
+  - community.general.attributes
+"""
 
-'''
-
-EXAMPLES = """
+EXAMPLES = r"""
 - name: Create a network interface address
   utm_proxy_backend:
     utm_host: sophos.host.name
@@ -81,39 +77,39 @@ EXAMPLES = """
     state: absent
 """
 
-RETURN = """
+RETURN = r"""
 result:
-    description: The utm object that was created
-    returned: success
-    type: complex
-    contains:
-        _ref:
-            description: The reference name of the object
-            type: str
-        _locked:
-            description: Whether or not the object is currently locked
-            type: bool
-        _type:
-            description: The type of the object
-            type: str
-        name:
-            description: The name of the object
-            type: str
-        address:
-             description: The ip4 address of the network/interface_address object
-             type: str
-        address6:
-             description: The ip6 address of the network/interface_address object
-             type: str
-        comment:
-            description: The comment string
-            type: str
-        resolved:
-             description: Whether or not the object is resolved
-             type: bool
-        resolved6:
-             description: Whether or not the object is resolved
-             type: bool
+  description: The utm object that was created.
+  returned: success
+  type: complex
+  contains:
+    _ref:
+      description: The reference name of the object.
+      type: str
+    _locked:
+      description: Whether or not the object is currently locked.
+      type: bool
+    _type:
+      description: The type of the object.
+      type: str
+    name:
+      description: The name of the object.
+      type: str
+    address:
+      description: The ip4 address of the network/interface_address object.
+      type: str
+    address6:
+      description: The ip6 address of the network/interface_address object.
+      type: str
+    comment:
+      description: The comment string.
+      type: str
+    resolved:
+      description: Whether or not the object is resolved.
+      type: bool
+    resolved6:
+      description: Whether or not the object is resolved.
+      type: bool
 """
 
 from ansible_collections.community.general.plugins.module_utils.utm_utils import UTM, UTMModule
