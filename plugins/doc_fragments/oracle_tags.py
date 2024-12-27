2da9f092a8bcfddedfbff73f2ac723108558a6af
Alexei Znamensky <russoz@gmail.com>
2024-12-27 22:53:50 +1300

    [m-z]*.py: normalize doc_fragments

--- plugins/doc_fragments/oracle_tags.py
+++ plugins/doc_fragments/oracle_tags.py
@@ -8,16 +8,14 @@ __metaclass__ = type
 
 
 class ModuleDocFragment(object):
-    DOCUMENTATION = """
-    options:
-        defined_tags:
-            description: Defined tags for this resource. Each key is predefined and scoped to a namespace. For more
-                         information, see
-                         U(https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm).
-            type: dict
-        freeform_tags:
-            description: Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name,
-                         type, or namespace. For more information, see
-                         U(https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm).
-            type: dict
-    """
+    DOCUMENTATION = r"""
+options:
+  defined_tags:
+    description: Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see
+      U(https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm).
+    type: dict
+  freeform_tags:
+    description: Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace.
+      For more information, see U(https://docs.us-phoenix-1.oraclecloud.com/Content/General/Concepts/resourcetags.htm).
+    type: dict
+"""
