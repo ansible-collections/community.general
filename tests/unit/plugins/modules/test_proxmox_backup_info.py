62f4e9fb72324e1c35920e998fbf6b6b698669b0
raoufnezhad <72685312+raoufnezhad@users.noreply.github.com>
2025-01-13 08:24:34 +0330

    Update tests/unit/plugins/modules/test_proxmox_backup_info.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- tests/unit/plugins/modules/test_proxmox_backup_info.py
+++ tests/unit/plugins/modules/test_proxmox_backup_info.py
@@ -13,10 +13,6 @@ import sys
 import pytest
 
 proxmoxer = pytest.importorskip("proxmoxer")
-mandatory_py_version = pytest.mark.skipif(
-    sys.version_info < (2, 7),
-    reason="The proxmoxer dependency requires python2.7 or higher",
-)
 
 from ansible_collections.community.general.plugins.modules import proxmox_backup_info
 from ansible_collections.community.general.tests.unit.compat.mock import patch
