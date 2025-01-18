a6bec27e55aa1b877d936f3d5e9c29e3d151d0e4
Stanislav Meduna <stano@meduna.org>
2025-01-18 16:58:10 +0100

    remove unneeded __future__ imports

--- plugins/filter/json_patch.py
+++ plugins/filter/json_patch.py
@@ -3,7 +3,7 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import absolute_import, annotations, division, print_function
+from __future__ import annotations
 from json import loads
 from typing import TYPE_CHECKING
 from ansible.errors import AnsibleFilterError
