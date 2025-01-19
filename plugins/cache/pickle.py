cd82cbae0a140695cbfdc46903cd711885743b28
Alexei Znamensky <russoz@gmail.com>
2025-01-20 02:34:44 +1300

    action/become/cache/callback: adjust import __future__ for Python3

--- plugins/cache/pickle.py
+++ plugins/cache/pickle.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
+from __future__ import annotations
 __metaclass__ = type
 
 DOCUMENTATION = r"""
