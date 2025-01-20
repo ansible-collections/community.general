b85d36a01fdc2c9c160254e4b2e0c65f37993234
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-01-21 07:37:14 +1300

    filter plugins: adjust import __future__ for Python3, remove __metaclass__ (#9585)
    * filter plugins: adjust import __future__ for Python3, remove __metaclass__
    
    * rollback chg in test file
    
    * add changelog frag

--- plugins/filter/lists.py
+++ plugins/filter/lists.py
@@ -3,8 +3,7 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
+from __future__ import annotations
 
 from ansible.errors import AnsibleFilterError
 from ansible.module_utils.common.collections import is_sequence
