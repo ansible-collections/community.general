dd02d72bb9147260d5fd2c389ed8efe7d7a8737a
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-01-21 07:37:14 +1300

    filter plugins: adjust import __future__ for Python3, remove __metaclass__ (#9585)
    * filter plugins: adjust import __future__ for Python3, remove __metaclass__
    
    * rollback chg in test file
    
    * add changelog frag

--- plugins/filter/crc32.py
+++ plugins/filter/crc32.py
@@ -2,8 +2,7 @@
 # Copyright (c) 2022, Julien Riou <julien@riou.xyz>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+from __future__ import annotations
 
 from ansible.errors import AnsibleFilterError
 from ansible.module_utils.common.text.converters import to_bytes
--- plugins/filter/to_ini.py
+++ plugins/filter/to_ini.py
@@ -4,7 +4,7 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import absolute_import, division, print_function
+from __future__ import annotations
 
 DOCUMENTATION = r"""
 name: to_ini
@@ -49,8 +49,6 @@ _value:
   type: string
 """
 
-
-__metaclass__ = type
 
 from ansible.errors import AnsibleFilterError
 from ansible.module_utils.common._collections_compat import Mapping
