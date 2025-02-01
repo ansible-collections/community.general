7a0376eca3c5efe2942cd7ac0894541cfeb3dd49
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-02-02 02:17:58 +1300

    test helper: add support for check and diff modes (#9666)

--- tests/unit/plugins/modules/helper.py
+++ tests/unit/plugins/modules/helper.py
@@ -72,7 +72,13 @@ class Helper(object):
             """
             Run unit tests for each test case in self.test_cases
             """
-            patch_ansible_module(test_case.input)
+            args = {}
+            args.update(test_case.input)
+            if test_case.flags.get("check"):
+                args["_ansible_check_mode"] = test_case.flags.get("check")
+            if test_case.flags.get("diff"):
+                args["_ansible_diff"] = test_case.flags.get("diff")
+            patch_ansible_module(args)
             self.runner.run(mocker, capfd, test_case)
 
         self.add_func_to_test_module("test_module", _test_module)
