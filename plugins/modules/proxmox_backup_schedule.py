a48fe7ae667932469df95797888dac26b6afdf52
raoufnezhad <72685312+raoufnezhad@users.noreply.github.com>
2025-02-01 09:56:37 +0330

    Update  license in proxmox_backup_schedule.py

--- plugins/modules/proxmox_backup_schedule.py
+++ plugins/modules/proxmox_backup_schedule.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (c) 2025 Marzieh Raoufnezhad <raoufnezhad@gmail.com>
-# Copyright (c) 2024 Maryam Mayabi <mayabi.ahm at gmail.com>
+# Copyright (c) 2025 Maryam Mayabi <mayabi.ahm at gmail.com>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
