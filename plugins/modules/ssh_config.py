6eee545045c20e69dce2de8d3660494294167c4e
Stephen Bradshaw <sbradshaw@seek.com.au>
2025-02-04 11:52:57 +1100

    Fix missing and modified stuff

--- plugins/modules/ssh_config.py
+++ plugins/modules/ssh_config.py
@@ -11,18 +11,18 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: ssh_config
 short_description: Manage SSH config for user
 version_added: '2.0.0'
 description:
-    - Configures SSH hosts with special C(IdentityFile)s and hostnames.
+  - Configures SSH hosts with special C(IdentityFile)s and hostnames.
 author:
-    - Björn Andersson (@gaqzi)
-    - Abhijeet Kasurde (@Akasurde)
+  - Björn Andersson (@gaqzi)
+  - Abhijeet Kasurde (@Akasurde)
 extends_documentation_fragment:
-    - community.general.attributes
+  - community.general.attributes
 attributes:
   check_mode:
     support: full
@@ -33,7 +33,7 @@ options:
     description:
       - Whether a host entry should exist or not.
     default: present
-    choices: [ 'present', 'absent' ]
+    choices: ['present', 'absent']
     type: str
   user:
     description:
@@ -50,8 +50,7 @@ options:
   host:
     description:
       - The endpoint this configuration is valid for.
-      - Can be an actual address on the internet or an alias that will
-        connect to the value of O(hostname).
+      - Can be an actual address on the internet or an alias that will connect to the value of O(hostname).
     required: true
     type: str
   hostname:
@@ -68,17 +67,14 @@ options:
     type: str
   identity_file:
     description:
-      - The path to an identity file (SSH private key) that will be used
-        when connecting to this host.
+      - The path to an identity file (SSH private key) that will be used when connecting to this host.
       - File need to exist and have mode V(0600) to be valid.
     type: path
   identities_only:
     description:
-      - Specifies that SSH should only use the configured authentication
-        identity and certificate files (either the default files, or
-        those explicitly configured in the C(ssh_config) files or passed on
-        the ssh command-line), even if ssh-agent or a PKCS11Provider or
-        SecurityKeyProvider offers more identities.
+      - Specifies that SSH should only use the configured authentication identity and certificate files (either the default 
+        files, or those explicitly configured in the C(ssh_config) files or passed on the ssh command-line), even if C(ssh-agent)
+        or a C(PKCS11Provider) or C(SecurityKeyProvider) offers more identities.
     type: bool
     version_added: 8.2.0
   user_known_hosts_file:
@@ -89,7 +85,7 @@ options:
     description:
       - Whether to strictly check the host key when doing connections to the remote host.
       - The value V(accept-new) is supported since community.general 8.6.0.
-    choices: [ 'yes', 'no', 'ask', 'accept-new' ]
+    choices: ['yes', 'no', 'ask', 'accept-new']
     type: str
   proxycommand:
     description:
@@ -126,7 +122,7 @@ options:
   controlmaster:
     description:
       - Sets the C(ControlMaster) option.
-    choices: [ 'yes', 'no', 'ask', 'auto', 'autoask' ]
+    choices: ['yes', 'no', 'ask', 'auto', 'autoask']
     type: str
     version_added: 8.1.0
   controlpath:
@@ -139,6 +135,11 @@ options:
       - Sets the C(ControlPersist) option.
     type: str
     version_added: 8.1.0
+  dynamicforward:
+    description:
+      - Sets the C(DynamicForward) option.
+    type: str
+    version_added: 10.1.0
   other_options:
     description:
       - Provides the option to specify arbitrary general SSH config entry options via a dictionary
@@ -146,9 +147,9 @@ options:
     version_added: 10.4.0
 requirements:
 - paramiko
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 - name: Add a host in the configuration
   community.general.ssh_config:
     user: akasurde
@@ -165,9 +166,9 @@ EXAMPLES = r'''
     ssh_config_file: "{{ ssh_config_test }}"
     host: "example.com"
     state: absent
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 hosts_added:
   description: A list of host added.
   returned: success
@@ -203,7 +204,7 @@ hosts_change_diff:
       }
     }
   ]
-'''
+"""
 
 import os
 
@@ -279,6 +280,7 @@ class SSHConfig(object):
             controlmaster=self.params.get('controlmaster'),
             controlpath=self.params.get('controlpath'),
             controlpersist=fix_bool_str(self.params.get('controlpersist')),
+            dynamicforward=self.params.get('dynamicforward'),
         )
         if self.params.get('other_options'):
             for key, value in self.params.get('other_options').items():
@@ -387,6 +389,7 @@ def main():
             controlmaster=dict(type='str', default=None, choices=['yes', 'no', 'ask', 'auto', 'autoask']),
             controlpath=dict(type='str', default=None),
             controlpersist=dict(type='str', default=None),
+            dynamicforward=dict(type='str'),
             user=dict(default=None, type='str'),
             user_known_hosts_file=dict(type='str', default=None),
         ),
