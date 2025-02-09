90301ae021a9afc08e77586cb4b1eedc17072ace
Vladimir Botka <vbotka@gmail.com>
2025-02-09 01:55:31 +0100

    Fix DOCUMENTATION YAML 2-space indentation.

--- plugins/inventory/iocage.py
+++ plugins/inventory/iocage.py
@@ -7,90 +7,90 @@
 from __future__ import annotations
 
 DOCUMENTATION = r'''
-    name: iocage
-    short_description: iocage inventory source
-    version_added: 10.2.0
-    author:
-        - Vladimir Botka (@vbotka)
-    requirements:
-        - iocage >= 1.8
+name: iocage
+short_description: iocage inventory source
+version_added: 10.2.0
+author:
+  - Vladimir Botka (@vbotka)
+requirements:
+  - iocage >= 1.8
+description:
+  - Get inventory hosts from the iocage jail manager running on O(host).
+  - By default, O(host) is V(localhost). If O(host) is not V(localhost) it
+    is expected that the user running Ansible on the controller can
+    connect to the O(host) account O(user) with SSH non-interactively and
+    execute the command C(iocage list).
+  - Uses a configuration file as an inventory source, it must end
+    in C(.iocage.yml) or C(.iocage.yaml).
+extends_documentation_fragment:
+  - ansible.builtin.constructed
+  - ansible.builtin.inventory_cache
+options:
+  plugin:
     description:
-        - Get inventory hosts from the iocage jail manager running on O(host).
-        - By default, O(host) is V(localhost). If O(host) is not V(localhost) it
-          is expected that the user running Ansible on the controller can
-          connect to the O(host) account O(user) with SSH non-interactively and
-          execute the command C(iocage list).
-        - Uses a configuration file as an inventory source, it must end
-          in C(.iocage.yml) or C(.iocage.yaml).
-    extends_documentation_fragment:
-        - ansible.builtin.constructed
-        - ansible.builtin.inventory_cache
-    options:
-        plugin:
-            description:
-                - The name of this plugin, it should always be set to
-                  V(community.general.iocage) for this plugin to recognize
-                  it as its own.
-            required: true
-            choices: ['community.general.iocage']
-            type: str
-        host:
-            description: The IP/hostname of the C(iocage) host.
-            type: str
-            default: localhost
-        user:
-            description:
-                - C(iocage) user.
-                  It is expected that the O(user) is able to connect to the
-                  O(host) with SSH and execute the command C(iocage list).
-                  This option is not required if O(host) is V(localhost).
-            type: str
-        sudo:
-            description:
-                - Enable execution as root.
-                - This requires passwordless sudo of the command C(iocage list*).
-            type: bool
-            default: false
-            version_added: 10.3.0
-        sudo_preserve_env:
-            description:
-                - Preserve environment if O(sudo) is enabled.
-                - This requires C(SETENV) sudoers tag.
-            type: bool
-            default: false
-            version_added: 10.3.0
-        get_properties:
-            description:
-                - Get jails' properties.
-                  Creates dictionary C(iocage_properties) for each added host.
-            type: bool
-            default: false
-        env:
-            description:
-                - O(user)'s environment on O(host).
-                - Enable O(sudo_preserve_env) if O(sudo) is enabled.
-            type: dict
-            default: {}
-        hooks_results:
-            description:
-                - List of paths to the files in a jail.
-                - Content of the files is stored in the items of the list C(iocage_hooks).
-                - If a file is not available the item keeps the dash character C(-).
-                - The variable C(iocage_hooks) is not created if O(hooks_results) is empty.
-            type: list
-            elements: path
-            version_added: 10.4.0
-    notes:
-        - You might want to test the command C(ssh user@host iocage list -l) on
-          the controller before using this inventory plugin with O(user) specified
-          and with O(host) other than V(localhost).
-        - If you run this inventory plugin on V(localhost) C(ssh) is not used.
-          In this case, test the command C(iocage list -l).
-        - This inventory plugin creates variables C(iocage_*) for each added host.
-        - The values of these variables are collected from the output of the
-          command C(iocage list -l).
-        - The names of these variables correspond to the output columns.
-        - The column C(NAME) is used to name the added host.
+      - The name of this plugin, it should always be set to
+        V(community.general.iocage) for this plugin to recognize
+        it as its own.
+    required: true
+    choices: ['community.general.iocage']
+    type: str
+  host:
+    description: The IP/hostname of the C(iocage) host.
+    type: str
+    default: localhost
+  user:
+    description:
+      - C(iocage) user.
+        It is expected that the O(user) is able to connect to the
+        O(host) with SSH and execute the command C(iocage list).
+        This option is not required if O(host) is V(localhost).
+    type: str
+  sudo:
+    description:
+      - Enable execution as root.
+      - This requires passwordless sudo of the command C(iocage list*).
+    type: bool
+    default: false
+    version_added: 10.3.0
+  sudo_preserve_env:
+    description:
+      - Preserve environment if O(sudo) is enabled.
+      - This requires C(SETENV) sudoers tag.
+    type: bool
+    default: false
+    version_added: 10.3.0
+  get_properties:
+    description:
+      - Get jails' properties.
+        Creates dictionary C(iocage_properties) for each added host.
+    type: bool
+    default: false
+  env:
+    description:
+      - O(user)'s environment on O(host).
+      - Enable O(sudo_preserve_env) if O(sudo) is enabled.
+    type: dict
+    default: {}
+  hooks_results:
+    description:
+      - List of paths to the files in a jail.
+      - Content of the files is stored in the items of the list C(iocage_hooks).
+      - If a file is not available the item keeps the dash character C(-).
+      - The variable C(iocage_hooks) is not created if O(hooks_results) is empty.
+    type: list
+    elements: path
+    version_added: 10.4.0
+notes:
+  - You might want to test the command C(ssh user@host iocage list -l) on
+    the controller before using this inventory plugin with O(user) specified
+    and with O(host) other than V(localhost).
+  - If you run this inventory plugin on V(localhost) C(ssh) is not used.
+    In this case, test the command C(iocage list -l).
+  - This inventory plugin creates variables C(iocage_*) for each added host.
+  - The values of these variables are collected from the output of the
+    command C(iocage list -l).
+  - The names of these variables correspond to the output columns.
+  - The column C(NAME) is used to name the added host.
 '''
 
 EXAMPLES = r'''
