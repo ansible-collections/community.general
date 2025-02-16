86b79f4cf73f303d8a1fe2f5b9533f3e33d0f10a
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-02-16 18:10:03 +1300

    profitbricks: fix typo in deprecation text (#9755)

--- plugins/modules/profitbricks.py
+++ plugins/modules/profitbricks.py
@@ -20,7 +20,7 @@ deprecated:
   alternative: >
     Profitbricks has rebranded as Ionos Cloud and they provide a collection named ionoscloudsdk.ionoscloud.
     Whilst it is likely it will provide the features of this module, that has not been verified.
-    Please refer to that collectionś documentation for more details.
+    Please refer to that collection's documentation for more details.
 extends_documentation_fragment:
   - community.general.attributes
 attributes:
