767ae60d2b01dd050d9b1358ac28b60a7d0e30eb
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-02-17 07:39:24 +1300

    clc_*: deprecation (#9754)
    * clc_*: deprecation
    
    * Apply suggestions from code review
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit b80fa80c90b953f8e5416d077f9911a828b84b4b)

--- plugins/modules/clc_group.py
+++ plugins/modules/clc_group.py
@@ -14,6 +14,12 @@ module: clc_group
 short_description: Create/delete Server Groups at Centurylink Cloud
 description:
   - Create or delete Server Groups at Centurylink Centurylink Cloud.
+deprecated:
+  removed_in: 11.0.0
+  why: >
+    Lumen Public Cloud (formerly known as CenturyLink Cloud) has gone End-of-Life in September 2023.
+    See more at U(https://www.ctl.io/knowledge-base/release-notes/2023/lumen-public-cloud-platform-end-of-life-notice/?).
+  alternative: There is none.
 extends_documentation_fragment:
   - community.general.attributes
   - community.general.clc
