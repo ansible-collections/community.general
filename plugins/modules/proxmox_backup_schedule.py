f9e94451c26d11173cb198d5d452e8bafa33f1bf
raoufnezhad <72685312+raoufnezhad@users.noreply.github.com>
2025-03-03 13:28:07 +0330

    Update RETURN proxmox_backup_schedule.py

--- plugins/modules/proxmox_backup_schedule.py
+++ plugins/modules/proxmox_backup_schedule.py
@@ -113,6 +113,7 @@ backup_schedule:
     - If V(present), the backup_schedule will return True after adding the VM ID to the backup job.
     - If V(absent), the backup_schedule will return True after removing it from the backup job.
   returned: always, but can be empty
+  type: raw
 """
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
