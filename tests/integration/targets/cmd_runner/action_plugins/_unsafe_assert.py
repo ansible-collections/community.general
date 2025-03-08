39645831a17e984ae13eaf755cd10d06ce445bef
Felix Fontein <felix@fontein.de>
2025-03-08 14:07:25 +0100

    Adjust to Data Tagging.

--- tests/integration/targets/cmd_runner/action_plugins/_unsafe_assert.py
+++ tests/integration/targets/cmd_runner/action_plugins/_unsafe_assert.py
@@ -9,6 +9,12 @@ from ansible.errors import AnsibleError
 from ansible.playbook.conditional import Conditional
 from ansible.plugins.action import ActionBase
 
+try:
+    from ansible.utils.datatag import trust_value as _trust_value
+except ImportError:
+    def _trust_value(input):
+        return input
+
 
 class ActionModule(ActionBase):
     ''' Fail with custom message '''
@@ -36,12 +42,16 @@ class ActionModule(ActionBase):
 
         thats = self._task.args['that']
 
-        cond = Conditional(loader=self._loader)
         result['_ansible_verbose_always'] = True
 
         for that in thats:
-            cond.when = [str(self._make_safe(that))]
-            test_result = cond.evaluate_conditional(templar=self._templar, all_vars=task_vars)
+            if hasattr(self._templar, 'evaluate_conditional'):
+                trusted_that = _trust_value(that) if _trust_value else that
+                test_result = self._templar.evaluate_conditional(conditional=trusted_that)
+            else:
+                cond = Conditional(loader=self._loader)
+                cond.when = [str(self._make_safe(that))]
+                test_result = cond.evaluate_conditional(templar=self._templar, all_vars=task_vars)
             if not test_result:
                 result['failed'] = True
                 result['evaluated_to'] = test_result
