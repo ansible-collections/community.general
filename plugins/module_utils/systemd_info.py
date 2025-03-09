cbfbbd7d2720f600bd3d82ff430d4caa1fa8e5ed
Nocchia <133043574+NomakCooper@users.noreply.github.com>
2025-03-09 17:18:05 +0000

    systemd_info - fix runner

--- plugins/module_utils/systemd_info.py
+++ plugins/module_utils/systemd_info.py
@@ -10,32 +10,29 @@ __metaclass__ = type
 from ansible_collections.community.general.plugins.module_utils.cmd_runner import CmdRunner, cmd_runner_fmt
 
 
-systemd_info_arg_formats = {
-    'version':   cmd_runner_fmt.as_fixed("--version"),
-    'list_units': cmd_runner_fmt.as_fixed("list-units"),
-    'no_pager':  cmd_runner_fmt.as_fixed("--no-pager"),
-    'typ':       cmd_runner_fmt.as_fixed("--type"),
-    'types':     cmd_runner_fmt.as_func(lambda v: [",".join(v)] if isinstance(v, list) else [v]),
-    'all':       cmd_runner_fmt.as_fixed("--all"),
-    'plain':     cmd_runner_fmt.as_fixed("--plain"),
-    'no_legend': cmd_runner_fmt.as_fixed("--no-legend"),
-    'show':      cmd_runner_fmt.as_fixed("show"),
-    'p':         cmd_runner_fmt.as_fixed("-p"),
-    'props':     cmd_runner_fmt.as_list(),
-    'dashdash':  cmd_runner_fmt.as_fixed("--"),
-    'unit':      cmd_runner_fmt.as_list(),
-}
-
-
-default_systemd_info_args_order = "version list_units no_pager typ types all plain no_legend show p props dashdash unit"
-
-
 def systemd_info_runner(module, command, **kwargs):
-    return CmdRunner(
+    arg_formats = dict(
+        version=cmd_runner_fmt.as_fixed("--version"),
+        list_units=cmd_runner_fmt.as_fixed("list-units"),
+        no_pager=cmd_runner_fmt.as_fixed("--no-pager"),
+        typ=cmd_runner_fmt.as_fixed("--type"),
+        types=cmd_runner_fmt.as_func(lambda v: [] if not v else [",".join(v)]),
+        all=cmd_runner_fmt.as_fixed("--all"),
+        plain=cmd_runner_fmt.as_fixed("--plain"),
+        no_legend=cmd_runner_fmt.as_fixed("--no-legend"),
+        show=cmd_runner_fmt.as_fixed("show"),
+        p=cmd_runner_fmt.as_fixed("-p"),
+        props=cmd_runner_fmt.as_list(),
+        dashdash=cmd_runner_fmt.as_fixed("--"),
+        unit=cmd_runner_fmt.as_list(),
+    )
+
+    runner = CmdRunner(
         module,
         command=command,
-        arg_formats=systemd_info_arg_formats,
-        default_args_order=default_systemd_info_args_order,
+        arg_formats=arg_formats,
+        environ_update={'LC_ALL': 'C', 'LANG': 'C'},
         check_rc=True,
         **kwargs
     )
+    return runner
