7938295d1edc4fb75c7289aeb26b8714a4605ec7
Timur Gadiev <Timur_Gadiev@epam.com>
2025-04-07 12:09:31 +0400

    refactor: :fire: Remove unnecessary code parts

--- plugins/filter/to_prettytable.py
+++ plugins/filter/to_prettytable.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
 
 DOCUMENTATION = '''
   name: to_prettytable
