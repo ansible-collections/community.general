68f99c01b79a3fa7a437279b47f937033bb4e3c3
Marcos Alano <marcoshalano@gmail.com>
2025-04-17 08:22:39 -0300

    Fix xdg_mime_get since the command is different

--- plugins/module_utils/xdg_mime.py
+++ plugins/module_utils/xdg_mime.py
@@ -15,6 +15,7 @@ def xdg_mime_runner(module, **kwargs):
         module,
         command=['xdg-mime'],
         arg_formats=dict(
+            ## Don't know if work because that can be `default` to set but `query` to query
             default=cmd_runner_fmt.as_fixed('default'),
             mime_type=cmd_runner_fmt.as_list(),
             handler=cmd_runner_fmt.as_list(),
@@ -31,5 +32,5 @@ def xdg_mime_get(runner, mime_type):
         out = out.splitlines()[0]
         return out.split()[-1]
 
-    with runner("default mime_type", output_process=process) as ctx:
+    with runner("query mime_type", output_process=process) as ctx:
         return ctx.run(mime_type=mime_type)
