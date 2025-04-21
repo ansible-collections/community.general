6c29cb7109ed5f5a2c0f09f37aa5fc43fe13f231
Alexei Znamensky <russoz@gmail.com>
2025-04-21 13:12:43 +1200

    pipx: accept python version specs in parameter "name"

---
+++ plugins/module_utils/pkg_specs.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025, Alexei Znamensky <russoz@gmail.com>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from ansible.module_utils.six import raise_from
+
+from ansible_collections.community.general.plugins.module_utils import deps
+
+
+with deps.declare("packaging"):
+    from packaging.requirements import Requirement
+    from packaging.version import parse as parse_version, InvalidVersion
+
+
+class PackageVersion:
+    def __init__(self, module):
+        self.module = module
+
+    def parse_package_name(self, name):
+        """
+        Parse a package name that may include version specifiers using PEP 508.
+        Returns a tuple of (name, version_spec) where version_spec may be None.
+
+        Example inputs:
+            "package"                     -> ("package", None)
+            "package>=1.0"                -> ("package", ">=1.0")
+            "package>=1.0,<2.0"           -> ("package", ">=1.0,<2.0")
+            "package[extra]>=1.0"         -> ("package", "[extra]>=1.0")
+            "package[foo,bar]>=1.0,!=1.5" -> ("package", "[foo,bar]>=1.0,!=1.5")
+
+        :param name: Package name with optional version specifiers and extras
+        :return: Tuple of (name, version_spec)
+        :raises ValueError: If the package specification is invalid
+        """
+        if not name:
+            return name, None
+
+        # Quick check for simple package names
+        if not any(c in name for c in '>=<!~[]'):
+            return name.strip(), None
+
+        deps.validate(self.module, "packaging")
+
+        try:
+            req = Requirement(name)
+            return req.name, req
+
+        except Exception as e:
+            raise_from(ValueError(f"Invalid package specification for '{name}': {str(e)}"), e)
+
+    def version_fulfills_spec(self, version_str, requirement):
+        """
+        Check if a version string fulfills a version specifier.
+
+        :param version_str: Version string to check
+        :param requirement: Requirement object (packaging.requirements.Requirement)
+        :return: True if version fulfills the spec, False otherwise
+        :raises ValueError: If version_str or spec_str is invalid
+        """
+        # If no spec provided, any version is valid
+        if not requirement:
+            return True
+
+        try:
+            # Parse version string
+            ver = parse_version(version_str)
+
+            return ver in requirement.specifier
+
+        except InvalidVersion as e:
+            raise ValueError(f"Invalid version '{version_str}': {str(e)}")
--- plugins/modules/pipx.py
+++ plugins/modules/pipx.py
@@ -54,11 +54,13 @@ options:
   name:
     type: str
     description:
-      - The name of the application. In C(pipx) documentation it is also referred to as the name of the virtual environment
-        where the application will be installed.
+      - The name of the application and also the name of the Python package being installed.
+      - In C(pipx) documentation it is also referred to as the name of the virtual environment
+        where the application is installed.
       - If O(name) is a simple package name without version specifiers, then that name is used as the Python package name
         to be installed.
-      - Use O(source) for passing package specifications or installing from URLs or directories.
+      - Package specifiers may be passed along, for example O(name=tox<4.0.0).
+      - Use O(source) for installing from URLs or directories.
   source:
     type: str
     description:
@@ -202,6 +204,7 @@ version:
 
 from ansible_collections.community.general.plugins.module_utils.module_helper import StateModuleHelper
 from ansible_collections.community.general.plugins.module_utils.pipx import pipx_runner, pipx_common_argspec, make_process_list
+from ansible_collections.community.general.plugins.module_utils.pkg_specs import PackageVersion
 
 from ansible.module_utils.facts.compat import ansible_facts
 
@@ -258,16 +261,16 @@ class PipX(StateModuleHelper):
     use_old_vardict = False
 
     def _retrieve_installed(self):
-        name = _make_name(self.vars.name, self.vars.suffix)
-        output_process = make_process_list(self, include_injected=True, name=name)
+        name = _make_name(self.parsed_name, self.vars.suffix)
+        output_process = make_process_list(self, include_injected=True)
         installed = self.runner('_list global', output_process=output_process).run()
 
         if name is not None:
-            app_list = [app for app in installed if app['name'] == name]
-            if app_list:
-                return {name: app_list[0]}
-            else:
-                return {}
+            return {
+                k: v
+                for k, v in installed.items()
+                if k == name and self.package_version.version_fulfills_spec(v['version'], self.parsed_req)
+            }
 
         return installed
 
@@ -278,6 +281,8 @@ class PipX(StateModuleHelper):
             facts = ansible_facts(self.module, gather_subset=['python'])
             self.command = [facts['python']['executable'], '-m', 'pipx']
         self.runner = pipx_runner(self.module, self.command)
+        self.package_version = PackageVersion(self.module)
+        self.parsed_name, self.parsed_req = self.package_version.parse_package_name(self.vars.name)
 
         self.vars.set('application', self._retrieve_installed(), change=True, diff=True)
 
@@ -295,11 +300,15 @@ class PipX(StateModuleHelper):
         self.vars.set('run_info', ctx.run_info, verbosity=4)
 
     def state_install(self):
+        # If we have a version spec and no source, use the version spec as source
+        if self.parsed_req and not self.vars.source:
+            self.vars.source = self.vars.name
+
         if not self.vars.application or self.vars.force:
             self.changed = True
             args_order = 'state global index_url install_deps force python system_site_packages editable pip_args suffix name_source'
             with self.runner(args_order, check_mode_skip=True) as ctx:
-                ctx.run(name_source=[self.vars.name, self.vars.source])
+                ctx.run(name_source=[self.parsed_name, self.vars.source])
                 self._capture_results(ctx)
 
     state_present = state_install
