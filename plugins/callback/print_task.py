b42363562f529a16d9d23f8e5d372e17f36a8865
Max Mitschke <mmitschk@redhat.com>
2025-05-02 10:38:14 -0400

    Fix for failing tests

--- plugins/callback/print_task.py
+++ plugins/callback/print_task.py
@@ -1,19 +1,24 @@
-# Make coding more python3-ish, this is required for contributions to Ansible
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025, Max Mitschke <maxmitschke@fastmail.com>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = r'''
-author: Max Mitschke
 name: print_task
-callback_type: aggregate
-requirements:
-  - enable in configuration
+type: aggregate
 short_description: Prints playbook task snippet to job output
-version_added: "2.0"
 description:
   - This is an Ansible callback plugin that will print the currently executing
     playbook task to the job output.
+author:
+  - Max Mitschke
+requirements:
+  - enable in configuration
 '''
+
 
 import yaml
 
@@ -30,21 +35,18 @@ class CallbackModule(CallbackBase):
     CALLBACK_NEEDS_ENABLED = True
 
     def __init__(self):
-      super(CallbackModule, self).__init__()
-
-      self._printed_message = False
+        super(CallbackModule, self).__init__()
+        self._printed_message = False
 
     def _print_task(self, task):
-      task_yaml = yaml.dump(yaml.load(str([task._ds.copy()]), 
-                            Loader=yaml.Loader), 
-                            sort_keys=False)
-      
-      self._display.v(f"\n{task_yaml}\n")
-      self._printed_message = True
+        task_snippet = yaml.load(str([task._ds.copy()]), Loader=yaml.Loader)
+        task_yaml = yaml.dump(task_snippet, sort_keys=False)
+        self._display.v(f"\n{task_yaml}\n")
+        self._printed_message = True
 
     def v2_playbook_on_task_start(self, task, is_conditional):
-       self._printed_message = False
+        self._printed_message = False
 
     def v2_runner_on_start(self, host, task):
         if not self._printed_message:
-          self._print_task(task)
+            self._print_task(task)
