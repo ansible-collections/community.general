4e7695ab4bd58e2930afee7b32e53c7cc7e6e4c8
Max Mitschke <maxmitschke@fastmail.com>
2025-05-07 22:41:48 -0400

    Change output function to self._display.display()

--- plugins/callback/print_task.py
+++ plugins/callback/print_task.py
@@ -46,7 +46,7 @@ class CallbackModule(CallbackBase):
         if hasattr(task, '_ds'):
             task_snippet = load(str([task._ds.copy()]), Loader=SafeLoader)
             task_yaml = dump(task_snippet, sort_keys=False)
-            self._display.v(f"\n{task_yaml}\n")
+            self._display.display(f"\n{task_yaml}\n")
             self._printed_message = True
 
     def v2_playbook_on_task_start(self, task, is_conditional):
