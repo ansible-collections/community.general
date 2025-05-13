7dd25442171d454c1a92732ecf5c75ddaa1cea02
Yousefnezhad <s-yousefnezhad@um.ac.ir>
2025-05-13 09:41:41 +0330

    SPDX Format

--- plugins/modules/nfs_exports_info.py
+++ plugins/modules/nfs_exports_info.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright: (c) 2025, Samaneh Yousefnezhad <s-yousefenzhad@um.ac.ir>
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
