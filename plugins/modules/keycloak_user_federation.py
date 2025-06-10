62c706ef7809598ac92a6bb3d5f8d369807dd087
Christian Kohlmeyer <kohly@gmx.de>
2025-06-10 22:48:54 +0200

    support client credentials in all keycloak modules

--- plugins/modules/keycloak_user_federation.py
+++ plugins/modules/keycloak_user_federation.py
@@ -838,8 +838,8 @@ def main():
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True,
                            required_one_of=([['id', 'name'],
-                                             ['token', 'auth_realm', 'auth_username', 'auth_password']]),
-                           required_together=([['auth_realm', 'auth_username', 'auth_password']]),
+                                             ['token', 'auth_realm', 'auth_username', 'auth_password', 'auth_client_id', 'auth_client_secret']]),
+                           required_together=([['auth_username', 'auth_password']]),
                            required_by={'refresh_token': 'auth_realm'},
                            )
 
