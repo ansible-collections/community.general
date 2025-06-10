e478a85f15a6d0d8895ad74f41a890b66cd13aa2
Vladimir Botka <vbotka@gmail.com>
2025-06-10 07:41:41 +0200

    Updated copyright.

--- plugins/inventory/iocage.py
+++ plugins/inventory/iocage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2024-2025 Vladimir Botka <vbotka@gmail.com>
+# Copyright (c) 2024 Vladimir Botka <vbotka@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
