7f79e77c9b3e4402491bf9a125ba8af6c195842b
Christian Kohlmeyer <kohly@gmx.de>
2025-06-10 23:47:43 +0200

    fix typos in required list

--- plugins/modules/keycloak_component.py
+++ plugins/modules/keycloak_component.py
@@ -155,7 +155,7 @@ def main():
 
     module = AnsibleModule(argument_spec=argument_spec,
                            supports_check_mode=True,
-                           required_one_of=([['token', 'auth_realm', 'auth_username', 'auth_password', 'auth_client_id', 'auth_client_secrect']]),
+                           required_one_of=([['token', 'auth_realm', 'auth_username', 'auth_password', 'auth_client_id', 'auth_client_secret']]),
                            required_together=([['auth_username', 'auth_password']]),
                            required_by={'refresh_token': 'auth_realm'},
                            )
