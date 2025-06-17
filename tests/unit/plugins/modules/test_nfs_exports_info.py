1a99b2ae3ad272549b99757ea5b87a32bd47022f
Yousefnezhad <s-yousefnezhad@um.ac.ir>
2025-06-17 09:25:01 +0330

    Changed1

--- tests/unit/plugins/modules/test_nfs_exports_info.py
+++ tests/unit/plugins/modules/test_nfs_exports_info.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: (c) 2025, Samaneh Yousefnezhad <s-yousefenzhad@um.ac.ir>
+# Copyright (c) 2025, Samaneh Yousefnezhad <s-yousefenzhad@um.ac.ir>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
