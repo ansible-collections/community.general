5487e3cd2141ee9855cb9177d5d1afba9d6181c6
Klention Mali <klention@gmail.com>
2025-07-10 21:58:26 +0200

    Added ability to move data from one LVM Physical Volume to another on module lvm_pv

--- plugins/modules/lvm_pv.py
+++ plugins/modules/lvm_pv.py
@@ -14,20 +14,21 @@ module: lvm_pv
 short_description: Manage LVM Physical Volumes
 version_added: "11.0.0"
 description:
-  - Creates, resizes or removes LVM Physical Volumes.
+  - Creates, resizes, removes LVM PVs, or moves data between them.
 author:
   - Klention Mali (@klention)
 options:
   device:
     description:
       - Path to the block device to manage.
+      - Source device when O(state=move).
     type: path
     required: true
   state:
     description:
-      - Control if the physical volume exists.
+      - Control if the physical volume exists or if data should be move.
     type: str
-    choices: [present, absent]
+    choices: [present, absent, move]
     default: present
   force:
     description:
@@ -41,9 +42,15 @@ options:
       - Resize PV to device size when O(state=present).
     type: bool
     default: false
+  dest_device:
+    description:
+      - Path to the destination block device when moving data (O(state=move)).
+    type: path
 notes:
   - Requires LVM2 utilities installed on the target system.
-  - Device path must exist when creating a PV.
+  - Device paths must exist when creating a PV or moving data.
+  - For O(state=move), source and destination PVs must be in the same volume group.
+  - When moving data, the destination PV must have enough free space to accommodate the source PV's allocated extents.
 """
 
 EXAMPLES = r"""
@@ -66,6 +73,12 @@ EXAMPLES = r"""
     device: /dev/sdb
     force: true
     state: absent
+
+- name: Moving data from /dev/sdb to /dev/sdc (both in same VG)
+  community.general.lvm_pv:
+    device: /dev/sdb
+    dest_device: /dev/sdc
+    state: move
 """
 
 RETURN = r"""
@@ -87,6 +100,47 @@ def get_pv_size(module, device):
     cmd = ['pvs', '--noheadings', '--nosuffix', '--units', 'b', '-o', 'pv_size', device]
     rc, out, err = module.run_command(cmd, check_rc=True)
     return int(out.strip())
+
+
+def get_pv_vg(module, device):
+    """Get the VG name of a PV."""
+    cmd = ['pvs', '--noheadings', '-o', 'vg_name', device]
+    rc, out, err = module.run_command(cmd)
+    if rc != 0:
+        module.fail_json(msg="Failed to get VG for device %s: %s" % (device, err))
+    vg = out.strip()
+    return None if vg == '' else vg
+
+
+def get_pv_allocated_pe(module, device):
+    """Get count of allocated physical extents in a PV."""
+    cmd = ['pvs', '--noheadings', '-o', 'pv_pe_alloc_count', device]
+    rc, out, err = module.run_command(cmd)
+    if rc != 0:
+        module.fail_json(msg="Failed to get allocated PE count for device %s: %s" % (device, err))
+    try:
+        return int(out.strip())
+    except ValueError:
+        module.fail_json(msg="Invalid allocated PE count for device %s: %s" % (device, out))
+
+
+def get_pv_total_pe(module, device):
+    """Get total number of physical extents in a PV."""
+    cmd = ['pvs', '--noheadings', '-o', 'pv_pe_count', device]
+    rc, out, err = module.run_command(cmd)
+    if rc != 0:
+        module.fail_json(msg="Failed to get total PE count for device %s: %s" % (device, err))
+    try:
+        return int(out.strip())
+    except ValueError:
+        module.fail_json(msg="Invalid total PE count for device %s: %s" % (device, out))
+
+
+def get_pv_free_pe(module, device):
+    """Calculate free physical extents in a PV."""
+    total_pe = get_pv_total_pe(module, device)
+    allocated_pe = get_pv_allocated_pe(module, device)
+    return total_pe - allocated_pe
 
 
 def rescan_device(module, device):
@@ -112,17 +166,22 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             device=dict(type='path', required=True),
-            state=dict(type='str', default='present', choices=['present', 'absent']),
+            state=dict(type='str', default='present', choices=['present', 'absent', 'move']),
             force=dict(type='bool', default=False),
             resize=dict(type='bool', default=False),
+            dest_device=dict(type='path'),
         ),
         supports_check_mode=True,
+        required_if=[
+            ['state', 'move', ['dest_device']],
+        ],
     )
 
     device = module.params['device']
     state = module.params['state']
     force = module.params['force']
     resize = module.params['resize']
+    dest_device = module.params['dest_device']
     changed = False
     actions = []
 
@@ -174,12 +233,52 @@ def main():
                 cmd = ['pvremove', '-y']
                 if force:
                     cmd.append('-ff')
-                changed = True
                 cmd.append(device)
                 rc, out, err = module.run_command(cmd, check_rc=True)
+                changed = True
                 actions.append('removed')
 
-    # Generate final message
+    elif state == 'move':
+        if not os.path.exists(device):
+            module.fail_json(msg="Source device %s not found" % device)
+        if not os.path.exists(dest_device):
+            module.fail_json(msg="Destination device %s not found" % dest_device)
+        if device == dest_device:
+            module.fail_json(msg="Source and destination devices must be different")
+
+        if not get_pv_status(module, device):
+            module.fail_json(msg="Source device %s is not a PV" % device)
+        if not get_pv_status(module, dest_device):
+            module.fail_json(msg="Destination device %s is not a PV" % dest_device)
+
+        vg_src = get_pv_vg(module, device)
+        vg_dest = get_pv_vg(module, dest_device)
+        if vg_src != vg_dest:
+            module.fail_json(
+                msg="Source and destination must be in the same VG. Source VG: '%s', Destination VG: '%s'." % (vg_src, vg_dest)
+            )
+
+        allocated = get_pv_allocated_pe(module, device)
+        if allocated == 0:
+            actions.append('no allocated extents to move')
+        else:
+            # Check if destination has enough free space
+            free_pe_dest = get_pv_free_pe(module, dest_device)
+            if free_pe_dest < allocated:
+                module.fail_json(
+                    msg="Destination device %s has only %d free physical extents, but source device %s has %d allocated extents. Not enough space." % 
+                    (dest_device, free_pe_dest, device, allocated)
+                )
+
+            if module.check_mode:
+                changed = True
+                actions.append('would move data from %s to %s' % (device, dest_device))
+            else:
+                cmd = ['pvmove']
+                rc, out, err = module.run_command(cmd + [device, dest_device], check_rc=True)
+                changed = True
+                actions.append('moved data from %s to %s' % (device, dest_device))
+
     if actions:
         msg = "PV %s: %s" % (device, ', '.join(actions))
     else:
