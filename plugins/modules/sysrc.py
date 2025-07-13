2078725242873baed6fe3578cd5bfd53edff8ac1
Vladimir Botka <vbotka@gmail.com>
2025-07-13 15:42:19 +0200

    Fix #10394 return condition.

--- plugins/modules/sysrc.py
+++ plugins/modules/sysrc.py
@@ -144,7 +144,7 @@ class Sysrc(object):
         if self.value is None:
             return self.name in conf
         else:
-            return conf[self.name] == '"%s"' % self.value
+            return self.name in conf and conf[self.name] == '"%s"' % self.value
 
     def contains(self):
         (rc, out, err) = self.run_sysrc('-n', self.name)
