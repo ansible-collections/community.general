993e3a736ec712a95bf3287f2e087bec3631f198
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2025-08-11 16:44:38 +1200

    ipa_*: add common connection notes to modules (#10615)
    * ipa_*: add common connection notes to modules
    
    * Update plugins/doc_fragments/ipa.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    * Update plugins/doc_fragments/ipa.py
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/ipa_role.py
+++ plugins/modules/ipa_role.py
@@ -84,6 +84,7 @@ options:
     elements: str
 extends_documentation_fragment:
   - community.general.ipa.documentation
+  - community.general.ipa.connection_notes
   - community.general.attributes
 """
 
