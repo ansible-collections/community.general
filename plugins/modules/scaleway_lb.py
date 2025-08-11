05b35783f8d34f7bb1760cd62601fc4594b3c711
Michael Scherer <misc@redhat.com>
2025-08-11 14:54:06 +0200

    Add a scaleway group to be able to use module_defaults

--- plugins/modules/scaleway_lb.py
+++ plugins/modules/scaleway_lb.py
@@ -22,12 +22,15 @@ description:
 extends_documentation_fragment:
   - community.general.scaleway
   - community.general.attributes
+  - community.general.scaleway.actiongroup_scaleway
 
 attributes:
   check_mode:
     support: full
   diff_mode:
     support: none
+  action_group:
+    version_added: 11.3.0
 
 options:
 
