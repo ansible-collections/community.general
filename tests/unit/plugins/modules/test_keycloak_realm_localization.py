c3198b6759db76e4a52d679fbe7ea1e738c10d7f
Jakub Danek <jakub.danek@yoso.fi>
2025-09-26 09:36:30 +0200

    rev: remove year from copyright per guidelines

--- tests/unit/plugins/modules/test_keycloak_realm_localization.py
+++ tests/unit/plugins/modules/test_keycloak_realm_localization.py
@@ -1,7 +1,7 @@
 # Python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2025, Jakub Danek <danek.ja@gmail.com>
+# Copyright Jakub Danek <danek.ja@gmail.com>
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
