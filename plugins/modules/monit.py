31b5ad8a34e43db1c4a9432322fadb331332d014
Alexei Znamensky <russoz@gmail.com>
2025-10-14 17:22:58 +1300

    more debugging: II1A (values)

--- plugins/modules/monit.py
+++ plugins/modules/monit.py
@@ -172,6 +172,7 @@ class Monit(object):
         if ' - ' not in status_val:
             status_val = status_val.replace(' ', '_')
             try:
+                raise Exception(f'II1A {getattr(Status, status_val)=}')
                 # raise Exception(f'II1 {status_val=} {dir(Status)=}')
                 return getattr(Status, status_val)
             except AttributeError:
