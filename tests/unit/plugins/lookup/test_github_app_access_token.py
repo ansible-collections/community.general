1439c24fd718bac08c0306a31487ca4dd27be71c
Sean McAvoy <seanmcavoy@gmail.com>
2025-11-29 12:42:35 -0300

    tests/unit/plugins/lookup/test_github_app_access_token.py: restore

--- tests/unit/plugins/lookup/test_github_app_access_token.py
+++ tests/unit/plugins/lookup/test_github_app_access_token.py
@@ -45,15 +45,17 @@ class TestLookupModule(unittest.TestCase):
     def test_get_token_with_file_with_pyjwt(self):
         pyjwt = types.ModuleType("jwt")
         pyjwt.encode = MagicMock(return_value=ENCODE_RESULT)
-        with patch.dict(sys.modules, {"jwt": pyjwt}):
-            with patch.multiple(
+        with (
+            patch.dict(sys.modules, {"jwt": pyjwt}),
+            patch.multiple(
                 "ansible_collections.community.general.plugins.lookup.github_app_access_token",
                 open=mock_open(read_data="foo_bar"),
                 open_url=MagicMock(return_value=MockResponse()),
                 HAS_JWT=True,
                 HAS_CRYPTOGRAPHY=True,
                 serialization=serialization(),
-            ):
+            ),
+        ):
             lookup = lookup_loader.get("community.general.github_app_access_token")
             self.assertListEqual(
                 [MockResponse.response_token],
@@ -63,57 +65,63 @@ class TestLookupModule(unittest.TestCase):
     def test_get_token_with_fact_with_pyjwt(self):
         pyjwt = types.ModuleType("jwt")
         pyjwt.encode = MagicMock(return_value=ENCODE_RESULT)
-        with patch.dict(sys.modules, {"jwt": pyjwt}):
-            with patch.multiple(
+        with (
+            patch.dict(sys.modules, {"jwt": pyjwt}),
+            patch.multiple(
                 "ansible_collections.community.general.plugins.lookup.github_app_access_token",
                 open=mock_open(read_data="foo_bar"),
                 open_url=MagicMock(return_value=MockResponse()),
                 HAS_JWT=True,
                 HAS_CRYPTOGRAPHY=True,
                 serialization=serialization(),
-            ):
-                lookup = lookup_loader.get("community.general.github_app_access_token")
-                self.assertListEqual(
-                    [MockResponse.response_token],
-                    lookup.run(
-                        [], app_id="app_id", installation_id="installation_id", private_key="foo_bar", token_expiry=600
-                    ),
-                )
+            ),
+        ):
+            lookup = lookup_loader.get("community.general.github_app_access_token")
+            self.assertListEqual(
+                [MockResponse.response_token],
+                lookup.run(
+                    [], app_id="app_id", installation_id="installation_id", private_key="foo_bar", token_expiry=600
+                ),
+            )
 
     def test_get_token_with_python_jwt(self):
         python_jwt = types.ModuleType("jwt")
         python_jwt.JWT = MagicMock()
         python_jwt.jwk_from_pem = MagicMock(return_value="private_key")
         python_jwt.jwt_instance = MockJWT()
-        with patch.dict(sys.modules, {"jwt": python_jwt}):
-            with patch.multiple(
+        with (
+            patch.dict(sys.modules, {"jwt": python_jwt}),
+            patch.multiple(
                 "ansible_collections.community.general.plugins.lookup.github_app_access_token",
                 open=mock_open(read_data="foo_bar"),
                 open_url=MagicMock(return_value=MockResponse()),
                 HAS_JWT=True,
-            ):
-                lookup = lookup_loader.get("community.general.github_app_access_token")
-                self.assertListEqual(
-                    [MockResponse.response_token],
-                    lookup.run([], key_path="key", app_id="app_id", installation_id="installation_id", token_expiry=600),
-                )
+            ),
+        ):
+            lookup = lookup_loader.get("community.general.github_app_access_token")
+            self.assertListEqual(
+                [MockResponse.response_token],
+                lookup.run([], key_path="key", app_id="app_id", installation_id="installation_id", token_expiry=600),
+            )
 
     def test_get_token_with_fact_with_python_jwt(self):
         python_jwt = types.ModuleType("jwt")
         python_jwt.JWT = MagicMock()
         python_jwt.jwk_from_pem = MagicMock(return_value="private_key")
         python_jwt.jwt_instance = MockJWT()
-        with patch.dict(sys.modules, {"jwt": python_jwt}):
-            with patch.multiple(
+        with (
+            patch.dict(sys.modules, {"jwt": python_jwt}),
+            patch.multiple(
                 "ansible_collections.community.general.plugins.lookup.github_app_access_token",
                 open=mock_open(read_data="foo_bar"),
                 open_url=MagicMock(return_value=MockResponse()),
                 HAS_JWT=True,
-            ):
-                lookup = lookup_loader.get("community.general.github_app_access_token")
-                self.assertListEqual(
-                    [MockResponse.response_token],
-                    lookup.run(
-                        [], app_id="app_id", installation_id="installation_id", private_key="foo_bar", token_expiry=600
-                    ),
-                )
+            ),
+        ):
+            lookup = lookup_loader.get("community.general.github_app_access_token")
+            self.assertListEqual(
+                [MockResponse.response_token],
+                lookup.run(
+                    [], app_id="app_id", installation_id="installation_id", private_key="foo_bar", token_expiry=600
+                ),
+            )
