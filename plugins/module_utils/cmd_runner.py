5de9fd890a81ef15dcc9ff56a51258c746c8ec1a
Felix Fontein <felix@fontein.de>
2025-12-01 07:56:38 +0100

    Add noqa.

--- plugins/module_utils/cmd_runner.py
+++ plugins/module_utils/cmd_runner.py
@@ -16,7 +16,7 @@ if t.TYPE_CHECKING:
     from ansible.module_utils.basic import AnsibleModule
     from ansible_collections.community.general.plugins.module_utils.cmd_runner_fmt import ArgFormatType
 
-    ArgFormatter = t.Union[ArgFormatType, cmd_runner_fmt._ArgFormat]
+    ArgFormatter = t.Union[ArgFormatType, cmd_runner_fmt._ArgFormat]  # noqa: UP007
 
 
 def _ensure_list(value):
