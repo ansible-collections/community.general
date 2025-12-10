d424907172b548aff5d2121e708c35cb7591a21d
s-hamann <10639154+s-hamann@users.noreply.github.com>
2025-12-10 12:29:41 +0000

    apk: fix packages return value for apk-tools >= 3 (fix #11264) (#11265)
    * apk: fix packages return value for apk-tools >= 3 (fix #11264)
    
    * Add changelog fragment

--- plugins/modules/apk.py
+++ plugins/modules/apk.py
@@ -174,7 +174,7 @@ from ansible.module_utils.basic import AnsibleModule
 def parse_for_packages(stdout):
     packages = []
     data = stdout.split("\n")
-    regex = re.compile(r"^\(\d+/\d+\)\s+\S+\s+(\S+)")
+    regex = re.compile(r"^\(\s*\d+/\d+\)\s+\S+\s+(\S+)")
     for l in data:
         p = regex.search(l)
         if p:
