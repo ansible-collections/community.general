8ded9f764d49b9a10a50e1badde693083231c194
Max Blome <max.blome@itebo.de>
2025-12-22 17:45:38 +0100

    Add selector option

--- plugins/modules/keycloak_userprofile.py
+++ plugins/modules/keycloak_userprofile.py
@@ -259,6 +259,18 @@ options:
                     default:
                       - user
 
+              selector:
+                description:
+                  - Selector when the attribute should be added.
+                type: dict
+                version_added: 12.2.0
+                suboptions:
+                  scopes:
+                    description:
+                      - Scopes to which the attribute should be added.
+                    type: list
+                    elements: str
+
           groups:
             description:
               - A list of attribute groups to be included in the User Profile.
@@ -573,6 +585,9 @@ def main():
                                 "multivalued": dict(type="bool", default=False),
                                 "required": dict(
                                     type="dict", options={"roles": dict(type="list", elements="str", default=["user"])}
+                                ),
+                                "selector": dict(
+                                    type="dict", options={"scopes": dict(type="list", elements="str")}
                                 ),
                             },
                         ),
