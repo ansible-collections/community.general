2857c39b5c112c4fabc05c55923fbc2fbeebc97c
Alexei Znamensky <russoz@gmail.com>
2025-12-28 17:11:11 +1300

    chore(devcontainer): use standard image, no docker build

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,9 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+sudo chown -R vscode:vscode /workspace/
+
+pip install -U pip
 pip install -r .devcontainer/requirements-dev.txt
 pip install -r tests/unit/requirements.txt
 
