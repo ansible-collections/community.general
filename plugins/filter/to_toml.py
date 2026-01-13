565419e797275f785d5388953d49f4b2abba50a6
Matt Williams <matt.williams@bristol.ac.uk>
2026-01-13 22:25:05 +0000

    Move common code to plugin_utils/_tags.py

--- plugins/filter/to_toml.py
+++ plugins/filter/to_toml.py
@@ -5,7 +5,9 @@
 from __future__ import annotations
 
 import typing as t
-from collections.abc import Mapping, Set
+from collections.abc import Mapping
+
+from ansible.module_utils.common.collections import is_sequence
 
 TOMLKIT_IMPORT_ERROR: ImportError | None
 try:
@@ -15,92 +17,28 @@ except ImportError as imp_exc:
 else:
     TOMLKIT_IMPORT_ERROR = None
 
-from ansible.errors import AnsibleError, AnsibleRuntimeError
-from ansible.module_utils.common.collections import is_sequence
+from ansible.errors import AnsibleError, AnsibleFilterError
 
-try:
-    # This is ansible-core 2.19+
-    from ansible.parsing.vault import VaultHelper, VaultLib
-    from ansible.utils.vars import transform_to_native_types
-
-    HAS_TRANSFORM_TO_NATIVE_TYPES = True
-except ImportError:
-    HAS_TRANSFORM_TO_NATIVE_TYPES = False
-
-from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
-from ansible.utils.unsafe_proxy import AnsibleUnsafe
+from ansible_collections.community.general.plugins.plugin_utils._tags import remove_all_tags
 
 
-def _to_native_types_compat(value: t.Any, *, redact_value: str | None) -> t.Any:
-    """Compatibility function for ansible-core 2.18 and before."""
-    if value is None:
-        return value
-    if isinstance(value, AnsibleUnsafe):
-        # This only works up to ansible-core 2.18:
-        return _to_native_types_compat(value._strip_unsafe(), redact_value=redact_value)  # type: ignore
-        # But that's fine, since this code path isn't taken on ansible-core 2.19+ anyway.
+def _stringify_keys(value: t.Any) -> t.Any:
+    """Recursively convert all keps to strings."""
     if isinstance(value, Mapping):
-        return {
-            _to_native_types_compat(str(key), redact_value=redact_value): _to_native_types_compat(
-                val, redact_value=redact_value
-            )
-            for key, val in value.items()
-        }
-    if isinstance(value, Set):
-        return {_to_native_types_compat(elt, redact_value=redact_value) for elt in value}
+        return {str(k): _stringify_keys(v) for k, v in value.items()}
     if is_sequence(value):
-        return [_to_native_types_compat(elt, redact_value=redact_value) for elt in value]
-    if isinstance(value, AnsibleVaultEncryptedUnicode):
-        if redact_value is not None:
-            return redact_value
-        # This only works up to ansible-core 2.18:
-        return value.data
-        # But that's fine, since this code path isn't taken on ansible-core 2.19+ anyway.
-    if isinstance(value, bytes):
-        return bytes(value)
-    if isinstance(value, str):
-        return str(value)
-
+        return [_stringify_keys(e) for e in value]
     return value
 
 
-def _to_native_types(value: t.Any, *, redact: bool) -> t.Any:
-    if isinstance(value, Mapping):
-        return {_to_native_types(str(k), redact=redact): _to_native_types(v, redact=redact) for k, v in value.items()}
-    if is_sequence(value):
-        return [_to_native_types(e, redact=redact) for e in value]
-    if redact:
-        ciphertext = VaultHelper.get_ciphertext(value, with_tags=False)
-        if ciphertext and VaultLib.is_encrypted(ciphertext):
-            return "<redacted>"
-    return transform_to_native_types(value, redact=redact)
-
-
-def remove_all_tags(value: t.Any, *, redact_sensitive_values: bool = False) -> t.Any:
-    """
-    Remove all tags from all values in the input.
-
-    If ``redact_sensitive_values`` is ``True``, all sensitive values will be redacted.
-    """
-    if HAS_TRANSFORM_TO_NATIVE_TYPES:
-        return _to_native_types(value, redact=redact_sensitive_values)
-
-    return _to_native_types_compat(  # type: ignore[unreachable]
-        value,
-        redact_value="<redacted>"
-        if redact_sensitive_values
-        else None,  # same string as in ansible-core 2.19 by transform_to_native_types()
-    )
-
-
 def to_toml(value: t.Mapping, *, redact_sensitive_values: bool = False) -> str:
-    """Serialize input as terse flow-style TOML."""
+    """Serialize input as TOML."""
     if TOMLKIT_IMPORT_ERROR:
         raise AnsibleError("tomlkit must be installed to use this plugin") from TOMLKIT_IMPORT_ERROR
     if not isinstance(value, Mapping):
         raise AnsibleFilterError("to_toml only accepts dictionaries.")
     return dumps(
-        remove_all_tags(value, redact_sensitive_values=redact_sensitive_values),
+        remove_all_tags(_stringify_keys(value), redact_sensitive_values=redact_sensitive_values),
     )
 
 
