c74ced35b54cf8d797dfc314e5e9332205f8ff1d
Fiehe Christoph <c.fiehe@eurodata.de>
2026-02-11 15:47:00 +0100

    Approach that removes decorator parametrization and relies on an object/class variable named `unhandled_exceptions`.
    Signed-off-by: Fiehe Christoph  <c.fiehe@eurodata.de>

--- plugins/module_utils/mh/base.py
+++ plugins/module_utils/mh/base.py
@@ -23,7 +23,7 @@ class ModuleHelperBase:
         "deprecate",
         "debug",
     )
-    unhandled_exceptions: tuple[type[Exception]] = ()
+    unhandled_exceptions: tuple[type[Exception], ...] = ()
 
     def __init__(self, module=None):
         self._changed = False
@@ -33,9 +33,6 @@ class ModuleHelperBase:
 
         if not isinstance(self.module, AnsibleModule):
             self.module = AnsibleModule(**self.module)
-
-        # Decorate run method
-        type(self).run = module_fails_on_exception(self.unhandled_exceptions)(type(self).run)
 
     @property
     def diff_mode(self):
@@ -83,6 +80,7 @@ class ModuleHelperBase:
     def output(self):
         raise NotImplementedError()
 
+    @module_fails_on_exception
     def run(self):
         self.__init_module__()
         self.__run__()
