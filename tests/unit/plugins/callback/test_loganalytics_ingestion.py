a1569b0905774287c6bbd52a2ae19b581fd4b011
Cline, Wade <wade.cline@intel.com>
2026-02-11 12:49:01 -0800

    Refactor LogAnalytics Ingestion unit tests
    Move all of the tests outside of the 'setUp' method.

--- tests/unit/plugins/callback/test_loganalytics_ingestion.py
+++ tests/unit/plugins/callback/test_loganalytics_ingestion.py
@@ -20,15 +20,28 @@ class TestAzureLogAnalyticsIngestion(unittest.TestCase):
     client_secret = "fake-client-secret"
     tenant_id = "fake-tenant-id"
     stream_name = "fake-stream-name"
+    fake_access_token = None
+
+    def setUp(self):
+        # Generate a fake access token here, before the 'json.dumps' method is mocked.
+        self.fake_access_token = json.dumps(
+            {"expires_in": time.time() + 3600, "access_token": "fake_access_token"}
+        ).encode("utf-8")
 
     @unittest.mock.patch(
         "ansible_collections.community.general.plugins.callback.loganalytics_ingestion.open_url", autospec=True
     )
-    def setUp(self, open_url_mock):
-        # Generate a fake access token.
-        open_url_mock.return_value.read.return_value = json.dumps(
-            {"expires_in": time.time() + 3600, "access_token": "fake_access_token"}
-        ).encode("utf-8")
+    @unittest.mock.patch("json.dumps")
+    @unittest.mock.patch("ansible.executor.task_result.TaskResult")
+    def test_sending_data(self, task_result_mock, json_dumps_mock, open_url_mock):
+        """
+        Tests sending data by verifying that the expected POST requests are submitted to the expected hosts.
+        """
+        # Neither the Mock objects nor its attributes are serializable but we don't care about getting accurate JSON in this case so just return fake JSON data.
+        json_dumps_mock.return_value = '{"name": "fake_json"}'
+
+        # The data returned from 'open_url' is only ever read during authentication.
+        open_url_mock.return_value.read.return_value = self.fake_access_token
 
         # TODO: How to set plugin default arguments?
         #       I tried instantiating the 'CallbackModule' but all it ever did was complain that 'client_id' wasn't defined.
@@ -51,25 +64,13 @@ class TestAzureLogAnalyticsIngestion(unittest.TestCase):
         url = urllib.parse.urlparse(open_url_mock.call_args_list[0][0][0])
         assert url.netloc == "login.microsoftonline.com"
 
-    @unittest.mock.patch(
-        "ansible_collections.community.general.plugins.callback.loganalytics_ingestion.open_url", autospec=True
-    )
-    @unittest.mock.patch("json.dumps")
-    @unittest.mock.patch("ansible.executor.task_result.TaskResult")
-    def test_sending_data(self, task_result_mock, json_dumps_mock, open_url_mock):
-        """
-        Tests sending data by verifying that the expected POST requests are submitted to the expected hosts.
-        """
-        # Neither the Mock objects nor its attributes are serializable but we don't care about getting accurate JSON in this case so just return fake JSON data.
-        json_dumps_mock.return_value = '{"name": "fake_json"}'
-
         self.loganalytics.send_to_loganalytics("fake_playbook", task_result_mock(), "OK")
         self.loganalytics.send_to_loganalytics("fake_playbook", task_result_mock(), "FAILED")
         self.loganalytics.send_to_loganalytics("fake_playbook", task_result_mock(), "OK")
 
         # Validate the three POST requests for sending data (one for each task).
-        assert open_url_mock.call_count == 3
+        assert open_url_mock.call_count == 4
         # Three POST requests to the DCE.
-        for i in range(0, 2):
+        for i in range(1, 4):
             url = urllib.parse.urlparse(open_url_mock.call_args_list[i][0][0])
             assert url.scheme + "://" + url.netloc == self.dce_url
