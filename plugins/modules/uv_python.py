5198132bc8f9dcc4e013ff25f47b3091e4135f9e
Mariam Ahhttouche <mariam.ahhttouche@gmail.com>
2026-02-16 10:04:38 +0100

    uv_python module: update latest state handling to sort versions without relying on uv behavior
    uv_python module: improve integration tests
    
    uv_python module: improve module return values

--- plugins/modules/uv_python.py
+++ plugins/modules/uv_python.py
@@ -51,17 +51,18 @@ python:
   type: dict
 '''
 
-import re
 import json
-from enum import Enum
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.compat.version import StrictVersion
 
 
 class UV:
     """
-      Documentation for uv python https://docs.astral.sh/uv/concepts/python-versions/#installing-a-python-version
+      Module for "uv python" command
+      Official documentation for uv python https://docs.astral.sh/uv/concepts/python-versions/#installing-a-python-version
     """
+    subcommand = "python"
+
     def __init__(self, module, **kwargs):
         self.module = module
         python_version = module.params["version"]
@@ -77,56 +78,58 @@ class UV:
           )
 
     def install_python(self):
-        rc, out, _ = self._find_python() 
+        rc, out, _ = self._find_python("--show-version") 
         if rc == 0:
-          return False, out
+          return False, out.split()[0]
         if self.module.check_mode:
-          return True, ""
+          return True, self.python_version_str
 
-        cmd = [self.module.get_bin_path("uv", required=True), "python", "install", self.python_version_str]
-        _, out, _ = self.module.run_command(cmd, check_rc=True)
-        return True, out
+        cmd = [self.module.get_bin_path("uv", required=True), self.subcommand, "install", self.python_version_str]
+        self.module.run_command(cmd, check_rc=True)
+        return True, self.python_version_str
     
     def uninstall_python(self):
       rc, out, _ = self._find_python()
       if rc != 0:
           return False, out
       if self.module.check_mode:
-          return True, out
+          return True, ""
       
-      cmd = [self.module.get_bin_path("uv", required=True), "python", "uninstall", self.python_version_str]
-      _, out, _ = self.module.run_command(cmd, check_rc=True)
-      return True, out
+      cmd = [self.module.get_bin_path("uv", required=True), self.subcommand, "uninstall", self.python_version_str]
+      self.module.run_command(cmd, check_rc=True)
+      return True, ""
     
     def upgrade_python(self):
       rc, out, _ = self._find_python("--show-version")
-      detected_version = out.split()[0]
       latest_version = self._get_latest_patch_release()
-      if rc == 0 and detected_version == latest_version:
-          return False, out
+      if rc == 0:
+        detected_version = out.split()[0]
+        if detected_version == latest_version:
+          return False, latest_version
       if self.module.check_mode:
           return True, latest_version
       
-      cmd = [self.module.get_bin_path("uv", required=True), "python", "install", latest_version]
-      _, out, _ = self.module.run_command(cmd, check_rc=True)
-      return True, out
+      cmd = [self.module.get_bin_path("uv", required=True), self.subcommand, "install", latest_version]
+      self.module.run_command(cmd, check_rc=True)
+      return True, latest_version
 
     def _find_python(self, *args):
-      # if multiple similar minor versions exist, find returns the one used by default if inside a virtualenv otherwise it returns latest installed patch version
-      cmd = [self.module.get_bin_path("uv", required=True), "python", "find", self.python_version_str, *args]
+      # if multiple similar minor versions exist, "uv python find" returns the one used by default if inside a virtualenv otherwise it returns latest installed patch version
+      cmd = [self.module.get_bin_path("uv", required=True), self.subcommand, "find", self.python_version_str, *args]
       rc, out, err = self.module.run_command(cmd)
       return rc, out, err
     
     def _list_python(self):
       # https://docs.astral.sh/uv/reference/cli/#uv-python-list
-      cmd = [self.module.get_bin_path("uv", required=True), "python", "list", self.python_version_str, "--output-format", "json"]
+      cmd = [self.module.get_bin_path("uv", required=True), self.subcommand, "list", self.python_version_str, "--output-format", "json"]
       rc, out, err = self.module.run_command(cmd)
       return rc, out, err
 
     def _get_latest_patch_release(self):
-      _, out, _ = self._list_python()
-      result = json.loads(out)
-      return result[0]["version"] # uv orders versions in descending order
+      _, out, _ = self._list_python() # uv returns versions in descending order but we sort them just in case future uv behavior changes
+      results = json.loads(out)
+      versions = [StrictVersion(result["version"]) for result in results]
+      return max(versions).__str__()
     
 
 def main():
