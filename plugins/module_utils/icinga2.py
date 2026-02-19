87df1821862924fc200e9fc7be092d048a2afbe8
Christoph Fiehe <cfiehe@users.noreply.github.com>
2026-02-19 13:35:32 +0100

    Update plugins/module_utils/icinga2.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/icinga2.py
+++ plugins/module_utils/icinga2.py
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2026 Christoph Fiehe <christoph.fiehe@gmail.com>
 
+# Note that this module util is **PRIVATE** to the collection. It can have breaking changes at any time.
+# Do not use this from other collections or standalone plugins/modules!
+
 from __future__ import annotations
 
 import json
