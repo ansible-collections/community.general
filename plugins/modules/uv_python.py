0f5a46c98b7050d1c2f1edc771c9b8ed08fe92b8
Mariam Ahhttouche <mariam.ahhttouche@gmail.com>
2026-03-05 21:55:32 +0100

    Update example to use quotes for major.minor example and update documentation

--- plugins/modules/uv_python.py
+++ plugins/modules/uv_python.py
@@ -30,7 +30,9 @@ options:
         Not all canonical Python versions are supported in this release. Valid version numbers consist of two or three dot-separated numeric components,
         with an optional 'pre-release' tag on the end such as V(3.12), V(3.12.3), V(3.15.0a5).
       - Advanced uv selectors such as V(>=3.12,<3.13) or V(cpython@3.12) are not supported in this release.
-      - When you specify only a major.minor version, behavior depends on the O(state) parameter.
+      - |
+        When you specify only a major.minor version, make sure the number is enclosed in quotes so that it gets parsed correctly.
+        Note that in this case behavior depends on the O(state) parameter.
     type: str
     required: true
   state:
@@ -76,7 +78,7 @@ EXAMPLES = r"""
 
 - name: Upgrade Python 3.14
   community.general.uv_python:
-    version: 3.14
+    version: "3.14"
     state: latest
 
 - name: Remove Python 3.13.5
