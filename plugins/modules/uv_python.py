c52ad1980dedabf944283067af5573fec0a60c29
Mariam Ahhttouche <mariam.ahhttouche@gmail.com>
2026-03-06 17:44:41 +0000

    Handle case when version given is an empty string in uv_python module

--- plugins/modules/uv_python.py
+++ plugins/modules/uv_python.py
@@ -101,9 +101,9 @@ rc:
 """
 
 import json
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.compat.version import LooseVersion, StrictVersion
-
 
 MINIMUM_UV_VERSION = "0.8.0"
 
@@ -118,10 +118,9 @@ class UV:
         self.bin_path = self.module.get_bin_path("uv", required=True)
         self._ensure_min_uv_version()
         try:
-            python_version = module.params["version"]
-            self.python_version = StrictVersion(python_version)
+            self.python_version = StrictVersion(module.params["version"])
             self.python_version_str = str(self.python_version)
-        except ValueError:
+        except (ValueError, AttributeError):
             self.module.fail_json(
                 msg="Unsupported version format. Valid version numbers consist of two or three dot-separated numeric components, \
                   with an optional 'pre-release' tag on the end (e.g. 3.12, 3.12.3, 3.15.0a5) are supported in this release."
