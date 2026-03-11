9b72d95452c755ec624e7d53af174508e5668685
Felix Fontein <felix@fontein.de>
2026-03-11 06:42:08 +0100

    Add missing __future__ imports (#11567)
    Add missing __future__ imports.

--- plugins/plugin_utils/_tags.py
+++ plugins/plugin_utils/_tags.py
@@ -5,6 +5,8 @@
 # Note that this module util is **PRIVATE** to the collection. It can have breaking changes at any time.
 # Do not use this from other collections or standalone plugins/modules!
 
+from __future__ import annotations
+
 import typing as t
 from collections.abc import Mapping, Set
 
