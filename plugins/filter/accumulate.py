3e4875cf9c47c34f911bef7bc0ea491f8a530fee
Felix Fontein <felix@fontein.de>
2026-03-11 06:42:08 +0100

    Add missing __future__ imports (#11567)
    Add missing __future__ imports.
    
    (cherry picked from commit 9b72d95452c755ec624e7d53af174508e5668685)

--- plugins/filter/accumulate.py
+++ plugins/filter/accumulate.py
@@ -2,6 +2,8 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
+
 DOCUMENTATION = r"""
 name: accumulate
 short_description: Produce a list of accumulated sums of the input list contents
