4db4686903afb0f26169b460134db7f97463ad10
Dor Breger <dor@hisprivate.net>
2026-03-13 21:51:21 +0200

    ipa_dnsrecord dnsrecord_find method refactor

--- plugins/modules/ipa_dnsrecord.py
+++ plugins/modules/ipa_dnsrecord.py
@@ -206,13 +206,10 @@ class DNSRecordIPAClient(IPAClient):
 
     def dnsrecord_find(self, zone_name, record_name):
         if record_name == "@":
-            result = self._post_json(
-                method="dnsrecord_show", name=zone_name, item={"idnsname": record_name, "all": True}
-            )
+            method = "dnsrecord_show"
         else:
-            result = self._post_json(
-                method="dnsrecord_find", name=zone_name, item={"idnsname": record_name, "all": True}
-            )
+            method = "dnsrecord_find"
+        result = self._post_json(method=method, name=zone_name, item={"idnsname": record_name, "all": True})
         result["dnsttl"] = [int(v) for v in result["dnsttl"]]
         return result
 
