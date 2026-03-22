011426701fc093058b3413c2bcdb05c2128234c7
Alexei Znamensky <russoz@gmail.com>
2026-03-22 16:10:23 +1300

    simplify the test

--- tests/integration/targets/python_runner/files/settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Alexei Znamensky <russoz@gmail.com>
-# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-SECRET_KEY = "test-secret-key-for-python-runner-integration-test-do-not-use-in-production"
-
-INSTALLED_APPS = []
-
-DATABASES = {}
