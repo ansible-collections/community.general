1250f5544f3dbbd49d4deef4e54209d64c514dc1
quasd <1747330+quasd@users.noreply.github.com>
2026-04-01 10:40:01 +0300

    Revert back to working if

--- plugins/modules/ipa_host.py
+++ plugins/modules/ipa_host.py
@@ -294,8 +294,13 @@ def ensure(module, client):
 
             diff = get_host_diff(client, ipa_host, module_host)
             ipa_host_show = client.host_show(name=name)
-            host_needs_to_be_disabled = ipa_host_show.get("has_keytab", True) and module.params.get(
-                "random_password", state == "disabled"
+            host_needs_to_be_disabled = (
+                True
+                if (
+                    ipa_host_show.get("has_keytab", True)
+                    and (state == "disabled" or module.params.get("random_password"))
+                )
+                else False
             )
             if diff or host_needs_to_be_disabled:
                 changed = True
