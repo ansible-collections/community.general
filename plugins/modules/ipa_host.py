17e47eb9bd80d88815fd0135dcaed4fc48336aa2
quasd <1747330+quasd@users.noreply.github.com>
2026-04-01 13:54:53 +0300

    Remove extra get

--- plugins/modules/ipa_host.py
+++ plugins/modules/ipa_host.py
@@ -295,7 +295,7 @@ def ensure(module, client):
             diff = get_host_diff(client, ipa_host, module_host)
             ipa_host_show = client.host_show(name=name)
             host_needs_to_be_disabled = ipa_host_show.get("has_keytab", True) and (
-                state == "disabled" or module.params.get("random_password")
+                module.params["random_password"] or state == "disabled"
             )
             if diff or host_needs_to_be_disabled:
                 changed = True
