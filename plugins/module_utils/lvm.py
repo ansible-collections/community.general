2636a3af2b2619be23a547058f5eeb71738edadb
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2026-04-10 14:28:25 +1200

    Update plugins/module_utils/lvm.py
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/lvm.py
+++ plugins/module_utils/lvm.py
@@ -2,6 +2,9 @@
 # GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# Note that this module util is **PRIVATE** to the collection. It can have breaking changes at any time.
+# Do not use this from other collections or standalone plugins/modules!
+
 from __future__ import annotations
 
 import typing as t
