76372a89753c86da6e19975189eba1ac931a0ea7
Felix Fontein <felix@fontein.de>
2026-04-17 07:03:28 +0200

    [stable-11] Fix flatpak module for Fedora 44 (#11847)
    CI: Replace Fedora 43 with 44 for devel (#11836)
    
    * Replace Fedora 43 with 44 for devel in CI.
    
    * Adjust tests.
    
    * Adjust flatpak module to Fedora 44.
    
    (cherry picked from commit ef656cb9b6062aa18c9809004d964951470072e3)

--- plugins/modules/flatpak.py
+++ plugins/modules/flatpak.py
@@ -226,7 +226,7 @@ def update_flat(module, binary, names, method, no_dependencies):
     command += installed_flat_names
     stdout = _flatpak_command(module, module.check_mode, command)
     result["changed"] = (
-        True if module.check_mode else stdout.find("Nothing to do.") == -1
+        True if module.check_mode else (stdout.find("Nothing to do.") == -1 and stdout.find("Nothing to update.") == -1)
     )
 
 
