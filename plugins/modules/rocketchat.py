af240b747d7bd592465554d8e59454bd62bf892e
Lars Krahl <57526005+mmslkr@users.noreply.github.com>
2026-04-22 20:09:41 +0200

    Replace default favicon URL again (#11909)
    * replace default favicon URL
    
    * add changelog fragment for PR 11909
    
    * fix syntax for change fragment
    
    Co-authored-by: Alexei Znamensky <103110+russoz@users.noreply.github.com>
    
    * use higher res favicon by default
    
    ---------
    
    Co-authored-by: Lars Krahl <lars.krahl@telekom.de>
    Co-authored-by: Alexei Znamensky <103110+russoz@users.noreply.github.com>
    (cherry picked from commit d57a667274d1e79040a948a52b669894c438c015)

--- plugins/modules/rocketchat.py
+++ plugins/modules/rocketchat.py
@@ -60,7 +60,7 @@ options:
     type: str
     description:
       - URL for the message sender's icon.
-    default: "https://docs.ansible.com/favicon.ico"
+    default: "https://docs.ansible.com/favicon/favicon.ico"
   icon_emoji:
     type: str
     description:
@@ -223,7 +223,7 @@ def main():
             msg=dict(type="str"),
             channel=dict(type="str"),
             username=dict(type="str", default="Ansible"),
-            icon_url=dict(type="str", default="https://docs.ansible.com/favicon.ico"),
+            icon_url=dict(type="str", default="https://docs.ansible.com/favicon/favicon.ico"),
             icon_emoji=dict(type="str"),
             link_names=dict(type="int", default=1, choices=[0, 1]),
             validate_certs=dict(default=True, type="bool"),
