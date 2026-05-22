eba5f1d12a7bb9c158436c0f34c7a96c988c7667
spike <spike@fedoraproject.org>
2026-05-22 09:28:57 +0200

    Renamed filter back to to_toml.py, moved from_toml filter to its own file making use of python's native tomllib

---
+++ plugins/filter/from_toml.py
@@ -0,0 +1,23 @@
+# Copyright (c) Contributors to the Ansible project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+
+import typing as t
+
+import tomllib
+from ansible.errors import AnsibleFilterError
+
+
+def from_toml(value: t.LiteralString) -> t.Mapping:
+    if not isinstance(value, str):
+        raise AnsibleFilterError("from_toml only accepts strings.")
+    return tomllib.loads(value)
+
+
+class FilterModule:
+    def filters(self):
+        return {
+            "from_toml": from_toml,
+        }
