bd5969b3be5d3773e131dca1cf51fb5287ef80de
Felix Fontein <felix@fontein.de>
2026-05-23 22:04:55 +0200

    Get rid of unnecessary dummy variables.

--- plugins/action/iptables_state.py
+++ plugins/action/iptables_state.py
@@ -164,7 +164,7 @@ class ActionModule(ActionBase):
                 # The module is aware to not process the main iptables-restore
                 # command before finding (and deleting) the 'starter' cookie on
                 # the host, so the previous query will not reach ssh timeout.
-                dummy = self._low_level_execute_command(starter_cmd, sudoable=self.DEFAULT_SUDOABLE)
+                self._low_level_execute_command(starter_cmd, sudoable=self.DEFAULT_SUDOABLE)
 
                 # As the main command is not yet executed on the target, here
                 # 'finished' means 'failed before main command be executed'.
@@ -174,7 +174,7 @@ class ActionModule(ActionBase):
                     except AttributeError:
                         pass
 
-                    for dummy2 in range(max_timeout):
+                    for dummy in range(max_timeout):
                         time.sleep(1)
                         remaining_time -= 1
                         # - AnsibleConnectionFailure covers rejected requests (i.e.
@@ -182,7 +182,7 @@ class ActionModule(ActionBase):
                         # - ansible_timeout is able to cover dropped requests (due
                         #   to a rule or policy DROP) if not lower than async_val.
                         try:
-                            dummy3 = self._low_level_execute_command(confirm_cmd, sudoable=self.DEFAULT_SUDOABLE)
+                            self._low_level_execute_command(confirm_cmd, sudoable=self.DEFAULT_SUDOABLE)
                             break
                         except AnsibleConnectionFailure:
                             continue
@@ -200,7 +200,7 @@ class ActionModule(ActionBase):
                             del result["invocation"]["module_args"][key]
 
                 async_status_args["mode"] = "cleanup"
-                dummy4 = self._async_result(async_status_args, task_vars, 0)
+                self._async_result(async_status_args, task_vars, 0)
 
         if not wrap_async:
             # remove a temporary path we created
