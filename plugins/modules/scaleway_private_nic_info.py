b035bbe0121df4af3596a99f1833c25d3ab480c1
Matej Stajduhar <matej.stajduhar@codeenigma.com>
2026-06-01 18:07:32 +0200

    Updating-scaleway-name-for-import

--- plugins/modules/scaleway_private_nic_info.py
+++ plugins/modules/scaleway_private_nic_info.py
@@ -120,7 +120,7 @@ private_nics:
 
 from ansible.module_utils.basic import AnsibleModule
 
-from ansible_collections.community.general.plugins.module_utils.scaleway import (
+from ansible_collections.community.general.plugins.module_utils._scaleway import (
     SCALEWAY_LOCATION,
     Scaleway,
     ScalewayException,
