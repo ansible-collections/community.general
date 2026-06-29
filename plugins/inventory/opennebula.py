eb009855e35cce6a7ce3a9e2df75a43c3568676f
Simon <4375225+skropf@users.noreply.github.com>
2026-06-29 21:39:08 +0200

    OpenNebula inventory fails if VM without NIC exists (#12361)
    * fix error of opennebula plugin when retrieving VM without NIC
    
    * add changelog fragment
    
    * added more descriptive name, added link to PR
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    
    ---------
    
    Co-authored-by: Felix Fontein <felix@fontein.de>
    (cherry picked from commit bbf8bb47d91aca4bc4293c474d7562e2b96dd70a)

--- plugins/inventory/opennebula.py
+++ plugins/inventory/opennebula.py
@@ -138,6 +138,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
     def _get_vm_ipv4(self, vm):
         nic = vm.TEMPLATE.get("NIC")
 
+        if not nic:
+            return False
+
         if isinstance(nic, dict):
             nic = [nic]
 
@@ -149,6 +152,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
 
     def _get_vm_ipv6(self, vm):
         nic = vm.TEMPLATE.get("NIC")
+
+        if not nic:
+            return False
 
         if isinstance(nic, dict):
             nic = [nic]
