b998e716c486dbedc8635d2457e23b92c950db32
Johannes Paul <jpaul@uni-mainz.de>
2026-08-05 18:11:10 +0200

    plugins/modules/ufw: Fix insert_relative_to

--- plugins/modules/ufw.py
+++ plugins/modules/ufw.py
@@ -287,6 +287,7 @@ EXAMPLES = r"""
 """
 
 import re
+from ipaddress import ip_address, IPv6Address
 from operator import itemgetter
 
 from ansible.module_utils.basic import AnsibleModule
@@ -530,21 +531,39 @@ def main():
                 if relative_to_cmd == "zero":
                     insert_to = params["insert"]
                 else:
-                    (dummy, numbered_state, dummy) = module.run_command([ufw_bin, "status", "numbered"])
-                    numbered_line_re = re.compile(R"^\[ *([0-9]+)\] ")
-                    lines = [(numbered_line_re.match(line), "(v6)" in line) for line in numbered_state.splitlines()]
-                    lines = [(int(matcher.group(1)), ipv6) for (matcher, ipv6) in lines if matcher]
-                    last_number = max([no for (no, ipv6) in lines]) if lines else 0
-                    has_ipv4 = any(not ipv6 for (no, ipv6) in lines)
-                    has_ipv6 = any(ipv6 for (no, ipv6) in lines)
-                    if relative_to_cmd == "first-ipv4":
-                        relative_to = 1
-                    elif relative_to_cmd == "last-ipv4":
-                        relative_to = max([no for (no, ipv6) in lines if not ipv6]) if has_ipv4 else 1
-                    elif relative_to_cmd == "first-ipv6":
-                        relative_to = max([no for (no, ipv6) in lines if not ipv6]) + 1 if has_ipv4 else 1
-                    elif relative_to_cmd == "last-ipv6":
-                        relative_to = last_number if has_ipv6 else last_number + 1
+                    _, numbered_state, _ = module.run_command([ufw_bin, "status", "numbered"])
+                    # Format is: [no] To Action From # Comment
+                    numbered_line_re = re.compile(r"^\[ *([0-9]+)] [^#\n]*(DENY|ALLOW|REJECT|LIMIT) (IN|OUT) +([^# \n][^#\n]+[^# \n])")
+                    last_number = last_ipv4 = first_ipv6 = last_ipv6 = 0
+                    for line in numbered_state.splitlines():
+                        if line_match := numbered_line_re.match(line):
+                            no = int(line_match.group(1))
+                            last_number = no
+                            try:
+                                ip = ip_address(line_match.group(4).strip())
+                                ipv6 = isinstance(ip, IPv6Address)
+                            except ValueError:
+                                ipv6 = "(v6)" in line
+                            if ipv6:
+                                first_ipv6 = first_ipv6 or no
+                                last_ipv6 = no
+                            elif first_ipv6:
+                                raise ValueError('ufw output could not be parsed correctly. ipv4 follows ipv6!')
+                            else:
+                                last_ipv4 = no
+
+                    match relative_to_cmd:
+                        case 'first-ipv4':
+                            relative_to = 1
+                        case 'last-ipv4':
+                            relative_to = last_ipv4 or 1
+                        case 'first-ipv6':
+                            relative_to = first_ipv6 or last_ipv4 + 1
+                        case 'last-ipv6':
+                            relative_to = last_ipv6 or last_number + 1
+                        case _:
+                            raise ValueError(f'Undefinded value for "insert_relative_to": {relative_to_cmd}')
+
                     insert_to = params["insert"] + relative_to
                     if insert_to > last_number:
                         # ufw does not like it when the insert number is larger than the
@@ -569,7 +588,7 @@ def main():
                 value = params[key]
                 cmd.append([value, template % (value)])
 
-            ufw_major, ufw_minor, dummy = ufw_version()
+            ufw_major, ufw_minor, _ = ufw_version()
             # comment is supported only in ufw version after 0.35
             if (ufw_major == 0 and ufw_minor >= 35) or ufw_major > 0:
                 cmd.append([params["comment"], f"comment '{params['comment']}'"])
