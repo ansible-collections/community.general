99df62621a861642ca480a8ee5f9e372e29b83a3
Felix Fontein <felix@fontein.de>
2026-08-12 11:30:13 +0200

    Add ansible-core 2.18 compatibility.

--- plugins/callback/oneline.py
+++ plugins/callback/oneline.py
@@ -33,12 +33,35 @@ if t.TYPE_CHECKING:
     from collections.abc import Mapping
 
     from ansible.executor.task_result import CallbackTaskResult
+    from ansible.inventory.host import Host
+    from ansible.playbook.task import Task
 
 COLOR_ERROR = C.COLOR_ERROR  # type: ignore[attr-defined]
 COLOR_CHANGED = C.COLOR_CHANGED  # type: ignore[attr-defined]
 COLOR_OK = C.COLOR_OK  # type: ignore[attr-defined]
 COLOR_UNREACHABLE = C.COLOR_UNREACHABLE  # type: ignore[attr-defined]
 COLOR_SKIP = C.COLOR_SKIP  # type: ignore[attr-defined]
+
+
+def get_result(result: CallbackTaskResult) -> Mapping[str, t.Any]:
+    if not hasattr(result, "result"):
+        # ansible-core 2.18 fallback
+        return result._result
+    return result.result
+
+
+def get_host(result: CallbackTaskResult) -> Host:
+    if not hasattr(result, "host"):
+        # ansible-core 2.18 fallback
+        return result._host
+    return result.host
+
+
+def get_task(result: CallbackTaskResult) -> Task:
+    if not hasattr(result, "task"):
+        # ansible-core 2.18 fallback
+        return result._task
+    return result.task
 
 
 class CallbackModule(CallbackBase):
@@ -61,8 +84,10 @@ class CallbackModule(CallbackBase):
             return f"{hostname} | {caption} | rc={rc} | (stdout) {stdout}"
 
     def v2_runner_on_failed(self, result: CallbackTaskResult, ignore_errors: bool = False) -> None:
-        if "exception" in result.result:
-            error_text = Templar().template(result.result["exception"])  # transform to a string
+        if "exception" in get_result(result):
+            error_text = get_result(result)["exception"]
+            if not isinstance(error_text, str):
+                error_text = Templar().template(get_result(result)["exception"])  # transform to a string
             if self._display.verbosity < 3:
                 # extract just the actual error message from the exception text
                 error = error_text.strip().split("\n")[-1]
@@ -72,38 +97,39 @@ class CallbackModule(CallbackBase):
                     "\n", ""
                 )
 
-            if result.task.action in C.MODULE_NO_JSON and "module_stderr" not in result.result:
+            if get_task(result).action in C.MODULE_NO_JSON and "module_stderr" not in get_result(result):
                 self._display.display(
-                    self._command_generic_msg(result.host.get_name(), result.result, "FAILED"), color=COLOR_ERROR
+                    self._command_generic_msg(get_host(result).get_name(), get_result(result), "FAILED"),
+                    color=COLOR_ERROR,
                 )
             else:
                 self._display.display(msg, color=COLOR_ERROR)
 
-        hostname = result.host.get_name()
-        msg = self._dump_results(result.result, indent=0).replace("\n", "")
+        hostname = get_host(result).get_name()
+        msg = self._dump_results(get_result(result), indent=0).replace("\n", "")
         self._display.display(f"{hostname} | FAILED! => {msg}", color=COLOR_ERROR)
 
     def v2_runner_on_ok(self, result: CallbackTaskResult) -> None:
 
-        if result.result.get("changed", False):
+        if get_result(result).get("changed", False):
             color = COLOR_CHANGED
             state = "CHANGED"
         else:
             color = COLOR_OK
             state = "SUCCESS"
 
-        hostname = result.host.get_name()
-        if result.task.action in C.MODULE_NO_JSON and "ansible_job_id" not in result.result:
-            self._display.display(self._command_generic_msg(hostname, result.result, state), color=color)
+        hostname = get_host(result).get_name()
+        if get_task(result).action in C.MODULE_NO_JSON and "ansible_job_id" not in get_result(result):
+            self._display.display(self._command_generic_msg(hostname, get_result(result), state), color=color)
         else:
-            msg = self._dump_results(result.result, indent=0).replace("\n", "")
+            msg = self._dump_results(get_result(result), indent=0).replace("\n", "")
             self._display.display(f"{hostname} | {state} => {msg}", color=color)
 
     def v2_runner_on_unreachable(self, result: CallbackTaskResult) -> None:
-        hostname = result.host.get_name()
-        msg = result.result.get("msg", "")
+        hostname = get_host(result).get_name()
+        msg = get_result(result).get("msg", "")
         self._display.display(f"{hostname} | UNREACHABLE!: {msg}", color=COLOR_UNREACHABLE)
 
     def v2_runner_on_skipped(self, result: CallbackTaskResult) -> None:
-        hostname = result.host.get_name()
+        hostname = get_host(result).get_name()
         self._display.display(f"{hostname} | SKIPPED", color=COLOR_SKIP)
