1e9831879c99eaf17fac60a63e7ada06adb1d326
Felix Fontein <felix@fontein.de>
2026-09-12 17:24:22 +0200

    Add masking API.

--- plugins/module_utils/_secrets.py
+++ plugins/module_utils/_secrets.py
@@ -8,6 +8,7 @@ import typing as t
 from collections.abc import Mapping, Sequence
 
 try:
+    from ansible.module_utils.secrets import mask_secrets as _mask_secrets  # type: ignore[import-not-found]
     from ansible.module_utils.secrets import register_secret  # type: ignore[import-not-found]
 
     HAS_SECRETS_API = True
@@ -28,7 +29,7 @@ def _mark_recursively(value: t.Any, *, int_to_string: bool = False) -> t.Any:
 
 
 def mark_values_as_secrets(value: t.Any, *, int_to_string: bool = False) -> t.Any:
-    """Register all strings appearing in the (potentially nested) data structure value secrets."""
+    """Register all strings appearing in the (potentially nested) data structure ``value`` as secrets."""
     if HAS_SECRETS_API:
         value = _mark_recursively(value)
     return value
@@ -38,4 +39,28 @@ def mark_as_secret(value: str) -> str:
     """Register a string as a secret."""
     if HAS_SECRETS_API:
         value = register_secret(value)
+    return value
+
+
+def mask_secrets(value: str, *, mask_placeholder="$REDACTED$") -> str:
+    """Mask secrets in value."""
+    if HAS_SECRETS_API:
+        return _mask_secrets(value, mask_placeholder=mask_placeholder)
+    return value
+
+
+def _mask_recursively(value: t.Any, *, mask_placeholder: str) -> t.Any:
+    if isinstance(value, Mapping):
+        return {k: _mask_recursively(v, mask_placeholder=mask_placeholder) for k, v in value.items()}
+    if isinstance(value, str):
+        return _mask_secrets(value, mask_placeholder=mask_placeholder)
+    if isinstance(value, Sequence):
+        return [_mask_recursively(v, mask_placeholder=mask_placeholder) for v in value]
+    return value
+
+
+def mask_secret_values(value: t.Any, *, mask_placeholder="$REDACTED$") -> t.Any:
+    """Mask secrets in the (potentially nested) data structure ``value``."""
+    if HAS_SECRETS_API:
+        return _mask_recursively(value, mask_placeholder=mask_placeholder)
     return value
