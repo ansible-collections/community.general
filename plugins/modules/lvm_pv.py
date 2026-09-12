64527d800ac7d9a46b7ab1594aac95f3f00cc445
Felix Fontein <felix@fontein.de>
2026-09-12 23:19:45 +0200

    Two more.
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/modules/lvm_pv.py
+++ plugins/modules/lvm_pv.py
@@ -18,7 +18,7 @@ description:
 author:
   - Klention Mali (@klention)
 extends_documentation_fragment:
-  - community.general._attributes
+  - community.general.attributes
 attributes:
   check_mode:
     support: full
