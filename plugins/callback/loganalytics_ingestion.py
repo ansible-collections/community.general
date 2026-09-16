a8e5543205ee132e216b5fd5ead5e06d48555237
Cline, Wade <wade.cline@intel.com>
2026-09-15 22:35:54 -0700

    Mark Log Analytics as supporting secrets masking
    All known secrets have been marked and masking applied at relevant
    egress points.

--- plugins/callback/loganalytics_ingestion.py
+++ plugins/callback/loganalytics_ingestion.py
@@ -297,6 +297,8 @@ class CallbackModule(CallbackBase):
     CALLBACK_NAME = "loganalytics_ingestion"
     CALLBACK_NEEDS_ENABLED = True
 
+    ANSIBLE_SUPPORTS_MASKING = True
+
     def __init__(self, display=None):
         super().__init__(display=display)
         self.start_datetimes = {}
